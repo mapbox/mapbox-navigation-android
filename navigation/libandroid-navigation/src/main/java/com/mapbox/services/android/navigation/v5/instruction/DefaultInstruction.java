a2a387ed012e1ba5887ec3dde8a7d421056cc6d4
Cameron Mace <cameron@mapbox.com>
2017-06-27 10:29:08 -0400

    initial instruction setup

---
+++ navigation/libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/instruction/DefaultInstruction.java
@@ -0,0 +1,10 @@
+package com.mapbox.services.android.navigation.v5.instruction;
+
+import com.mapbox.services.android.navigation.v5.RouteProgress;
+
+public class DefaultInstruction extends Instruction {
+
+  DefaultInstruction(RouteProgress routeProgress) {
+    super(routeProgress);
+  }
+}
---
+++ navigation/libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/instruction/Instruction.java
@@ -0,0 +1,11 @@
+package com.mapbox.services.android.navigation.v5.instruction;
+
+import com.mapbox.services.android.navigation.v5.RouteProgress;
+
+public abstract class Instruction {
+
+  Instruction(RouteProgress routeProgress) {
+
+  }
+
+}
