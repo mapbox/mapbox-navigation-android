ab34a36f6e88ffafa36a40af4f488d22b65da9cb
danesfeder <dan.nesfeder@mapbox.com>
2017-09-11 15:57:24 -0400

    Fix removed colors

--- navigation/libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/snap/Snap.java
+++ navigation/libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/snap/Snap.java
@@ -3,10 +3,14 @@ package com.mapbox.services.android.navigation.v5.snap;
 import android.location.Location;
 
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
+import com.mapbox.services.commons.models.Position;
+
+import java.util.List;
 
 public abstract class Snap {
 
-  public abstract Location getSnappedLocation(Location location, RouteProgress routeProgress);
+  public abstract Location getSnappedLocation(Location location, RouteProgress routeProgress,
+                                              List<Position> coords);
 
   public boolean validLocationToSnap(Location location) {
     // If users not moving, don't snap their position or bearing
