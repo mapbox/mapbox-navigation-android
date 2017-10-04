c2fc60c85a99752bed10c3a287d15a8210a1b751
Cameron Mace <cameron@mapbox.com>
2017-10-04 17:21:39 -0400

    removed unused import

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/offroute/OffRoute.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/offroute/OffRoute.java
@@ -5,9 +5,6 @@ import android.location.Location;
 import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigationOptions;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 import com.mapbox.services.android.navigation.v5.utils.RingBuffer;
-import com.mapbox.services.commons.models.Position;
-
-import java.util.List;
 
 public abstract class OffRoute {
 
