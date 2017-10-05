016b04b23d5a9c75a3f24274ea979495e3f96bdb
Cameron Mace <cameron@mapbox.com>
2017-10-05 14:53:20 -0400

    Added off-route logic improvements (#302)
    * added u-turn detection
    
    * removed unnecessary check on stepPosition list
    
    * moved logic into a new method
    
    * removed unused import

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/offroute/OffRoute.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/offroute/OffRoute.java
@@ -4,10 +4,11 @@ import android.location.Location;
 
 import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigationOptions;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
+import com.mapbox.services.android.navigation.v5.utils.RingBuffer;
 
 public abstract class OffRoute {
 
   public abstract boolean isUserOffRoute(Location location, RouteProgress routeProgress,
-                                         MapboxNavigationOptions options);
-
+                                         MapboxNavigationOptions options,
+                                         RingBuffer<Integer> recentDistancesFromManeuverInMeters);
 }
