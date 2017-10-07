1a39983ee94185f0529dd24ee798d25ba79b9add
Cameron Mace <cameron@mapbox.com>
2017-10-07 18:06:16 -0400

    updated MAS with new metric fixes

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
@@ -376,10 +376,7 @@ public class MapboxNavigation implements ServiceConnection, ProgressChangeListen
         .rerouteCount(sessionState.rerouteCount() + 1)
         .build();
 
-      // In rare cases the service could be disconnected causing a NPE, thus we check.
-      if (navigationService != null) {
-        navigationService.rerouteOccurred();
-      }
+      navigationService.rerouteOccurred();
     }
   }
 
