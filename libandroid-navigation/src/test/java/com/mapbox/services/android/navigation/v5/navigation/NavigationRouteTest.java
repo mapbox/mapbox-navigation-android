4e9a1c9da8883fac877bc74470c51016be2612a8
Cameron Mace <cameron@mapbox.com>
2017-10-23 16:35:22 -0400

    Added test to make sure bearing are correct

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteTest.java
@@ -5,6 +5,7 @@ import com.mapbox.geojson.Point;
 import com.mapbox.services.android.navigation.v5.BaseTest;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
@@ -44,10 +45,22 @@ public class NavigationRouteTest extends BaseTest {
       .origin(Point.fromLngLat(1.0, 2.0), 90d, 90d)
       .addBearing(2.0, 3.0)
       .destination(Point.fromLngLat(1.0, 5.0))
-      .destination(Point.fromLngLat(1.0, 10.0))
       .build();
 
     assertThat(navigationRoute.getCall().request().url().toString(),
-      containsString("bearings=90,90;2,3;;"));
+      containsString("bearings=90,90;2,3;"));
+  }
+
+  @Test
+  @Ignore
+  public void reverseOriginDestinationDoesntMessUpBearings() throws Exception {
+    NavigationRoute navigationRoute = NavigationRoute.builder()
+      .accessToken(ACCESS_TOKEN)
+      .destination(Point.fromLngLat(1.0, 5.0),1d,5d)
+      .origin(Point.fromLngLat(1.0, 2.0), 90d, 90d)
+      .build();
+
+    assertThat(navigationRoute.getCall().request().url().toString(),
+      containsString("bearings=90,90;1,5"));
   }
 }
