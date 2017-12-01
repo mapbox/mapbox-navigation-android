45561f16f9d0c8e2dfdad2321976a7c58794e92f
Dan Nesfeder <dan.nesfeder@mapbox.com>
2017-12-01 17:23:30 -0500

    Add Default Audio focus + Android O updates (#539)
    * Update audio focus to work with both DefaultPlayer and PollyPlayer
    
    * Updates for Android O
    
    * Update initial capacity

--- libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/voice/polly/PollyPlayer.java
+++ libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/voice/polly/PollyPlayer.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.polly.AmazonPollyPresigningClient;
+import com.mapbox.services.android.navigation.ui.v5.voice.InstructionListener;
 import com.mapbox.services.android.navigation.ui.v5.voice.InstructionPlayer;
 
 import java.io.IOException;
@@ -28,12 +29,10 @@ import timber.log.Timber;
  */
 public class PollyPlayer implements InstructionPlayer {
 
-  private static final int STREAM_TYPE = AudioManager.STREAM_MUSIC;
-
   private AmazonPollyPresigningClient pollyClient;
-  private AudioManager pollyAudioManager;
   private MediaPlayer pollyMediaPlayer;
   private List<String> instructionUrls = new ArrayList<>();
+  private InstructionListener instructionListener;
   private boolean isMuted;
 
   /**
@@ -44,7 +43,6 @@ public class PollyPlayer implements InstructionPlayer {
    */
   public PollyPlayer(Context context, String awsPoolId) {
     initPollyClient(context, awsPoolId);
-    initAudioManager(context);
   }
 
   /**
@@ -58,14 +56,14 @@ public class PollyPlayer implements InstructionPlayer {
   }
 
   @Override
-  public void setMuted(boolean isMuted) {
-    this.isMuted = isMuted;
-    mutePolly(isMuted);
+  public boolean isMuted() {
+    return isMuted;
   }
 
   @Override
-  public boolean isMuted() {
-    return isMuted;
+  public void setMuted(boolean isMuted) {
+    this.isMuted = isMuted;
+    mutePolly(isMuted);
   }
 
   @Override
@@ -79,6 +77,11 @@ public class PollyPlayer implements InstructionPlayer {
     stopPollyMediaPlayerPlaying();
   }
 
+  @Override
+  public void addInstructionListener(InstructionListener instructionListener) {
+    this.instructionListener = instructionListener;
+  }
+
   private void initPollyClient(Context context, String awsPoolId) {
     CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
       context,
@@ -86,10 +89,6 @@ public class PollyPlayer implements InstructionPlayer {
       Regions.US_EAST_1
     );
     pollyClient = new AmazonPollyPresigningClient(credentialsProvider);
-  }
-
-  private void initAudioManager(Context context) {
-    pollyAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
   }
 
   private void mutePolly(boolean isMuted) {
@@ -104,20 +103,11 @@ public class PollyPlayer implements InstructionPlayer {
       if (pollyMediaPlayer != null && pollyMediaPlayer.isPlaying()) {
         pollyMediaPlayer.stop();
         pollyMediaPlayer.release();
-        abandonAudioFocus();
+        instructionListener.onDone();
       }
     } catch (IllegalStateException exception) {
       Timber.e(exception.getMessage());
     }
-  }
-
-  private void duckBackgroundAudio() {
-    pollyAudioManager.requestAudioFocus(null, STREAM_TYPE,
-      AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
-  }
-
-  private void abandonAudioFocus() {
-    pollyAudioManager.abandonAudioFocus(null);
   }
 
   private void executeInstructionTask(String instruction) {
@@ -129,6 +119,13 @@ public class PollyPlayer implements InstructionPlayer {
           playInstruction(instructionUrls.get(0));
         } else {
           instructionUrls.add(instructionUrl);
+        }
+      }
+
+      @Override
+      public void onError() {
+        if (instructionListener != null) {
+          instructionListener.onError();
         }
       }
     }).executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, instruction);
@@ -166,7 +163,9 @@ public class PollyPlayer implements InstructionPlayer {
     pollyMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
       @Override
       public void onPrepared(MediaPlayer mp) {
-        duckBackgroundAudio();
+        if (instructionListener != null) {
+          instructionListener.onStart();
+        }
         mp.start();
       }
     });
@@ -174,7 +173,9 @@ public class PollyPlayer implements InstructionPlayer {
       @Override
       public void onCompletion(MediaPlayer mp) {
         mp.release();
-        abandonAudioFocus();
+        if (instructionListener != null) {
+          instructionListener.onDone();
+        }
         onInstructionFinished();
       }
     });
