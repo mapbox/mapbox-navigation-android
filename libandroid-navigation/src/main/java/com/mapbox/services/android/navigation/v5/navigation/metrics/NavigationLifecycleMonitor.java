e254f43ac0439a9de048e21c781022176cf9174c
danesfeder <dan.nesfeder@mapbox.com>
2017-12-14 11:06:08 -0500

    Fixup orientation logic

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/metrics/NavigationLifecycleMonitor.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/metrics/NavigationLifecycleMonitor.java
@@ -2,42 +2,49 @@ package com.mapbox.services.android.navigation.v5.navigation.metrics;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 
 public class NavigationLifecycleMonitor implements Application.ActivityLifecycleCallbacks {
 
+  private static final int ONE_HUNDRED_PERCENT = 100;
+
   private long startSessionTime = 0;
   private ArrayList<Long> resumes;
   private ArrayList<Long> pauses;
-  private ArrayList<Long> orientationChanges;
-  private Integer orientation;
+  private Integer currentOrientation;
+  private long portraitStartTime = 0;
+  private double portraitTimeInMillis = 0;
 
   public NavigationLifecycleMonitor(Application application) {
     application.registerActivityLifecycleCallbacks(this);
     startSessionTime = System.currentTimeMillis();
     resumes = new ArrayList<>();
     pauses = new ArrayList<>();
-    orientationChanges = new ArrayList<>();
-    orientation = application.getResources().getConfiguration().orientation;
-    orientationChanges.add(System.currentTimeMillis());
+    initCurrentOrientation(application);
   }
 
   @Override
   public void onActivityStarted(Activity activity) {
     int newOrientation = activity.getResources().getConfiguration().orientation;
-    // If not equal, add orientation change time
-    if (orientation != newOrientation) {
-      orientation = newOrientation;
+    // If a new orientation is found, set it to the current
+    if (!currentOrientation.equals(newOrientation)) {
+      currentOrientation = newOrientation;
+      long currentTimeMillis = System.currentTimeMillis();
+      // If the current orientation is now landscape, add the time the phone was just in portrait
+      if (currentOrientation.equals(Configuration.ORIENTATION_LANDSCAPE)) {
+        portraitTimeInMillis = portraitTimeInMillis + (currentTimeMillis - portraitStartTime);
+      } else if (currentOrientation.equals(Configuration.ORIENTATION_PORTRAIT)) {
+        portraitStartTime = currentTimeMillis;
+      }
     }
   }
 
   @Override
   public void onActivityResumed(Activity activity) {
     resumes.add(System.currentTimeMillis());
-    orientationChanges.add(System.currentTimeMillis());
   }
 
   @Override
@@ -72,47 +79,27 @@ public class NavigationLifecycleMonitor implements Application.ActivityLifecycle
   //endregion
 
   public int obtainPortraitPercentage() {
-    return calculatePortraitPercentage();
-  }
-
-  private int calculatePortraitPercentage() {
-    boolean isPortrait = orientation.equals(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-    // If there were no orientation changes and currently in portrait, return 100
-    // Otherwise, return 0 (landscape the whole time)
-    if (orientationChanges.size() < 2) {
-      return isPortrait ? 100 : 0;
+    // If no changes to landscape
+    if (currentOrientation.equals(Configuration.ORIENTATION_PORTRAIT) && portraitTimeInMillis == 0) {
+      return ONE_HUNDRED_PERCENT;
     }
-
-    // Find the time spent in portrait
-    double portraitTimeInMillis = 0;
-    long lastChangeTimeInMillis = 0;
-    long currentTimeMillis = System.currentTimeMillis();
-    for (int i = orientationChanges.size() - 1; i > -1; i--) {
-      if (isPortrait) {
-        long diffFromLastChange;
-        // First iteration of the loop
-        if (lastChangeTimeInMillis == 0) {
-          diffFromLastChange = currentTimeMillis - orientationChanges.get(i);
-        } else {
-          diffFromLastChange = lastChangeTimeInMillis - orientationChanges.get(i);
-        }
-        lastChangeTimeInMillis = orientationChanges.get(i);
-        portraitTimeInMillis = portraitTimeInMillis + diffFromLastChange;
-        // Next change time in array is landscape
-        isPortrait = false;
-      } else {
-        lastChangeTimeInMillis = orientationChanges.get(i);
-        // Next change time in array is portrait
-        isPortrait = true;
-      }
-    }
-    return (int) (100 * (portraitTimeInMillis / (currentTimeMillis - startSessionTime)));
+    // Calculate given the time spent in portrait
+    double portraitFraction = portraitTimeInMillis / (System.currentTimeMillis() - startSessionTime);
+    return (int) (ONE_HUNDRED_PERCENT * portraitFraction);
   }
 
   public int obtainForegroundPercentage() {
     long currentTime = System.currentTimeMillis();
     double foregroundTime = calculateForegroundTime(currentTime);
     return (int) (100 * (foregroundTime / (currentTime - startSessionTime)));
+  }
+
+  private void initCurrentOrientation(Application application) {
+    currentOrientation = application.getResources().getConfiguration().orientation;
+    // If starting in portrait, set the portrait start time
+    if (currentOrientation.equals(Configuration.ORIENTATION_PORTRAIT)) {
+      portraitStartTime = System.currentTimeMillis();
+    }
   }
 
   private double calculateForegroundTime(long currentTime) {
