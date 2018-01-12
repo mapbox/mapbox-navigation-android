88cb2b6ac58df1b022f138e5e619980ed372bc1f
Devota Aabel <devota.aabel@mapbox.com>
2018-01-12 13:25:36 -0500

    Added EmbeddedNavigationActivity which is currently a clone of NavigationActivity

---
+++ libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/EmbeddedNavigationActivity.java
@@ -0,0 +1,120 @@
+package com.mapbox.services.android.navigation.ui.v5;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.geojson.Point;
+import com.mapbox.services.android.navigation.ui.v5.listeners.NavigationListener;
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigationOptions;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationConstants;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationUnitType;
+
+import java.util.HashMap;
+
+/**
+ * Serves as a launching point for the custom drop-in UI, {@link NavigationView}.
+ * <p>
+ * Demonstrates the proper setup and usage of the view, including all lifecycle methods.
+ */
+public class EmbeddedNavigationActivity extends AppCompatActivity implements OnNavigationReadyCallback, NavigationListener {
+
+  private NavigationView navigationView;
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    setTheme(R.style.Theme_AppCompat_NoActionBar);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_navigation);
+    navigationView = findViewById(R.id.navigationView);
+    navigationView.onCreate(savedInstanceState);
+    navigationView.getNavigationAsync(this);
+  }
+
+  @Override
+  public void onLowMemory() {
+    super.onLowMemory();
+    navigationView.onLowMemory();
+  }
+
+  @Override
+  public void onBackPressed() {
+    // If the navigation view didn't need to do anything, call super
+    if (!navigationView.onBackPressed()) {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    navigationView.onSaveInstanceState(outState);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    navigationView.onRestoreInstanceState(savedInstanceState);
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    navigationView.onDestroy();
+  }
+
+  @Override
+  public void onNavigationReady() {
+    NavigationViewOptions.Builder options = NavigationViewOptions.builder();
+    options.navigationListener(this);
+    extractRoute(options);
+    extractCoordinates(options);
+    extractConfiguration(options);
+    navigationView.startNavigation(options.build());
+  }
+
+  @Override
+  public void onCancelNavigation() {
+    // Navigation canceled, finish the activity
+    finish();
+  }
+
+  @Override
+  public void onNavigationFinished() {
+    // Navigation finished, finish the activity
+    finish();
+  }
+
+  @Override
+  public void onNavigationRunning() {
+    // Intentionally empty
+  }
+
+  private void extractRoute(NavigationViewOptions.Builder options) {
+    options.directionsRoute(NavigationLauncher.extractRoute(this));
+  }
+
+  private void extractCoordinates(NavigationViewOptions.Builder options) {
+    HashMap<String, Point> coordinates = NavigationLauncher.extractCoordinates(this);
+    if (coordinates.size() > 0) {
+      options.origin(coordinates.get(NavigationConstants.NAVIGATION_VIEW_ORIGIN));
+      options.destination(coordinates.get(NavigationConstants.NAVIGATION_VIEW_DESTINATION));
+    }
+  }
+
+  private void extractConfiguration(NavigationViewOptions.Builder options) {
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    options.awsPoolId(preferences
+      .getString(NavigationConstants.NAVIGATION_VIEW_AWS_POOL_ID, null));
+    options.shouldSimulateRoute(preferences
+      .getBoolean(NavigationConstants.NAVIGATION_VIEW_SIMULATE_ROUTE, false));
+
+    MapboxNavigationOptions navigationOptions = MapboxNavigationOptions.builder()
+      .unitType(preferences.getInt(NavigationConstants.NAVIGATION_VIEW_UNIT_TYPE,
+        NavigationUnitType.TYPE_IMPERIAL))
+      .build();
+    options.navigationOptions(navigationOptions);
+  }
+}
