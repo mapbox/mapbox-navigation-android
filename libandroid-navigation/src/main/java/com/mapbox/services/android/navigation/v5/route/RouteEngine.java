9edd677db18cfeeccb06342b2d0991ffdbfd081c
danesfeder <dan.nesfeder@mapbox.com>
2018-01-16 11:43:23 -0500

    Add calculation for remaining waypoints

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/route/RouteEngine.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/route/RouteEngine.java
@@ -8,6 +8,8 @@ import com.mapbox.geojson.Point;
 import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -32,13 +34,22 @@ public class RouteEngine implements Callback<DirectionsResponse> {
       this.routeProgress = routeProgress;
     }
 
+    // Calculate remaining waypoints
+    List<Point> coordinates = routeProgress.directionsRoute().routeOptions().coordinates();
+    List<Point> remainingCoordinates = coordinates.subList(0, routeProgress.remainingWaypoints());
+    Point destination = remainingCoordinates.remove(remainingCoordinates.size() - 1);
+
     // Build new route request with current route options
     RouteOptions currentOptions = routeProgress.directionsRoute().routeOptions();
-    NavigationRoute.builder()
+     NavigationRoute.Builder builder = NavigationRoute.builder()
       .origin(origin)
-      .routeOptions(currentOptions) // TODO Route options should have waypoints
-      .build()
-      .getRoute(this);
+      .routeOptions(currentOptions);
+
+    // Add waypoints with the remaining coordinate values
+    addWaypoints(remainingCoordinates, builder);
+
+    builder.destination(destination);
+    builder.build().getRoute(this);
   }
 
   @Override
@@ -57,5 +68,13 @@ public class RouteEngine implements Callback<DirectionsResponse> {
 
   public interface Callback {
     void onResponseReceived(Response<DirectionsResponse> response, RouteProgress routeProgress);
+  }
+
+  private void addWaypoints(List<Point> remainingCoordinates, NavigationRoute.Builder builder) {
+    if (!remainingCoordinates.isEmpty()) {
+      for (Point coordinate : remainingCoordinates) {
+        builder.addWaypoint(coordinate);
+      }
+    }
   }
 }
