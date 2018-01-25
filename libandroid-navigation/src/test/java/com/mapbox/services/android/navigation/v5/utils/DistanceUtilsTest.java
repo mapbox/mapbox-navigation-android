765460591f8e0668f7d752526bc551afbcbd3f41
Devota Aabel <devota.aabel@mapbox.com>
2018-01-25 11:56:44 -0500

    Formatted tests

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/utils/DistanceUtilsTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/utils/DistanceUtilsTest.java
@@ -12,45 +12,49 @@ import java.util.Locale;
 
 @RunWith(RobolectricTestRunner.class)
 public class DistanceUtilsTest {
+  private static final double LARGE_LARGE_UNIT = 18124.65;
+  private static final double MEDIUM_LARGE_UNIT = 9812.33;
+  private static final double SMALL_SMALL_UNIT = 13.71;
+  private static final double LARGE_SMALL_UNIT = 109.73;
 
   @Test
   public void testFormatDistance_largeMiles() {
-    assertOutput("11 mi", 18124.65, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
-  }
-
-  @Test
-  public void testFormatDistance_mediumMiles() {
-    assertOutput("5.6 mi", 9012.33, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
-  }
-
-  @Test
-  public void testFormatDistance_smallFeet() {
-    assertOutput("50 ft", 13.71, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
-  }
-
-  @Test
-  public void testFormatDistance_largeFeet() {
-    assertOutput("350 ft", 109.73, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
+    assertOutput("11 mi", LARGE_LARGE_UNIT, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
   }
 
   @Test
   public void testFormatDistance_largeKilometers() {
-    assertOutput("18 km", 18124.65, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+    assertOutput("18 km", LARGE_LARGE_UNIT, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+  }
+
+  @Test
+  public void testFormatDistance_mediumMiles() {
+    assertOutput("6.1 mi", MEDIUM_LARGE_UNIT, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
   }
 
   @Test
   public void testFormatDistance_mediumKilometers() {
-    assertOutput("9,8 km", 9812.33, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+    assertOutput("9,8 km", MEDIUM_LARGE_UNIT, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+  }
+
+  @Test
+  public void testFormatDistance_smallFeet() {
+    assertOutput("50 ft", SMALL_SMALL_UNIT, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
   }
 
   @Test
   public void testFormatDistance_smallMeters() {
-    assertOutput("50 m", 48.72, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+    assertOutput("50 m", SMALL_SMALL_UNIT, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+  }
+
+  @Test
+  public void testFormatDistance_largeFeet() {
+    assertOutput("350 ft", LARGE_SMALL_UNIT, NavigationUnitType.TYPE_IMPERIAL, Locale.US);
   }
 
   @Test
   public void testFormatDistance_largeMeters() {
-    assertOutput("100 m", 109.73, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
+    assertOutput("100 m", LARGE_SMALL_UNIT, NavigationUnitType.TYPE_METRIC, Locale.FRANCE);
   }
 
   private void assertOutput(String output, double distance, int unitType, Locale locale) {
