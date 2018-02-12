d8261650cb9e736e2e117aef1e5b610f9345a159
danesfeder <dan.nesfeder@mapbox.com>
2018-02-12 17:15:19 -0500

    Remove average logic

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/location/LocationValidator.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/location/LocationValidator.java
@@ -3,7 +3,6 @@ package com.mapbox.services.android.navigation.v5.location;
 import android.location.Location;
 
 import com.mapbox.geojson.Point;
-import com.mapbox.services.android.navigation.v5.utils.RingBuffer;
 import com.mapbox.turf.TurfConstants;
 import com.mapbox.turf.TurfMeasurement;
 
@@ -11,50 +10,48 @@ import java.util.concurrent.TimeUnit;
 
 import timber.log.Timber;
 
-public class LocationProcessor {
+public class LocationValidator {
 
   private static final int TIME_THRESHOLD = 5000; // 5 seconds
   private static final int ACCURACY_THRESHOLD = 10; // percent
   private static final int VELOCITY_THRESHOLD = 200; // milliseconds
 
-  private static LocationProcessor instance;
-  private RingBuffer<Location> locations;
+  private static LocationValidator instance;
+  private Location lastValidLocation;
   private boolean loggingEnabled;
 
   /**
    * Primary access method (using singleton pattern)
    *
-   * @return LocationProcessor
+   * @return LocationValidator
    */
-  public static synchronized LocationProcessor getInstance() {
+  public static synchronized LocationValidator getInstance() {
     if (instance == null) {
-      instance = new LocationProcessor();
+      instance = new LocationValidator();
     }
 
     return instance;
   }
 
-  private LocationProcessor() {
-    // Create a buffer for the last five location updates
-    locations = new RingBuffer<>(5);
+  private LocationValidator() {
   }
 
   public boolean isValidUpdate(Location location) {
     // First update
-    if (locations.isEmpty()) {
-      locations.add(location);
+    if (lastValidLocation == null) {
+      lastValidLocation = location;
       return true;
     }
 
     float currentAccuracy = location.getAccuracy();
-    float previousAccuracy = locations.peekLast().getAccuracy();
+    float previousAccuracy = lastValidLocation.getAccuracy();
     float accuracyDifference = Math.abs(previousAccuracy - currentAccuracy);
 
     log("Accuracy - Current: %s, Previous: %s, Difference: %s",
       currentAccuracy, previousAccuracy, accuracyDifference);
 
     boolean currentAccuracyWorse = currentAccuracy > previousAccuracy;
-    boolean hasSameProvider = getLastValidLocation().getProvider().equals(location.getProvider());
+    boolean hasSameProvider = lastValidLocation.getProvider().equals(location.getProvider());
     boolean lessThanPercentThreshold = (accuracyDifference <= (previousAccuracy / ACCURACY_THRESHOLD));
 
     // New location update is acceptable, even with worse accuracy, if it is from
@@ -65,14 +62,13 @@ public class LocationProcessor {
 
     // Calculate the velocity between these two points
     Point currentPoint = Point.fromLngLat(location.getLongitude(), location.getLatitude());
-    Point previousValidPoint = Point.fromLngLat(getLastValidLocation().getLongitude(),
-      getLastValidLocation().getLatitude());
+    Point previousValidPoint = Point.fromLngLat(lastValidLocation.getLongitude(), lastValidLocation.getLatitude());
     double distanceInMeters = TurfMeasurement.distance(previousValidPoint, currentPoint, TurfConstants.UNIT_METERS);
 
     log("Distance between updates: %s", distanceInMeters);
 
     // Average velocity = distance traveled over time
-    long timeSinceLastValidUpdate = location.getTime() - getLastValidLocation().getTime();
+    long timeSinceLastValidUpdate = location.getTime() - lastValidLocation.getTime();
     double velocity = distanceInMeters / timeSinceLastValidUpdate / 1000;
 
     log("Time since last update: %s seconds", TimeUnit.MILLISECONDS.toSeconds(timeSinceLastValidUpdate));
@@ -92,7 +88,7 @@ public class LocationProcessor {
 
     // Valid velocity and location, set last valid location
     if (validVelocity && validLocation) {
-      locations.add(location);
+      lastValidLocation = location;
       return true;
     }
 
@@ -100,45 +96,8 @@ public class LocationProcessor {
     return false;
   }
 
-  public Location retrieveProcessedLocation() {
-    return averageValidLocations();
-  }
-
   public void setLocationLoggingEnabled(boolean loggingEnabled) {
     this.loggingEnabled = loggingEnabled;
-  }
-
-  private Location averageValidLocations() {
-
-    // TODO check if average is behind us don't count it
-
-    double totalX = 0;
-    double totalY = 0;
-    double totalZ = 0;
-
-    for (Location thisLoc : locations) {
-      CartesianLocation thisCart = new CartesianLocation(thisLoc);
-      totalX = totalX + thisCart.getxCoordinate();
-      totalY = totalY + thisCart.getyCoordinate();
-      totalZ = totalZ + thisCart.getzCoordinate();
-    }
-
-    double locationNumber = locations.size();
-    double avgX = totalX / locationNumber;
-    double avgY = totalY / locationNumber;
-    double avgZ = totalZ / locationNumber;
-
-
-    CartesianLocation avgCart = new CartesianLocation(new Location("null"));
-    avgCart.setxCoordinate(avgX);
-    avgCart.setyCoordinate(avgY);
-    avgCart.setzCoordinate(avgZ);
-
-    return avgCart.getGeodeticLocation();
-  }
-
-  private Location getLastValidLocation() {
-    return locations.peek();
   }
 
   private void log(String message, Object... args) {
