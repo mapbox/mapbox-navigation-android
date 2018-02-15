a805eddab16a3944ba432777436698768446a5b1
Devota Aabel <devota.aabel@mapbox.com>
2018-02-15 11:12:17 -0500

    Added comment and fixed checkstyle

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/utils/DistanceUtils.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/utils/DistanceUtils.java
@@ -50,7 +50,9 @@ public class DistanceUtils {
     numberFormat = NumberFormat.getNumberInstance(locale);
 
     if (unitType == NavigationUnitType.NONE_SPECIFIED) {
-      unitType = LocaleUtils.getUnitTypeForLocale(locale.getCountry() == null ? LocaleUtils.getDeviceLocale(context) : locale);
+      // If given locale does not include a country, use the device locale to get the default unitType
+      unitType = LocaleUtils.getUnitTypeForLocale(
+        locale.getCountry() == null ? LocaleUtils.getDeviceLocale(context) : locale);
     }
 
     largeUnit = unitType == NavigationUnitType.TYPE_IMPERIAL ? UNIT_MILES : UNIT_KILOMETERS;
