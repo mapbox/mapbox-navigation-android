8f814611b918f466c869064544d9063181243ee7
danesfeder <dan.nesfeder@mapbox.com>
2018-03-05 18:08:03 -0500

    Remove test code

--- app/src/main/java/com/mapbox/services/android/navigation/testapp/activity/navigationui/NavigationViewActivity.java
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/activity/navigationui/NavigationViewActivity.java
@@ -270,12 +270,9 @@ public class NavigationViewActivity extends AppCompatActivity implements OnMapRe
   }
 
   private void fetchRoute() {
-    Point origin = Point.fromLngLat(-118.05008299999997,34.14601600000003);
-    Point destination = Point.fromLngLat(-118.06110999999999,34.14892299999997);
-
     NavigationRoute.Builder builder = NavigationRoute.builder()
       .accessToken(Mapbox.getAccessToken())
-      .origin(origin)
+      .origin(currentLocation)
       .destination(destination)
       .alternatives(true);
     setFieldsFromSharedPreferences(builder);
