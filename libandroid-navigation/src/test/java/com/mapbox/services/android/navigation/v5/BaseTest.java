be3896108bee1dd6b2e97534197cfe1237163478
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-03-23 10:55:22 -0400

    Extract NavigationEngine processing logic and add tests (#784)
    * Fix Index Bug decoding step points
    
    * Finish adding processor tests
    
    * Cleanup per review
    
    * Remove robo for mocking location

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/BaseTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/BaseTest.java
@@ -1,13 +1,27 @@
 package com.mapbox.services.android.navigation.v5;
 
-import static junit.framework.Assert.assertEquals;
-import static okhttp3.internal.Util.UTF_8;
+import android.location.Location;
+import android.support.annotation.NonNull;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
+import com.mapbox.api.directions.v5.DirectionsAdapterFactory;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Point;
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
+import com.mapbox.turf.TurfConstants;
+import com.mapbox.turf.TurfMeasurement;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
+
+import static junit.framework.Assert.assertEquals;
+import static okhttp3.internal.Util.UTF_8;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class BaseTest {
 
@@ -15,6 +29,7 @@ public class BaseTest {
   public static final double LARGE_DELTA = 0.1;
 
   public static final String ACCESS_TOKEN = "pk.XXX";
+  private static final String DIRECTIONS_PRECISION_6 = "directions_v5_precision_6.json";
 
   public void compareJson(String json1, String json2) {
     JsonParser parser = new JsonParser();
@@ -26,5 +41,55 @@ public class BaseTest {
     InputStream inputStream = classLoader.getResourceAsStream(filename);
     Scanner scanner = new Scanner(inputStream, UTF_8.name()).useDelimiter("\\A");
     return scanner.hasNext() ? scanner.next() : "";
+  }
+
+  protected RouteProgress buildDefaultRouteProgress() throws Exception {
+    DirectionsRoute aRoute = buildDirectionsRoute();
+    RouteProgress defaultRouteProgress = RouteProgress.builder()
+      .stepDistanceRemaining(100)
+      .legDistanceRemaining(100)
+      .distanceRemaining(100)
+      .directionsRoute(aRoute)
+      .stepIndex(0)
+      .legIndex(0)
+      .build();
+
+    return defaultRouteProgress;
+  }
+
+  protected DirectionsRoute buildDirectionsRoute() throws IOException {
+    Gson gson = new GsonBuilder()
+      .registerTypeAdapterFactory(DirectionsAdapterFactory.create()).create();
+    String body = loadJsonFixture(DIRECTIONS_PRECISION_6);
+    DirectionsResponse response = gson.fromJson(body, DirectionsResponse.class);
+    DirectionsRoute aRoute = response.routes().get(0);
+
+    return aRoute;
+  }
+
+  protected Location buildDefaultLocationUpdate(double lng, double lat) {
+    return buildLocationUpdate(lng, lat, 30f, 10f, System.currentTimeMillis());
+  }
+
+  protected Location buildLocationUpdate(double lng, double lat, float speed, float horizontalAccuracy, long time) {
+    Location location = mock(Location.class);
+    when(location.getLongitude()).thenReturn(lng);
+    when(location.getLatitude()).thenReturn(lat);
+    when(location.getSpeed()).thenReturn(speed);
+    when(location.getAccuracy()).thenReturn(horizontalAccuracy);
+    when(location.getTime()).thenReturn(time);
+    return location;
+  }
+
+  @NonNull
+  protected Point buildPointAwayFromLocation(Location location, double distanceAway) {
+    Point fromLocation = Point.fromLngLat(
+      location.getLongitude(), location.getLatitude());
+    return TurfMeasurement.destination(fromLocation, distanceAway, 90, TurfConstants.UNIT_METERS);
+  }
+
+  @NonNull
+  protected Point buildPointAwayFromPoint(Point point, double distanceAway, double bearing) {
+    return TurfMeasurement.destination(point, distanceAway, bearing, TurfConstants.UNIT_METERS);
   }
 }
---
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteProcessorTest.java
@@ -0,0 +1,158 @@
+package com.mapbox.services.android.navigation.v5.navigation;
+
+import android.content.Context;
+import android.location.Location;
+
+import com.mapbox.api.directions.v5.models.LegStep;
+import com.mapbox.core.constants.Constants;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.Point;
+import com.mapbox.services.android.navigation.v5.BaseTest;
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class NavigationRouteProcessorTest extends BaseTest {
+
+  private NavigationRouteProcessor routeProcessor;
+  private MapboxNavigation navigation;
+
+  @Before
+  public void before() throws Exception {
+    routeProcessor = new NavigationRouteProcessor();
+    MapboxNavigationOptions options = MapboxNavigationOptions.builder().build();
+    navigation = new MapboxNavigation(mock(Context.class), ACCESS_TOKEN, options, mock(NavigationTelemetry.class),
+      mock(LocationEngine.class));
+    navigation.startNavigation(buildDirectionsRoute());
+  }
+
+  @Test
+  public void sanity() throws Exception {
+    assertNotNull(routeProcessor);
+  }
+
+  @Test
+  public void onFirstRouteProgressBuilt_newRouteIsDecoded() throws Exception {
+    RouteProgress progress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    assertEquals(0, progress.legIndex());
+    assertEquals(0, progress.currentLegProgress().stepIndex());
+  }
+
+  @Test
+  public void onShouldIncreaseStepIndex_indexIsIncreased() throws Exception {
+    RouteProgress progress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    int currentStepIndex = progress.currentLegProgress().stepIndex();
+    routeProcessor.onShouldIncreaseIndex();
+    routeProcessor.checkIncreaseStepIndex(navigation);
+    RouteProgress secondProgress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    int secondStepIndex = secondProgress.currentLegProgress().stepIndex();
+    assertTrue(currentStepIndex != secondStepIndex);
+  }
+
+  @Test
+  public void onSnapToRouteEnabledAndUserOnRoute_snappedLocationReturns() throws Exception {
+    RouteProgress progress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    boolean snapEnabled = true;
+    boolean userOffRoute = false;
+    LegStep currentStep = progress.currentLegProgress().currentStep();
+    LineString lineString = LineString.fromPolyline(currentStep.geometry(), Constants.PRECISION_6);
+    List<Point> coordinates = lineString.coordinates();
+    Point lastPointInCurrentStep = coordinates.remove(coordinates.size() - 1);
+    Location rawLocation = buildDefaultLocationUpdate(
+      lastPointInCurrentStep.longitude(), lastPointInCurrentStep.latitude()
+    );
+
+    Location snappedLocation = routeProcessor.buildSnappedLocation(
+      navigation, snapEnabled, rawLocation, progress, userOffRoute
+    );
+
+    assertTrue(!rawLocation.equals(snappedLocation));
+  }
+
+  @Test
+  public void onSnapToRouteDisabledAndUserOnRoute_rawLocationReturns() throws Exception {
+    RouteProgress progress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    boolean snapEnabled = false;
+    boolean userOffRoute = false;
+    LegStep currentStep = progress.currentLegProgress().currentStep();
+    LineString lineString = LineString.fromPolyline(currentStep.geometry(), Constants.PRECISION_6);
+    List<Point> coordinates = lineString.coordinates();
+    Point lastPointInCurrentStep = coordinates.remove(coordinates.size() - 1);
+    Location rawLocation = buildDefaultLocationUpdate(
+      lastPointInCurrentStep.longitude(), lastPointInCurrentStep.latitude()
+    );
+
+    Location snappedLocation = routeProcessor.buildSnappedLocation(
+      navigation, snapEnabled, rawLocation, progress, userOffRoute
+    );
+
+    assertTrue(rawLocation.equals(snappedLocation));
+  }
+
+  @Test
+  public void onSnapToRouteEnabledAndUserOffRoute_rawLocationReturns() throws Exception {
+    RouteProgress progress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    boolean snapEnabled = false;
+    boolean userOffRoute = false;
+    LegStep currentStep = progress.currentLegProgress().currentStep();
+    LineString lineString = LineString.fromPolyline(currentStep.geometry(), Constants.PRECISION_6);
+    List<Point> coordinates = lineString.coordinates();
+    Point lastPointInCurrentStep = coordinates.remove(coordinates.size() - 1);
+    Location rawLocation = buildDefaultLocationUpdate(
+      lastPointInCurrentStep.longitude(), lastPointInCurrentStep.latitude()
+    );
+
+    Location snappedLocation = routeProcessor.buildSnappedLocation(
+      navigation, snapEnabled, rawLocation, progress, userOffRoute
+    );
+
+    assertTrue(rawLocation.equals(snappedLocation));
+  }
+
+  @Test
+  public void onStepDistanceRemainingZeroAndNoBearingMatch_stepIndexForceIncreased() throws Exception {
+    RouteProgress firstProgress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    int firstProgressIndex = firstProgress.currentLegProgress().stepIndex();
+    LegStep currentStep = firstProgress.currentLegProgress().currentStep();
+    LineString lineString = LineString.fromPolyline(currentStep.geometry(), Constants.PRECISION_6);
+    List<Point> coordinates = lineString.coordinates();
+    Point lastPointInCurrentStep = coordinates.remove(coordinates.size() - 1);
+    Location rawLocation = buildDefaultLocationUpdate(
+      lastPointInCurrentStep.longitude(), lastPointInCurrentStep.latitude()
+    );
+
+    RouteProgress secondProgress = routeProcessor.buildNewRouteProgress(navigation, rawLocation);
+    int secondProgressIndex = secondProgress.currentLegProgress().stepIndex();
+
+    assertTrue(firstProgressIndex != secondProgressIndex);
+  }
+
+  @Test
+  public void withinManeuverRadiusAndBearingMatches_stepIndexIsIncreased() throws Exception {
+    RouteProgress firstProgress = routeProcessor.buildNewRouteProgress(navigation, mock(Location.class));
+    int firstProgressIndex = firstProgress.currentLegProgress().stepIndex();
+    LegStep currentStep = firstProgress.currentLegProgress().currentStep();
+    LineString lineString = LineString.fromPolyline(currentStep.geometry(), Constants.PRECISION_6);
+    List<Point> coordinates = lineString.coordinates();
+    Point lastPointInCurrentStep = coordinates.remove(coordinates.size() - 2);
+    Location rawLocation = buildDefaultLocationUpdate(
+      lastPointInCurrentStep.longitude(), lastPointInCurrentStep.latitude()
+    );
+    when(rawLocation.getBearing()).thenReturn(145f);
+
+    RouteProgress secondProgress = routeProcessor.buildNewRouteProgress(navigation, rawLocation);
+    int secondProgressIndex = secondProgress.currentLegProgress().stepIndex();
+
+    assertTrue(firstProgressIndex != secondProgressIndex);
+  }
+}
