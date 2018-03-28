ec7f0af51d3de0c0ba4834f3dc74fa3d5ebc8304
Pablo Guardiola <guardiola31337@gmail.com>
2018-03-28 17:00:06 +0200

    add 12/24 hour format navigation view option (#805)

--- libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/summary/SummaryModel.java
+++ libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/summary/SummaryModel.java
@@ -2,14 +2,17 @@ package com.mapbox.services.android.navigation.ui.v5.summary;
 
 import android.content.Context;
 import android.text.SpannableStringBuilder;
+import android.text.format.DateFormat;
 
+import com.mapbox.services.android.navigation.v5.navigation.NavigationTimeFormat;
 import com.mapbox.services.android.navigation.v5.navigation.NavigationUnitType;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 import com.mapbox.services.android.navigation.v5.utils.DistanceUtils;
 
+import java.util.Calendar;
 import java.util.Locale;
 
-import static com.mapbox.services.android.navigation.v5.utils.time.TimeUtils.formatArrivalTime;
+import static com.mapbox.services.android.navigation.v5.utils.time.TimeUtils.formatTime;
 import static com.mapbox.services.android.navigation.v5.utils.time.TimeUtils.formatTimeRemaining;
 
 public class SummaryModel {
@@ -18,12 +21,14 @@ public class SummaryModel {
   private final SpannableStringBuilder timeRemaining;
   private final String arrivalTime;
 
-  public SummaryModel(Context context, RouteProgress progress,
-                      Locale locale, @NavigationUnitType.UnitType int unitType) {
+  public SummaryModel(Context context, RouteProgress progress, Locale locale,
+                      @NavigationUnitType.UnitType int unitType, @NavigationTimeFormat.Type int timeFormatType) {
     distanceRemaining = new DistanceUtils(context, locale, unitType)
       .formatDistance(progress.distanceRemaining()).toString();
     timeRemaining = formatTimeRemaining(progress.durationRemaining());
-    arrivalTime = formatArrivalTime(progress.durationRemaining());
+    Calendar time = Calendar.getInstance();
+    boolean isTwentyFourHourFormat = DateFormat.is24HourFormat(context);
+    arrivalTime = formatTime(time, progress.durationRemaining(), timeFormatType, isTwentyFourHourFormat);
   }
 
   String getDistanceRemaining() {
