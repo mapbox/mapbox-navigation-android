3685eb151fb1bca49a11ecc8a3aa7e8c3ac456e4
danesfeder <dan.nesfeder@mapbox.com>
2018-04-16 16:53:38 -0400

    Update text fixtures and add RouteUtils tests

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/utils/RouteUtilsTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/utils/RouteUtilsTest.java
@@ -1,15 +1,21 @@
 package com.mapbox.services.android.navigation.v5.utils;
 
 import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.directions.v5.models.BannerInstructions;
+import com.mapbox.api.directions.v5.models.BannerText;
 import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.directions.v5.models.LegStep;
 import com.mapbox.api.directions.v5.models.RouteLeg;
+import com.mapbox.api.directions.v5.models.VoiceInstructions;
 import com.mapbox.services.android.navigation.v5.BaseTest;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
@@ -167,10 +173,190 @@ public class RouteUtilsTest extends BaseTest {
     assertFalse(isValidProfile);
   }
 
+  @Test
+  public void findCurrentBannerInstructions_returnsNullWithNullCurrentStep() throws Exception {
+    LegStep currentStep = null;
+    double stepDistanceRemaining = 0;
+
+    BannerInstructions currentBannerInstructions = RouteUtils.findCurrentBannerInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(null, currentBannerInstructions);
+  }
+
+  @Test
+  public void findCurrentBannerInstructions_returnsNullWithCurrentStepEmptyInstructions() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+    List<BannerInstructions> currentInstructions = currentStep.bannerInstructions();
+    currentInstructions.clear();
+
+    BannerInstructions currentBannerInstructions = RouteUtils.findCurrentBannerInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(null, currentBannerInstructions);
+  }
+
+  @Test
+  public void findCurrentBannerInstructions_returnsCorrectCurrentInstruction() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    BannerInstructions currentBannerInstructions = RouteUtils.findCurrentBannerInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(currentStep.bannerInstructions().get(0), currentBannerInstructions);
+  }
+
+  @Test
+  public void findCurrentBannerInstructions_adjustedDistanceRemainingReturnsCorrectInstruction() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    routeProgress = routeProgress.toBuilder()
+      .stepIndex(1)
+      .stepDistanceRemaining(50)
+      .build();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    BannerInstructions currentBannerInstructions = RouteUtils.findCurrentBannerInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(currentStep.bannerInstructions().get(1), currentBannerInstructions);
+  }
+
+  @Test
+  public void findCurrentBannerInstructions_adjustedDistanceRemainingRemovesCorrectInstructions() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    routeProgress = routeProgress.toBuilder()
+      .stepIndex(1)
+      .stepDistanceRemaining(500)
+      .build();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    BannerInstructions currentBannerInstructions = RouteUtils.findCurrentBannerInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(currentStep.bannerInstructions().get(0), currentBannerInstructions);
+  }
+
+  @Test
+  public void findCurrentBannerText_returnsCorrectPrimaryBannerText() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    routeProgress = routeProgress.toBuilder()
+      .stepIndex(1)
+      .stepDistanceRemaining(50)
+      .build();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    BannerText currentBannerText = RouteUtils.findCurrentBannerText(
+      currentStep, stepDistanceRemaining, true
+    );
+
+    assertEquals(currentStep.bannerInstructions().get(1).primary(), currentBannerText);
+  }
+
+  @Test
+  public void findCurrentBannerText_returnsCorrectSecondaryBannerText() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    routeProgress = routeProgress.toBuilder()
+      .stepIndex(1)
+      .stepDistanceRemaining(50)
+      .build();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    BannerText currentBannerText = RouteUtils.findCurrentBannerText(
+      currentStep, stepDistanceRemaining, false
+    );
+
+    assertEquals(currentStep.bannerInstructions().get(1).secondary(), currentBannerText);
+  }
+
+  @Test
+  public void findCurrentBannerText_returnsNullWithNullCurrentStep() throws Exception {
+    LegStep currentStep = null;
+    double stepDistanceRemaining = 0;
+
+    BannerText currentBannerText = RouteUtils.findCurrentBannerText(
+      currentStep, stepDistanceRemaining, false
+    );
+
+    assertEquals(null, currentBannerText);
+  }
+
+  @Test
+  public void findCurrentVoiceInstructions_returnsNullWithNullCurrentStep() throws Exception {
+    LegStep currentStep = null;
+    double stepDistanceRemaining = 0;
+
+    VoiceInstructions currentVoiceInstructions = RouteUtils.findCurrentVoiceInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(null, currentVoiceInstructions);
+  }
+
+  @Test
+  public void findCurrentVoiceInstructions_returnsNullWithCurrentStepEmptyInstructions() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+    List<VoiceInstructions> currentInstructions = currentStep.voiceInstructions();
+    currentInstructions.clear();
+
+    VoiceInstructions voiceInstructions = RouteUtils.findCurrentVoiceInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(null, voiceInstructions);
+  }
+
+  @Test
+  public void findCurrentVoiceInstructions_returnsCorrectInstructionsBeginningOfStepDistanceRemaining() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    routeProgress = routeProgress.toBuilder()
+      .stepIndex(1)
+      .stepDistanceRemaining(400)
+      .build();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    VoiceInstructions currentVoiceInstructions = RouteUtils.findCurrentVoiceInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(currentStep.voiceInstructions().get(1), currentVoiceInstructions);
+  }
+
+  @Test
+  public void findCurrentVoiceInstructions_returnsCorrectInstructionsEndOfStepDistanceRemaining() throws Exception {
+    RouteProgress routeProgress = buildDefaultRouteProgress();
+    routeProgress = routeProgress.toBuilder()
+      .stepIndex(1)
+      .stepDistanceRemaining(50)
+      .build();
+    LegStep currentStep = routeProgress.currentLegProgress().currentStep();
+    double stepDistanceRemaining = routeProgress.currentLegProgress().currentStepProgress().distanceRemaining();
+
+    VoiceInstructions currentVoiceInstructions = RouteUtils.findCurrentVoiceInstructions(
+      currentStep, stepDistanceRemaining
+    );
+
+    assertEquals(currentStep.voiceInstructions().get(2), currentVoiceInstructions);
+  }
+
   private int obtainLastStepIndex(DirectionsRoute route) throws IOException {
     RouteLeg lastLeg = route.legs().get(route.legs().size() - 1);
     int lastStepIndex = lastLeg.steps().indexOf(lastLeg.steps().get(lastLeg.steps().size() - 1));
-
     return lastStepIndex;
   }
 }
