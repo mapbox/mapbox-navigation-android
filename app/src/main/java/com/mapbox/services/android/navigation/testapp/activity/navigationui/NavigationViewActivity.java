e1b24705267a6b379e4265fa63d537b590c57461
Devota Aabel <devota.aabel@mapbox.com>
2018-04-27 15:13:12 -0400

    Fixed bug where we weren't checking if the unit type changed before launching route

--- app/src/main/java/com/mapbox/services/android/navigation/testapp/activity/navigationui/NavigationViewActivity.java
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/activity/navigationui/NavigationViewActivity.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import com.mapbox.api.directions.v5.DirectionsCriteria;
 import com.mapbox.api.directions.v5.models.DirectionsResponse;
 import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.directions.v5.models.RouteOptions;
 import com.mapbox.geojson.LineString;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
@@ -331,7 +332,7 @@ public class NavigationViewActivity extends AppCompatActivity implements OnMapRe
       .unitType(getUnitType())
       .directionsProfile(getRouteProfile());
 
-    if (route.routeOptions().language().equals(locale.getLanguage())) {
+    if (sameLocaleAndUnitType(route.routeOptions())) {
       optionsBuilder.directionsRoute(route);
     } else {
       optionsBuilder
@@ -340,6 +341,11 @@ public class NavigationViewActivity extends AppCompatActivity implements OnMapRe
     }
 
     NavigationLauncher.startNavigation(this, optionsBuilder.build());
+  }
+
+  private boolean sameLocaleAndUnitType(RouteOptions routeOptions) {
+    return routeOptions.language().equals(getLocale().getLanguage())
+      && routeOptions.voiceUnits().equals(NavigationUnitType.getDirectionsCriteriaUnitType(getUnitType(), getLocale()));
   }
 
   private boolean validRouteResponse(Response<DirectionsResponse> response) {
