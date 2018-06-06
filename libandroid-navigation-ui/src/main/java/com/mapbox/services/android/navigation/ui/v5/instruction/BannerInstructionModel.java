fbda85552548e05366f47f90d619ed77c110e361
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-06-06 09:23:15 -0400

    Update InstructionView with BannerMilestone only when callback is received (#969)

--- libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/instruction/BannerInstructionModel.java
+++ libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/instruction/BannerInstructionModel.java
@@ -3,27 +3,25 @@ package com.mapbox.services.android.navigation.ui.v5.instruction;
 import android.content.Context;
 
 import com.mapbox.api.directions.v5.DirectionsCriteria;
-import com.mapbox.api.directions.v5.models.BannerText;
 import com.mapbox.services.android.navigation.v5.milestone.BannerInstructionMilestone;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 
 public class BannerInstructionModel extends InstructionModel {
 
-  private BannerInstructionMilestone milestone;
-
   public BannerInstructionModel(Context context, BannerInstructionMilestone milestone, RouteProgress progress,
                                 String language, @DirectionsCriteria.VoiceUnitCriteria String unitType) {
     super(context, progress, language, unitType);
-    this.milestone = milestone;
+    primaryBannerText = milestone.getBannerInstructions().primary();
+    secondaryBannerText = milestone.getBannerInstructions().secondary();
   }
 
   @Override
-  BannerText getPrimaryBannerText() {
-    return milestone.getBannerInstructions().primary();
+  String getManeuverType() {
+    return primaryBannerText.type();
   }
 
   @Override
-  BannerText getSecondaryBannerText() {
-    return milestone.getBannerInstructions().secondary();
+  String getManeuverModifier() {
+    return primaryBannerText.modifier();
   }
 }
