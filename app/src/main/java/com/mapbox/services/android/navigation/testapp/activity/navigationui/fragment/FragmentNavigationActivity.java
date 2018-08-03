4954c7a03623b0d5bf121731abb3244bdd4dc609
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-08-03 16:04:49 -0400

    Provide example of showing and hiding Fragment with NavigationView (#1113)

--- app/src/main/java/com/mapbox/services/android/navigation/testapp/activity/navigationui/fragment/FragmentNavigationActivity.java
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/activity/navigationui/fragment/FragmentNavigationActivity.java
@@ -2,15 +2,64 @@ package com.mapbox.services.android.navigation.testapp.activity.navigationui.fra
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.services.android.navigation.testapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class FragmentNavigationActivity extends AppCompatActivity {
+
+  @BindView(R.id.restart_navigation_fab)
+  FloatingActionButton restartNavigationFab;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_navigation_fragment);
+    ButterKnife.bind(this);
+    initializeNavigationViewFragment(savedInstanceState);
+  }
+
+  @OnClick(R.id.restart_navigation_fab)
+  public void onClick(FloatingActionButton restartNavigationFab) {
+    replaceFragment(new NavigationFragment(), true);
+    restartNavigationFab.hide();
+  }
+
+  public void showNavigationFab() {
+    restartNavigationFab.show();
+  }
+
+  public void showPlaceholderFragment() {
+    replaceFragment(new PlaceholderFragment(), false);
+  }
+
+  private void initializeNavigationViewFragment(@Nullable Bundle savedInstanceState) {
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    if (savedInstanceState == null) {
+      FragmentTransaction ft = fragmentManager.beginTransaction();
+      ft.add(R.id.navigation_fragment_frame, new NavigationFragment()).commit();
+    }
+  }
+
+  private void replaceFragment(Fragment newFragment, boolean addToBackStack) {
+    String tag = String.valueOf(newFragment.getId());
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+    int fadeInAnimId = android.support.v7.appcompat.R.anim.abc_fade_in;
+    int fadeOutAnimId = android.support.v7.appcompat.R.anim.abc_fade_out;
+    transaction.setCustomAnimations(fadeInAnimId, fadeOutAnimId, fadeInAnimId, fadeOutAnimId);
+    transaction.replace(R.id.navigation_fragment_frame, newFragment, tag);
+    if (addToBackStack) {
+      transaction.addToBackStack(tag);
+    }
+    transaction.commit();
   }
 }
