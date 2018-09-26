8956ef58ce38252e65236c22959b8528e1b8468f
danesfeder <dan.nesfeder@mapbox.com>
2018-09-26 11:46:46 -0400

    Add example test Activity for Navigation Test Application

---
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/example/ui/ExampleActivity.kt
@@ -0,0 +1,271 @@
+package com.mapbox.services.android.navigation.testapp.example.ui
+
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.location.Location
+import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
+import android.support.transition.TransitionManager
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.geocoding.v5.models.CarmenFeature
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.camera.CameraUpdate
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLngBounds
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.services.android.navigation.testapp.NavigationSettingsActivity
+import com.mapbox.services.android.navigation.testapp.R
+import com.mapbox.services.android.navigation.testapp.example.ui.autocomplete.AutoCompleteBottomSheetCallback
+import com.mapbox.services.android.navigation.testapp.example.ui.autocomplete.ExampleAutocompleteAdapter
+import com.mapbox.services.android.navigation.testapp.example.ui.permissions.PermissionRequestDialog
+import com.mapbox.services.android.navigation.testapp.example.utils.hideKeyboard
+import com.mapbox.services.android.navigation.testapp.example.utils.showKeyboard
+import com.mapbox.services.android.navigation.ui.v5.map.NavigationMapboxMap
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation
+import kotlinx.android.synthetic.main.activity_example.*
+
+class ExampleActivity : AppCompatActivity(), ExampleView {
+
+  private val permissionsManager = PermissionsManager(this)
+  private lateinit var presenter: ExamplePresenter
+  private var map: NavigationMapboxMap? = null
+
+  companion object {
+    const val ZERO_PADDING = 0
+    const val BOTTOMSHEET_MULTIPLER = 4
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_example)
+    setupWith(savedInstanceState)
+  }
+
+  public override fun onStart() {
+    super.onStart()
+    mapView.onStart()
+    map?.onStart()
+  }
+
+  public override fun onResume() {
+    super.onResume()
+    mapView.onResume()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    mapView.onSaveInstanceState(outState)
+  }
+
+  public override fun onPause() {
+    super.onPause()
+    mapView.onPause()
+  }
+
+  public override fun onStop() {
+    super.onStop()
+    mapView.onStop()
+    map?.onStop()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+    presenter.onDestroy()
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                          grantResults: IntArray) {
+    permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+  }
+
+  override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+    // No impl - will use PermissionListener as our cue to show dialog
+  }
+
+  override fun onMapReady(mapboxMap: MapboxMap) {
+    map = NavigationMapboxMap(mapView, mapboxMap)
+    presenter.buildDynamicCameraFrom(mapboxMap)
+  }
+
+  override fun onFeatureClicked(feature: CarmenFeature) {
+    presenter.onDestinationFound(feature)
+  }
+
+  override fun onPermissionResult(granted: Boolean) {
+    presenter.onPermissionResult(granted)
+  }
+
+  override fun initialize() {
+    presenter.subscribe(this)
+    mapView.getMapAsync(this)
+  }
+
+  override fun showSoftKeyboard() {
+    showKeyboard()
+  }
+
+  override fun hideSoftKeyboard() {
+    hideKeyboard()
+  }
+
+  override fun showPermissionDialog() {
+    PermissionRequestDialog(this, permissionsManager).show()
+  }
+
+  override fun updateMapCamera(cameraUpdate: CameraUpdate, duration: Int) {
+    map?.retrieveMap()?.animateCamera(cameraUpdate, duration)
+  }
+
+  override fun updateMapCameraFor(bounds: LatLngBounds, padding: IntArray, duration: Int) {
+    map?.retrieveMap()?.let {
+      val position = it.getCameraForLatLngBounds(bounds, padding)
+      it.animateCamera(CameraUpdateFactory.newCameraPosition(position), duration)
+    }
+  }
+
+  override fun updateMapLocation(location: Location?) {
+    map?.updateLocation(location)
+  }
+
+  override fun updateRoute(route: DirectionsRoute) {
+    map?.drawRoute(route)
+  }
+
+  override fun updateDestinationMarker(destination: Point) {
+    map?.addMarker(this, destination)
+  }
+
+  override fun updateAutocompleteBottomSheetState(state: Int) {
+    val behavior = BottomSheetBehavior.from(autocompleteBottomSheet)
+    behavior.state = state
+  }
+
+  override fun updateAutocompleteBottomSheetHideable(isHideable: Boolean) {
+    val behavior = BottomSheetBehavior.from(autocompleteBottomSheet)
+    behavior.isHideable = isHideable
+  }
+
+  override fun updateAutocompleteProximity(location: Location?) {
+    autocompleteView.updateProximity(location)
+  }
+
+  override fun selectAllAutocompleteText() {
+    if (autocompleteView.text.isNotEmpty()) {
+      autocompleteView.selectAll()
+    }
+  }
+
+  override fun updateLocationFabVisibility(visibility: Int) {
+    locationFab.visibility = visibility
+  }
+
+  override fun updateDirectionsFabVisibility(visibility: Int) {
+    directionsFab.visibility = visibility
+  }
+
+  override fun updateNavigationFabVisibility(visibility: Int) {
+    navigationFab.visibility = visibility
+  }
+
+  override fun updateCancelFabVisibility(visibility: Int) {
+    cancelFab.visibility = visibility
+  }
+
+  override fun updateSettingsFabVisibility(visibility: Int) {
+    settingsFab.visibility = visibility
+  }
+
+  override fun updateNavigationDataVisibility(visibility: Int) {
+    TransitionManager.beginDelayedTransition(mainLayout)
+    navigationDataCardView.visibility = visibility
+  }
+
+  override fun updateManeuverView(maneuverType: String?, maneuverModifier: String?) {
+    maneuverView.setManeuverTypeAndModifier(maneuverType, maneuverModifier)
+  }
+
+  override fun updateStepDistanceRemaining(distance: String) {
+    if (stepDistanceRemainingTextView.text != distance) {
+      stepDistanceRemainingTextView.text = distance
+    }
+  }
+
+  override fun updateArrivalTime(time: String) {
+    if (arrivalTimeTextView.text != time) {
+      arrivalTimeTextView.text = time
+    }
+  }
+
+  override fun addMapProgressChangeListener(navigation: MapboxNavigation) {
+    map?.addProgressChangeListener(navigation)
+  }
+
+  override fun removeRoute() {
+    map?.removeRoute()
+  }
+
+  override fun clearMarkers() {
+    map?.clearMarkers()
+  }
+
+  override fun makeToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+  }
+
+  override fun showSettings() {
+    startActivity(Intent(this, NavigationSettingsActivity::class.java))
+  }
+
+  override fun adjustMapPaddingForNavigation() {
+    val mapViewHeight = mapView.height
+    val bottomSheetHeight = resources.getDimension(R.dimen.bottom_sheet_peek_height).toInt()
+    val topPadding = mapViewHeight - bottomSheetHeight * BOTTOMSHEET_MULTIPLER
+    map?.retrieveMap()?.setPadding(ZERO_PADDING, topPadding, ZERO_PADDING, ZERO_PADDING)
+  }
+
+  override fun resetMapPadding() {
+    map?.retrieveMap()?.setPadding(ZERO_PADDING, ZERO_PADDING, ZERO_PADDING, ZERO_PADDING)
+  }
+
+  private fun setupWith(savedInstanceState: Bundle?) {
+    val viewModel = ViewModelProviders.of(this).get(ExampleViewModel::class.java)
+    presenter = ExamplePresenter(this, viewModel)
+
+    mapView.onCreate(savedInstanceState)
+
+    val behavior = BottomSheetBehavior.from(autocompleteBottomSheet)
+    behavior.isHideable = false
+    behavior.peekHeight = resources.getDimension(R.dimen.bottom_sheet_peek_height).toInt()
+    behavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    behavior.setBottomSheetCallback(AutoCompleteBottomSheetCallback(presenter))
+
+    autocompleteView.setOnClickListener { presenter.onAutocompleteClick() }
+    autocompleteView.setAdapter(ExampleAutocompleteAdapter(this))
+    autocompleteView.setFeatureClickListener(this)
+
+    settingsFab.setOnClickListener { presenter.onSettingsFabClick() }
+    locationFab.setOnClickListener { presenter.onLocationFabClick() }
+    directionsFab.setOnClickListener { presenter.onDirectionsFabClick() }
+    navigationFab.setOnClickListener { presenter.onNavigationFabClick() }
+    cancelFab.setOnClickListener { presenter.onCancelFabClick() }
+
+    val granted = PermissionsManager.areLocationPermissionsGranted(this)
+    presenter.onPermissionResult(granted)
+  }
+
+  override fun onBackPressed() {
+    val exitActivity = presenter.onBackPressed()
+    if (exitActivity) {
+      super.onBackPressed()
+    }
+  }
+}
---
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/example/ui/ExamplePresenter.kt
@@ -0,0 +1,248 @@
+package com.mapbox.services.android.navigation.testapp.example.ui
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.Observer
+import android.location.Location
+import android.support.design.widget.BottomSheetBehavior
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.geocoding.v5.models.CarmenFeature
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdate
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.geometry.LatLngBounds
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.services.android.navigation.testapp.NavigationApplication
+import com.mapbox.services.android.navigation.testapp.R
+import com.mapbox.services.android.navigation.testapp.example.ui.offline.OfflineFilesLoadedCallback
+import com.mapbox.services.android.navigation.testapp.example.utils.formatArrivalTime
+import com.mapbox.services.android.navigation.ui.v5.camera.DynamicCamera
+import com.mapbox.services.android.navigation.v5.milestone.BannerInstructionMilestone
+import com.mapbox.services.android.navigation.v5.milestone.Milestone
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress
+import timber.log.Timber
+import kotlin.math.roundToInt
+
+class ExamplePresenter(private val view: ExampleView, private val viewModel: ExampleViewModel) {
+
+  companion object {
+    const val DEFAULT_ZOOM = 12.0
+    const val DEFAULT_BEARING = 0.0
+    const val DEFAULT_TILT = 0.0
+    const val TWO_SECONDS = 2000
+    const val ONE_SECOND = 1000
+  }
+
+  private var state: PresenterState = PresenterState.SHOW_LOCATION
+  private val offlineCallback = object : OfflineFilesLoadedCallback {
+    override fun onFilesLoaded() {
+      Timber.d("Offline files loaded")
+    }
+  }
+
+  fun onPermissionResult(granted: Boolean) {
+    if (granted) {
+      view.initialize()
+    } else {
+      view.showPermissionDialog()
+    }
+  }
+
+  fun onAutocompleteClick() {
+    view.selectAllAutocompleteText()
+    view.updateLocationFabVisibility(INVISIBLE)
+    view.updateSettingsFabVisibility(INVISIBLE)
+    view.updateAutocompleteBottomSheetHideable(false)
+    view.updateAutocompleteBottomSheetState(BottomSheetBehavior.STATE_EXPANDED)
+  }
+
+  fun onSettingsFabClick() {
+    view.showSettings()
+  }
+
+  fun onLocationFabClick() {
+    viewModel.location.value?.let {
+      view.updateMapCamera(buildCameraUpdateFrom(it), ONE_SECOND)
+    }
+  }
+
+  fun onDirectionsFabClick() {
+    state = PresenterState.FIND_ROUTE
+    viewModel.findRouteToDestination()
+  }
+
+  fun onNavigationFabClick() {
+    viewModel.route.value?.let {
+      state = PresenterState.NAVIGATE
+      view.addMapProgressChangeListener(viewModel.retrieveNavigation())
+      viewModel.startNavigationWith(it)
+      view.updateNavigationFabVisibility(INVISIBLE)
+      view.updateCancelFabVisibility(VISIBLE)
+      view.updateNavigationDataVisibility(VISIBLE)
+      view.updateAutocompleteBottomSheetHideable(true)
+      view.updateAutocompleteBottomSheetState(BottomSheetBehavior.STATE_HIDDEN)
+      view.adjustMapPaddingForNavigation()
+    }
+  }
+
+  fun onCancelFabClick() {
+    state = PresenterState.SHOW_LOCATION
+    viewModel.stopNavigation()
+    view.removeRoute()
+    view.clearMarkers()
+    view.resetMapPadding()
+    view.updateLocationFabVisibility(VISIBLE)
+    view.updateSettingsFabVisibility(VISIBLE)
+    view.updateCancelFabVisibility(INVISIBLE)
+    view.updateNavigationDataVisibility(INVISIBLE)
+    view.updateAutocompleteBottomSheetHideable(false)
+    view.updateAutocompleteBottomSheetState(BottomSheetBehavior.STATE_COLLAPSED)
+  }
+
+  fun onAutocompleteBottomSheetStateChange(state: Int) {
+    when (state) {
+      BottomSheetBehavior.STATE_COLLAPSED -> {
+        viewModel.collapsedBottomSheet = true
+        view.hideSoftKeyboard()
+        if (this.state == PresenterState.SHOW_LOCATION) {
+          view.updateLocationFabVisibility(VISIBLE)
+          view.updateSettingsFabVisibility(VISIBLE)
+        }
+      }
+      BottomSheetBehavior.STATE_EXPANDED -> {
+        viewModel.collapsedBottomSheet = false
+      }
+    }
+  }
+
+  fun onDestinationFound(feature: CarmenFeature) {
+    feature.center()?.let {
+      state = PresenterState.SELECTED_DESTINATION
+      viewModel.destination.value = it
+      view.clearMarkers()
+      view.hideSoftKeyboard()
+      view.updateAutocompleteBottomSheetState(BottomSheetBehavior.STATE_COLLAPSED)
+      view.updateDestinationMarker(it)
+      view.updateMapCamera(buildCameraUpdateFrom(it), TWO_SECONDS)
+      view.updateLocationFabVisibility(INVISIBLE)
+      view.updateSettingsFabVisibility(INVISIBLE)
+      view.updateDirectionsFabVisibility(VISIBLE)
+    }
+  }
+
+  fun onLocationUpdate(location: Location?) {
+    location?.let {
+      if (state == PresenterState.SHOW_LOCATION) {
+        view.updateMapCamera(buildCameraUpdateFrom(location), TWO_SECONDS)
+      }
+      view.updateAutocompleteProximity(location)
+      view.updateMapLocation(location)
+    }
+  }
+
+  fun onRouteFound(route: DirectionsRoute?) {
+    route?.let { directionsRoute ->
+      when (state) {
+        PresenterState.FIND_ROUTE -> {
+          state = PresenterState.ROUTE_FOUND
+          view.updateRoute(directionsRoute)
+          view.updateDirectionsFabVisibility(INVISIBLE)
+          view.updateNavigationFabVisibility(VISIBLE)
+          viewModel.destination.value?.let { destination ->
+            moveCameraToInclude(destination)
+          }
+        }
+        PresenterState.NAVIGATE -> {
+          viewModel.startNavigationWith(directionsRoute)
+        }
+        else -> {
+          // TODO no impl
+        }
+      }
+    }
+  }
+
+  fun onProgressUpdate(progress: RouteProgress?) {
+    progress?.let {
+      view.updateArrivalTime(it.formatArrivalTime(NavigationApplication.instance))
+      val distance = it.currentLegProgress().currentStepProgress().distanceRemaining()
+      view.updateStepDistanceRemaining("${distance.roundToInt()} m")
+    }
+  }
+
+  fun onMilestoneUpdate(milestone: Milestone?) {
+    milestone?.let {
+      if (milestone is BannerInstructionMilestone) {
+        val type = milestone.bannerInstructions.primary()?.type()
+        val modifier = milestone.bannerInstructions.primary()?.modifier()
+        view.updateManeuverView(type, modifier)
+      }
+    }
+  }
+
+  fun onDestroy() {
+    viewModel.onDestroy()
+  }
+
+  fun subscribe(owner: LifecycleOwner) {
+    viewModel.location.observe(owner, Observer { onLocationUpdate(it) })
+    viewModel.route.observe(owner, Observer { onRouteFound(it) })
+    viewModel.progress.observe(owner, Observer { onProgressUpdate(it) })
+    viewModel.milestone.observe(owner, Observer { onMilestoneUpdate(it) })
+    viewModel.activateLocationEngine()
+    viewModel.loadOfflineFiles(offlineCallback)
+  }
+
+  fun buildDynamicCameraFrom(mapboxMap: MapboxMap) {
+    // TODO fix this leak
+    viewModel.retrieveNavigation().cameraEngine = DynamicCamera(mapboxMap)
+  }
+
+  private fun buildCameraUpdateFrom(location: Location): CameraUpdate {
+    return CameraUpdateFactory.newCameraPosition(CameraPosition.Builder()
+        .zoom(DEFAULT_ZOOM)
+        .target(LatLng(location.latitude, location.longitude))
+        .bearing(DEFAULT_BEARING)
+        .tilt(DEFAULT_TILT)
+        .build())
+  }
+
+  private fun buildCameraUpdateFrom(point: Point): CameraUpdate {
+    return CameraUpdateFactory.newCameraPosition(CameraPosition.Builder()
+        .zoom(DEFAULT_ZOOM)
+        .target(LatLng(point.latitude(), point.longitude()))
+        .bearing(DEFAULT_BEARING)
+        .tilt(DEFAULT_TILT)
+        .build())
+  }
+
+  private fun moveCameraToInclude(destination: Point) {
+    viewModel.location.value?.let {
+      val origin = LatLng(it)
+      val bounds = LatLngBounds.Builder()
+          .include(origin)
+          .include(LatLng(destination.latitude(), destination.longitude()))
+          .build()
+
+      val resources = NavigationApplication.instance.resources
+      val left = resources.getDimension(R.dimen.route_overview_padding_left).toInt()
+      val top = resources.getDimension(R.dimen.route_overview_padding_top).toInt()
+      val right = resources.getDimension(R.dimen.route_overview_padding_right).toInt()
+      val bottom = resources.getDimension(R.dimen.route_overview_padding_bottom).toInt()
+      // left top right bottom
+      val padding = intArrayOf(left, top, right, bottom)
+      view.updateMapCameraFor(bounds, padding, TWO_SECONDS)
+    }
+  }
+
+  fun onBackPressed(): Boolean {
+    if (!viewModel.collapsedBottomSheet) {
+      view.updateAutocompleteBottomSheetState(BottomSheetBehavior.STATE_COLLAPSED)
+      return false
+    }
+    return true
+  }
+}
