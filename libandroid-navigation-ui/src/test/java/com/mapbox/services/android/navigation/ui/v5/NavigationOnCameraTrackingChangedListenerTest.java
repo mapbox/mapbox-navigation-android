c7ae8e9e2209cd475f530de83060327053b19e0d
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-10-03 17:10:48 -0400

    Add OnCameraTrackingChangedListener to NavigationMapboxMap (#1386)

---
+++ libandroid-navigation-ui/src/test/java/com/mapbox/services/android/navigation/ui/v5/NavigationOnCameraTrackingChangedListenerTest.java
@@ -0,0 +1,42 @@
+package com.mapbox.services.android.navigation.ui.v5;
+
+import android.support.design.widget.BottomSheetBehavior;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class NavigationOnCameraTrackingChangedListenerTest {
+
+  @Test
+  public void onCameraTrackingDismissed_presenterNotifiedWithVisibleBottomsheet() {
+    NavigationPresenter presenter = mock(NavigationPresenter.class);
+    BottomSheetBehavior behavior = mock(BottomSheetBehavior.class);
+    when(behavior.getState()).thenReturn(BottomSheetBehavior.STATE_EXPANDED);
+    NavigationOnCameraTrackingChangedListener listener = new NavigationOnCameraTrackingChangedListener(
+      presenter, behavior
+    );
+
+    listener.onCameraTrackingDismissed();
+
+    verify(presenter).onCameraTrackingDismissed();
+  }
+
+  @Test
+  public void onCameraTrackingDismissed_ignoredWithHiddenBottomsheet() {
+    NavigationPresenter presenter = mock(NavigationPresenter.class);
+    BottomSheetBehavior behavior = mock(BottomSheetBehavior.class);
+    when(behavior.getState()).thenReturn(BottomSheetBehavior.STATE_HIDDEN);
+    NavigationOnCameraTrackingChangedListener listener = new NavigationOnCameraTrackingChangedListener(
+      presenter, behavior
+    );
+
+    listener.onCameraTrackingDismissed();
+
+    verify(presenter, times(0)).onCameraTrackingDismissed();
+  }
+}
