8b018a0286b3b276e0d503ef0c39154193cb6168
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-11-12 11:17:26 -0500

    Use most recent raw Location when building snapped Location (#1522)

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/snap/SnapToRouteTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/snap/SnapToRouteTest.java
@@ -2,16 +2,21 @@ package com.mapbox.services.android.navigation.v5.snap;
 
 import android.location.Location;
 
+import com.mapbox.geojson.Point;
 import com.mapbox.navigator.NavigationStatus;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
+import java.util.Date;
+
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@Ignore
+@RunWith(RobolectricTestRunner.class)
 public class SnapToRouteTest {
 
   @Test
@@ -22,13 +27,62 @@ public class SnapToRouteTest {
   }
 
   @Test
-  public void getSnappedLocation_returnsProviderNameCorrectly() {
-    // TODO mock final class
-    NavigationStatus status = mock(NavigationStatus.class);
+  public void getSnappedLocationWith_returnsRawLocationProvider() {
+    NavigationStatus status = buildMockStatus();
+    String provider = "location_provider";
+    Location rawLocation = new Location(provider);
     SnapToRoute snap = new SnapToRoute();
 
-    Location snappedLocation = snap.getSnappedLocationWith(status);
+    Location snappedLocation = snap.getSnappedLocationWith(status, rawLocation);
 
-    assertTrue(snappedLocation.getProvider().equals("NavigatorSnappedLocation"));
+    assertEquals(provider, snappedLocation.getProvider());
+  }
+
+  @Test
+  public void getSnappedLocationWith_returnsRawLocationSpeed() {
+    NavigationStatus status = buildMockStatus();
+    Location rawLocation = new Location("location_provider");
+    float speed = 1.4f;
+    rawLocation.setSpeed(speed);
+    SnapToRoute snap = new SnapToRoute();
+
+    Location snappedLocation = snap.getSnappedLocationWith(status, rawLocation);
+
+    assertEquals(speed, snappedLocation.getSpeed());
+  }
+
+  @Test
+  public void getSnappedLocationWith_returnsRawLocationAltitude() {
+    NavigationStatus status = buildMockStatus();
+    Location rawLocation = new Location("location_provider");
+    double altitude = 25d;
+    rawLocation.setAltitude(altitude);
+    SnapToRoute snap = new SnapToRoute();
+
+    Location snappedLocation = snap.getSnappedLocationWith(status, rawLocation);
+
+    assertEquals(altitude, snappedLocation.getAltitude());
+  }
+
+  @Test
+  public void getSnappedLocationWith_returnsRawLocationAccuracy() {
+    NavigationStatus status = buildMockStatus();
+    Location rawLocation = new Location("location_provider");
+    float accuracy = 20f;
+    rawLocation.setAccuracy(accuracy);
+    SnapToRoute snap = new SnapToRoute();
+
+    Location snappedLocation = snap.getSnappedLocationWith(status, rawLocation);
+
+    assertEquals(accuracy, snappedLocation.getAccuracy());
+  }
+
+  private NavigationStatus buildMockStatus() {
+    NavigationStatus status = mock(NavigationStatus.class);
+    Point location = Point.fromLngLat(0.0, 0.0);
+    when(status.getLocation()).thenReturn(location);
+    when(status.getTime()).thenReturn(new Date());
+    when(status.getBearing()).thenReturn(0.0f);
+    return status;
   }
 }
