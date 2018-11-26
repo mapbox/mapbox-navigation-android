ffee594a179b7ad850bb4874422dcb6223a83076
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-11-26 13:18:34 -0800

    Fix UI tests with DirectionsRoute test fixture (#1548)

--- app/src/androidTest/java/testapp/activity/BaseNavigationActivityTest.java
+++ app/src/androidTest/java/testapp/activity/BaseNavigationActivityTest.java
@@ -2,6 +2,7 @@ package testapp.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.test.espresso.IdlingRegistry;
@@ -17,6 +18,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import testapp.utils.OnNavigationReadyIdlingResource;
 import timber.log.Timber;
 
@@ -29,21 +33,30 @@ public abstract class BaseNavigationActivityTest {
 
   @Rule
   public ActivityTestRule<Activity> rule = new ActivityTestRule<>(getActivityClass());
+
   private NavigationView navigationView;
+  private AssetManager assetManager;
   private OnNavigationReadyIdlingResource idlingResource;
 
   @Before
   public void beforeTest() {
     try {
-      idlingResource = new OnNavigationReadyIdlingResource(rule.getActivity());
+      Activity activity = rule.getActivity();
+      idlingResource = new OnNavigationReadyIdlingResource(activity);
       IdlingRegistry.getInstance().register(idlingResource);
       checkViewIsDisplayed(R.id.navigationView);
       navigationView = idlingResource.getNavigationView();
+      assetManager = activity.getAssets();
     } catch (IdlingResourceTimeoutException idlingResourceTimeoutException) {
       Timber.e("Idling resource timed out. Could not validate if navigation is ready.");
       throw new RuntimeException("Could not start test for " + getActivityClass().getSimpleName() + ".\n"
         + "The ViewHierarchy doesn't contain a view with resource id = R.id.navigationView");
     }
+  }
+
+  @After
+  public void afterTest() {
+    IdlingRegistry.getInstance().unregister(idlingResource);
   }
 
   protected void validateTestSetup() {
@@ -57,6 +70,21 @@ public abstract class BaseNavigationActivityTest {
 
   protected abstract Class getActivityClass();
 
+  protected String loadJsonFromAsset(String filename) {
+    try {
+      InputStream is = assetManager.open(filename);
+      int size = is.available();
+      byte[] buffer = new byte[size];
+      is.read(buffer);
+      is.close();
+      return new String(buffer, "UTF-8");
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+      return null;
+    }
+  }
+
   private void checkViewIsDisplayed(int id) {
     onView(withId(id)).check(matches(isDisplayed()));
   }
@@ -66,10 +94,5 @@ public abstract class BaseNavigationActivityTest {
       = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
     NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
     return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-  }
-
-  @After
-  public void afterTest() {
-    IdlingRegistry.getInstance().unregister(idlingResource);
   }
 }
