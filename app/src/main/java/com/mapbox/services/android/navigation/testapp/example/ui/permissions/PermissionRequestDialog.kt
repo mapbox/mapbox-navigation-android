407b264d22f67cad267ce351171e274cccd2dfbe
Devota Aabel <devota.aabel@mapbox.com>
2018-11-27 18:14:35 -0500

    Added request for External Storage Permissions

--- app/src/main/java/com/mapbox/services/android/navigation/testapp/example/ui/permissions/PermissionRequestDialog.kt
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/example/ui/permissions/PermissionRequestDialog.kt
@@ -2,18 +2,26 @@ package com.mapbox.services.android.navigation.testapp.example.ui.permissions
 
 import android.app.Activity
 import android.content.DialogInterface
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
-import com.mapbox.android.core.permissions.PermissionsManager
 
-class PermissionRequestDialog(private val activity: Activity,
-                              private val permissionsManager: PermissionsManager) : AlertDialog(activity) {
+
+
+
+
+class PermissionRequestDialog(private val activity: Activity) : AlertDialog(activity) {
 
   init {
     // TODO string localization
     setTitle("Location Permissions")
-    setMessage("To use navigation, you need to grant location permissions.")
+    setMessage("To use navigation, you need to grant location permissions. To use offline " +
+            "navigation, you have to enable file storage permissions.")
     setButton(DialogInterface.BUTTON_POSITIVE, "Request Permissions") { _, _ ->
-      permissionsManager.requestLocationPermissions(activity)
+
+      val PERMISSIONS = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,
+              android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        ActivityCompat.requestPermissions(activity, PERMISSIONS, 1)
     }
     setButton(DialogInterface.BUTTON_NEGATIVE, "Close Navigation") { dialog, _ ->
       dialog.dismiss()
