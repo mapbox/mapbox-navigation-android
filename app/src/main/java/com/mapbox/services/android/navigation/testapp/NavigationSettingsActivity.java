336290e290fad094cf23f2e7be98999f5c517844
Devota Aabel <devota.aabel@mapbox.com>
2018-11-29 18:50:17 -0500

    Added permissions check to SettingsActivity

--- app/src/main/java/com/mapbox/services/android/navigation/testapp/NavigationSettingsActivity.java
+++ app/src/main/java/com/mapbox/services/android/navigation/testapp/NavigationSettingsActivity.java
@@ -9,17 +9,23 @@ import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.support.v4.content.PermissionChecker.PERMISSION_GRANTED;
+
 public class NavigationSettingsActivity extends PreferenceActivity {
 
   public static final String UNIT_TYPE_CHANGED = "unit_type_changed";
   public static final String LANGUAGE_CHANGED = "language_changed";
   public static final String OFFLINE_CHANGED = "offline_changed";
+  private static final int EXTERNAL_STORAGE_PERMISSION = 1;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -42,20 +48,32 @@ public class NavigationSettingsActivity extends PreferenceActivity {
     return super.isValidFragment(fragmentName);
   }
 
+
   public static class NavigationViewPreferenceFragment extends PreferenceFragment {
     @Override
     public void onCreate(final Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
+
       addPreferencesFromResource(R.xml.fragment_navigation_preferences);
     }
 
     @Override
     public void onResume() {
       super.onResume();
+
+      PreferenceManager.setDefaultValues(getActivity(), R.xml.fragment_navigation_preferences, false);
+
+      if (checkOfflinePermission()) {
+        getOfflineVersions();
+      }
+    }
+
+    private void getOfflineVersions() {
       File file = new File(Environment.getExternalStoragePublicDirectory("Offline"), "tiles");
       if (!file.exists()) {
         file.mkdirs();
       }
+
       List<String> list;
 
       if (file.list() != null && file.list().length != 0) {
@@ -65,15 +83,22 @@ public class NavigationSettingsActivity extends PreferenceActivity {
       }
       list.add(getString(R.string.offline_disabled));
 
+      ListPreference offlineVersions =
+        (ListPreference) findPreference(getString(R.string.offline_preference_key));
+      offlineVersions.setOnPreferenceClickListener(preference -> checkOfflinePermission());
+      
       String[] entries = list.toArray(new String[list.size()]);
-
-      ListPreference offlineVersions = (ListPreference) findPreference(getString(R.string
-        .offline_preference_key));
-
       offlineVersions.setEntries(entries);
       offlineVersions.setEntryValues(entries);
+    }
 
-      PreferenceManager.setDefaultValues(getActivity(), R.xml.fragment_navigation_preferences, false);
+    private boolean checkOfflinePermission() {
+      if (ContextCompat.checkSelfPermission(getActivity(), WRITE_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(
+          getActivity(), new String[] { WRITE_EXTERNAL_STORAGE }, EXTERNAL_STORAGE_PERMISSION);
+        return true;
+      }
+      return false;
     }
   }
 }
