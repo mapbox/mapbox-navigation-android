a7b2da23b50614c172462f5bab9e8c751fce21a6
Pablo Guardiola <guardiola31337@gmail.com>
2018-12-12 10:07:41 -0500

    fix update route in turn by turn ui (navigation view model) to be called only if navigation is not running (first time / once) or if it is off route - preventing setting new routes unnecessarily which ended up in announcing voice instructions already announced (#1622)

---
+++ libandroid-navigation-ui/src/test/java/com/mapbox/services/android/navigation/ui/v5/NavigationViewRouteEngineListenerTest.java
@@ -0,0 +1,102 @@
+package com.mapbox.services.android.navigation.ui.v5;
+
+import android.arch.lifecycle.MutableLiveData;
+
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.geojson.Point;
+
+import org.junit.Test;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class NavigationViewRouteEngineListenerTest {
+
+  @Test
+  public void checksUpdateRouteCalledIfNavigationIsNotRunning() {
+    NavigationViewModel mockedNavigationViewModel = mock(NavigationViewModel.class);
+    when(mockedNavigationViewModel.isRunning()).thenReturn(false);
+    DirectionsRoute aDirectionsRoute = mock(DirectionsRoute.class);
+    NavigationViewRouteEngineListener theRouteEngineListener
+      = new NavigationViewRouteEngineListener(mockedNavigationViewModel);
+
+    theRouteEngineListener.onRouteUpdate(aDirectionsRoute);
+
+    verify(mockedNavigationViewModel).updateRoute(eq(aDirectionsRoute));
+  }
+
+  @Test
+  public void checksUpdateRouteCalledIfNavigationIsOffRoute() {
+    NavigationViewModel mockedNavigationViewModel = mock(NavigationViewModel.class);
+    when(mockedNavigationViewModel.isRunning()).thenReturn(true);
+    when(mockedNavigationViewModel.isOffRoute()).thenReturn(true);
+    DirectionsRoute aDirectionsRoute = mock(DirectionsRoute.class);
+    NavigationViewRouteEngineListener theRouteEngineListener
+      = new NavigationViewRouteEngineListener(mockedNavigationViewModel);
+
+    theRouteEngineListener.onRouteUpdate(aDirectionsRoute);
+
+    verify(mockedNavigationViewModel).updateRoute(eq(aDirectionsRoute));
+  }
+
+  @Test
+  public void checksUpdateRouteNotCalledIfNavigationIsRunningAndIsNotOffRoute() {
+    NavigationViewModel mockedNavigationViewModel = mock(NavigationViewModel.class);
+    when(mockedNavigationViewModel.isRunning()).thenReturn(true);
+    when(mockedNavigationViewModel.isOffRoute()).thenReturn(false);
+    DirectionsRoute aDirectionsRoute = mock(DirectionsRoute.class);
+    NavigationViewRouteEngineListener theRouteEngineListener
+      = new NavigationViewRouteEngineListener(mockedNavigationViewModel);
+
+    theRouteEngineListener.onRouteUpdate(aDirectionsRoute);
+
+    verify(mockedNavigationViewModel, times(0)).updateRoute(eq(aDirectionsRoute));
+  }
+
+  @Test
+  public void checksSendEventFailedRerouteCalledIfNavigationIsOffRoute() {
+    NavigationViewModel mockedNavigationViewModel = mock(NavigationViewModel.class);
+    when(mockedNavigationViewModel.isOffRoute()).thenReturn(true);
+    Throwable aThrowable = mock(Throwable.class);
+    String anError = "An error occurred!";
+    when(aThrowable.getMessage()).thenReturn(anError);
+    NavigationViewRouteEngineListener theRouteEngineListener
+      = new NavigationViewRouteEngineListener(mockedNavigationViewModel);
+
+    theRouteEngineListener.onRouteRequestError(aThrowable);
+
+    verify(mockedNavigationViewModel).sendEventFailedReroute(eq(anError));
+  }
+
+  @Test
+  public void checksSendEventFailedRerouteNotCalledIfNavigationIsNotOffRoute() {
+    NavigationViewModel mockedNavigationViewModel = mock(NavigationViewModel.class);
+    when(mockedNavigationViewModel.isOffRoute()).thenReturn(false);
+    Throwable aThrowable = mock(Throwable.class);
+    String anError = "An error occurred!";
+    when(aThrowable.getMessage()).thenReturn(anError);
+    NavigationViewRouteEngineListener theRouteEngineListener
+      = new NavigationViewRouteEngineListener(mockedNavigationViewModel);
+
+    theRouteEngineListener.onRouteRequestError(aThrowable);
+
+    verify(mockedNavigationViewModel, times(0)).sendEventFailedReroute(eq(anError));
+  }
+
+  @Test
+  public void checksOnDestinationSet() {
+    NavigationViewModel mockedNavigationViewModel = mock(NavigationViewModel.class);
+    Point mockedPoint = mock(Point.class);
+    MutableLiveData<Point> mockedDestination = mock(MutableLiveData.class);
+    when(mockedNavigationViewModel.retrieveDestination()).thenReturn(mockedDestination);
+    NavigationViewRouteEngineListener theRouteEngineListener
+      = new NavigationViewRouteEngineListener(mockedNavigationViewModel);
+
+    theRouteEngineListener.onDestinationSet(mockedPoint);
+
+    verify(mockedDestination).setValue(eq(mockedPoint));
+  }
+}
