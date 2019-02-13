c63e82c6cdb62834b42b3e37a130ec29b327e889
Devota Aabel <devota.aabel@mapbox.com>
2019-02-13 16:26:49 -0500

    Added RouteAnnotationUpdater to update a DirectionsRoute with annotations

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/RouteRefresh.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/RouteRefresh.java
@@ -5,46 +5,60 @@ import com.mapbox.api.directionsrefresh.v1.MapboxDirectionsRefresh;
 import com.mapbox.api.directionsrefresh.v1.models.DirectionsRefreshResponse;
 import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress;
 
+import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  *
  */
 public final class RouteRefresh {
-  private final MapboxDirectionsRefresh mapboxDirectionsRefresh;
+  private final RefreshCallback refreshCallback;
+  private final RouteAnnotationUpdater routeAnnotationUpdater;
+//  private final MapboxDirectionsRefresh mapboxDirectionsRefresh;
 
   /**
    * Creates a {@link RouteRefresh} object configured with the specified {@link RouteProgress}.
    *
    * @param routeProgress to extract a route and current leg index
    */
-  public RouteRefresh(RouteProgress routeProgress) {
-    this(routeProgress.directionsRoute(), routeProgress.legIndex());
-
+  public RouteRefresh(RefreshCallback refreshCallback) {
+    this(refreshCallback, new RouteAnnotationUpdater());
   }
 
-  RouteRefresh(DirectionsRoute directionsRoute, int legIndex) {
-    this(MapboxDirectionsRefresh.builder()
-      .requestId(directionsRoute.routeOptions().requestUuid())
-      .routeIndex(Integer.valueOf(directionsRoute.routeIndex()))
-      .legIndex(legIndex)
-      .build());
-  }
-
-  RouteRefresh(MapboxDirectionsRefresh mapboxDirectionsRefresh) {
-    this.mapboxDirectionsRefresh = mapboxDirectionsRefresh;
+  RouteRefresh(RefreshCallback refreshCallback, RouteAnnotationUpdater routeAnnotationUpdater) {
+    this.refreshCallback = refreshCallback;
   }
 
   /**
    * Refreshes the {@link DirectionsRoute} included in the {@link RouteProgress} and returns it
-   * to the callback passed in. The client will then have to update their {@link DirectionsRoute}
-   * with the {@link com.mapbox.api.directions.v5.models.LegAnnotation}s returned in this
-   * response. The leg annotations start at the current leg index of the {@link RouteProgress},
-   * so the client should keep
+   * to the callback that was originally passed in. The client will then have to update their
+   * {@link DirectionsRoute} with the {@link com.mapbox.api.directions.v5.models.LegAnnotation}s
+   * returned in this response. The leg annotations start at the current leg index of the
+   * {@link RouteProgress}
    *
-   * @param callback to return {@link DirectionsRefreshResponse}
+   * @param routeProgress to refresh
    */
-  public void refresh(Callback<DirectionsRefreshResponse> callback) {
-    mapboxDirectionsRefresh.enqueueCall(callback);
+  public void refresh(RouteProgress routeProgress) {
+    refresh(routeProgress.directionsRoute(), routeProgress.legIndex());
+  }
+
+  void refresh(final DirectionsRoute directionsRoute, final int legIndex) {
+    MapboxDirectionsRefresh.builder()
+      .requestId(directionsRoute.routeOptions().requestUuid())
+      .routeIndex(Integer.valueOf(directionsRoute.routeIndex()))
+      .legIndex(legIndex)
+      .build().enqueueCall(new Callback<DirectionsRefreshResponse>() {
+      @Override
+      public void onResponse(Call<DirectionsRefreshResponse> call, Response<DirectionsRefreshResponse> response) {
+        refreshCallback.onRefresh(routeAnnotationUpdater.update(directionsRoute,
+          response.body().route(), legIndex));
+      }
+
+      @Override
+      public void onFailure(Call<DirectionsRefreshResponse> call, Throwable throwable) {
+        refreshCallback.onError("There was a network error.");
+      }
+    });
   }
 }
