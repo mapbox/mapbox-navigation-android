3e16a354e2da7477ee4517cb17495c4507baeb89
Devota Aabel <devota.aabel@mapbox.com>
2019-03-07 15:52:04 -0500

    Added attribute to capture event name in performance trace event

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/BatteryEvent.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/BatteryEvent.java
@@ -8,9 +8,10 @@ import android.os.Parcelable;
 class BatteryEvent extends NavigationPerformanceEvent implements Parcelable {
   private static final String BATTERY_PERCENTAGE_KEY = "battery_percentage";
   private static final String IS_PLUGGED_IN_KEY = "is_plugged_in";
+  private static final String BATTERY_EVENT_NAME = "battery_event";
 
   BatteryEvent(String sessionId, float batteryPercentage, boolean isPluggedIn) {
-    super(sessionId);
+    super(sessionId, BATTERY_EVENT_NAME);
 
     addCounter(new FloatCounter(BATTERY_PERCENTAGE_KEY, batteryPercentage));
     addAttribute(new Attribute(IS_PLUGGED_IN_KEY, String.valueOf(isPluggedIn)));
--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/InitialGpsEvent.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/InitialGpsEvent.java
@@ -7,9 +7,10 @@ import android.os.Parcelable;
 @SuppressWarnings("ParcelableCreator")
 class InitialGpsEvent extends NavigationPerformanceEvent implements Parcelable {
   private static final String TIME_TO_FIRST_GPS = "time_to_first_gps";
+  private static final String INITIAL_GPS_EVENT_NAME = "initial_gps_event";
 
   InitialGpsEvent(double elapsedTime, String sessionId) {
-    super(sessionId);
+    super(sessionId, INITIAL_GPS_EVENT_NAME);
     addCounter(new DoubleCounter(TIME_TO_FIRST_GPS, elapsedTime));
   }
 }
