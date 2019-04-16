f62bf613093e1f35dba8c10fbe6dc2da50806c55
Dan Nesfeder <dan.nesfeder@mapbox.com>
2019-04-16 11:38:58 -0400

    Add NavigationMapboxMap#addCustomMarker for usage of SymbolManager (#1891)

--- libandroid-navigation-ui/src/test/java/com/mapbox/services/android/navigation/ui/v5/map/NavigationSymbolManagerTest.java
+++ libandroid-navigation-ui/src/test/java/com/mapbox/services/android/navigation/ui/v5/map/NavigationSymbolManagerTest.java
@@ -15,14 +15,14 @@ import static org.mockito.Mockito.when;
 public class NavigationSymbolManagerTest {
 
   @Test
-  public void addMarkerFor_symbolManagerAddsOptions() {
+  public void addDestinationMarkerFor_symbolManagerAddsOptions() {
     SymbolManager symbolManager = mock(SymbolManager.class);
     Symbol symbol = mock(Symbol.class);
     when(symbolManager.create(any(SymbolOptions.class))).thenReturn(symbol);
     NavigationSymbolManager navigationSymbolManager = new NavigationSymbolManager(symbolManager);
     Point position = Point.fromLngLat(1.2345, 1.3456);
 
-    navigationSymbolManager.addMarkerFor(position);
+    navigationSymbolManager.addDestinationMarkerFor(position);
 
     verify(symbolManager).create(any(SymbolOptions.class));
   }
@@ -34,10 +34,23 @@ public class NavigationSymbolManagerTest {
     when(symbolManager.create(any(SymbolOptions.class))).thenReturn(symbol);
     NavigationSymbolManager navigationSymbolManager = new NavigationSymbolManager(symbolManager);
     Point position = Point.fromLngLat(1.2345, 1.3456);
-    navigationSymbolManager.addMarkerFor(position);
+    navigationSymbolManager.addDestinationMarkerFor(position);
 
     navigationSymbolManager.removeAllMarkerSymbols();
 
     verify(symbolManager).delete(symbol);
+  }
+
+  @Test
+  public void addDestinationMarkerFor_previouslyAddedMarkersRemoved() {
+    SymbolManager symbolManager = mock(SymbolManager.class);
+    Symbol symbol = mock(Symbol.class);
+    SymbolOptions symbolOptions = mock(SymbolOptions.class);
+    when(symbolManager.create(symbolOptions)).thenReturn(symbol);
+    NavigationSymbolManager navigationSymbolManager = new NavigationSymbolManager(symbolManager);
+
+    navigationSymbolManager.addCustomSymbolFor(symbolOptions);
+
+    verify(symbolManager).create(symbolOptions);
   }
 }
