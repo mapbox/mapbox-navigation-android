c56491fb0ef0d143c1a2ac0b3ec25854ff1adaf1
danesfeder <dan.nesfeder@mapbox.com>
2019-04-30 16:12:36 -0400

    Add tests for OfflineRegion provider, observer, and region download callback

--- libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/RegionDownloadCallback.java
+++ libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/RegionDownloadCallback.java
@@ -1,7 +1,5 @@
 package com.mapbox.services.android.navigation.ui.v5;
 
-import timber.log.Timber;
-
 class RegionDownloadCallback implements OfflineRegionDownloadCallback {
 
   private static final Boolean DISCONNECT_STATE = false;
@@ -13,17 +11,11 @@ class RegionDownloadCallback implements OfflineRegionDownloadCallback {
 
   @Override
   public void onComplete() {
-    // TODO good to go?
-    // TODO Remove debug log after testing
     connectivityController.assign(DISCONNECT_STATE);
-    Timber.d("onComplete!");
   }
 
   @Override
   public void onError(String error) {
-    // TODO fail silently?
-    // TODO Remove debug log after testing
     connectivityController.assign(DISCONNECT_STATE);
-    Timber.d("onError %s", error);
   }
 }
