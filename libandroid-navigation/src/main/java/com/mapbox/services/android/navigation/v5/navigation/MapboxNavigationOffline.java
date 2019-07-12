7990117f5e55235b6b135da4a24a97085303cf4b
Guardiola31337 <pablo.guardiola@mapbox.com>
2019-07-13 01:18:08 +0200

    add adding dynamic download api to mapbox navigation offline

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigationOffline.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigationOffline.java
@@ -1,8 +1,11 @@
 package com.mapbox.services.android.navigation.v5.navigation;
 
 import android.support.annotation.NonNull;
+import com.mapbox.geojson.BoundingBox;
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.navigation.plugin.OfflinePlugin;
 import com.mapbox.navigator.Navigator;
+import com.mapbox.turf.TurfMeasurement;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -26,6 +29,37 @@ public class MapboxNavigationOffline {
         synchronized (this) {
             return navigator.configureRouter(tileSetPath, null, null);
         }
+    }
+
+    /**
+     * Starts the download of tiles specified by the bounding box generated
+     * by the point (center) and apothem passed in.
+     *
+     * @param point          center of the bounding box
+     * @param distanceInKm   distance of the bounding box apothem in km
+     * @param tileSetPath    tile set path in which the tile set is stored
+     * @param accessToken    a valid Enterprise Mapbox access token
+     * @param tileSetVersion version of tile set being requested
+     * @param listener       which is updated on error, on progress update and on completion
+     */
+    public void download(@NonNull Point point, float distanceInKm, @NonNull String tileSetPath,
+                         @NonNull String accessToken, @NonNull String tileSetVersion,
+                         @NonNull RouteTileDownloadListener listener) {
+        BoundingBox boundingBox = generateFrom(point, distanceInKm);
+        OfflineTiles tileSet = OfflineTiles.builder()
+                .accessToken(accessToken)
+                .version(tileSetVersion)
+                .boundingBox(boundingBox)
+                .build();
+        new RouteTileDownloader(new OfflineNavigator(navigator), tileSetPath, listener).startDownload(tileSet);
+    }
+
+    private BoundingBox generateFrom(Point center, float apothem) {
+        Point northFromCenter = TurfMeasurement.destination(center, apothem, 0, "kilometers");
+        Point northeast = TurfMeasurement.destination(northFromCenter, apothem, 90, "kilometers");
+        Point southFromCenter = TurfMeasurement.destination(center, apothem, 180, "kilometers");
+        Point southwest = TurfMeasurement.destination(southFromCenter, apothem, -90, "kilometers");
+        return BoundingBox.fromPoints(southwest, northeast);
     }
 
     public static final class Builder {
