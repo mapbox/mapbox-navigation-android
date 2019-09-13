fb5ec40d00187754cd4d17e914ed2f0df5f37ee0
Guardiola31337 <pablo.guardiola@mapbox.com>
2019-09-13 12:56:32 -0400

    refactor out maneuver map into mapbox navigation notification

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigationNotification.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigationNotification.java
@@ -34,9 +34,27 @@ import java.util.Calendar;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.NAVIGATION_NOTIFICATION_CHANNEL;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.NAVIGATION_NOTIFICATION_ID;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_LEFT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_RIGHT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_SHARP_LEFT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_SHARP_RIGHT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_SLIGHT_LEFT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_STRAIGHT;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_MODIFIER_UTURN;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_ARRIVE;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_CONTINUE;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_DEPART;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_END_OF_ROAD;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_FORK;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_MERGE;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_NEW_NAME;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_NOTIFICATION;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_OFF_RAMP;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_ON_RAMP;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_ROTARY;
 import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_ROUNDABOUT;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_ROUNDABOUT_TURN;
+import static com.mapbox.services.android.navigation.v5.navigation.NavigationConstants.STEP_MANEUVER_TYPE_TURN;
 import static com.mapbox.services.android.navigation.v5.utils.time.TimeFormatter.formatTime;
 
 /**
@@ -293,11 +311,153 @@ class MapboxNavigationNotification implements NavigationNotification {
     if (!TextUtils.isEmpty(maneuverModifier)) {
       String drivingSide = step.drivingSide();
       if (isLeftDrivingSideAndRoundaboutOrRotaryOrUturn(maneuverType, maneuverModifier, drivingSide)) {
-        return ManeuverMap.getManeuverResource(maneuverType + maneuverModifier + drivingSide);
+        return obtainManeuverResourceFrom(maneuverType + maneuverModifier + drivingSide);
       }
-      return ManeuverMap.getManeuverResource(maneuverType + maneuverModifier);
+      return obtainManeuverResourceFrom(maneuverType + maneuverModifier);
     }
-    return ManeuverMap.getManeuverResource(maneuverType);
+    return obtainManeuverResourceFrom(maneuverType);
+  }
+
+  private int obtainManeuverResourceFrom(String maneuver) {
+    switch (maneuver) {
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_UTURN:
+      case STEP_MANEUVER_TYPE_CONTINUE + STEP_MANEUVER_MODIFIER_UTURN:
+        return R.drawable.ic_maneuver_turn_180;
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_UTURN + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_CONTINUE + STEP_MANEUVER_MODIFIER_UTURN + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_turn_180_left_driving_side;
+
+      case STEP_MANEUVER_TYPE_ARRIVE + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_arrive_left;
+      case STEP_MANEUVER_TYPE_ARRIVE + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_arrive_right;
+      case STEP_MANEUVER_TYPE_ARRIVE:
+        return R.drawable.ic_maneuver_arrive;
+
+      case STEP_MANEUVER_TYPE_DEPART + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_depart_left;
+      case STEP_MANEUVER_TYPE_DEPART + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_depart_right;
+      case STEP_MANEUVER_TYPE_DEPART:
+        return R.drawable.ic_maneuver_depart;
+
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_SHARP_RIGHT:
+      case STEP_MANEUVER_TYPE_ON_RAMP + STEP_MANEUVER_MODIFIER_SHARP_RIGHT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_SHARP_RIGHT:
+        return R.drawable.ic_maneuver_turn_75;
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_RIGHT:
+      case STEP_MANEUVER_TYPE_ON_RAMP + STEP_MANEUVER_MODIFIER_RIGHT:
+      case STEP_MANEUVER_TYPE_ROUNDABOUT_TURN + STEP_MANEUVER_MODIFIER_RIGHT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_turn_45;
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+      case STEP_MANEUVER_TYPE_ON_RAMP + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+        return R.drawable.ic_maneuver_turn_30;
+
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_SHARP_LEFT:
+      case STEP_MANEUVER_TYPE_ON_RAMP + STEP_MANEUVER_MODIFIER_SHARP_LEFT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_SHARP_LEFT:
+        return R.drawable.ic_maneuver_turn_75_left;
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ON_RAMP + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROUNDABOUT_TURN + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_turn_45_left;
+      case STEP_MANEUVER_TYPE_TURN + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+      case STEP_MANEUVER_TYPE_ON_RAMP + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+        return R.drawable.ic_maneuver_turn_30_left;
+
+      case STEP_MANEUVER_TYPE_MERGE + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_MERGE + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+        return R.drawable.ic_maneuver_merge_left;
+      case STEP_MANEUVER_TYPE_MERGE + STEP_MANEUVER_MODIFIER_RIGHT:
+      case STEP_MANEUVER_TYPE_MERGE + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+        return R.drawable.ic_maneuver_merge_right;
+
+      case STEP_MANEUVER_TYPE_OFF_RAMP + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_off_ramp_left;
+      case STEP_MANEUVER_TYPE_OFF_RAMP + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+        return R.drawable.ic_maneuver_off_ramp_slight_left;
+
+      case STEP_MANEUVER_TYPE_OFF_RAMP + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_off_ramp_right;
+      case STEP_MANEUVER_TYPE_OFF_RAMP + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+        return R.drawable.ic_maneuver_off_ramp_slight_right;
+
+      case STEP_MANEUVER_TYPE_FORK + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_fork_left;
+      case STEP_MANEUVER_TYPE_FORK + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+        return R.drawable.ic_maneuver_fork_slight_left;
+      case STEP_MANEUVER_TYPE_FORK + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_fork_right;
+      case STEP_MANEUVER_TYPE_FORK + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+        return R.drawable.ic_maneuver_fork_slight_right;
+      case STEP_MANEUVER_TYPE_FORK + STEP_MANEUVER_MODIFIER_STRAIGHT:
+        return R.drawable.ic_maneuver_fork_straight;
+      case STEP_MANEUVER_TYPE_FORK:
+        return R.drawable.ic_maneuver_fork;
+
+      case STEP_MANEUVER_TYPE_END_OF_ROAD + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_end_of_road_left;
+      case STEP_MANEUVER_TYPE_END_OF_ROAD + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_end_of_road_right;
+
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_left;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SHARP_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SHARP_LEFT:
+        return R.drawable.ic_maneuver_roundabout_sharp_left;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT:
+        return R.drawable.ic_maneuver_roundabout_slight_left;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_RIGHT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_RIGHT:
+        return R.drawable.ic_maneuver_roundabout_right;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SHARP_RIGHT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SHARP_RIGHT:
+        return R.drawable.ic_maneuver_roundabout_sharp_right;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT:
+        return R.drawable.ic_maneuver_roundabout_slight_right;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_STRAIGHT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_STRAIGHT:
+        return R.drawable.ic_maneuver_roundabout_straight;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT:
+      case STEP_MANEUVER_TYPE_ROTARY:
+        return R.drawable.ic_maneuver_roundabout;
+
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_LEFT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_LEFT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_left_left_driving_side;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SHARP_LEFT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SHARP_LEFT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_sharp_left_left_driving_side;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SLIGHT_LEFT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_slight_left_left_driving_side;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_RIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_RIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_right_left_driving_side;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SHARP_RIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SHARP_RIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_sharp_right_left_driving_side;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_SLIGHT_RIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_slight_right_left_driving_side;
+      case STEP_MANEUVER_TYPE_ROUNDABOUT + STEP_MANEUVER_MODIFIER_STRAIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+      case STEP_MANEUVER_TYPE_ROTARY + STEP_MANEUVER_MODIFIER_STRAIGHT + STEP_MANEUVER_MODIFIER_LEFT:
+        return R.drawable.ic_maneuver_roundabout_straight_left_driving_side;
+
+      case STEP_MANEUVER_TYPE_MERGE + STEP_MANEUVER_MODIFIER_STRAIGHT:
+      case STEP_MANEUVER_TYPE_NOTIFICATION + STEP_MANEUVER_MODIFIER_STRAIGHT:
+      case STEP_MANEUVER_TYPE_CONTINUE + STEP_MANEUVER_MODIFIER_STRAIGHT:
+      case STEP_MANEUVER_TYPE_NEW_NAME + STEP_MANEUVER_MODIFIER_STRAIGHT:
+      default:
+        return R.drawable.ic_maneuver_turn_0;
+    }
   }
 
   private boolean isLeftDrivingSideAndRoundaboutOrRotaryOrUturn(String maneuverType, String maneuverModifier,
