69702d7d2188c56bf884d2f15bc12f4ef0cc4189
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-09-24 14:22:28 -0700

    add androidx dependencies

--- app/src/androidTest/java/testapp/robot/feedback/FeedbackResultRobot.java
+++ app/src/androidTest/java/testapp/robot/feedback/FeedbackResultRobot.java
@@ -1,9 +1,10 @@
 package testapp.robot.feedback;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class FeedbackResultRobot {
   public FeedbackResultRobot isSuccess(String successText) {
--- app/src/androidTest/java/testapp/robot/feedback/FeedbackRobot.java
+++ app/src/androidTest/java/testapp/robot/feedback/FeedbackRobot.java
@@ -1,12 +1,11 @@
 package testapp.robot.feedback;
 
-import android.support.test.espresso.contrib.RecyclerViewActions;
 
 import com.mapbox.services.android.navigation.testapp.R;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 public class FeedbackRobot {
   public FeedbackRobot openFeedback() {
