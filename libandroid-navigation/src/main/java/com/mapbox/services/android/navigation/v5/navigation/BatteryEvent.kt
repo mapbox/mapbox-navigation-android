eb8715f05fa5fe53b599281b8683ca774f1bdf38
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-10-02 16:35:55 +0300

    Fix nullable types in BatteryEvent

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/BatteryEvent.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/BatteryEvent.kt
@@ -5,10 +5,10 @@ import android.os.Parcelable
 
 @SuppressLint("ParcelCreator")
 internal data class BatteryEvent(
-    private val sessionId: String,
+    private val sessionId: String?,
     private val batteryPercentage: Float,
     private val isPluggedIn: Boolean,
-    private val metadata: NavigationPerformanceMetadata
+    private val metadata: NavigationPerformanceMetadata?
 ) : NavigationPerformanceEvent(sessionId, BATTERY_EVENT_NAME, metadata), Parcelable {
 
     companion object {
