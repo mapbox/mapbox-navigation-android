a58c1033b0922bc2931af38b2c0690c2d7ebf986
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-02 19:48:49 +0300

    Mock dependency and small changes as a sample

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/MapboxOfflineRouterTest.kt
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/MapboxOfflineRouterTest.kt
@@ -2,65 +2,66 @@ package com.mapbox.services.android.navigation.v5.navigation
 
 import com.mapbox.geojson.BoundingBox
 import com.mapbox.geojson.Point
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.Test
-import org.mockito.ArgumentMatchers.eq
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
 
 class MapboxOfflineRouterTest {
 
     @Test
     fun initializeOfflineData_filePathIncludesVersion() {
         val tilePath = "/some/path/"
-        val offlineNavigator = mock(OfflineNavigator::class.java)
-        val callback = mock(OnOfflineTilesConfiguredCallback::class.java)
+        val offlineNavigator = mockk<OfflineNavigator>(relaxUnitFun = true)
+        val callback = mockk<OnOfflineTilesConfiguredCallback>(relaxUnitFun = true)
         val offlineRouter = buildRouter(tilePath, offlineNavigator)
 
         offlineRouter.configure("version", callback)
 
-        verify(offlineNavigator).configure("/some/path/version", callback)
+        verify { offlineNavigator.configure("/some/path/version", callback) }
     }
 
     @Test
     fun findOfflineRoute_offlineNavigatorIsCalled() {
-        val offlineNavigator = mock(OfflineNavigator::class.java)
-        val offlineRoute = mock(OfflineRoute::class.java)
-        val callback = mock(OnOfflineRouteFoundCallback::class.java)
+        val offlineNavigator = mockk<OfflineNavigator>(relaxUnitFun = true)
+        val offlineRoute = mockk<OfflineRoute>(relaxUnitFun = true)
+        val callback = mockk<OnOfflineRouteFoundCallback>(relaxUnitFun = true)
         val offlineRouter = buildRouter(offlineNavigator)
 
         offlineRouter.findRoute(offlineRoute, callback)
 
-        verify(offlineNavigator).retrieveRouteFor(offlineRoute, callback)
+        verify { offlineNavigator.retrieveRouteFor(offlineRoute, callback) }
     }
 
     @Test
     fun fetchAvailableTileVersions() {
         val accessToken = "access_token"
-        val callback = mock(OnTileVersionsFoundCallback::class.java)
-        val offlineTileVersions = mock(OfflineTileVersions::class.java)
+        val callback = mockk<OnTileVersionsFoundCallback>(relaxUnitFun = true)
+        val offlineTileVersions = mockk<OfflineTileVersions>(relaxUnitFun = true)
         val offlineRouter = buildRouter(offlineTileVersions)
 
         offlineRouter.fetchAvailableTileVersions(accessToken, callback)
 
-        verify(offlineTileVersions).fetchRouteTileVersions(accessToken, callback)
+        verify { offlineTileVersions.fetchRouteTileVersions(accessToken, callback) }
     }
 
     @Test
     fun checksRemoveTiles() {
         val aTilePath = "/some/path/"
-        val anOfflineNavigator = mock(OfflineNavigator::class.java)
+        val anOfflineNavigator = mockk<OfflineNavigator>(relaxUnitFun = true)
         val theOfflineRouter = buildRouter(aTilePath, anOfflineNavigator)
         val southwest = Point.fromLngLat(1.0, 2.0)
         val northeast = Point.fromLngLat(3.0, 4.0)
         val aBoundingBox = BoundingBox.fromPoints(southwest, northeast)
-        val aCallback = mock(OnOfflineTilesRemovedCallback::class.java)
+        val aCallback = mockk<OnOfflineTilesRemovedCallback>()
 
         theOfflineRouter.removeTiles("a_version", aBoundingBox, aCallback)
 
-        verify(anOfflineNavigator).removeTiles(
-            eq("/some/path/a_version"), eq(southwest), eq(northeast),
-            eq(aCallback)
-        )
+        verify {
+            anOfflineNavigator.removeTiles(
+                eq("/some/path/a_version"), eq(southwest), eq(northeast),
+                eq(aCallback)
+            )
+        }
     }
 
     private fun buildRouter(
@@ -70,15 +71,15 @@ class MapboxOfflineRouterTest {
         return MapboxOfflineRouter(
             tilePath,
             offlineNavigator,
-            mock(OfflineTileVersions::class.java)
+            mockk(relaxUnitFun = true)
         )
     }
 
     private fun buildRouter(offlineNavigator: OfflineNavigator): MapboxOfflineRouter {
-        return MapboxOfflineRouter("", offlineNavigator, mock(OfflineTileVersions::class.java))
+        return MapboxOfflineRouter("", offlineNavigator, mockk(relaxUnitFun = true))
     }
 
     private fun buildRouter(offlineTileVersions: OfflineTileVersions): MapboxOfflineRouter {
-        return MapboxOfflineRouter("", mock(OfflineNavigator::class.java), offlineTileVersions)
+        return MapboxOfflineRouter("", mockk(relaxUnitFun = true), offlineTileVersions)
     }
 }
