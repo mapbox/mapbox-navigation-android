4eaaf95b721dd0911ec7c9f32bab72709f85a8b5
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-04 16:44:20 +0300

    TarFetchedCallback and RouteTileDownloadListener: to kotlin

---
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/TarFetchedCallback.kt
@@ -0,0 +1,34 @@
+package com.mapbox.services.android.navigation.v5.navigation
+
+import android.os.AsyncTask
+import com.mapbox.services.android.navigation.v5.utils.DownloadTask
+import java.util.HashMap
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * Triggers the downloading of the tar file included in the [ResponseBody] onto disk.
+ */
+internal class TarFetchedCallback(
+    private val downloader: RouteTileDownloader,
+    private val downloadTask: DownloadTask
+) : Callback<ResponseBody> {
+
+    override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+        if (response.isSuccessful) {
+            downloadTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, response.body())
+        } else {
+            val errorCodes = HashMap<Int, String>()
+            val errorMap = TarResponseErrorMap(errorCodes)
+            val error = OfflineError(errorMap.buildErrorMessageWith(response))
+            downloader.onError(error)
+        }
+    }
+
+    override fun onFailure(call: Call<ResponseBody>, throwable: Throwable) {
+        val error = OfflineError(throwable.message ?: "Unspecified error")
+        downloader.onError(error)
+    }
+}
