5391fe3281d63ab6a73f2f8bfb8b0ae4cb016d57
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-10-06 21:47:36 +0300

    Move to internal package

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/NavigationRouteCallback.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/NavigationRouteCallback.kt
@@ -1,4 +1,4 @@
-package com.mapbox.services.android.navigation.v5.navigation
+package com.mapbox.services.android.navigation.v5.internal.navigation
 
 import com.mapbox.api.directions.v5.models.DirectionsResponse
 import retrofit2.Call
