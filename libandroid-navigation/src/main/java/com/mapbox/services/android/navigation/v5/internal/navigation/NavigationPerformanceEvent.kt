044c5887eb0040bf368d9c76b6f560c0163ff20b
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-10-06 21:54:45 +0300

    Move to internal package

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/NavigationPerformanceEvent.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/NavigationPerformanceEvent.kt
@@ -1,4 +1,4 @@
-package com.mapbox.services.android.navigation.v5.navigation
+package com.mapbox.services.android.navigation.v5.internal.navigation
 
 import android.annotation.SuppressLint
 import android.os.Parcel
