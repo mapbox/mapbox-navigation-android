b2fe184fde5504be901ffc76939c2a4aa096b188
Pablo Guardiola <pablo.guardiola@mapbox.com>
2019-10-07 15:11:13 +0200

    fix javadoc kotlin issues (#2103)

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/OnOfflineTilesConfiguredCallback.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/OnOfflineTilesConfiguredCallback.java
@@ -4,15 +4,15 @@ import androidx.annotation.NonNull;
 
 /**
  * Listener that needs to be added to
- * {@link MapboxOfflineRouter#configure(String, OnOfflineTilesConfiguredCallback)} to know when
+ * <tt>MapboxOfflineRouter#configure(String, OnOfflineTilesConfiguredCallback)</tt> to know when
  * offline data is initialized and
- * {@link MapboxOfflineRouter#findRoute(OfflineRoute, OnOfflineRouteFoundCallback)} could be called.
+ * <tt>MapboxOfflineRouter#findRoute(OfflineRoute, OnOfflineRouteFoundCallback)</tt> could be called.
  */
 public interface OnOfflineTilesConfiguredCallback {
 
   /**
    * Called whe the offline data is initialized and
-   * {@link MapboxOfflineRouter#findRoute(OfflineRoute, OnOfflineRouteFoundCallback)}.
+   * <tt>MapboxOfflineRouter#findRoute(OfflineRoute, OnOfflineRouteFoundCallback)</tt>.
    * could be called safely.
    *
    * @param numberOfTiles initialized in the path provided
