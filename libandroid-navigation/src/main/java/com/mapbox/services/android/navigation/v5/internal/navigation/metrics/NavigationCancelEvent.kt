083e1df4e40230e4bd3b12b52eab95b146e9f506
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-10-08 18:51:40 +0300

    NavigationArriveEvent, NavigationCancelEvent, NavigationDepartEvent convert to Kotlin

---
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/metrics/NavigationCancelEvent.kt
@@ -0,0 +1,25 @@
+package com.mapbox.services.android.navigation.v5.internal.navigation.metrics
+
+import android.annotation.SuppressLint
+
+@SuppressLint("ParcelCreator")
+internal class NavigationCancelEvent(
+    phoneState: PhoneState
+) : NavigationEvent(phoneState) {
+
+    companion object {
+        private const val NAVIGATION_CANCEL = "navigation.cancel"
+    }
+
+    /**
+     * Don't remove any fields, cause they are should match with
+     * the schema downloaded from S3. Look at {@link SchemaTest}
+     */
+    var arrivalTimestamp: String = ""
+    var rating: Int = 0
+    var comment: String = ""
+
+    override fun getEventName(): String {
+        return NAVIGATION_CANCEL
+    }
+}
