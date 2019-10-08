2d67843e5d4e271015b0f11325b5c7abaf6b93ee
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2019-10-08 14:18:03 -0700

    convert navigationFasterRouteListener to kotlin (#2119)

---
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/NavigationFasterRouteListener.kt
@@ -0,0 +1,27 @@
+package com.mapbox.services.android.navigation.v5.internal.navigation
+
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.services.android.navigation.v5.route.FasterRoute
+import com.mapbox.services.android.navigation.v5.route.RouteListener
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress
+import timber.log.Timber
+
+internal class NavigationFasterRouteListener(
+    private val navigationEventDispatcher: NavigationEventDispatcher,
+    private val fasterRouteEngine: FasterRoute
+) : RouteListener {
+
+    companion object {
+        private const val FIRST_ROUTE = 0
+    }
+
+    override fun onResponseReceived(response: DirectionsResponse, routeProgress: RouteProgress?) {
+        if (fasterRouteEngine.isFasterRoute(response, routeProgress)) {
+            navigationEventDispatcher.onFasterRouteEvent(response.routes()[FIRST_ROUTE])
+        }
+    }
+
+    override fun onErrorReceived(throwable: Throwable) {
+        Timber.e(throwable)
+    }
+}
