e789b7bde62c4e3528b430e48eb5a47494af9333
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-10-07 17:16:30 -0700

    convert navigationNotificationProvider to kotlin

---
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/NavigationNotificationProvider.kt
@@ -0,0 +1,38 @@
+package com.mapbox.services.android.navigation.v5.internal.navigation
+
+import android.content.Context
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation
+import com.mapbox.services.android.navigation.v5.navigation.notification.NavigationNotification
+import com.mapbox.services.android.navigation.v5.routeprogress.RouteProgress
+
+internal class NavigationNotificationProvider(applicationContext: Context, mapboxNavigation: MapboxNavigation) {
+
+    private var navigationNotification: NavigationNotification? = null
+    private var shouldUpdate: Boolean = true
+
+    init {
+        navigationNotification = buildNotificationFrom(applicationContext, mapboxNavigation)
+    }
+
+    fun retrieveNotification(): NavigationNotification? {
+        return navigationNotification
+    }
+
+    fun updateNavigationNotification(routeProgress: RouteProgress) {
+        if (shouldUpdate) {
+            navigationNotification?.updateNotification(routeProgress)
+        }
+    }
+
+    fun shutdown(applicationContext: Context) {
+        navigationNotification?.onNavigationStopped(applicationContext)
+        navigationNotification = null
+        shouldUpdate = false
+    }
+
+    private fun buildNotificationFrom(applicationContext: Context, mapboxNavigation: MapboxNavigation): NavigationNotification {
+        val options = mapboxNavigation.options()
+        val navNotification = options.navigationNotification()
+        return navNotification ?: MapboxNavigationNotification(applicationContext, mapboxNavigation)
+    }
+}
