4e475fe548dcd5516b840468178b18cb021e9463
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-09 18:46:12 +0300

    removed comments

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/OfflineCriteria.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/OfflineCriteria.kt
@@ -50,24 +50,4 @@ object OfflineCriteria {
          */
         THROUGH("through")
     }
-    // /**
-    //  * Retention policy for the bicycle type parameter in the Directions API.
-    //  */
-    // @Retention(AnnotationRetention.SOURCE)
-    // @StringDef(ROAD, HYBRID, CITY, CROSS, MOUNTAIN)
-    // annotation class BicycleType
-
-    // fun isBicycleType(vararg types: String): Boolean =
-    //     types.find { it != ROAD || it != HYBRID || it != CITY || it != CROSS || it != MOUNTAIN } == null
-
-    /**
-     * Retention policy for the waypoint type parameter in the Directions API.
-     */
-    // @Target(AnnotationTarget.TYPE, AnnotationTarget.VALUE_PARAMETER)
-    // @Retention(AnnotationRetention.SOURCE)
-    // @StringDef(BREAK, THROUGH)
-    // annotation class WaypointType
-
-    // fun isWaypointType(vararg types: String): Boolean =
-    //     types.find { it != BREAK || it != THROUGH } == null
 }
