45f84a02cf287b00ac3e3f74eb73b1fc514fa30e
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-10-11 16:29:42 +0300

    MetricsRouteProgress convert to Kotlin

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/metrics/NavigationStepData.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/navigation/metrics/NavigationStepData.kt
@@ -6,14 +6,14 @@ import com.mapbox.services.android.navigation.v5.internal.navigation.routeprogre
  * Class that contains step meta data
  */
 internal class NavigationStepData(metricsRouteProgress: MetricsRouteProgress) {
-    val upcomingInstruction: String = metricsRouteProgress.upcomingStepInstruction
-    val upcomingModifier: String = metricsRouteProgress.upcomingStepModifier
-    val upcomingName: String = metricsRouteProgress.upcomingStepName
-    val upcomingType: String = metricsRouteProgress.upcomingStepType
-    val previousInstruction: String = metricsRouteProgress.previousStepInstruction
-    val previousModifier: String = metricsRouteProgress.previousStepModifier
-    val previousName: String = metricsRouteProgress.previousStepName
-    val previousType: String = metricsRouteProgress.previousStepType
+    val upcomingInstruction: String? = metricsRouteProgress.upcomingStepInstruction
+    val upcomingModifier: String? = metricsRouteProgress.upcomingStepModifier
+    val upcomingName: String? = metricsRouteProgress.upcomingStepName
+    val upcomingType: String? = metricsRouteProgress.upcomingStepType
+    val previousInstruction: String? = metricsRouteProgress.previousStepInstruction
+    val previousModifier: String? = metricsRouteProgress.previousStepModifier
+    val previousName: String? = metricsRouteProgress.previousStepName
+    val previousType: String? = metricsRouteProgress.previousStepType
     val distance: Int = metricsRouteProgress.currentStepDistance
     val duration: Int = metricsRouteProgress.currentStepDuration
     val distanceRemaining: Int = metricsRouteProgress.currentStepDistanceRemaining
