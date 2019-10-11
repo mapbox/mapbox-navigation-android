620c57672a75b3a1c8378bce2e349966eaac23de
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-10-10 17:35:23 -0700

    fix nullability

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/RouteAnnotationUpdater.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/RouteAnnotationUpdater.kt
@@ -7,19 +7,23 @@ class RouteAnnotationUpdater {
 
     fun update(
         oldRoute: DirectionsRoute,
-        annotationHolder: DirectionsRoute,
+        annotationHolder: DirectionsRoute?,
         currentLegIndex: Int
     ): DirectionsRoute {
-
-        val legs = ArrayList(oldRoute.legs()!!)
-
-        for (i in currentLegIndex until legs.size) {
-            val updatedAnnotation = annotationHolder.legs()!![i - currentLegIndex].annotation()
-            legs[i] = legs[i].toBuilder().annotation(updatedAnnotation).build()
+        oldRoute.legs()?.let { routeLegList ->
+            val legs = ArrayList(routeLegList)
+            for (i in currentLegIndex until legs.size) {
+                annotationHolder?.legs().let { routeLegs ->
+                    routeLegs?.let { routeLegs ->
+                        val updatedAnnotation = routeLegs[i - currentLegIndex].annotation()
+                        legs[i] = legs[i].toBuilder().annotation(updatedAnnotation).build()
+                    }
+                }
+            }
+            return oldRoute.toBuilder()
+                    .legs(legs)
+                    .build()
         }
-
-        return oldRoute.toBuilder()
-                .legs(legs)
-                .build()
+        return oldRoute
     }
 }
