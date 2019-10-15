187d6e227979bcac570211c7f6f1971ae8e25532
Oleg Zilberman <oleg.zilberman@mapbox.com>
2019-10-14 20:59:09 -0700

    updating dependent code

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/NavigationWalkingOptionsTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/navigation/NavigationWalkingOptionsTest.java
@@ -8,21 +8,21 @@ public class NavigationWalkingOptionsTest {
 
   @Test
   public void alleyBias_walkingOptionSet() {
-    NavigationWalkingOptions options = NavigationWalkingOptions.Companion.builder().alleyBias(0.7).build();
+    NavigationWalkingOptions options = NavigationWalkingOptions.builder().alleyBias(0.7).build();
 
     assertEquals(Double.valueOf(0.7), options.getWalkingOptions().alleyBias());
   }
 
   @Test
   public void walkwayBias_walkingOptionSet() {
-    NavigationWalkingOptions options = NavigationWalkingOptions.Companion.builder().walkwayBias(0.8).build();
+    NavigationWalkingOptions options = NavigationWalkingOptions.builder().walkwayBias(0.8).build();
 
     assertEquals(Double.valueOf(0.8), options.getWalkingOptions().walkwayBias());
   }
 
   @Test
   public void walkingSpeed_walkingOptionSet() {
-    NavigationWalkingOptions options = NavigationWalkingOptions.Companion.builder().walkingSpeed(2.0).build();
+    NavigationWalkingOptions options = NavigationWalkingOptions.builder().walkingSpeed(2.0).build();
 
     assertEquals(Double.valueOf(2.0), options.getWalkingOptions().walkingSpeed());
   }
