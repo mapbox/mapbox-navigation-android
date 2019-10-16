033b3e9cc2d25138e83ae5d7854ef5ca775675fd
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-10-15 18:30:28 -0700

    convert routeProgressStateMap to kotlin

---
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/routeprogress/RouteProgressStateMap.kt
@@ -0,0 +1,15 @@
+package com.mapbox.services.android.navigation.v5.routeprogress
+
+import com.mapbox.navigator.RouteState
+import java.util.HashMap
+
+class RouteProgressStateMap : HashMap<RouteState, RouteProgressState?>() {
+    init {
+        put(RouteState.INVALID, RouteProgressState.ROUTE_INVALID)
+        put(RouteState.INITIALIZED, RouteProgressState.ROUTE_INITIALIZED)
+        put(RouteState.COMPLETE, RouteProgressState.ROUTE_ARRIVED)
+        put(RouteState.TRACKING, RouteProgressState.LOCATION_TRACKING)
+        put(RouteState.STALE, RouteProgressState.LOCATION_STALE)
+        put(RouteState.OFFROUTE, null) // Ignore off-route (info already provided via listener)
+    }
+}
