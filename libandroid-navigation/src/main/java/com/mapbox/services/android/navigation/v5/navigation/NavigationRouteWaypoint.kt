b9c60e8286956599421f2972263d9f59d226d335
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-25 11:20:39 +0300

    NavigationRouteWaypoint: make public

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteWaypoint.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteWaypoint.kt
@@ -2,7 +2,7 @@ package com.mapbox.services.android.navigation.v5.navigation
 
 import com.mapbox.geojson.Point
 
-internal data class NavigationRouteWaypoint(
+data class NavigationRouteWaypoint(
     val waypoint: Point,
     val bearingAngle: Double?,
     val tolerance: Double?
