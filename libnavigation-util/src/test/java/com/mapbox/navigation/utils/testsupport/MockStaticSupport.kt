c709ae67a661bb13ee661347263153c597a968c5
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-29 18:05:38 +0300

    Fix tests and import

--- libnavigation-util/src/test/java/com/mapbox/navigation/utils/testsupport/MockStaticSupport.kt
+++ libnavigation-util/src/test/java/com/mapbox/navigation/utils/testsupport/MockStaticSupport.kt
@@ -1,12 +1,12 @@
-package com.mapbox.services.android.navigation.v5.testsupport
+package com.mapbox.navigation.utils.testsupport
 
 import io.mockk.mockkStatic
 
-internal inline fun mockkStaticSupport(vararg extension: Extensions) {
+inline fun mockkStaticSupport(vararg extension: Extensions) {
     mockkStatic(*extension.map { it.path }.toTypedArray())
 }
 
-internal enum class Extensions(val path: String) {
+enum class Extensions(val path: String) {
     ContextEx("com.mapbox.navigation.utils.extensions.ContextEx"),
     LocaleEx("com.mapbox.navigation.utils.extensions.LocaleEx"),
     SpanEx("com.mapbox.navigation.utils.extensions.SpanEx");
