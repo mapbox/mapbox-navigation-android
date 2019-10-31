5f4bd1b65103ac2f59a23aaaa2a27fac8b0aa182
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-31 17:17:36 +0300

    moved classes to `libderections-common` module

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/route/model/StepManeuverNavigation.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/route/model/StepManeuverNavigation.kt
@@ -1,72 +1,154 @@
 package com.mapbox.navigation.base.route.model
 
 import androidx.annotation.StringDef
-import com.mapbox.api.directions.v5.models.StepManeuver
-import com.mapbox.geojson.Point
 
-class StepManeuverNavigation(
-    val location: Point,
+data class StepManeuverNavigation(
+    val location: PointNavigation,
     val bearingBefore: Double?,
     val bearingAfter: Double?,
-    @StepManeuverTypeNavigation type: String?,
+    @StepManeuverTypeNavigation val type: String?,
     val exit: Int?
-
 ) {
     companion object {
 
-        const val UNKNOWN = "UNKNOWN"
+        /**
+         * A basic turn in the direction of the modifier.
+         *
+         * @since 1.0
+         */
+        const val TURN = "turn"
 
-        private val typesArray = arrayOf(
-            StepManeuver.TURN,
-            StepManeuver.NEW_NAME,
-            StepManeuver.DEPART,
-            StepManeuver.ARRIVE,
-            StepManeuver.MERGE,
-            StepManeuver.ON_RAMP,
-            StepManeuver.OFF_RAMP,
-            StepManeuver.FORK,
-            StepManeuver.END_OF_ROAD,
-            StepManeuver.CONTINUE,
-            StepManeuver.ROUNDABOUT,
-            StepManeuver.ROTARY,
-            StepManeuver.ROUNDABOUT_TURN,
-            StepManeuver.NOTIFICATION,
-            StepManeuver.EXIT_ROUNDABOUT,
-            StepManeuver.EXIT_ROTARY
-        )
+        /**
+         * The road name changes (after a mandatory turn).
+         *
+         * @since 1.0
+         */
+        const val NEW_NAME = "new name"
 
-        fun typeExist(@StepManeuver.StepManeuverType type: String?): Boolean {
-            return type in typesArray
-        }
+        /**
+         * Indicates departure from a leg.
+         * The  modifier value indicates the position of the departure point
+         * in relation to the current direction of travel.
+         *
+         * @since 1.0
+         */
+        const val DEPART = "depart"
+
+        /**
+         * Indicates arrival to a destination of a leg.
+         * The modifier value indicates the position of the arrival point
+         * in relation to the current direction of travel.
+         *
+         * @since 1.0
+         */
+        const val ARRIVE = "arrive"
+
+        /**
+         * Merge onto a street.
+         *
+         * @since 1.0
+         */
+        const val MERGE = "merge"
+
+        /**
+         * Take a ramp to enter a highway.
+         * @since 1.0
+         */
+        const val ON_RAMP = "on ramp"
+
+        /**
+         * Take a ramp to exit a highway.
+         *
+         * @since 1.0
+         */
+        const val OFF_RAMP = "off ramp"
+
+        /**
+         * Take the left or right side of a fork.
+         *
+         * @since 1.0
+         */
+        const val FORK = "fork"
+
+        /**
+         * Road ends in a T intersection.
+         *
+         * @since 1.0
+         */
+        const val END_OF_ROAD = "end of road"
+
+        /**
+         * Continue on a street after a turn.
+         *
+         * @since 1.0
+         */
+        const val CONTINUE = "continue"
+
+        /**
+         * Traverse roundabout.
+         * Has an additional property  exit in the route step that contains
+         * the exit number. The  modifier specifies the direction of entering the roundabout.
+         *
+         * @since 1.0
+         */
+        const val ROUNDABOUT = "roundabout"
+
+        /**
+         * A traffic circle. While very similar to a larger version of a roundabout,
+         * it does not necessarily follow roundabout rules for right of way.
+         *
+         * @since 1.0
+         */
+        const val ROTARY = "rotary"
+
+        /**
+         * A small roundabout that is treated as an intersection.
+         *
+         * @since 1.0
+         */
+        const val ROUNDABOUT_TURN = "roundabout turn"
+
+        /**
+         * Indicates a change of driving conditions, for example changing the  mode
+         * from driving to ferry.
+         *
+         * @since 1.0
+         */
+        const val NOTIFICATION = "notification"
+
+        /**
+         * Indicates the exit maneuver from a roundabout.
+         *
+         * @since 1.0
+         */
+        const val EXIT_ROUNDABOUT = "exit roundabout"
+
+        /**
+         * Indicates the exit maneuver from a rotary.
+         *
+         * @since 1.0
+         */
+        const val EXIT_ROTARY = "exit rotary"
     }
 
     @Retention(AnnotationRetention.RUNTIME)
     @StringDef(
-        StepManeuver.TURN,
-        StepManeuver.NEW_NAME,
-        StepManeuver.DEPART,
-        StepManeuver.ARRIVE,
-        StepManeuver.MERGE,
-        StepManeuver.ON_RAMP,
-        StepManeuver.OFF_RAMP,
-        StepManeuver.FORK,
-        StepManeuver.END_OF_ROAD,
-        StepManeuver.CONTINUE,
-        StepManeuver.ROUNDABOUT,
-        StepManeuver.ROTARY,
-        StepManeuver.ROUNDABOUT_TURN,
-        StepManeuver.NOTIFICATION,
-        StepManeuver.EXIT_ROUNDABOUT,
-        StepManeuver.EXIT_ROTARY,
-        UNKNOWN
+        TURN,
+        NEW_NAME,
+        DEPART,
+        ARRIVE,
+        MERGE,
+        ON_RAMP,
+        OFF_RAMP,
+        FORK,
+        END_OF_ROAD,
+        CONTINUE,
+        ROUNDABOUT,
+        ROTARY,
+        ROUNDABOUT_TURN,
+        NOTIFICATION,
+        EXIT_ROUNDABOUT,
+        EXIT_ROTARY
     )
     annotation class StepManeuverTypeNavigation
 }
-
-fun StepManeuver.mapToManeuverStep() = StepManeuverNavigation(
-    location = location(),
-    bearingBefore = bearingBefore(),
-    bearingAfter = bearingAfter(),
-    type = if (StepManeuverNavigation.typeExist(type())) type() else StepManeuverNavigation.UNKNOWN,
-    exit = exit()
-)
