dbee5c3e561709434bb648f7918e5224bc47545d
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-31 17:24:24 +0300

    minor changes

---
+++ libnavigation-util/src/main/java/com/mapbox/navigation/utils/exceptions/NavigationException.kt
@@ -0,0 +1,14 @@
+package com.mapbox.navigation.utils.exceptions
+
+/**
+ * Generic Exception for all things Mapbox Navigation.
+ *
+ * A form of `Throwable` that indicates conditions that a reasonable application might
+ * want to catch.
+ *
+ * @param message the detail message (which is saved for later retrieval by the
+ * [Throwable.message] method).
+ *
+ * @since 0.2.0
+ */
+class NavigationException(message: String) : RuntimeException(message)
