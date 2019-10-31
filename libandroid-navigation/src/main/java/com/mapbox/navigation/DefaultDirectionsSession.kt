e8805e69b1d909014b77f7af1bd69544f9e04e4b
Yury Kanetski <yury.kanetski@mapbox.com>
2019-10-31 17:54:36 +0300

    DefaultDirectionsSession: lint refactored

--- libandroid-navigation/src/main/java/com/mapbox/navigation/DefaultDirectionsSession.kt
+++ libandroid-navigation/src/main/java/com/mapbox/navigation/DefaultDirectionsSession.kt
@@ -6,7 +6,11 @@ import com.mapbox.navigation.base.route.model.PointNavigation
 import com.mapbox.navigation.base.route.model.Route
 import java.util.concurrent.CopyOnWriteArrayList
 
-class DefaultDirectionsSession(private val router: Router, origin: PointNavigation, waypoints: List<PointNavigation>) : DirectionsSession {
+class DefaultDirectionsSession(
+    private val router: Router,
+    origin: PointNavigation,
+    waypoints: List<PointNavigation>
+) : DirectionsSession {
 
     override var currentRoute: Route? = null
         set(value) {
