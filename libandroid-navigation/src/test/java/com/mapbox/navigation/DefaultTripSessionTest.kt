dd37b79aaffbf16f4b988288d793cb7c3c35f477
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-11-01 15:34:16 -0700

    fix ci error

--- libandroid-navigation/src/test/java/com/mapbox/navigation/DefaultTripSessionTest.kt
+++ libandroid-navigation/src/test/java/com/mapbox/navigation/DefaultTripSessionTest.kt
@@ -8,6 +8,7 @@ import com.mapbox.android.core.location.LocationEngineCallback
 import com.mapbox.android.core.location.LocationEngineRequest
 import com.mapbox.android.core.location.LocationEngineResult
 import com.mapbox.navigation.base.model.route.Route
+import com.mapbox.navigation.base.model.route.RouteProgress
 import com.mapbox.navigation.base.trip.TripService
 import com.mapbox.navigation.base.trip.TripSession
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
---
+++ libtrip-notification/src/test/java/com/mapbox/navigation/trip/notification/MapboxTripNotificationTest.kt
@@ -0,0 +1,4 @@
+package com.mapbox.navigation.trip.notification
+
+class MapboxTripNotificationTest {
+}
