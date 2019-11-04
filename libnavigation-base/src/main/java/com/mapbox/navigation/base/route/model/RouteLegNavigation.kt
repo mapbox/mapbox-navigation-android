43ec0afb253054113e3ab369f46349de534e65c6
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-04 12:36:29 +0300

    Return dependecies to core api

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/route/model/RouteLegNavigation.kt
+++
@@ -1,8 +0,0 @@
-package com.mapbox.navigation.base.route.model
-
-data class RouteLegNavigation(
-    val distance: Double?,
-    val duration: Double?,
-    val summary: String?,
-    val steps: List<LegStepNavigation>?
-)
