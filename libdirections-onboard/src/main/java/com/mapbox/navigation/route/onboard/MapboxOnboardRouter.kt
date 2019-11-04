d3d65ff643fc1b8055cf4ae338ab00d5e26e60e9
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-04 18:57:45 +0300

    Navigation onboard: initial setup

--- libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
+++ libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
@@ -4,9 +4,49 @@ import com.mapbox.annotation.navigation.module.MapboxNavigationModule
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType
 import com.mapbox.navigation.base.route.Router
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
+import com.mapbox.navigation.navigator.MapboxNativeNavigatorImpl
+import java.io.File
 
 @MapboxNavigationModule(MapboxNavigationModuleType.OnboardRouter, skipConfiguration = true)
 class MapboxOnboardRouter(private val navigator: MapboxNativeNavigator) : Router {
+
+    companion object {
+
+        private const val TILE_PATH_NAME = "tiles"
+    }
+
+    // private val offlineNavigator: OfflineNavigator
+    private val tilePath: String
+    private val offlineTileVersions: OfflineTileVersions
+    private val navigator: MapboxNativeNavigator
+
+    /**
+     * Creates an offline router which uses the specified offline path for storing and retrieving
+     * data.
+     *
+     * @param offlinePath directory path where the offline data is located
+     */
+    constructor(offlinePath: String) {
+        val tileDir = File(offlinePath, TILE_PATH_NAME)
+        if (!tileDir.exists()) {
+            tileDir.mkdirs()
+        }
+
+        this.tilePath = tileDir.absolutePath
+        offlineTileVersions = OfflineTileVersions()
+        this.navigator = MapboxNativeNavigatorImpl
+    }
+
+    // Package private for testing purposes
+    internal constructor(
+        tilePath: String,
+        offlineTileVersions: OfflineTileVersions,
+        navigator: MapboxNativeNavigator
+    ) {
+        this.tilePath = tilePath
+        this.offlineTileVersions = offlineTileVersions
+        this.navigator = navigator
+    }
 
     override fun getRoute(
         routeOptions: RouteOptionsNavigation,
