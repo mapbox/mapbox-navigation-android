3c5a9549f81a9067f2ffa5f97575f4dbe3fceedf
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-04 12:36:29 +0300

    Return dependecies to core api

--- libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
+++ libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
@@ -4,16 +4,15 @@ import com.mapbox.annotation.navigation.module.MapboxNavigationModule
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType
 import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.route.Router
-import com.mapbox.navigation.base.route.model.PointNavigation
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
 
 @MapboxNavigationModule(MapboxNavigationModuleType.OnboardRouter, skipConfiguration = true)
 class MapboxOnboardRouter(private val navigator: MapboxNativeNavigator) : Router {
 
     override fun getRoute(
-        origin: PointNavigation,
-        waypoints: List<PointNavigation>?,
-        destination: PointNavigation,
+        origin: Point,
+        waypoints: List<Point>?,
+        destination: Point,
         listener: Router.RouteListener
     ) {
         TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
--- libdirections-session/src/test/java/com/mapbox/navigation/directions/session/MapboxDirectionsSessionTest.kt
+++ libdirections-session/src/test/java/com/mapbox/navigation/directions/session/MapboxDirectionsSessionTest.kt
@@ -1,8 +1,8 @@
 package com.mapbox.navigation.directions.session
 
+import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.route.DirectionsSession
 import com.mapbox.navigation.base.route.Router
-import com.mapbox.navigation.base.route.model.PointNavigation
 import com.mapbox.navigation.base.route.model.Route
 import io.mockk.every
 import io.mockk.mockk
@@ -18,9 +18,9 @@ class MapboxDirectionsSessionTest {
     private lateinit var session: MapboxDirectionsSession
 
     private val router: Router = mockk(relaxUnitFun = true)
-    private val origin: PointNavigation = mockk(relaxUnitFun = true)
-    private val destination: PointNavigation = mockk(relaxUnitFun = true)
-    private val waypoints: List<PointNavigation> = mockk(relaxUnitFun = true)
+    private val origin: Point = mockk(relaxUnitFun = true)
+    private val destination: Point = mockk(relaxUnitFun = true)
+    private val waypoints: List<Point> = mockk(relaxUnitFun = true)
     private val observer: DirectionsSession.RouteObserver = mockk(relaxUnitFun = true)
     private lateinit var routeListener: Router.RouteListener
     private val route: Route = mockk(relaxUnitFun = true)
