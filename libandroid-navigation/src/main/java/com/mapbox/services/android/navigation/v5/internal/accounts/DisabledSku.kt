2567a9651f12fb9dbb94c24899cd79abdaa27df8
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-11-04 10:59:48 -0800

    fix lint errors

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/accounts/DisabledSku.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/internal/accounts/DisabledSku.kt
@@ -2,7 +2,7 @@ package com.mapbox.services.android.navigation.v5.internal.accounts
 
 import com.mapbox.android.accounts.navigation.sku.v1.SkuGenerator
 
-//TODO: Remove this class when SKU is ready to be released publicly
+// TODO: Remove this class when SKU is ready to be released publicly
 internal class DisabledSku : SkuGenerator {
 
     override fun generateToken(): String {
