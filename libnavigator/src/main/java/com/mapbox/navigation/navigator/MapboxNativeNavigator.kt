8e461db1591c5a957d171476a0ed1ff3c8cca580
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-05 18:25:50 +0300

    Navigator make public

--- libnavigator/src/main/java/com/mapbox/navigation/navigator/MapboxNativeNavigator.kt
+++ libnavigator/src/main/java/com/mapbox/navigation/navigator/MapboxNativeNavigator.kt
@@ -1,10 +1,13 @@
 package com.mapbox.navigation.navigator
 
 import android.location.Location
+import com.mapbox.navigator.Navigator
 import com.mapbox.navigation.base.route.model.Route
 import java.util.Date
 
 interface MapboxNativeNavigator {
+    val navigator: Navigator
+
     fun updateLocation(rawLocation: Location)
     fun getStatus(date: Date): TripStatus
     fun setRoute(route: Route)
