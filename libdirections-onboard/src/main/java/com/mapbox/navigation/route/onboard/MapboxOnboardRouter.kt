b1e01add2ce335a55a5ddd47790fc0483a5b00a1
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-06 17:41:50 +0300

    MapboxOnboardRouter: fetch route

--- libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
+++ libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
@@ -1,9 +1,16 @@
 package com.mapbox.navigation.route.onboard
 
+import android.app.Application
+import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.route.Router
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
 import com.mapbox.navigation.navigator.MapboxNativeNavigatorImpl
+import com.mapbox.navigation.route.common.NavigationRoute
+import com.mapbox.navigation.route.common.extension.mapToRoute
+import com.mapbox.navigation.route.onboard.model.OfflineError
+import com.mapbox.navigation.route.onboard.task.OfflineRouteRetrievalTask
+import com.mapbox.navigation.utils.exceptions.NavigationException
 import java.io.File
 
 class MapboxOnboardRouter : Router {
@@ -16,6 +23,8 @@ class MapboxOnboardRouter : Router {
     private val tilePath: String
     private val offlineTileVersions: OfflineTileVersions
     private val navigatorNative: MapboxNativeNavigator
+    private val application: Application
+    private val accessToken: String
 
     /**
      * Creates an offline router which uses the specified offline path for storing and retrieving
@@ -23,7 +32,7 @@ class MapboxOnboardRouter : Router {
      *
      * @param offlinePath directory path where the offline data is located
      */
-    constructor(offlinePath: String) {
+    constructor(offlinePath: String, application: Application, accessToken: String) {
         val tileDir = File(offlinePath, TILE_PATH_NAME)
         if (!tileDir.exists()) {
             tileDir.mkdirs()
@@ -32,17 +41,23 @@ class MapboxOnboardRouter : Router {
         this.tilePath = tileDir.absolutePath
         offlineTileVersions = OfflineTileVersions()
         this.navigatorNative = MapboxNativeNavigatorImpl
+        this.application = application
+        this.accessToken = accessToken
     }
 
     // Package private for testing purposes
     internal constructor(
         tilePath: String,
         offlineTileVersions: OfflineTileVersions,
-        navigator: MapboxNativeNavigator
+        navigator: MapboxNativeNavigator,
+        application: Application,
+        accessToken: String
     ) {
         this.tilePath = tilePath
         this.offlineTileVersions = offlineTileVersions
         this.navigatorNative = navigator
+        this.application = application
+        this.accessToken = accessToken
     }
 
     override fun getRoute(
@@ -51,11 +66,29 @@ class MapboxOnboardRouter : Router {
         destination: Point,
         listener: Router.RouteListener
     ) {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+        val offlineRouter = OfflineRoute.builder(
+            NavigationRoute.builder(application)
+                .accessToken(accessToken)
+                .origin(origin)
+                .apply { waypoints?.forEach { addWaypoint(it) } }
+                .destination(destination)
+        )
+            .build()
+
+        OfflineRouteRetrievalTask(navigatorNative, object : OnOfflineRouteFoundCallback {
+            override fun onRouteFound(route: DirectionsRoute) {
+                listener.onRouteReady(route.mapToRoute())
+            }
+
+            override fun onError(error: OfflineError) {
+                listener.onFailure(NavigationException(error.message))
+            }
+        })
+            .execute(offlineRouter)
     }
 
     override fun cancel() {
-        TODO("not implemented")
+        // TODO not implemented by Native
     }
 
     class Config {
