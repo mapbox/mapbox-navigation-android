37930d9ca84c13639373406a7f67bae3b22b30b9
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-15 15:44:17 +0300

    Removed `JsonMapper` interface; return to `Gson`.

--- libnavigation-metrics/src/main/java/com/mapbox/navigation/metrics/internal/NavigationAppUserTurnstileEvent.kt
+++ libnavigation-metrics/src/main/java/com/mapbox/navigation/metrics/internal/NavigationAppUserTurnstileEvent.kt
@@ -1,9 +1,9 @@
 package com.mapbox.navigation.metrics.internal
 
+import com.google.gson.Gson
 import com.mapbox.android.telemetry.AppUserTurnstile
 import com.mapbox.navigation.base.internal.metrics.NavigationMetrics
 import com.mapbox.navigation.base.metrics.MetricEvent
-import com.mapbox.navigation.base.util.JsonMapper
 
 internal class NavigationAppUserTurnstileEvent(
     val event: AppUserTurnstile
@@ -12,5 +12,5 @@ internal class NavigationAppUserTurnstileEvent(
     override val metric: String
         get() = NavigationMetrics.APP_USER_TURNSTILE
 
-    override fun toJson(jsonMapper: JsonMapper): String = jsonMapper.toJson(this)
+    override fun toJson(gson: Gson): String = gson.toJson(this)
 }
