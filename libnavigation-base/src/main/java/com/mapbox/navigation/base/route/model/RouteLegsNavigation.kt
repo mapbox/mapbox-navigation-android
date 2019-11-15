6f9fcf320155253873e2631db2f3c75acbaa9ac5
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-15 12:58:36 +0300

    removed `libdirections-common` module; added `Gson` impl for `JsonMapper`

---
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/route/model/RouteLegsNavigation.kt
@@ -0,0 +1,5 @@
+package com.mapbox.navigation.base.route.model
+
+data class RouteLegsNavigation(
+    val legs: List<*>
+)
--- libnavigation-base/src/main/java/com/mapbox/navigation/base/util/JsonMapper.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/util/JsonMapper.kt
@@ -1,5 +1,13 @@
 package com.mapbox.navigation.base.util
 
+import com.google.gson.Gson
+
 interface JsonMapper {
     fun <T> toJson(obj: T): String
+
+    object GsonImpl : JsonMapper {
+        private val gson by lazy { Gson() }
+
+        override fun <T> toJson(obj: T): String = gson.toJson(obj)
+    }
 }
