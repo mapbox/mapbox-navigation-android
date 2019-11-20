0645dde418c76b15c8377a3ebdfdb0c9233d3c7a
Oleg Zilberman <oleg.zilberman@mapbox.com>
2019-11-20 07:22:52 -0800

    channels example

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayRouteLocationConverter.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/location/replay/ReplayRouteLocationConverter.java
@@ -20,14 +20,13 @@ class ReplayRouteLocationConverter {
     private static final double ONE_KM_IN_METERS = 1000d;
     private static final int ONE_HOUR_IN_SECONDS = 3600;
     private static final double cosOf45 = 0.017452406437284;
-    private static final int MAX_SUBDIVISIONS = 3;
+    private static final double factor = 1/10.0;
 
     private static final String REPLAY_ROUTE = "com.mapbox.services.android.navigation.v5.location.replay"
             + ".ReplayRouteLocationEngine";
     private DirectionsRoute route;
     private int speed;
     private int delay;
-    private double distance;
     private int currentLeg;
     private int currentStep;
     private long time;
@@ -35,9 +34,8 @@ class ReplayRouteLocationConverter {
     ReplayRouteLocationConverter(DirectionsRoute route, int speed, int delay) {
         initialize();
         update(route);
-        this.speed = speed;
+        this.speed = speed * 10;
         this.delay = delay;
-        this.distance = calculateDistancePerSec();
     }
 
     void updateSpeed(int customSpeedInKmPerHour) {
@@ -70,17 +68,31 @@ class ReplayRouteLocationConverter {
      * @return list of sliced {@link Point}s.
      */
     List<Point> sliceRoute(LineString lineString) {
+        double distance = calculateDistancePerSec(speed);
         double distanceMeters = TurfMeasurement.length(lineString, TurfConstants.UNIT_METERS);
         if (distanceMeters <= 0) {
             return Collections.emptyList();
         }
 
         List<Point> points = new ArrayList<>();
-        for (double i = 0; i < distanceMeters; i += distance) {
-            Point point = TurfMeasurement.along(lineString, i, TurfConstants.UNIT_METERS);
-            points.add(point);
+        Point point1 = TurfMeasurement.along(lineString, 0, TurfConstants.UNIT_METERS);
+        Point point2 = TurfMeasurement.along(lineString, distance, TurfConstants.UNIT_METERS);
+
+        if (computeCosine(point1, point2) <= cosOf45 ){
+            double d = calculateDistancePerSec(speed*0.1);
+            for (double i = 0; i < distanceMeters; i += d) {
+                Point point = TurfMeasurement.along(lineString, i, TurfConstants.UNIT_METERS);
+                points.add(point);
+            }
+        } else {
+            double d = calculateDistancePerSec(speed);
+            for (double i = 0; i < distanceMeters; i += d) {
+                Point point = TurfMeasurement.along(lineString, i, TurfConstants.UNIT_METERS);
+                points.add(point);
+            }
         }
-        return subdivideRouteSegment(points, MAX_SUBDIVISIONS);
+
+        return points;
     }
 
     List<Location> calculateMockLocations(List<Point> points) {
@@ -101,24 +113,11 @@ class ReplayRouteLocationConverter {
         return mockedLocations;
     }
 
-    private List<Point> addSubdivisions(Point from, Point to, int count) {
-        if (count == 0) {
-            return null;
-        }
-        ArrayList<Point> pointList = new ArrayList<>();
-        double deltaLng = (to.longitude() - from.longitude()) / count; // Compute midpoint if count is 2 or delta if count > 2
-        double deltaLat = (to.latitude() - from.latitude()) / count;
-        double fromLng = from.longitude();
-        double fromLat = from.latitude();
-        for (int i = 0; i < count; i++) {   // Generate higher resolution data
-            fromLng += deltaLng;
-            fromLat += deltaLat;
-            pointList.add(Point.fromLngLat(fromLng, fromLat));
-        }
-        return pointList;
+    private double pointOnLine(double slope, double x, double yIntercept) {
+        return slope * x + yIntercept;
     }
 
-    private double computeCosine(Point thisPoint, Point nextPoint){
+    private double computeCosine(Point thisPoint, Point nextPoint) {
         double u = thisPoint.latitude() * thisPoint.latitude() + thisPoint.longitude() * thisPoint.longitude();
         double v = nextPoint.latitude() * nextPoint.latitude() + nextPoint.longitude() * nextPoint.longitude();
         double numerator = thisPoint.latitude() * nextPoint.latitude() + thisPoint.longitude() * thisPoint.latitude();
@@ -126,66 +125,6 @@ class ReplayRouteLocationConverter {
         double cosine = numerator / denominator;
         return cosine;
     }
-
-    private ArrayList<Point>  computeSubdivisions(List<Point> points, int count, int index) {
-        ArrayList<Point> retVal = new ArrayList<>();
-        Point thisPoint = Point.fromLngLat(points.get(index).longitude(), points.get(index).latitude());
-        Point nextPoint = Point.fromLngLat(points.get(index + 1).longitude(), points.get(index + 1).latitude());
-        //Compute the cosine of the angle between two line segments
-        double cosine = computeCosine(thisPoint, nextPoint);
-        if (cosine <= cosOf45) {
-            List<Point> subdivisions = addSubdivisions(thisPoint, nextPoint, count);
-            if (subdivisions != null) {
-                retVal.add(thisPoint);  // Automatically add the first point
-                retVal.addAll(subdivisions);
-                retVal.add(nextPoint);
-            } else {
-                retVal.add(thisPoint);
-                retVal.add(nextPoint);
-            }
-        }
-        return retVal;
-    }
-
-    private List<Point> subdivideRouteSegment(List<Point> points, int count) {
-        ArrayList<Point> retVal = null;
-        if (points.size() == 1){
-            retVal = new ArrayList<>(points);
-        } else if (points.size() == 2) {
-            retVal = new ArrayList<>();
-            retVal.addAll(computeSubdivisions(points, count, 0));
-        } else {
-            retVal = new ArrayList<>();
-            for (int i = 0; i < points.size() - 2; i += 2) {
-                retVal.addAll(computeSubdivisions(points, count, i));
-            }
-        }
-        // If we got something back, return that. Otherwise, return the original list of points
-        String debugMsg1 = "original points";
-        String debugMsg2 = "computed points";
-        if (retVal.isEmpty()) {
-            return points;
-        } else {
-            StringBuilder stringBuilderDebug1 = new StringBuilder();
-            StringBuilder stringBuilderDebug2 = new StringBuilder();
-            stringBuilderDebug1.append(debugMsg1);
-            stringBuilderDebug2.append(debugMsg2);
-            stringBuilderDebug1.append(String.format("%s count: %d", debugMsg1, points.size()));
-            stringBuilderDebug2.append(String.format("%s count: %d", debugMsg2, retVal.size()));
-
-            for (Point p : points) {
-                stringBuilderDebug1.append(String.format("[%.16f %.16f]", p.longitude(), p.latitude()));
-            }
-            for (Point pp : retVal) {
-                stringBuilderDebug2.append(String.format("[%.16f %.16f]", pp.longitude(), pp.latitude()));
-            }
-            Log.d("DEBUG_TAG", stringBuilderDebug1.toString());
-            Log.d("DEBUG_TAG", stringBuilderDebug2.toString());
-            Log.d("DEBUG_TAG", "***************************************************");
-        }
-        return retVal;
-    }
-
 
     private void update(DirectionsRoute route) {
         this.route = route;
@@ -196,9 +135,8 @@ class ReplayRouteLocationConverter {
      *
      * @return a double value representing the distance given a speed and time.
      */
-    private double calculateDistancePerSec() {
-        double distance = (speed * ONE_KM_IN_METERS * delay) / ONE_HOUR_IN_SECONDS;
-        return distance;
+    private double calculateDistancePerSec(double speed) {
+        return  (speed * ONE_KM_IN_METERS * delay) / ONE_HOUR_IN_SECONDS;
     }
 
     private void initialize() {
