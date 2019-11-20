8882b238443e08ee377b9f51bc398d962bd9fabd
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-20 15:58:12 +0300

    Fix Build issue

--- libnavigation-core/src/main/java/com/mapbox/navigation/NavigationController.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/NavigationController.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
-import com.google.gson.reflect.TypeToken
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineRequest
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType
@@ -14,19 +13,19 @@ import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.Hybrid
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.Logger as LoggerModule
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.OffboardRouter
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.OnboardRouter
-import com.mapbox.geojson.Point
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.TripNotification as TripNotificationModule
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.TripService as TripServiceModule
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType.TripSession as TripSessionModule
+import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.logger.Logger
 import com.mapbox.navigation.base.route.DirectionsSession
 import com.mapbox.navigation.base.route.Router
-import com.mapbox.navigation.base.route.model.Route
 import com.mapbox.navigation.base.trip.TripNotification
 import com.mapbox.navigation.base.trip.TripService
 import com.mapbox.navigation.base.trip.TripSession
 import com.mapbox.navigation.module.NavigationModuleProvider
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
+import com.mapbox.navigation.util.rawType
 
 class NavigationController(
     private val application: Application,
@@ -62,8 +61,14 @@ class NavigationController(
     private fun paramsProvider(type: MapboxNavigationModuleType): Array<Pair<Class<*>?, Any?>> {
         return when (type) {
             HybridRouter -> arrayOf(
-                Router::class.java to NavigationModuleProvider.createModule(OnboardRouter, ::paramsProvider),
-                Router::class.java to NavigationModuleProvider.createModule(OffboardRouter, ::paramsProvider)
+                Router::class.java to NavigationModuleProvider.createModule(
+                    OnboardRouter,
+                    ::paramsProvider
+                ),
+                Router::class.java to NavigationModuleProvider.createModule(
+                    OffboardRouter,
+                    ::paramsProvider
+                )
             )
             OffboardRouter -> arrayOf(
                 Context::class.java to application,
@@ -73,22 +78,27 @@ class NavigationController(
                 MapboxNativeNavigator::class.java to navigator
             )
             DirectionsSessionModule -> arrayOf(
-                Router::class.java to NavigationModuleProvider.createModule(HybridRouter, ::paramsProvider),
+                Router::class.java to NavigationModuleProvider.createModule(
+                    HybridRouter,
+                    ::paramsProvider
+                ),
                 Point::class.java to origin,
-                // List::class.java to waypoints,
+                rawType<List<Point>>() to waypoints,
                 Point::class.java to destination,
-                DirectionsSession.RouteObserver::class.java to object : DirectionsSession.RouteObserver{
-                    override fun onRouteChanged(route: Route?) = Unit
-
-                    override fun onFailure(throwable: Throwable) = Unit
-                }
+                DirectionsSession.RouteObserver::class.java to null
             )
             TripNotificationModule -> arrayOf()
             TripServiceModule -> arrayOf(
-                TripNotification::class.java to NavigationModuleProvider.createModule(TripNotificationModule, ::paramsProvider)
+                TripNotification::class.java to NavigationModuleProvider.createModule(
+                    TripNotificationModule,
+                    ::paramsProvider
+                )
             )
             TripSessionModule -> arrayOf(
-                TripService::class.java to NavigationModuleProvider.createModule(TripServiceModule, ::paramsProvider),
+                TripService::class.java to NavigationModuleProvider.createModule(
+                    TripServiceModule,
+                    ::paramsProvider
+                ),
                 LocationEngine::class.java to locationEngine,
                 LocationEngineRequest::class.java to locationEngineRequest,
                 MapboxNativeNavigator::class.java to navigator,
