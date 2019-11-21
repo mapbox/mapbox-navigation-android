e0de81bccec779cc4a48eb7fd4031a619d9d9447
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-21 16:29:15 +0300

    Route Mapper: `distance` and `duration` are non-null

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/route/model/Route.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/route/model/Route.kt
@@ -2,8 +2,8 @@ package com.mapbox.navigation.base.route.model
 
 data class Route(
     val routeIndex: String?,
-    val distance: Double?,
-    val duration: Long?,
+    val distance: Double,
+    val duration: Long,
     val geometry: String?,
     val weight: Double?,
     val weightName: String?,
