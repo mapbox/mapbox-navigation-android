baf7f1c1b535c8ad6ab280f799ebe75eac8b9cc6
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-11-22 19:03:27 +0300

    Fix tests. Remove unneeded RouteRetrieveCallback, roll back OffboardRouter changes.

--- libdirections-offboard/src/test/java/com/mapbox/navigation/route/offboard/MapboxOffboardRouterTest.kt
+++ libdirections-offboard/src/test/java/com/mapbox/navigation/route/offboard/MapboxOffboardRouterTest.kt
@@ -1,21 +1,44 @@
 package com.mapbox.navigation.route.offboard
 
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.route.Router
-import com.mapbox.navigation.route.offboard.callback.RouteRetrieveCallback
+import com.mapbox.navigation.route.offboard.base.BaseTest
+import com.mapbox.navigation.route.offboard.extension.mapToRoute
 import com.mapbox.navigation.route.offboard.router.NavigationRoute
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.verify
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Callback
+import retrofit2.Response
 
-class MapboxOffboardRouterTest {
+class MapboxOffboardRouterTest : BaseTest() {
 
+    companion object {
+        private const val ACCESS_TOKEN = "pk.token"
+    }
+
+    private val navigationRoute = mockk<NavigationRoute>(relaxed = true)
+    private val navigationRouteBuilder = mockk<NavigationRoute.Builder>(relaxed = true)
     private lateinit var offboardRouter: MapboxOffboardRouter
+    private lateinit var callback: Callback<DirectionsResponse>
+    private val origin = Point.fromLngLat(1.0, 2.0)
+    private val waypoints = listOf<Point>()
+    private val destination = Point.fromLngLat(1.0, 5.0)
 
     @Before
     fun setUp() {
-        offboardRouter = MapboxOffboardRouter(mockk(), "pk_token")
+        val listener = slot<Callback<DirectionsResponse>>()
+        every { navigationRouteBuilder.build() } returns navigationRoute
+        every { navigationRoute.getRoute(capture(listener)) } answers {
+            callback = listener.captured
+        }
+        offboardRouter = MapboxOffboardRouter(mockk(), ACCESS_TOKEN) { navigationRouteBuilder }
     }
 
     @Test
@@ -25,24 +48,103 @@ class MapboxOffboardRouterTest {
 
     @Test
     fun getRoute_NavigationRouteGetRouteCalled() {
-        val navigationRoute = mockk<NavigationRoute>(relaxed = true)
-        offboardRouter.setNavigationRoute(navigationRoute)
-        val routerCallback = mockk<Router.Callback>(relaxed = true)
-        val routeRetrieveCallback = RouteRetrieveCallback(routerCallback)
-        offboardRouter.setRouteRetrieveCallback(routeRetrieveCallback)
+        getRoute(mockk())
 
-        offboardRouter.getRoute(mockk(), mockk(), mockk(), routerCallback)
-
-        verify { navigationRoute.getRoute(routeRetrieveCallback) }
+        verify { navigationRoute.getRoute(callback) }
     }
 
     @Test
     fun cancel_NavigationRouteCancelCallCalled() {
-        val navigationRoute = mockk<NavigationRoute>(relaxed = true)
-        offboardRouter.setNavigationRoute(navigationRoute)
+        getRoute(mockk())
 
         offboardRouter.cancel()
 
         verify { navigationRoute.cancelCall() }
+    }
+
+    @Test
+    fun cancel_NavigationRouteCancelCallNotCalled() {
+        offboardRouter.cancel()
+
+        verify(exactly = 0) { navigationRoute.cancelCall() }
+    }
+
+    @Test
+    fun onSuccessfulResponseAndHasRoutes_onRouteReadyCalled() {
+        val routerCallback = mockk<Router.Callback>(relaxed = true)
+        val route = buildMultipleLegRoute()
+        val response = buildResponse(listOf(route), true)
+        getRoute(routerCallback)
+
+        callback.onResponse(mockk(), response)
+
+        verify { routerCallback.onRouteReady(listOf(route.mapToRoute())) }
+    }
+
+    @Test
+    fun onUnsuccessfulResponseAndHasRoutes_errorIsProvided() {
+        val routerCallback = mockk<Router.Callback>(relaxed = true)
+        val route = buildMultipleLegRoute()
+        val response = buildResponse(listOf(route), false)
+        getRoute(routerCallback)
+
+        callback.onResponse(mockk(), response)
+
+        verify { routerCallback.onFailure(any()) }
+    }
+
+    @Test
+    fun onSuccessfulResponseAndNoRoutes_errorIsProvided() {
+        val routerCallback = mockk<Router.Callback>(relaxed = true)
+        val response = buildResponse(listOf(), true)
+        getRoute(routerCallback)
+
+        callback.onResponse(mockk(), response)
+
+        verify { routerCallback.onFailure(any()) }
+    }
+
+    @Test
+    fun onUnsuccessfulResponseAndNoRoutes_errorIsProvided() {
+        val routerCallback = mockk<Router.Callback>(relaxed = true)
+        val response = buildResponse(listOf(), false)
+        getRoute(routerCallback)
+
+        callback.onResponse(mockk(), response)
+
+        verify { routerCallback.onFailure(any()) }
+    }
+
+    @Test
+    fun onFailure_errorIsProvided() {
+        val routerCallback = mockk<Router.Callback>(relaxed = true)
+        val throwable = mockk<Throwable>()
+        getRoute(routerCallback)
+
+        callback.onFailure(mockk(), throwable)
+
+        verify { routerCallback.onFailure(throwable) }
+    }
+
+    private fun getRoute(routerCallback: Router.Callback) {
+        offboardRouter.getRoute(
+            origin,
+            waypoints,
+            destination,
+            routerCallback
+        )
+    }
+
+    private fun buildResponse(
+        routeList: List<DirectionsRoute>,
+        isSuccessful: Boolean
+    ): Response<DirectionsResponse> {
+        val response = mockk<Response<DirectionsResponse>>()
+        val directionsResponse = mockk<DirectionsResponse>()
+        every { directionsResponse.routes() } returns routeList
+        every { response.body() } returns directionsResponse
+        every { response.isSuccessful } returns isSuccessful
+
+        return response
     }
 }
