da3f715c225c5841e6a042eae8fdd5784a7397a1
Yury Kanetski <yury.kanetski@mapbox.com>
2019-12-11 15:36:27 +0300

    - JsonMapper: revert to Gson; - fix lint issues; - minor rename

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteWaypoint.kt
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/NavigationRouteWaypoint.kt
@@ -3,7 +3,7 @@ package com.mapbox.services.android.navigation.v5.navigation
 import com.mapbox.geojson.Point
 
 data class NavigationRouteWaypoint(
-    val waypoint: Point,
+    val point: Point,
     val bearingAngle: Double?,
     val tolerance: Double?
 )
