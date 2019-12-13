24ff8e73ecdf177b93d89eef41c866f5e4c03195
Łukasz Paczos <lukasz.paczos@mapbox.com>
2019-12-13 17:24:56 +0100

    do not leak NavigationRoute outside of OffboardRouter module

--- libnavigation-core/src/test/java/com/mapbox/navigation/NavigationControllerTest.kt
+++ libnavigation-core/src/test/java/com/mapbox/navigation/NavigationControllerTest.kt
@@ -1,12 +1,15 @@
 package com.mapbox.navigation
 
+import android.content.Context
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineRequest
 import com.mapbox.navigation.base.route.DirectionsSession
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
-import com.mapbox.navigation.route.offboard.router.NavigationRoute
+import com.mapbox.navigation.utils.extensions.inferDeviceLocale
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
+import java.util.Locale
 import org.junit.Assert
 import org.junit.Before
 import org.junit.BeforeClass
@@ -15,7 +18,7 @@ import org.junit.Test
 class NavigationControllerTest {
 
     private lateinit var navigationController: NavigationController
-    private val routeBuilderProvider: NavigationRoute.Builder = mockk()
+    private val context: Context = mockk()
     private val navigator: MapboxNativeNavigator = mockk()
     private val locationEngine: LocationEngine = mockk()
     private val locationEngineRequest: LocationEngineRequest = mockk()
@@ -31,9 +34,10 @@ class NavigationControllerTest {
 
     @Before
     fun setUp() {
+        every { context.inferDeviceLocale() } returns Locale.US
         navigationController =
             NavigationController(
-                routeBuilderProvider,
+                context,
                 navigator,
                 locationEngine,
                 locationEngineRequest,
