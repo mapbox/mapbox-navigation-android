def2551de61d9faf8674dfdda67f0abd4f932ab4
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-12-18 14:05:15 -0800

    fix lint errors

--- libtrip-notification/src/test/java/com/mapbox/navigation/trip/notification/MapboxTripNotificationTest.kt
+++ libtrip-notification/src/test/java/com/mapbox/navigation/trip/notification/MapboxTripNotificationTest.kt
@@ -10,8 +10,8 @@ import android.text.format.DateFormat
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
-import org.junit.Before
 import java.util.Locale
+import org.junit.Before
 
 class MapboxTripNotificationTest {
 
@@ -20,7 +20,7 @@ class MapboxTripNotificationTest {
     fun setUp() {
         mockkStatic(DateFormat::class)
         mockkStatic(PendingIntent::class)
-        val context =  createContext()
+        val context = createContext()
         notification = MapboxTripNotification(context)
     }
 
@@ -29,15 +29,15 @@ class MapboxTripNotificationTest {
         val mockedConfiguration = Configuration()
         mockedConfiguration.locale = Locale("en")
         val mockedResources = mockk<Resources>(relaxed = true)
-        every { mockedResources.configuration } returns(mockedConfiguration)
-        every { mockedContext.resources } returns(mockedResources)
+        every { mockedResources.configuration } returns (mockedConfiguration)
+        every { mockedContext.resources } returns (mockedResources)
         val mockedPackageManager = mockk<PackageManager>(relaxed = true)
         every { mockedContext.packageManager } returns (mockedPackageManager)
         every { mockedContext.packageName } returns ("com.mapbox.navigation.trip.notification")
         every { mockedContext.getString(any()) } returns ("%s 454545 ETA")
         val notificationManager = mockk<NotificationManager>(relaxed = true)
         every { mockedContext.getSystemService(Context.NOTIFICATION_SERVICE) } returns (notificationManager)
-        every { DateFormat.is24HourFormat(mockedContext) } returns(false)
+        every { DateFormat.is24HourFormat(mockedContext) } returns (false)
         return mockedContext
     }
 }
