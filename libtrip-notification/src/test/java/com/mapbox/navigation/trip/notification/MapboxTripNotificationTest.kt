592f5671ca7d0f0526f7fca29359c8fc203b7575
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-12-18 13:57:58 -0800

    remove the sanity test

--- libtrip-notification/src/test/java/com/mapbox/navigation/trip/notification/MapboxTripNotificationTest.kt
+++ libtrip-notification/src/test/java/com/mapbox/navigation/trip/notification/MapboxTripNotificationTest.kt
@@ -1,22 +1,43 @@
 package com.mapbox.navigation.trip.notification
 
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.text.format.DateFormat
+import io.mockk.every
 import io.mockk.mockk
-import org.junit.Assert
+import io.mockk.mockkStatic
 import org.junit.Before
-import org.junit.Test
+import java.util.Locale
 
 class MapboxTripNotificationTest {
 
     private lateinit var notification: MapboxTripNotification
     @Before
     fun setUp() {
-        val context = mockk<Context>(relaxed = true)
+        mockkStatic(DateFormat::class)
+        mockkStatic(PendingIntent::class)
+        val context =  createContext()
         notification = MapboxTripNotification(context)
     }
 
-    @Test
-    fun generationSanityTest() {
-        Assert.assertNotNull(notification)
+    private fun createContext(): Context {
+        val mockedContext = mockk<Context>()
+        val mockedConfiguration = Configuration()
+        mockedConfiguration.locale = Locale("en")
+        val mockedResources = mockk<Resources>(relaxed = true)
+        every { mockedResources.configuration } returns(mockedConfiguration)
+        every { mockedContext.resources } returns(mockedResources)
+        val mockedPackageManager = mockk<PackageManager>(relaxed = true)
+        every { mockedContext.packageManager } returns (mockedPackageManager)
+        every { mockedContext.packageName } returns ("com.mapbox.navigation.trip.notification")
+        every { mockedContext.getString(any()) } returns ("%s 454545 ETA")
+        val notificationManager = mockk<NotificationManager>(relaxed = true)
+        every { mockedContext.getSystemService(Context.NOTIFICATION_SERVICE) } returns (notificationManager)
+        every { DateFormat.is24HourFormat(mockedContext) } returns(false)
+        return mockedContext
     }
 }
