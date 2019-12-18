9c37419cd704a15e2f2a347fba1f53a10653c5cf
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-12-18 14:41:47 -0800

    fix TripService test

--- libtrip-service/src/test/java/com/mapbox/navigation/trip/service/MapboxTripServiceTest.kt
+++ libtrip-service/src/test/java/com/mapbox/navigation/trip/service/MapboxTripServiceTest.kt
@@ -2,18 +2,23 @@ package com.mapbox.navigation.trip.service
 
 import com.mapbox.navigation.base.trip.TripNotification
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
+@InternalCoroutinesApi
 class MapboxTripServiceTest {
 
     private lateinit var service: MapboxTripService
     private val notification: TripNotification = mockk()
+    private val callback: () -> Unit = { }
 
     @Before
     fun setUp() {
-        service = MapboxTripService(notification)
+        service = MapboxTripService(notification, callback)
     }
 
     @Test
