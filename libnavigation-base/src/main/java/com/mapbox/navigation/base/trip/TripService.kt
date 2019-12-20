ad87796f80b03e4bd79c376454b90aed191410d0
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-12-19 18:55:52 -0800

    make changes to manifest and build gradle

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/trip/TripService.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/trip/TripService.kt
@@ -2,7 +2,6 @@ package com.mapbox.navigation.base.trip
 
 import android.app.Notification
 import android.location.Location
-const val NAVIGATION_NOTIFICATION_ID = 5678
 
 data class NavigationTripDescriptor(val routeProgress: RouteProgress, val rawLocation: Location, val enhancedLocation: Location)
 data class MapboxNotificationData(val notificationID: Int, val notification: Notification)
