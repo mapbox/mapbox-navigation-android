825de3b02cce8e278195ac50011125e601f9ed72
abhishek1508 <abhishek.kejriwal@mapbox.com>
2020-01-18 12:35:21 -0800

    change the nomenclature

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/fasterroute/FasterRouteListener.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/fasterroute/FasterRouteListener.kt
@@ -2,7 +2,7 @@ package com.mapbox.navigation.base.fasterroute
 
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 
-interface FasterRouteDetector {
+interface FasterRouteListener {
 
     fun onFasterRouteAvailable(fasterRoute: DirectionsRoute)
 
