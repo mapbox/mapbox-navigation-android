de7a13287a9dcd5086c5c5fb637aa47afd6285de
Oleg Zilberman <oleg.zilberman@mapbox.com>
2020-01-22 15:49:07 -0800

    replacing MainCoroutineRule with code that waits for a coroutine to finish

--- libdirections-onboard/src/test/java/com/mapbox/navigation/route/onboard/MainCoroutineRule.kt
+++ libdirections-onboard/src/test/java/com/mapbox/navigation/route/onboard/MainCoroutineRule.kt
@@ -3,31 +3,37 @@ package com.mapbox.navigation.route.onboard
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
-import org.junit.rules.TestWatcher
+import org.junit.rules.TestRule
 import org.junit.runner.Description
+import org.junit.runners.model.Statement
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRule(
-    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher() {
+class MainCoroutineRule : TestRule {
+    val coroutineDispatcher = TestCoroutineDispatcher()
+    val coroutineScope = TestCoroutineScope(coroutineDispatcher)
 
-    override fun starting(description: Description?) {
-        super.starting(description)
-        Dispatchers.setMain(testDispatcher)
+    override fun apply(base: Statement, description: Description?) = object : Statement() {
+        @Throws(Throwable::class)
+        override fun evaluate() {
+            Dispatchers.setMain(coroutineDispatcher)
+
+            base.evaluate()
+
+            Dispatchers.resetMain() // Restore original main dispatcher
+            coroutineScope.cleanupTestCoroutines()
+        }
     }
 
-    override fun finished(description: Description?) {
-        super.finished(description)
-        Dispatchers.resetMain()
-        testDispatcher.cleanupTestCoroutines()
-    }
+    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
+            coroutineScope.runBlockingTest { block() }
 }
 
 @ExperimentalCoroutinesApi
 fun MainCoroutineRule.runBlockingTest(block: suspend () -> Unit) =
-    this.testDispatcher.runBlockingTest {
-        block()
-    }
+        this.coroutineDispatcher.runBlockingTest {
+            block()
+        }
