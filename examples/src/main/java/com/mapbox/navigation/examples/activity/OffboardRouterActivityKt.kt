7777efa00a1d6defb91220a48a5fc6a63b12870b
Łukasz Paczos <lukasz.paczos@mapbox.com>
2020-01-24 12:37:11 +0100

    remove accounts internal package

--- examples/src/main/java/com/mapbox/navigation/examples/activity/OffboardRouterActivityKt.kt
+++ examples/src/main/java/com/mapbox/navigation/examples/activity/OffboardRouterActivityKt.kt
@@ -19,7 +19,7 @@ import com.mapbox.navigation.base.extensions.coordinates
 import com.mapbox.navigation.base.logger.model.Message
 import com.mapbox.navigation.base.logger.model.Tag
 import com.mapbox.navigation.base.route.Router
-import com.mapbox.navigation.core.internal.accounts.MapboxNavigationAccounts
+import com.mapbox.navigation.core.accounts.MapboxNavigationAccounts
 import com.mapbox.navigation.examples.R
 import com.mapbox.navigation.examples.utils.Utils
 import com.mapbox.navigation.logger.DEBUG
