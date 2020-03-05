52608367c68bd41514bb2e79711d7f20511b5d75
Seth Bourget <seth.bourget@mapbox.com>
2020-03-05 15:58:05 -0800

    formatting fixes

--- examples/src/main/java/com/mapbox/navigation/examples/UIActivity.kt
+++ examples/src/main/java/com/mapbox/navigation/examples/UIActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
-import com.mapbox.navigation.examples.ui.NavigationViewActivity
 import com.mapbox.navigation.examples.ui.ArrivalUiBuildingExtrusionLayerActivityKt
 import com.mapbox.navigation.examples.ui.FinalDestinationBuildingFootprintHighlightActivityKt
 import com.mapbox.navigation.examples.ui.NavigationViewActivity
