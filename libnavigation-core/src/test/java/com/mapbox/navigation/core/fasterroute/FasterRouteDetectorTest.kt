b90ed674d8022885532a18398a66039810c032fc
Kyle Madsen <>
2020-03-09 11:54:47 -0700

    Unmock object

--- libnavigation-core/src/test/java/com/mapbox/navigation/core/fasterroute/FasterRouteDetectorTest.kt
+++ libnavigation-core/src/test/java/com/mapbox/navigation/core/fasterroute/FasterRouteDetectorTest.kt
@@ -4,25 +4,31 @@ import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.navigation.base.trip.model.RouteProgress
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.unmockkObject
 import java.util.concurrent.TimeUnit
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class FasterRouteDetectorTest {
 
-    // TODO This test fails on PRs
-//    @Test
-//    fun shouldDetectWhenRouteIsFaster() {
-//        val newRoute: DirectionsRoute = mockk()
-//        every { newRoute.duration() } returns TimeUnit.MINUTES.toSeconds(5).toDouble()
-//        val routeProgress: RouteProgress = mockk()
-//        every { routeProgress.durationRemaining() } returns TimeUnit.MINUTES.toSeconds(10)
-//
-//        val isFasterRoute = FasterRouteDetector.isRouteFaster(newRoute, routeProgress)
-//
-//        assertTrue(newRoute.duration()!! < routeProgress.durationRemaining())
-//        assertTrue(isFasterRoute)
-//    }
+    @Before
+    fun setup() {
+        unmockkObject(FasterRouteDetector)
+    }
+
+    @Test
+    fun shouldDetectWhenRouteIsFaster() {
+        val newRoute: DirectionsRoute = mockk()
+        every { newRoute.duration() } returns TimeUnit.MINUTES.toSeconds(5).toDouble()
+        val routeProgress: RouteProgress = mockk()
+        every { routeProgress.durationRemaining() } returns TimeUnit.MINUTES.toSeconds(10)
+
+        val isFasterRoute = FasterRouteDetector.isRouteFaster(newRoute, routeProgress)
+
+        assertTrue(isFasterRoute)
+    }
 
     @Test
     fun shouldDefaultToFalseWhenDurationIsNull() {
