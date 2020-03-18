c230f23d27b978b7e3b70b3aef781999818e7366
Seth Bourget <seth.bourget@mapbox.com>
2020-03-18 12:53:39 -0700

    name change in response to PR comment and rebase with master

--- examples/src/main/java/com/mapbox/navigation/examples/CoreActivity.kt
+++ examples/src/main/java/com/mapbox/navigation/examples/CoreActivity.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import com.mapbox.navigation.examples.core.BasicNavigationActivity
-import com.mapbox.navigation.examples.core.NavigateWithInstructionViewActivity
 import com.mapbox.navigation.examples.core.GuidanceViewActivity
 import com.mapbox.navigation.examples.core.OffboardRouterActivityJava
 import com.mapbox.navigation.examples.core.OffboardRouterActivityKt
@@ -74,11 +73,6 @@ class CoreActivity : AppCompatActivity() {
                 getString(R.string.title_trip_session_kotlin),
                 getString(R.string.description_trip_session_kotlin),
                 TripSessionActivityKt::class.java
-            ),
-            SampleItem(
-                getString(R.string.title_navigate_instruction_view),
-                getString(R.string.description_navigate_instruction_view),
-                NavigateWithInstructionViewActivity::class.java
             ),
             SampleItem(
                 getString(R.string.title_guidance_view),
