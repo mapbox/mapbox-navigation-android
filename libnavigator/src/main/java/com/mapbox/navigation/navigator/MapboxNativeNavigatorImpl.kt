08e0b01ab32b4632fb7f19932a45df8ca274ef8b
Yury Kanetski <yury.kanetski@mapbox.com>
2020-04-13 14:30:14 +0300

    libnavigator: remaining docs

--- libnavigator/src/main/java/com/mapbox/navigation/navigator/MapboxNativeNavigatorImpl.kt
+++ libnavigator/src/main/java/com/mapbox/navigation/navigator/MapboxNativeNavigatorImpl.kt
@@ -57,12 +57,26 @@ object MapboxNativeNavigatorImpl : MapboxNativeNavigator {
 
     // Route following
 
+    /**
+     * Passes in the current raw location of the user.
+     *
+     * @param rawLocation The current raw [Location] of user.
+     *
+     * @return true if the raw location was usable, false if not.
+     */
     override suspend fun updateLocation(rawLocation: Location, date: Date): Boolean {
         mutex.withLock {
             return navigator.updateLocation(rawLocation.toFixLocation(date))
         }
     }
 
+    /**
+     * Passes in the current sensor data of the user.
+     *
+     * @param sensorEvent The current sensor data of user.
+     *
+     * @return true if the sensor data was usable, false if not.
+     */
     override fun updateSensorEvent(sensorEvent: SensorEvent): Boolean {
         val value = SensorMapper.toNavigatorSensorData(sensorEvent)
         return ifNonNull(value) { navigatorSensorData ->
@@ -70,6 +84,20 @@ object MapboxNativeNavigatorImpl : MapboxNativeNavigator {
         } ?: false
     }
 
+    /**
+     * Gets the status as an offset in time from the last fixed location. This
+     * allows the caller to get predicted statuses in the future along the route if
+     * the device is unable to get fixed locations. Poor reception would be one reason.
+     *
+     * This method uses previous fixes to snap the user's location to the route
+     * and verify that the user is still on the route. This method also determines
+     * if an instruction needs to be called out for the user.
+     *
+     * @param date the point in time to receive the status for.
+     *
+     * @return the last [TripStatus] as a result of fixed location updates. If the timestamp
+     * is earlier than a previous call, the last status will be returned. The function does not support re-winding time.
+     */
     override suspend fun getStatus(date: Date): TripStatus {
         mutex.withLock {
             val status = navigator.getStatus(date)
@@ -83,7 +111,18 @@ object MapboxNativeNavigatorImpl : MapboxNativeNavigator {
     }
 
     // Routing
-
+    /**
+     * Sets the route path for the navigator to process.
+     * Returns initialized route state if no errors occurred.
+     * Otherwise, it returns a invalid route state.
+     *
+     * @param route [DirectionsRoute] to follow.
+     * @param routeIndex Which route to follow
+     * @param legIndex Which leg to follow
+     *
+     * @return a [NavigationStatus] route state if no errors occurred.
+     * Otherwise, it returns a invalid route state.
+     */
     override suspend fun setRoute(
         route: DirectionsRoute?,
         routeIndex: Int,
@@ -99,65 +138,189 @@ object MapboxNativeNavigatorImpl : MapboxNativeNavigator {
         }
     }
 
+    /**
+     * Updates annotations so that subsequent calls to getStatus will
+     * reflect the most current annotations for the route.
+     *
+     * @param legAnnotationJson A string containing the json/pbf annotations
+     * @param routeIndex Which route to apply the annotation update to
+     * @param legIndex Which leg to apply the annotation update to
+     *
+     * @return True if the annotations could be updated false if not (wrong number of annotations)
+     */
     override fun updateAnnotations(
         legAnnotationJson: String,
         routeIndex: Int,
         legIndex: Int
     ): Boolean = navigator.updateAnnotations(legAnnotationJson, routeIndex, legIndex)
 
+    /**
+     * Gets the banner at a specific step index in the route. If there is no
+     * banner at the specified index method return *null*.
+     *
+     * @param index Which step you want to get [BannerInstruction] for
+     *
+     * @return [BannerInstruction] for step index you passed
+     */
     override fun getBannerInstruction(index: Int): BannerInstruction? =
         navigator.getBannerInstruction(index)
 
+    /**
+     * Gets a polygon around the currently loaded route. The method uses a bitmap approach
+     * in which you specify a grid size (pixel size) and a dilation (how many pixels) to
+     * expand the initial grid cells that are intersected by the route.
+     *
+     * @param gridSize the size of the individual grid cells
+     * @param bufferDilation the number of pixels to dilate the initial intersection by it can
+     * be thought of as controlling the halo thickness around the route
+     *
+     * @return a geojson as [String] representing the route buffer polygon
+     */
     override fun getRouteGeometryWithBuffer(gridSize: Float, bufferDilation: Short): String? =
         navigator.getRouteBufferGeoJson(gridSize, bufferDilation)
 
+    /**
+     * Follows a new route and leg of the already loaded directions.
+     * Returns an initialized route state if no errors occurred
+     * otherwise, it returns an invalid route state.
+     *
+     * @param routeIndex new route index
+     * @param legIndex new leg index
+     *
+     * @return an initialized route state as [NavigationStatus]
+     */
     override fun updateLegIndex(routeIndex: Int, legIndex: Int): NavigationStatus =
         navigator.changeRouteLeg(routeIndex, legIndex)
 
     // Free Drive
-
+    /**
+     * Uses routing engine and local tile data to generate electronic horizon json.
+     *
+     * Consumes a list of points, matches them to the routing graph
+     * (i.e. does traceAttributes) and prolongs this path
+     * in selected directions (one way, one way with branches, all ways)
+     * according to the provided eHorizon distance (the speed is derived from input points).
+     *
+     * @param request the uri used when hitting the http service
+     *
+     * @return a [RouterResult] object with the json and a success/fail boolean
+     */
     override fun getElectronicHorizon(request: String): RouterResult =
         navigator.getElectronicHorizon(request)
 
     // Offline
 
+    /**
+     * Caches tiles around the last set route
+     */
     override fun cacheLastRoute() {
         navigator.cacheLastRoute()
     }
 
+    /**
+     * Configures routers for getting routes offline.
+     *
+     * @param routerParams Optional [RouterParams] object which contains router configurations for
+     * getting routes offline.
+     * @param httpClient A platform specific [HttpInterface]. Can be null so default
+     * implementation will be used.
+     *
+     * @return number of tiles founded in the directory
+     */
     override fun configureRouter(routerParams: RouterParams, httpClient: HttpInterface?): Long =
         navigator.configureRouter(routerParams, httpClient)
 
+    /**
+     * Uses valhalla and local tile data to generate mapbox-directions-api-like json.
+     *
+     * @param url the directions-based uri used when hitting the http service
+     * @return a [RouterResult] object with the json and a success/fail boolean
+     */
     override fun getRoute(url: String): RouterResult = navigator.getRoute(url)
 
+    /**
+     * Passes in an input path to the tar file and output path.
+     *
+     * @param tarPath The path to the packed tiles.
+     * @param destinationPath The path to the unpacked files.
+     *
+     * @return the number of unpacked tiles
+     */
     override fun unpackTiles(tarPath: String, destinationPath: String): Long =
         navigator.unpackTiles(tarPath, destinationPath)
 
+    /**
+     * Removes tiles wholly within the supplied bounding box. If the tile is not
+     * contained completely within the bounding box, it will remain in the cache.
+     * After removing files from the cache, any routers should be reconfigured
+     * to synchronize their in-memory cache with the disk.
+     *
+     * @param tilePath The path to the tiles.
+     * @param southwest The lower left coord of the bounding box.
+     * @param northeast The upper right coord of the bounding box.
+     *
+     * @return the number of tiles removed
+     */
     override fun removeTiles(tilePath: String, southwest: Point, northeast: Point): Long =
         navigator.removeTiles(tilePath, southwest, northeast)
 
     // History traces
-
+    /**
+     * Gets the history of state-changing calls to the navigator. This can be used to
+     * replay a sequence of events for the purpose of bug fixing.
+     *
+     * @return a json representing the series of events that happened since the last time
+     * the history was toggled on.
+     */
     override fun getHistory(): String = navigator.history
 
+    /**
+     * Toggles the recording of history on or off.
+     * Toggling will reset all history calls [getHistory] first before toggling to retain a copy.
+     *
+     * @param isEnabled set this to true to turn on history recording and false to turn it off
+     */
     override fun toggleHistory(isEnabled: Boolean) {
         navigator.toggleHistory(isEnabled)
     }
 
+    /**
+     * Adds a custom event to the navigator's history. This can be useful to log things that
+     * happen during navigation that are specific to your application.
+     *
+     * @param eventType the event type in the events log for your custom event
+     * @param eventJsonProperties the json to attach to the "properties" key of the event
+     */
     override fun addHistoryEvent(eventType: String, eventJsonProperties: String) {
         navigator.pushHistory(eventType, eventJsonProperties)
     }
 
     // Configuration
-
+    /**
+     * Gets the current configuration used for navigation.
+     *
+     * @return the [NavigatorConfig] used for navigation.
+     */
     override fun getConfig(): NavigatorConfig = navigator.config
 
+    /**
+     * Updates the configuration used for navigation. Passing null resets the config.
+     *
+     * @param config the new [NavigatorConfig]
+     */
     override fun setConfig(config: NavigatorConfig?) {
         navigator.setConfig(config)
     }
 
     // Other
-
+    /**
+     * Gets the voice instruction at a specific step index in the route. If there is no
+     * voice instruction at the specified index, *null* is returned.
+     *
+     * @param index Which step you want to get [VoiceInstruction] for
+     *
+     * @return [VoiceInstruction] for step index you passed
+     */
     override fun getVoiceInstruction(index: Int): VoiceInstruction? =
         navigator.getVoiceInstruction(index)
 
--- libnavigator/src/main/java/com/mapbox/navigation/navigator/TripStatus.kt
+++ libnavigator/src/main/java/com/mapbox/navigation/navigator/TripStatus.kt
@@ -3,6 +3,16 @@ package com.mapbox.navigation.navigator
 import android.location.Location
 import com.mapbox.navigation.base.trip.model.RouteProgress
 
+/**
+ * State of a trip at a particular timestamp.
+ *
+ * @param enhancedLocation the user's location
+ * @param keyPoints list of predicted locations. Might be empty.
+ * @param routeProgress [RouteProgress] is progress information
+ * @param offRoute *true* if user is off-route, *false* otherwise
+ *
+ * @see [MapboxNativeNavigator.getStatus]
+ */
 data class TripStatus(
     val enhancedLocation: Location,
     val keyPoints: List<Location>,
