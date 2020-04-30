731ce08d3041ae0b380e30278ecc9bf02b19ea19
Jun Dai <jun66.dai@gmail.com>
2020-04-30 15:32:46 -0700

    Fix issue #2840

--- examples/src/main/java/com/mapbox/navigation/examples/core/FeedbackButtonActivity.kt
+++ examples/src/main/java/com/mapbox/navigation/examples/core/FeedbackButtonActivity.kt
@@ -126,9 +126,9 @@ class FeedbackButtonActivity : AppCompatActivity(), OnMapReadyCallback,
         mapboxMap.setStyle(Style.MAPBOX_STREETS) {
             mapboxMap.moveCamera(CameraUpdateFactory.zoomTo(15.0))
             navigationMapboxMap = NavigationMapboxMap(mapView, mapboxMap)
+            LocationEngineProvider.getBestLocationEngine(this)
+                .getLastLocation(locationListenerCallback)
         }
-
-        LocationEngineProvider.getBestLocationEngine(this).getLastLocation(locationListenerCallback)
 
         mapboxMap.addOnMapLongClickListener { latLng ->
             destination = latLng
