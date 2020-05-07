94ede2c3b302b8b73dc6b5cbf5fbf34526a866c7
Kyle Madsen <>
2020-05-07 09:53:08 -0700

    Ignore test until fixed

--- libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/internal/navigation/metrics/SchemaTest.java
+++ libandroid-navigation/src/test/java/com/mapbox/services/android/navigation/v5/internal/navigation/metrics/SchemaTest.java
@@ -10,6 +10,7 @@ import com.google.gson.annotations.SerializedName;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -90,6 +91,7 @@ public class SchemaTest {
   }
 
   @Test
+  @Ignore // TODO https://github.com/mapbox/mapbox-navigation-android/issues/2923
   public void checkNavigationFeedbackEventSize() throws Exception {
     JsonObject schema = grabSchema(NAVIGATION_FEEDBACK);
     List<Field> fields = grabClassFields(NavigationFeedbackEvent.class);
