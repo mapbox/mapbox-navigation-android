437a3f6562ae222e5baee9081cf04bcec65e2efe
Kyle Madsen <>
2020-05-07 16:56:16 -0700

    Reduce replay cornering speed

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/replay/route2/ReplayRouteOptions.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/replay/route2/ReplayRouteOptions.kt
@@ -33,7 +33,7 @@ class ReplayRouteOptions(
      */
     class Builder {
         private var maxSpeedMps = 30.0
-        private var turnSpeedMps = 6.0
+        private var turnSpeedMps = 3.0
         private var uTurnSpeedMps = 1.0
         private var maxAcceleration = 4.0
         private var minAcceleration = -4.0
