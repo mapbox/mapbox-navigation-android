6772a4098a0613826a83b46ce769e7d0c8784bc4
Seth Bourget <seth@cafesilencio.net>
2020-05-12 11:11:55 -0700

    The route line congestion is correctly represented with mult-leg routes. The issue with the route line expression sometimes getting created with invalid values represented as scientific notation is fixed.

--- libnavigation-ui/src/test/java/com/mapbox/navigation/ui/route/MapRouteLineTest.kt
+++ libnavigation-ui/src/test/java/com/mapbox/navigation/ui/route/MapRouteLineTest.kt
@@ -25,8 +25,10 @@ import com.mapbox.navigation.ui.route.RouteConstants.WAYPOINT_LAYER_ID
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import java.util.Scanner
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -533,7 +535,7 @@ class MapRouteLineTest {
     @Test
     fun buildRouteLineExpression() {
         every { style.layers } returns listOf(primaryRouteLayer)
-        val expectedExpression = "[\"step\", [\"line-progress\"], [\"rgba\", 0.0, 0.0, 0.0, 0.0], 0.2, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.31436133, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.66388464, [\"rgba\", 233.0, 51.0, 64.0, 1.0], 0.6948727, [\"rgba\", 86.0, 168.0, 251.0, 1.0]]"
+        val expectedExpression = "[\"step\", [\"line-progress\"], [\"rgba\", 0.0, 0.0, 0.0, 0.0], 0.2, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.31436133, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.92972755, [\"rgba\", 233.0, 51.0, 64.0, 1.0], 1.0003215, [\"rgba\", 86.0, 168.0, 251.0, 1.0]]"
         val route = getDirectionsRoute(true)
         val mapRouteLine = MapRouteLine(
             ctx,
@@ -544,6 +546,24 @@ class MapRouteLineTest {
             mapRouteSourceProvider).also { it.draw(listOf(route)) }
 
         val expression = mapRouteLine.getExpressionAtOffset(.2f)
+
+        assertEquals(expectedExpression, expression.toString())
+    }
+
+    @Test
+    fun buildRouteLineExpressionMultileg() {
+        every { style.layers } returns listOf(primaryRouteLayer)
+        val expectedExpression = "[\"step\", [\"line-progress\"], [\"rgba\", 0.0, 0.0, 0.0, 0.0], 0.0, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.021346012, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.06847635, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.09496192, [\"rgba\", 243.0, 166.0, 79.0, 1.0], 0.1054035, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.31133384, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.31479248, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.38133165, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.38438845, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.41593167, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.45903113, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.7533547, [\"rgba\", 243.0, 166.0, 79.0, 1.0], 0.7613792, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.7993399, [\"rgba\", 243.0, 166.0, 79.0, 1.0], 0.8467529, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.86620295, [\"rgba\", 243.0, 166.0, 79.0, 1.0], 0.8693383, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.9069854, [\"rgba\", 233.0, 51.0, 64.0, 1.0], 0.9224731, [\"rgba\", 233.0, 51.0, 64.0, 1.0], 0.9338598, [\"rgba\", 86.0, 168.0, 251.0, 1.0], 0.9950478, [\"rgba\", 86.0, 168.0, 251.0, 1.0]]"
+        val route = getMultilegRoute()
+        val mapRouteLine = MapRouteLine(
+            ctx,
+            style,
+            styleRes,
+            null,
+            layerProvider,
+            mapRouteSourceProvider).also { it.draw(listOf(route)) }
+
+        val expression = mapRouteLine.getExpressionAtOffset(0f)
 
         assertEquals(expectedExpression, expression.toString())
     }
@@ -585,18 +605,44 @@ class MapRouteLineTest {
     }
 
     @Test
-    fun getStopsFromLegReturnsCorrectNumStops() {
-        val route = getDirectionsRoute(true)
+    fun calculateRouteLineSegmentsMultilegRoute() {
+        val route = getMultilegRoute()
         val lineString = LineString.fromPolyline(route.geometry()!!, Constants.PRECISION_6)
 
-        val result = MapRouteLine.MapRouteLineSupport.calculateRouteLineSegmentFromLeg(
-            route.legs()!![0],
+        val result = MapRouteLine.MapRouteLineSupport.calculateRouteLineSegments(
+            route,
+            lineString,
+            true) { _, _ -> 1 }
+
+        assertEquals(21, result.size)
+    }
+
+    @Test
+    fun calculateRouteLineSegmentsMultilegRouteFirstDistanceValueAboveMinimumOffset() {
+        val route = getMultilegRoute()
+        val lineString = LineString.fromPolyline(route.geometry()!!, Constants.PRECISION_6)
+
+        val result = MapRouteLine.MapRouteLineSupport.calculateRouteLineSegments(
+            route,
+            lineString,
+            true) { _, _ -> 1 }
+
+        assertTrue(result[1].offset > .001f)
+    }
+
+    @Test
+    fun calculateRouteLineSegmentFromCongestion() {
+        val route = getMultilegRoute()
+        val lineString = LineString.fromPolyline(route.geometry()!!, Constants.PRECISION_6)
+
+        val result = MapRouteLine.MapRouteLineSupport.calculateRouteLineSegmentsFromCongestion(
+            route.legs()!![0].annotation()!!.congestion()!!.toList(),
             lineString,
             route.distance()!!,
             true
         ) { _, _ -> 1 }
 
-        assertEquals(4, result.size)
+        assertEquals(9, result.size)
     }
 
     @Test
@@ -653,5 +699,17 @@ class MapRouteLineTest {
         val tokenHere = "someToken"
         val directionsRouteAsJson = "{\"routeIndex\":\"0\",\"distance\":66.9,\"duration\":45.0,\"geometry\":\"urylgArvfuhFjJ`CbC{[pAZ\",\"weight\":96.6,\"weight_name\":\"routability\",\"legs\":[{\"distance\":66.9,\"duration\":45.0,\"summary\":\"Laurel Place, Lincoln Avenue\",\"steps\":[{\"distance\":21.0,\"duration\":16.7,\"geometry\":\"urylgArvfuhFjJ`C\",\"name\":\"\",\"mode\":\"driving\",\"maneuver\":{\"location\":[-122.523514,37.975355],\"bearing_before\":0.0,\"bearing_after\":196.0,\"instruction\":\"Head south\",\"type\":\"depart\",\"modifier\":\"right\"},\"voiceInstructions\":[{\"distanceAlongGeometry\":21.0,\"announcement\":\"Head south, then turn left onto Laurel Place\",\"ssmlAnnouncement\":\"\\u003cspeak\\u003e\\u003camazon:effect name\\u003d\\\"drc\\\"\\u003e\\u003cprosody rate\\u003d\\\"1.08\\\"\\u003eHead south, then turn left onto Laurel Place\\u003c/prosody\\u003e\\u003c/amazon:effect\\u003e\\u003c/speak\\u003e\"},{\"distanceAlongGeometry\":18.9,\"announcement\":\"Turn left onto Laurel Place, then turn right onto Lincoln Avenue\",\"ssmlAnnouncement\":\"\\u003cspeak\\u003e\\u003camazon:effect name\\u003d\\\"drc\\\"\\u003e\\u003cprosody rate\\u003d\\\"1.08\\\"\\u003eTurn left onto Laurel Place, then turn right onto Lincoln Avenue\\u003c/prosody\\u003e\\u003c/amazon:effect\\u003e\\u003c/speak\\u003e\"}],\"bannerInstructions\":[{\"distanceAlongGeometry\":21.0,\"primary\":{\"text\":\"Laurel Place\",\"components\":[{\"text\":\"Laurel Place\",\"type\":\"text\",\"abbr\":\"Laurel Pl\",\"abbr_priority\":0}],\"type\":\"turn\",\"modifier\":\"left\"}},{\"distanceAlongGeometry\":18.9,\"primary\":{\"text\":\"Laurel Place\",\"components\":[{\"text\":\"Laurel Place\",\"type\":\"text\",\"abbr\":\"Laurel Pl\",\"abbr_priority\":0}],\"type\":\"turn\",\"modifier\":\"left\"},\"sub\":{\"text\":\"Lincoln Avenue\",\"components\":[{\"text\":\"Lincoln Avenue\",\"type\":\"text\",\"abbr\":\"Lincoln Ave\",\"abbr_priority\":0}],\"type\":\"turn\",\"modifier\":\"right\"}}],\"driving_side\":\"right\",\"weight\":52.6,\"intersections\":[{\"location\":[-122.523514,37.975355],\"bearings\":[196],\"entry\":[true],\"out\":0}]},{\"distance\":41.2,\"duration\":27.3,\"geometry\":\"igylgAtzfuhFbC{[\",\"name\":\"Laurel Place\",\"mode\":\"driving\",\"maneuver\":{\"location\":[-122.523579,37.975173],\"bearing_before\":195.0,\"bearing_after\":99.0,\"instruction\":\"Turn left onto Laurel Place\",\"type\":\"turn\",\"modifier\":\"left\"},\"voiceInstructions\":[{\"distanceAlongGeometry\":22.6,\"announcement\":\"Turn right onto Lincoln Avenue, then you will arrive at your destination\",\"ssmlAnnouncement\":\"\\u003cspeak\\u003e\\u003camazon:effect name\\u003d\\\"drc\\\"\\u003e\\u003cprosody rate\\u003d\\\"1.08\\\"\\u003eTurn right onto Lincoln Avenue, then you will arrive at your destination\\u003c/prosody\\u003e\\u003c/amazon:effect\\u003e\\u003c/speak\\u003e\"}],\"bannerInstructions\":[{\"distanceAlongGeometry\":41.2,\"primary\":{\"text\":\"Lincoln Avenue\",\"components\":[{\"text\":\"Lincoln Avenue\",\"type\":\"text\",\"abbr\":\"Lincoln Ave\",\"abbr_priority\":0}],\"type\":\"turn\",\"modifier\":\"right\"}}],\"driving_side\":\"right\",\"weight\":43.0,\"intersections\":[{\"location\":[-122.523579,37.975173],\"bearings\":[15,105,285],\"entry\":[false,true,true],\"in\":0,\"out\":1}]},{\"distance\":4.7,\"duration\":1.0,\"geometry\":\"ecylgAx}euhFpAZ\",\"name\":\"Lincoln Avenue\",\"mode\":\"driving\",\"maneuver\":{\"location\":[-122.523117,37.975107],\"bearing_before\":99.0,\"bearing_after\":194.0,\"instruction\":\"Turn right onto Lincoln Avenue\",\"type\":\"turn\",\"modifier\":\"right\"},\"voiceInstructions\":[{\"distanceAlongGeometry\":4.7,\"announcement\":\"You have arrived at your destination\",\"ssmlAnnouncement\":\"\\u003cspeak\\u003e\\u003camazon:effect name\\u003d\\\"drc\\\"\\u003e\\u003cprosody rate\\u003d\\\"1.08\\\"\\u003eYou have arrived at your destination\\u003c/prosody\\u003e\\u003c/amazon:effect\\u003e\\u003c/speak\\u003e\"}],\"bannerInstructions\":[{\"distanceAlongGeometry\":4.7,\"primary\":{\"text\":\"You have arrived\",\"components\":[{\"text\":\"You have arrived\",\"type\":\"text\"}],\"type\":\"arrive\",\"modifier\":\"straight\"}}],\"driving_side\":\"right\",\"weight\":1.0,\"intersections\":[{\"location\":[-122.523117,37.975107],\"bearings\":[15,105,195,285],\"entry\":[true,true,true,false],\"in\":3,\"out\":2}]},{\"distance\":0.0,\"duration\":0.0,\"geometry\":\"s`ylgAt~euhF\",\"name\":\"Lincoln Avenue\",\"mode\":\"driving\",\"maneuver\":{\"location\":[-122.523131,37.975066],\"bearing_before\":195.0,\"bearing_after\":0.0,\"instruction\":\"You have arrived at your destination\",\"type\":\"arrive\"},\"voiceInstructions\":[],\"bannerInstructions\":[],\"driving_side\":\"right\",\"weight\":0.0,\"intersections\":[{\"location\":[-122.523131,37.975066],\"bearings\":[15],\"entry\":[true],\"in\":0}]}],\"annotation\":{\"distance\":[21.030105037432428,41.16669115760234,4.722589365163041],\"congestion\":[$congestion]}}],\"routeOptions\":{\"baseUrl\":\"https://api.mapbox.com\",\"user\":\"mapbox\",\"profile\":\"driving-traffic\",\"coordinates\":[[-122.5237559,37.9754094],[-122.5231475,37.9750697]],\"alternatives\":true,\"language\":\"en\",\"continue_straight\":false,\"roundabout_exits\":false,\"geometries\":\"polyline6\",\"overview\":\"full\",\"steps\":true,\"annotations\":\"congestion,distance\",\"voice_instructions\":true,\"banner_instructions\":true,\"voice_units\":\"imperial\",\"access_token\":\"$tokenHere\",\"uuid\":\"ck9g2sbdk6pod7ynuece0r2yo\"},\"voiceLocale\":\"en-US\"}"
         return DirectionsRoute.fromJson(directionsRouteAsJson)
+    }
+
+    private fun getMultilegRoute(): DirectionsRoute {
+        val routeAsJson = loadJsonFixture("multileg_route.json")
+        return DirectionsRoute.fromJson(routeAsJson)
+    }
+
+    private fun loadJsonFixture(filename: String): String? {
+        val classLoader = javaClass.classLoader
+        val inputStream = classLoader.getResourceAsStream(filename)
+        val scanner = Scanner(inputStream, "UTF-8").useDelimiter("\\A")
+        return if (scanner.hasNext()) scanner.next() else ""
     }
 }
