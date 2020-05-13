23ec9a1764b83df729451d5752044d4f178c8399
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2020-05-13 05:11:52 +0300

    Added doc for missed util method

--- libnavigation-util/src/main/java/com/mapbox/navigation/utils/extensions/ContextEx.kt
+++ libnavigation-util/src/main/java/com/mapbox/navigation/utils/extensions/ContextEx.kt
@@ -3,6 +3,11 @@ package com.mapbox.navigation.utils.extensions
 import android.content.Context
 import android.location.LocationManager
 
+/**
+ * Check if enabled one of location providers: GPS_PROVIDER or NETWORK_PROVIDER.
+ *
+ * @return true if enabled at least one of location providers
+ */
 fun Context.isLocationProviderEnabled(): Boolean {
     val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
     return manager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
