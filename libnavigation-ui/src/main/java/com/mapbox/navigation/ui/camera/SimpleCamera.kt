486628de2eb7ebfe8eb876ea0ee6fb9767acfecc
Seth Bourget <seth@cafesilencio.net>
2020-06-25 14:00:21 -0700

    Removing cached route state.

--- libnavigation-ui/src/main/java/com/mapbox/navigation/ui/camera/SimpleCamera.kt
+++ libnavigation-ui/src/main/java/com/mapbox/navigation/ui/camera/SimpleCamera.kt
@@ -4,8 +4,6 @@ import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.core.constants.Constants
 import com.mapbox.geojson.LineString
 import com.mapbox.geojson.Point
-import com.mapbox.navigation.utils.internal.ifNonNull
-import java.util.ArrayList
 
 /**
  * The default camera used by [MapboxNavigation].
@@ -24,9 +22,6 @@ open class SimpleCamera : Camera() {
         protected const val DEFAULT_ZOOM = 15.0
     }
 
-    private var routeCoordinates: List<Point> = ArrayList()
-    private var initialRoute: DirectionsRoute? = null
-
     override fun tilt(routeInformation: RouteInformation): Double {
         return DEFAULT_TILT.toDouble()
     }
@@ -35,29 +30,16 @@ open class SimpleCamera : Camera() {
         return DEFAULT_ZOOM
     }
 
-    override fun overview(routeInformation: RouteInformation): List<Point> {
-        if (routeCoordinates.isEmpty()) {
-            buildRouteCoordinatesFromRouteData(routeInformation)
-        }
-        return routeCoordinates
-    }
+    override fun overview(routeInformation: RouteInformation): List<Point> =
+        getRoute(routeInformation)?.run {
+            generateRouteCoordinates(this)
+        } ?: emptyList()
 
-    private fun buildRouteCoordinatesFromRouteData(routeInformation: RouteInformation) {
-        ifNonNull(routeInformation.route) { route ->
-            setupLineStringAndBearing(route)
+    private fun getRoute(routeInformation: RouteInformation): DirectionsRoute? =
+        when (routeInformation.route) {
+            null -> routeInformation.routeProgress?.route
+            else -> routeInformation.route
         }
-            ?: ifNonNull(routeInformation.routeProgress?.route) { directionsRoute ->
-                setupLineStringAndBearing(directionsRoute)
-            }
-    }
-
-    private fun setupLineStringAndBearing(route: DirectionsRoute) {
-        if (route == initialRoute) {
-            return // no need to recalculate these values
-        }
-        initialRoute = route
-        routeCoordinates = generateRouteCoordinates(route)
-    }
 
     private fun generateRouteCoordinates(route: DirectionsRoute?): List<Point> =
         route?.geometry()?.let { geometry ->
