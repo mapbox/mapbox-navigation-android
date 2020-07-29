e4a79cd1c3f7f991c0521c1578d2d518cf7700f6
Łukasz Paczos <lukasz.paczos@mapbox.com>
2020-07-29 11:59:39 +0200

    refactor all Nav UI Kotlin builder classes

--- libnavigation-ui/src/main/java/com/mapbox/navigation/ui/internal/utils/BitmapEncodeOptions.kt
+++ libnavigation-ui/src/main/java/com/mapbox/navigation/ui/internal/utils/BitmapEncodeOptions.kt
@@ -3,7 +3,36 @@ package com.mapbox.navigation.ui.internal.utils
 const val DEFAULT_BITMAP_ENCODE_WIDTH = 250
 const val DEFAULT_BITMAP_ENCODE_COMPRESS_QUALITY = 20
 
-data class BitmapEncodeOptions(val width: Int, val compressQuality: Int) {
+class BitmapEncodeOptions private constructor(val width: Int, val compressQuality: Int) {
+
+    /**
+     * @return builder matching the one used to create this instance
+     */
+    fun toBuilder() = Builder()
+        .compressQuality(compressQuality)
+        .width(width)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BitmapEncodeOptions
+
+        if (width != other.width) return false
+        if (compressQuality != other.compressQuality) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = width
+        result = 31 * result + compressQuality
+        return result
+    }
+
+    override fun toString(): String {
+        return "BitmapEncodeOptions(width=$width, compressQuality=$compressQuality)"
+    }
 
     class Builder {
         private var width: Int = DEFAULT_BITMAP_ENCODE_WIDTH
