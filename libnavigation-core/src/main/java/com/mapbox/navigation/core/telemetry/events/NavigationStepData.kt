6d29a816747e1dcc90272526aecea4146d02a991
Nikita Korshak <mikita.korshak@mapbox.com>
2020-07-30 16:44:49 +0300

    add NavigationFeedbackEventTest

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/telemetry/events/NavigationStepData.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/telemetry/events/NavigationStepData.kt
@@ -16,4 +16,42 @@ internal class NavigationStepData(metricsRouteProgress: MetricsRouteProgress) {
     val duration: Int = metricsRouteProgress.currentStepDuration
     val distanceRemaining: Int = metricsRouteProgress.currentStepDistanceRemaining
     val durationRemaining: Int = metricsRouteProgress.currentStepDurationRemaining
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NavigationStepData
+
+        if (upcomingInstruction != other.upcomingInstruction) return false
+        if (upcomingModifier != other.upcomingModifier) return false
+        if (upcomingName != other.upcomingName) return false
+        if (upcomingType != other.upcomingType) return false
+        if (previousInstruction != other.previousInstruction) return false
+        if (previousModifier != other.previousModifier) return false
+        if (previousName != other.previousName) return false
+        if (previousType != other.previousType) return false
+        if (distance != other.distance) return false
+        if (duration != other.duration) return false
+        if (distanceRemaining != other.distanceRemaining) return false
+        if (durationRemaining != other.durationRemaining) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = upcomingInstruction?.hashCode() ?: 0
+        result = 31 * result + (upcomingModifier?.hashCode() ?: 0)
+        result = 31 * result + (upcomingName?.hashCode() ?: 0)
+        result = 31 * result + (upcomingType?.hashCode() ?: 0)
+        result = 31 * result + (previousInstruction?.hashCode() ?: 0)
+        result = 31 * result + (previousModifier?.hashCode() ?: 0)
+        result = 31 * result + (previousName?.hashCode() ?: 0)
+        result = 31 * result + (previousType?.hashCode() ?: 0)
+        result = 31 * result + distance
+        result = 31 * result + duration
+        result = 31 * result + distanceRemaining
+        result = 31 * result + durationRemaining
+        return result
+    }
 }
