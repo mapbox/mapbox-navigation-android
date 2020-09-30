48a6afe00ef65da2175192e4573675a931c397a0
Pablo Guardiola <guardiola31337@gmail.com>
2020-09-30 16:43:20 -0400

    rename border crossing alert to country border crossing alert

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/trip/model/alert/CountryBorderCrossingAdminInfo.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/trip/model/alert/CountryBorderCrossingAdminInfo.kt
@@ -3,21 +3,21 @@ package com.mapbox.navigation.base.trip.model.alert
 /**
  * Administrative information.
  */
-class BorderCrossingAdminInfo private constructor(
+class CountryBorderCrossingAdminInfo private constructor(
     /**
      * ISO 3166-1, 2 letter country code.
      */
-    val countryCode: String,
+    val code: String,
     /**
      * ISO 3166-1 alpha-3, 3 letter country code.
      */
-    val countryCodeAlpha3: String,
+    val codeAlpha3: String,
 ) {
 
     /**
      * Transform this object into a builder to mutate the values.
      */
-    fun toBuilder(): Builder = Builder(countryCode, countryCodeAlpha3)
+    fun toBuilder(): Builder = Builder(code, codeAlpha3)
 
     /**
      * Indicates whether some other object is "equal to" this one.
@@ -26,10 +26,10 @@ class BorderCrossingAdminInfo private constructor(
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as BorderCrossingAdminInfo
+        other as CountryBorderCrossingAdminInfo
 
-        if (countryCode != other.countryCode) return false
-        if (countryCodeAlpha3 != other.countryCodeAlpha3) return false
+        if (code != other.code) return false
+        if (codeAlpha3 != other.codeAlpha3) return false
 
         return true
     }
@@ -38,8 +38,8 @@ class BorderCrossingAdminInfo private constructor(
      * Returns a hash code value for the object.
      */
     override fun hashCode(): Int {
-        var result = countryCode.hashCode()
-        result = 31 * result + countryCodeAlpha3.hashCode()
+        var result = code.hashCode()
+        result = 31 * result + codeAlpha3.hashCode()
         return result
     }
 
@@ -47,26 +47,27 @@ class BorderCrossingAdminInfo private constructor(
      * Returns a string representation of the object.
      */
     override fun toString(): String {
-        return "BorderCrossingAdminInfo(countryCode='$countryCode', " +
-            "countryCodeAlpha3='$countryCodeAlpha3')"
+        return "CountryBorderCrossingAdminInfo(code='$code', " +
+            "codeAlpha3='$codeAlpha3')"
     }
 
     /**
      * Use to create a new instance.
      *
-     * @see BorderCrossingAdminInfo
+     * @see CountryBorderCrossingAdminInfo
      */
     class Builder(
-        private val countryCode: String,
-        private val countryCodeAlpha3: String
+        private val code: String,
+        private val codeAlpha3: String
     ) {
+
         /**
          * Build the object instance.
          */
         fun build() =
-            BorderCrossingAdminInfo(
-                countryCode,
-                countryCodeAlpha3
+            CountryBorderCrossingAdminInfo(
+                code,
+                codeAlpha3
             )
     }
 }
