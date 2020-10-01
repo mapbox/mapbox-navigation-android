02608b363ead3bbbc766c8dfd86b41fc68c0edf5
Pablo Guardiola <guardiola31337@gmail.com>
2020-10-01 15:00:51 -0400

    expose tunnel info as part of tunnel entrance alert

--- libnavigation-base/src/test/java/com/mapbox/navigation/base/trip/model/alert/TunnelEntranceAlertTest.kt
+++ libnavigation-base/src/test/java/com/mapbox/navigation/base/trip/model/alert/TunnelEntranceAlertTest.kt
@@ -2,6 +2,7 @@ package com.mapbox.navigation.base.trip.model.alert
 
 import com.mapbox.geojson.Point
 import com.mapbox.navigation.testing.BuilderTest
+import io.mockk.mockk
 import org.junit.Test
 
 class TunnelEntranceAlertTest : BuilderTest<TunnelEntranceAlert, TunnelEntranceAlert.Builder>() {
@@ -19,7 +20,7 @@ class TunnelEntranceAlertTest : BuilderTest<TunnelEntranceAlert, TunnelEntranceA
             Point.fromLngLat(33.0, 44.0),
             2
         ).build()
-    )
+    ).info(mockk(relaxed = true))
 
     @Test
     override fun trigger() {
