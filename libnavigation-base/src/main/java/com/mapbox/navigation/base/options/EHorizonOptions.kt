3ed960fd8a370d392a9f690cf0d23da031d80621
Pablo Guardiola <guardiola31337@gmail.com>
2020-11-06 12:42:15 -0500

    add details to e horizon options docs and set electronic horizon options when the navigator is created

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/options/EHorizonOptions.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/options/EHorizonOptions.kt
@@ -4,13 +4,15 @@ package com.mapbox.navigation.base.options
  * Defines options for [EHorizon].
  *
  * @param length the minimum length of the MPP in meters. This does not include the trailingLength.
- * The actual MPP length may be bigger
+ * The actual MPP length may be bigger. Double in range [1.0, 20000.0]. Default value 500.0
  * @param expansion the number of branches to include from the MPP. When set to 0 only the MPP
- * is returned. Higher values will result in deeper nesting
+ * is returned. Higher values will result in deeper nesting. Int in range [0, 2]. Default value 0
  * @param branchLength when expansion is set to anything but 0, this specifies the minimum length
- * in meters branches will be expanded from the MPP
+ * in meters branches will be expanded from the MPP. Double in range [1.0, 5000.0].
+ * Default value 50.0
  * @param includeGeometries will geometries be included for edges. Excluding the edge shapes may
- * save some processing time on shape extraction and decoding
+ * save some processing time on shape extraction and decoding. Boolean to enable/disable.
+ * Default value false
  */
 class EHorizonOptions private constructor(
     val length: Double,
