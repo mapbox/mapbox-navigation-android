d1b763525c0b6e5ba8ae0f884dfc81a3567489be
Dave Prukop <dave.prukop@mapbox.com>
2020-11-17 09:05:51 -0800

    Fixed indent

--- librouteline/src/main/java/com/mapbox/navigation/ui/maps/camera/MapboxNavigationCameraUtils.kt
+++ librouteline/src/main/java/com/mapbox/navigation/ui/maps/camera/MapboxNavigationCameraUtils.kt
@@ -5,19 +5,19 @@ import com.mapbox.maps.EdgeInsets
 import com.mapbox.maps.ScreenCoordinate
 import com.mapbox.maps.Size
 
-    internal fun getScaledEdgeInsets(edgeInsets: EdgeInsets): EdgeInsets {
-        val displayMetrics = Resources.getSystem().getDisplayMetrics()
-        val scale = displayMetrics.density
-        return EdgeInsets(edgeInsets.top * scale, edgeInsets.left * scale, edgeInsets.bottom * scale, edgeInsets.right * scale)
-    }
+internal fun getScaledEdgeInsets(edgeInsets: EdgeInsets): EdgeInsets {
+    val displayMetrics = Resources.getSystem().getDisplayMetrics()
+    val scale = displayMetrics.density
+    return EdgeInsets(edgeInsets.top * scale, edgeInsets.left * scale, edgeInsets.bottom * scale, edgeInsets.right * scale)
+}
 
-    internal fun shortestRotation(from: Double, to: Double): Double {
-        return (to - from + 540) % 360 - 180
-    }
+internal fun shortestRotation(from: Double, to: Double): Double {
+    return (to - from + 540) % 360 - 180
+}
 
-    internal fun convertScreenCenterOffsetToEdgeInsets(mapSize: Size, centerOffset: ScreenCoordinate = ScreenCoordinate(0.0, 0.0)): EdgeInsets {
-        val mapCenterScreenCoordinate = ScreenCoordinate((mapSize.width / 2).toDouble(), (mapSize.height / 2).toDouble())
-        val top = mapCenterScreenCoordinate.y + centerOffset.y
-        val left = mapCenterScreenCoordinate.x + centerOffset.x
-        return getScaledEdgeInsets(EdgeInsets(top, left, mapSize.height - top, mapSize.width - left))
-    }
+internal fun convertScreenCenterOffsetToEdgeInsets(mapSize: Size, centerOffset: ScreenCoordinate = ScreenCoordinate(0.0, 0.0)): EdgeInsets {
+    val mapCenterScreenCoordinate = ScreenCoordinate((mapSize.width / 2).toDouble(), (mapSize.height / 2).toDouble())
+    val top = mapCenterScreenCoordinate.y + centerOffset.y
+    val left = mapCenterScreenCoordinate.x + centerOffset.x
+    return getScaledEdgeInsets(EdgeInsets(top, left, mapSize.height - top, mapSize.width - left))
+}
