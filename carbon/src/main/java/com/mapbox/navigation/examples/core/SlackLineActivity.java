fa72fc547cdc236559bd4b68a373c90d44c824a9
Seth Bourget <seth@cafesilencio.net>
2020-11-18 15:56:15 -0800

    Added main activity to server as entry point for launching other activities. Also added a utility for easily using route line in example activities.

---
+++ carbon/src/main/java/com/mapbox/navigation/examples/core/SlackLineActivity.java
@@ -0,0 +1,352 @@
+package com.mapbox.navigation.examples.core;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.mapbox.android.core.location.LocationEngineCallback;
+import com.mapbox.android.core.location.LocationEngineResult;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.directions.v5.models.RouteOptions;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.*;
+import com.mapbox.maps.plugin.animation.CameraAnimationsPlugin;
+import com.mapbox.maps.plugin.animation.CameraAnimationsPluginImplKt;
+import com.mapbox.maps.plugin.gestures.GesturesPluginImpl;
+import com.mapbox.maps.plugin.gestures.OnMapLongClickListener;
+import com.mapbox.maps.plugin.location.LocationComponentActivationOptions;
+import com.mapbox.maps.plugin.location.LocationComponentPlugin;
+import com.mapbox.maps.plugin.location.modes.RenderMode;
+import com.mapbox.navigation.base.internal.route.RouteUrl;
+import com.mapbox.navigation.base.options.NavigationOptions;
+import com.mapbox.navigation.core.MapboxNavigation;
+import com.mapbox.navigation.core.directions.session.RoutesRequestCallback;
+import com.mapbox.navigation.core.replay.MapboxReplayer;
+import com.mapbox.navigation.core.replay.ReplayLocationEngine;
+import com.mapbox.navigation.core.replay.route.ReplayProgressObserver;
+import com.mapbox.navigation.core.trip.session.LocationObserver;
+import com.mapbox.navigation.examples.util.LocationPermissionsHelper;
+import com.mapbox.navigation.examples.util.Slackline;
+import org.jetbrains.annotations.NotNull;
+import timber.log.Timber;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static com.mapbox.navigation.examples.util.LocationPermissionsHelperKt.LOCATION_PERMISSIONS_REQUEST_CODE;
+
+public class SlackLineActivity  extends AppCompatActivity implements PermissionsListener, OnMapLongClickListener {
+
+
+  private static final int ONE_HUNDRED_MILLISECONDS = 100;
+  private LocationPermissionsHelper permissionsHelper = new LocationPermissionsHelper(this);
+  private MapView mapView;
+  private MapboxMap mapboxMap;
+  private LocationComponentPlugin locationComponent;
+  private CameraAnimationsPlugin mapCamera;
+  private MapboxReplayer mapboxReplayer = new MapboxReplayer();
+  private MapboxNavigation mapboxNavigation;
+  private Button startNavigation;
+  private ProgressBar routeLoading;
+  private Slackline slackline = new Slackline(this);
+
+  @SuppressLint("MissingPermission")
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_navigation_map_route);
+    findViewById(R.id.fabToggleStyle).setVisibility(View.INVISIBLE);
+    mapView = findViewById(R.id.mapView);
+    startNavigation = findViewById(R.id.startNavigation);
+    routeLoading = findViewById(R.id.routeLoadingProgressBar);
+    mapboxMap = mapView.getMapboxMap();
+    locationComponent = getLocationComponent();
+    mapCamera = getMapCamera();
+
+    if (LocationPermissionsHelper.areLocationPermissionsGranted(this)) {
+      requestPermissionIfNotGranted(WRITE_EXTERNAL_STORAGE);
+    } else {
+      permissionsHelper.requestLocationPermissions(this);
+    }
+  }
+
+  private void init() {
+    initNavigation();
+    initStyle();
+    slackline.initialize(mapView, mapboxNavigation);
+    initListeners();
+  }
+
+  @SuppressLint("MissingPermission") private void initListeners() {
+    startNavigation.setOnClickListener(v -> {
+      locationComponent.setRenderMode(RenderMode.GPS);
+      mapboxNavigation.startTripSession();
+      startNavigation.setVisibility(View.GONE);
+    });
+  }
+
+  @SuppressLint("MissingPermission")
+  private void initNavigation() {
+    NavigationOptions navigationOptions = MapboxNavigation
+        .defaultNavigationOptionsBuilder(SlackLineActivity.this, getMapboxAccessTokenFromResources())
+        .locationEngine(new ReplayLocationEngine(mapboxReplayer))
+        .build();
+    mapboxNavigation = new MapboxNavigation(navigationOptions);
+    mapboxNavigation.registerLocationObserver(locationObserver);
+    mapboxNavigation.registerRouteProgressObserver(replayProgressObserver);
+
+    mapboxReplayer.pushRealLocation(this, 0.0);
+    mapboxReplayer.play();
+  }
+
+  @SuppressLint("MissingPermission")
+  private void initStyle() {
+    mapboxMap.loadStyleUri(Style.MAPBOX_STREETS, style -> {
+      initializeLocationComponent(style);
+      mapboxNavigation.getNavigationOptions().getLocationEngine().getLastLocation(locationEngineCallback);
+      getGesturePlugin().addOnMapLongClickListener(this);
+    }, (mapLoadError, s) -> Timber.e("Error loading map: %s", mapLoadError.name()));
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    mapView.onStart();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    mapView.onStop();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    mapView.onDestroy();
+    mapboxNavigation.onDestroy();
+  }
+
+  @Override public void onLowMemory() {
+    super.onLowMemory();
+    mapView.onLowMemory();
+  }
+
+  @Override
+  public boolean onMapLongClick(@NotNull Point point) {
+    vibrate();
+    startNavigation.setVisibility(View.GONE);
+
+    Location currentLocation = getLocationComponent().getLastKnownLocation();
+    if (currentLocation != null) {
+      Point originPoint = Point.fromLngLat(
+          currentLocation.getLongitude(),
+          currentLocation.getLatitude()
+      );
+      findRoute(originPoint, point);
+      routeLoading.setVisibility(View.VISIBLE);
+    }
+    return false;
+  }
+
+  public void findRoute(Point origin, Point destination) {
+    RouteOptions routeOptions = RouteOptions.builder()
+        .baseUrl(RouteUrl.BASE_URL)
+        .user(RouteUrl.PROFILE_DEFAULT_USER)
+        .profile(RouteUrl.PROFILE_DRIVING_TRAFFIC)
+        .geometries(RouteUrl.GEOMETRY_POLYLINE6)
+        .requestUuid("")
+        .accessToken(getMapboxAccessTokenFromResources())
+        .coordinates(Arrays.asList(origin, destination))
+        .alternatives(true)
+        .build();
+
+    mapboxNavigation.requestRoutes(
+        routeOptions,
+        routesReqCallback
+    );
+  }
+
+  private RoutesRequestCallback routesReqCallback = new RoutesRequestCallback() {
+    @Override
+    public void onRoutesReady(@NotNull List<? extends DirectionsRoute> routes) {
+      if (!routes.isEmpty()) {
+        routeLoading.setVisibility(View.INVISIBLE);
+        startNavigation.setVisibility(View.VISIBLE);
+      }
+    }
+
+    @Override
+    public void onRoutesRequestFailure(@NotNull Throwable throwable, @NotNull RouteOptions routeOptions) {
+      Timber.e("route request failure %s", throwable.toString());
+    }
+
+    @Override
+    public void onRoutesRequestCanceled(@NotNull RouteOptions routeOptions) {
+      Timber.d("route request canceled");
+    }
+  };
+
+  @SuppressLint("MissingPermission")
+  private void vibrate() {
+    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    if (vibrator == null) {
+      return;
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      vibrator.vibrate(VibrationEffect.createOneShot(ONE_HUNDRED_MILLISECONDS, VibrationEffect.DEFAULT_AMPLITUDE));
+    } else {
+      vibrator.vibrate(ONE_HUNDRED_MILLISECONDS);
+    }
+  }
+
+  @SuppressWarnings("MissingPermission")
+  private void initializeLocationComponent(Style style) {
+    LocationComponentActivationOptions activationOptions = LocationComponentActivationOptions.builder(this, style)
+        .useDefaultLocationEngine(false)
+        .build();
+    locationComponent.activateLocationComponent(activationOptions);
+    locationComponent.setEnabled(true);
+    locationComponent.setRenderMode(RenderMode.COMPASS);
+  }
+
+  private String getMapboxAccessTokenFromResources() {
+    return getString(this.getResources().getIdentifier("mapbox_access_token", "string", getPackageName()));
+  }
+
+  private LocationObserver locationObserver = new LocationObserver() {
+    @Override
+    public void onRawLocationChanged(@NotNull Location rawLocation) {
+      Timber.d("raw location %s", rawLocation.toString());
+    }
+
+    @Override
+    public void onEnhancedLocationChanged(
+        @NotNull Location enhancedLocation,
+        @NotNull List<? extends Location> keyPoints
+    ) {
+      if (keyPoints.isEmpty()) {
+        updateLocation(enhancedLocation);
+      } else {
+        updateLocation((List<Location>) keyPoints);
+      }
+    }
+  };
+
+  private void updateLocation(Location location) {
+    updateLocation(Arrays.asList(location));
+  }
+
+  private void updateLocation(List<Location> locations) {
+    Location location = locations.get(0);
+    getLocationComponent().forceLocationUpdate(locations, false);
+
+    mapCamera.easeTo(
+        new CameraOptions.Builder()
+            .center(Point.fromLngLat(location.getLongitude(), location.getLatitude()))
+            .bearing((double) location.getBearing())
+            .pitch(45.0)
+            .zoom(17.0)
+            .padding(new EdgeInsets(1000, 0, 0, 0))
+            .build(),
+        1500L,
+        null,
+        null
+    );
+  }
+
+  private LocationComponentPlugin getLocationComponent() {
+    return mapView.getPlugin(LocationComponentPlugin.class);
+  }
+
+  private CameraAnimationsPlugin getMapCamera() {
+    return CameraAnimationsPluginImplKt.getCameraAnimationsPlugin(mapView);
+  }
+
+  private GesturesPluginImpl getGesturePlugin() {
+    return mapView.getPlugin(GesturesPluginImpl.class);
+  }
+
+  private ReplayProgressObserver replayProgressObserver = new ReplayProgressObserver(mapboxReplayer);
+
+  private SlackLineActivity.MyLocationEngineCallback
+      locationEngineCallback = new SlackLineActivity.MyLocationEngineCallback(this);
+
+  private static class MyLocationEngineCallback implements LocationEngineCallback<LocationEngineResult> {
+
+    private WeakReference<SlackLineActivity> activityRef;
+
+    MyLocationEngineCallback(SlackLineActivity activity) {
+      this.activityRef = new WeakReference<>(activity);
+    }
+
+    @Override
+    public void onSuccess(LocationEngineResult result) {
+      Location location = result.getLastLocation();
+      SlackLineActivity activity = activityRef.get();
+      if (location != null && activity != null) {
+        Point point = Point.fromLngLat(location.getLongitude(), location.getLatitude());
+        CameraOptions cameraOptions = new CameraOptions.Builder().center(point).zoom(13.0).build();
+        activity.mapboxMap.jumpTo(cameraOptions);
+        activity.locationComponent.forceLocationUpdate(location);
+      }
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception exception) {
+      Timber.i(exception);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == LOCATION_PERMISSIONS_REQUEST_CODE) {
+      permissionsHelper.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    } else if (grantResults.length > 0) {
+      init();
+    } else {
+      Toast.makeText(this, "You didn't grant storage and/or location permissions.", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  @Override
+  public void onExplanationNeeded(List<String> permissionsToExplain) {
+    Toast.makeText(this, "This app needs location and storage permissions in order to show its functionality.", Toast.LENGTH_LONG).show();
+  }
+
+  @Override
+  public void onPermissionResult(boolean granted) {
+    if (granted) {
+      requestPermissionIfNotGranted(WRITE_EXTERNAL_STORAGE);
+    } else {
+      Toast.makeText(this, "You didn't grant location permissions.", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  private void requestPermissionIfNotGranted(String permission) {
+    List<String> permissionsNeeded = new ArrayList<>();
+    if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+      permissionsNeeded.add(permission);
+      ActivityCompat.requestPermissions(this, permissionsNeeded.toArray(new String[0]), 10);
+    } else {
+      init();
+    }
+  }
+}
---
+++ carbon/src/main/java/com/mapbox/navigation/examples/util/Slackline.kt
@@ -0,0 +1,133 @@
+package com.mapbox.navigation.examples.util
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.location.LocationComponentPlugin
+import com.mapbox.maps.plugin.location.OnIndicatorPositionChangedListener
+import com.mapbox.navigation.base.trip.model.RouteProgress
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.directions.session.RoutesObserver
+import com.mapbox.navigation.core.trip.session.RouteProgressObserver
+import com.mapbox.navigation.examples.core.R
+import com.mapbox.navigation.ui.maps.route.RouteArrowLayerInitializer
+import com.mapbox.navigation.ui.maps.route.RouteLineLayerInitializer
+import com.mapbox.navigation.ui.maps.route.routearrow.api.RouteArrowAPI
+import com.mapbox.navigation.ui.maps.route.routearrow.internal.MapboxRouteArrowAPI
+import com.mapbox.navigation.ui.maps.route.routearrow.internal.MapboxRouteArrowActions
+import com.mapbox.navigation.ui.maps.route.routearrow.internal.MapboxRouteArrowView
+import com.mapbox.navigation.ui.maps.route.routeline.api.RouteLineAPI
+import com.mapbox.navigation.ui.maps.route.routeline.internal.MapboxRouteLineAPI
+import com.mapbox.navigation.ui.maps.route.routeline.internal.MapboxRouteLineActions
+import com.mapbox.navigation.ui.maps.route.routeline.internal.MapboxRouteLineResourceProviderFactory.getRouteLineResourceProvider
+import com.mapbox.navigation.ui.maps.route.routeline.internal.MapboxRouteLineView
+
+class Slackline(private val activity: AppCompatActivity) : LifecycleObserver {
+
+    private val routeLineView = MapboxRouteLineView()
+    private val routeArrowView = MapboxRouteArrowView()
+    private lateinit var mapView: MapView
+    private lateinit var mapboxNavigation: MapboxNavigation
+
+    private val routeStyleRes: Int by lazy {
+        ThemeUtil.retrieveAttrResourceId(
+            activity,
+            R.attr.navigationViewRouteStyle,
+            R.style.MapboxStyleNavigationMapRoute
+        )
+    }
+
+    private val routeLineAPI: RouteLineAPI by lazy {
+        val resourceProvider = getRouteLineResourceProvider(activity, routeStyleRes)
+        MapboxRouteLineAPI(MapboxRouteLineActions(resourceProvider), routeLineView)
+    }
+
+    private val routeArrowAPI: RouteArrowAPI by lazy {
+        MapboxRouteArrowAPI(MapboxRouteArrowActions(), routeArrowView)
+    }
+
+    private val routeLineLayerInitializer: RouteLineLayerInitializer by lazy {
+        RouteLineLayerInitializer.Builder(activity).build()
+    }
+
+    private val routeArrowLayerInitializer: RouteArrowLayerInitializer by lazy {
+        RouteArrowLayerInitializer.Builder(activity).build()
+    }
+
+    init {
+        activity.lifecycle.addObserver(this)
+    }
+
+    fun initialize(mapView: MapView, mapboxNavigation: MapboxNavigation) {
+        this.mapView = mapView
+        this.mapboxNavigation = mapboxNavigation
+        mapView.getMapboxMap().getStyle(
+            object : Style.OnStyleLoaded {
+                override fun onStyleLoaded(style: Style) {
+                    routeLineLayerInitializer.initializeLayers(style)
+                    routeArrowLayerInitializer.initializeLayers(style)
+                    routeLineAPI.updateViewStyle(style)
+                    routeArrowAPI.updateViewStyle(style)
+                }
+            }
+        )
+        getLocationComponent(mapView)!!.addOnIndicatorPositionChangedListener(
+            onIndicatorPositionChangedListener
+        )
+        mapboxNavigation.registerRoutesObserver(routesObserver)
+        mapboxNavigation.registerRouteProgressObserver(routeProgressObserver)
+    }
+
+    private fun getLocationComponent(mapView: MapView): LocationComponentPlugin? {
+        return mapView.getPlugin<LocationComponentPlugin>(LocationComponentPlugin::class.java)
+    }
+
+    private val onIndicatorPositionChangedListener = object : OnIndicatorPositionChangedListener {
+        override fun onIndicatorPositionChanged(point: Point) {
+            routeLineAPI.updateTraveledRouteLine(point)
+        }
+    }
+
+    private val routesObserver: RoutesObserver = object : RoutesObserver {
+        override fun onRoutesChanged(routes: List<DirectionsRoute>) {
+            routeLineAPI.setRoutes(listOf(routes[0]))
+        }
+    }
+
+    private val routeProgressObserver: RouteProgressObserver = object : RouteProgressObserver {
+        override fun onRouteProgressChanged(routeProgress: RouteProgress) {
+            routeLineAPI.updateUpcomingRoutePointIndex(routeProgress)
+            routeLineAPI.updateVanishingPointState(routeProgress.currentState)
+            routeArrowAPI.addUpComingManeuverArrow(routeProgress)
+
+            val currentRoute = routeProgress.route
+            var hasGeometry = false
+            if (currentRoute.geometry() != null && currentRoute.geometry()!!.isNotEmpty()) {
+                hasGeometry = true
+            }
+
+            var isNewRoute = false
+            if (hasGeometry && currentRoute !== routeLineAPI.getPrimaryRoute()) {
+                isNewRoute = true
+            }
+
+            if (isNewRoute) {
+                routeLineAPI.setRoutes(listOf(routeProgress.route))
+            }
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    private fun onStop() {
+        getLocationComponent(mapView)!!.removeOnIndicatorPositionChangedListener(
+            onIndicatorPositionChangedListener
+        )
+        mapboxNavigation.unregisterRoutesObserver(routesObserver)
+        mapboxNavigation.unregisterRouteProgressObserver(routeProgressObserver)
+    }
+}
