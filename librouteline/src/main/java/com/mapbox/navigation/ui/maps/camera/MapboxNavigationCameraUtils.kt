15f79d5d463f8eb77fe1a11a068698c3a6f91424
Dave Prukop <dave.prukop@mapbox.com>
2020-11-18 09:27:34 -0800

    Scaling map size in center offset calculation

--- librouteline/src/main/java/com/mapbox/navigation/ui/maps/camera/MapboxNavigationCameraUtils.kt
+++ librouteline/src/main/java/com/mapbox/navigation/ui/maps/camera/MapboxNavigationCameraUtils.kt
@@ -1,5 +1,6 @@
 package com.mapbox.navigation.ui.maps.camera
 
+import android.content.res.Resources
 import com.mapbox.maps.EdgeInsets
 import com.mapbox.maps.ScreenCoordinate
 import com.mapbox.maps.Size
@@ -9,8 +10,11 @@ internal fun shortestRotation(from: Double, to: Double): Double {
 }
 
 internal fun convertScreenCenterOffsetToEdgeInsets(mapSize: Size, centerOffset: ScreenCoordinate = ScreenCoordinate(0.0, 0.0)): EdgeInsets {
-    val mapCenterScreenCoordinate = ScreenCoordinate((mapSize.width / 2).toDouble(), (mapSize.height / 2).toDouble())
+    val displayMetrics = Resources.getSystem().getDisplayMetrics()
+    val scale = displayMetrics.density
+    val scaledMapSize = Size(mapSize.width * scale, mapSize.height * scale)
+    val mapCenterScreenCoordinate = ScreenCoordinate((scaledMapSize.width / 2).toDouble(), (scaledMapSize.height / 2).toDouble())
     val top = mapCenterScreenCoordinate.y + centerOffset.y
     val left = mapCenterScreenCoordinate.x + centerOffset.x
-    return EdgeInsets(top, left, mapSize.height - top, mapSize.width - left)
+    return EdgeInsets(top, left, scaledMapSize.height - top, scaledMapSize.width - left)
 }
