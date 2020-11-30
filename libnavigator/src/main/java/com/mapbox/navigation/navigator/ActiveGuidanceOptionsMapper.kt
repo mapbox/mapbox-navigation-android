30b933eccac36e212f0614c4c950a35b6125cf94
Pablo Guardiola <guardiola31337@gmail.com>
2020-11-30 11:39:21 -0500

    bump mapbox-navigation-native dependency version to 27.0.0

--- libnavigator/src/main/java/com/mapbox/navigation/navigator/ActiveGuidanceOptionsMapper.kt
+++ libnavigator/src/main/java/com/mapbox/navigation/navigator/ActiveGuidanceOptionsMapper.kt
@@ -2,9 +2,11 @@ package com.mapbox.navigation.navigator
 
 import com.mapbox.api.directions.v5.DirectionsCriteria
 import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
 import com.mapbox.navigator.ActiveGuidanceGeometryEncoding
 import com.mapbox.navigator.ActiveGuidanceMode
 import com.mapbox.navigator.ActiveGuidanceOptions
+import com.mapbox.navigator.Waypoint
 
 internal object ActiveGuidanceOptionsMapper {
 
@@ -13,7 +15,10 @@ internal object ActiveGuidanceOptionsMapper {
     fun mapFrom(directionsRoute: DirectionsRoute?): ActiveGuidanceOptions {
         val mode = mapToActiveGuidanceMode(directionsRoute?.routeOptions()?.profile())
         val geometry = mapToActiveGuidanceGeometry(directionsRoute?.routeOptions()?.geometries())
-        return ActiveGuidanceOptions(mode, geometry)
+        val coordinates = mapToActiveGuidanceCoordinates(
+            directionsRoute?.routeOptions()?.coordinates() ?: emptyList()
+        )
+        return ActiveGuidanceOptions(mode, geometry, coordinates)
     }
 
     private fun mapToActiveGuidanceMode(profile: String?): ActiveGuidanceMode {
@@ -41,5 +46,16 @@ internal object ActiveGuidanceOptionsMapper {
             else ->
                 ActiveGuidanceGeometryEncoding.KPOLYLINE6
         }
+    }
+
+    private fun mapToActiveGuidanceCoordinates(coordinates: List<Point>): List<Waypoint> {
+        val waypoints = mutableListOf<Waypoint>()
+        for (coordinate in coordinates) {
+            // TODO: Hard-coding isSilent to false, needs to be updated in https://github.com/mapbox/mapbox-navigation-android/pull/3581
+            //   Noting that origin and destination must be non-silent i.e. isSilent === false
+            val waypoint = Waypoint(Point.fromJson(coordinate.toJson()), false)
+            waypoints.add(waypoint)
+        }
+        return waypoints.toList()
     }
 }
