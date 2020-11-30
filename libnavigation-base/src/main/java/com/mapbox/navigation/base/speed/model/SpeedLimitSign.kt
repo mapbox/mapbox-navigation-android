08f8af3fc278b1ef52939181cdd522277e3e0873
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2020-11-30 12:39:25 -0800

    add speed limit to map matcher

---
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/speed/model/SpeedLimitSign.kt
@@ -0,0 +1,16 @@
+package com.mapbox.navigation.base.speed.model
+
+/**
+ * Defines Speed Limit Sign
+ */
+enum class SpeedLimitSign {
+    /**
+     * Speed limit sign that follows MUTCD conventions
+     */
+    MUTCD,
+
+    /**
+     * Speed limit sign that follows VIENNA conventions
+     */
+    VIENNA
+}
---
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/speed/model/SpeedLimitUnit.kt
@@ -0,0 +1,16 @@
+package com.mapbox.navigation.base.speed.model
+
+/**
+ * Defines Speed Limit Unit
+ */
+enum class SpeedLimitUnit {
+    /**
+     * Speed limit in kilometers per hour
+     */
+    KILOMETRES_PER_HOUR,
+
+    /**
+     * Speed limit in miles per hour
+     */
+    MILES_PER_HOUR
+}
