78720d723471a46a2013bb414740dd6bf0f83d9b
Łukasz Paczos <lukasz.paczos@mapbox.com>
2021-01-06 13:24:56 +0100

    refactor banner instructions test

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/ui/navigation_view/BannerInstructionsTest.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/ui/navigation_view/BannerInstructionsTest.kt
@@ -7,6 +7,7 @@ import com.mapbox.navigation.core.internal.formatter.MapboxDistanceFormatter
 import com.mapbox.navigation.core.trip.session.RouteProgressObserver
 import com.mapbox.navigation.instrumentation_tests.R
 import com.mapbox.navigation.instrumentation_tests.utils.idling.BannerInstructionsIdlingResource
+import com.mapbox.navigation.instrumentation_tests.utils.runOnMainSync
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions
 import kotlinx.android.synthetic.main.activity_basic_navigation_view.*
 import org.junit.Assert.assertEquals
@@ -16,23 +17,26 @@ class BannerInstructionsTest : SimpleNavigationViewTest() {
 
     @Test
     fun banner_text_distance_displayed_and_updated() {
-        mockRoute.bannerInstructions.forEach {
-            val distanceView = activity.navigationView.findViewById<TextView>(R.id.stepDistanceText)
-            val formatter = MapboxDistanceFormatter.Builder(activity).build()
-            val routeProgressObserver = object : RouteProgressObserver {
-                override fun onRouteProgressChanged(routeProgress: RouteProgress) {
-                    val text = formatter.formatDistance(
-                        routeProgress
-                            .currentLegProgress!!
-                            .currentStepProgress!!
-                            .distanceRemaining
-                            .toDouble()
-                    ).toString()
-                    assertEquals(text, (distanceView.text as SpannedString).toString())
-                }
+        val distanceView = activity.navigationView.findViewById<TextView>(R.id.stepDistanceText)
+        val formatter = MapboxDistanceFormatter.Builder(activity).build()
+        val routeProgressObserver = object : RouteProgressObserver {
+            override fun onRouteProgressChanged(routeProgress: RouteProgress) {
+                val text = formatter.formatDistance(
+                    routeProgress
+                        .currentLegProgress!!
+                        .currentStepProgress!!
+                        .distanceRemaining
+                        .toDouble()
+                ).toString()
+                assertEquals(text, (distanceView.text as SpannedString).toString())
             }
-            mapboxNavigation.registerRouteProgressObserver(routeProgressObserver)
+        }
 
+        runOnMainSync {
+            mapboxNavigation.registerRouteProgressObserver(routeProgressObserver)
+        }
+
+        mockRoute.bannerInstructions.forEach {
             val bannerInstructionsIdlingResource = BannerInstructionsIdlingResource(
                 mapboxNavigation,
                 it
@@ -48,8 +52,11 @@ class BannerInstructionsTest : SimpleNavigationViewTest() {
                 BaristaVisibilityAssertions.assertContains(R.id.subStepText, this.text())
             }?.run { BaristaVisibilityAssertions.assertNotDisplayed(R.id.subStepLayout) }
 
-            mapboxNavigation.unregisterRouteProgressObserver(routeProgressObserver)
             bannerInstructionsIdlingResource.unregister()
+        }
+
+        runOnMainSync {
+            mapboxNavigation.unregisterRouteProgressObserver(routeProgressObserver)
         }
     }
 }
