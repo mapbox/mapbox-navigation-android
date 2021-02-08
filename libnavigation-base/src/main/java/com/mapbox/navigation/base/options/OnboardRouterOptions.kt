506731548fce51663756285acd73831e2787cccd
Yury Kanetski <yury.kanetski@mapbox.com>
2021-02-08 16:14:16 +0300

    - NavNative: -> upgraded to v.0.0.31; - set up auto-version tiles choosing.

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/options/OnboardRouterOptions.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/options/OnboardRouterOptions.kt
@@ -7,13 +7,21 @@ import java.net.URI
  * This allows the navigator to map-match your location onto the road network without a route.
  *
  * @param tilesUri tiles endpoint
- * @param tilesVersion version of tiles
+ * @param tilesVersion version of tiles. If not specified (empty), latest version is used
  * @param filePath used for storing road network tiles
+ * @param keepOlderTilesVersions true if older version tiles must be kept, false if not. Note
+ * that this only works if [tilesVersion] is empty
+ * @param minDaysBetweenServerAndLocalTilesVersion is the minimum time in days between local version of tiles
+ * and latest on the server to consider using the latest version of routing tiles from the server.
+ * **As updating tiles frequently consumes considerably energy and bandwidth**.
+ * Note that this only works if [tilesVersion] is empty.
  */
 class OnboardRouterOptions private constructor(
     val tilesUri: URI,
     val tilesVersion: String,
-    val filePath: String?
+    val filePath: String?,
+    val keepOlderTilesVersions: Boolean,
+    val minDaysBetweenServerAndLocalTilesVersion: Int
 ) {
     /**
      * @return the builder that created the [OnboardRouterOptions]
@@ -22,6 +30,8 @@ class OnboardRouterOptions private constructor(
         tilesUri(tilesUri)
         tilesVersion(tilesVersion)
         filePath(filePath)
+        keepOlderTilesVersions(keepOlderTilesVersions)
+        minDaysBetweenServerAndLocalTilesVersion(minDaysBetweenServerAndLocalTilesVersion)
     }
 
     /**
@@ -36,6 +46,10 @@ class OnboardRouterOptions private constructor(
         if (tilesUri != other.tilesUri) return false
         if (tilesVersion != other.tilesVersion) return false
         if (filePath != other.filePath) return false
+        if (keepOlderTilesVersions != other.keepOlderTilesVersions) return false
+        if (minDaysBetweenServerAndLocalTilesVersion
+            != other.minDaysBetweenServerAndLocalTilesVersion
+        ) return false
 
         return true
     }
@@ -47,6 +61,8 @@ class OnboardRouterOptions private constructor(
         var result = tilesUri.hashCode()
         result = 31 * result + tilesVersion.hashCode()
         result = 31 * result + (filePath?.hashCode() ?: 0)
+        result = 31 * result + keepOlderTilesVersions.hashCode()
+        result = 31 * result + minDaysBetweenServerAndLocalTilesVersion.hashCode()
         return result
     }
 
@@ -55,7 +71,11 @@ class OnboardRouterOptions private constructor(
      */
     override fun toString(): String {
         return "OnboardRouterOptions(" +
-            "tilesUri=$tilesUri, tilesVersion='$tilesVersion', filePath=$filePath" +
+            "tilesUri=$tilesUri, " +
+            "tilesVersion='$tilesVersion', " +
+            "filePath=$filePath, " +
+            "keepOlderTilesVersions=$keepOlderTilesVersions, " +
+            "minDaysBetweenServerAndLocalTilesVersion=$minDaysBetweenServerAndLocalTilesVersion" +
             ")"
     }
 
@@ -65,8 +85,10 @@ class OnboardRouterOptions private constructor(
      */
     class Builder {
         private var tilesUri: URI = URI("https://api.mapbox.com")
-        private var tilesVersion: String = "2021_01_24-03_00_00"
+        private var tilesVersion: String = ""
         private var filePath: String? = null
+        private var keepOlderTilesVersions: Boolean = false
+        private var minDaysBetweenServerAndLocalTilesVersion: Int = 56 // 8 weeks
 
         /**
          * Override the routing tiles endpoint with a [URI]
@@ -76,6 +98,9 @@ class OnboardRouterOptions private constructor(
 
         /**
          * Override the routing tiles version.
+         * If not specified (empty), latest tiles version is used.
+         *
+         * Default value is **empty**.
          */
         fun tilesVersion(version: String): Builder =
             apply { this.tilesVersion = version }
@@ -87,13 +112,49 @@ class OnboardRouterOptions private constructor(
             apply { this.filePath = filePath }
 
         /**
+         * Override flag to keep/remove older tiles versions. Note that this only works if tiles
+         * version is empty.
+         *
+         * Default value is **false**.
+         *
+         * @see [tilesVersion]
+         */
+        fun keepOlderTilesVersions(keepOlderTilesVersions: Boolean): Builder =
+            apply { this.keepOlderTilesVersions = keepOlderTilesVersions }
+
+        /**
+         * Override minimum time in days between local version of tiles and latest on the server
+         * to consider using the latest version of routing tiles from the server. This only works
+         * if tiles version is empty.
+         *
+         * Note that **updating tiles frequently consumes considerably energy and bandwidth**.
+         *
+         * Default value is **56** (8 weeks).
+         *
+         * @param minDaysBetweenServerAndLocalTilesVersion must be greater than or equal to 0
+         * @see [tilesVersion]
+         */
+        fun minDaysBetweenServerAndLocalTilesVersion(
+            minDaysBetweenServerAndLocalTilesVersion: Int
+        ): Builder =
+            apply {
+                check(minDaysBetweenServerAndLocalTilesVersion >= 0) {
+                    "minDaysBetweenServerAndLocalTilesVersion must be greater than or equal to 0"
+                }
+                this.minDaysBetweenServerAndLocalTilesVersion =
+                    minDaysBetweenServerAndLocalTilesVersion
+            }
+
+        /**
          * Build the [OnboardRouterOptions]
          */
         fun build(): OnboardRouterOptions {
             return OnboardRouterOptions(
                 tilesUri = tilesUri,
                 tilesVersion = tilesVersion,
-                filePath = filePath
+                filePath = filePath,
+                keepOlderTilesVersions = keepOlderTilesVersions,
+                minDaysBetweenServerAndLocalTilesVersion = minDaysBetweenServerAndLocalTilesVersion
             )
         }
     }
