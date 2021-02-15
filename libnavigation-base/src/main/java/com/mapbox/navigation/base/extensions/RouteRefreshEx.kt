14171d68e670a2c4ba5e0fb95d228a06051f1adb
Łukasz Paczos <lukasz.paczos@mapbox.com>
2021-02-15 14:08:35 +0100

    print clear errors when route refresh fails and update docs

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/extensions/RouteRefreshEx.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/extensions/RouteRefreshEx.kt
@@ -1,4 +1,6 @@
-package com.mapbox.navigation.base.internal.extensions
+@file:JvmName("DirectionsRefreshEx")
+
+package com.mapbox.navigation.base.extensions
 
 import com.mapbox.api.directions.v5.DirectionsCriteria
 import com.mapbox.api.directions.v5.models.RouteOptions
@@ -6,10 +8,20 @@ import com.mapbox.api.directions.v5.models.RouteOptions
 /**
  * Indicates whether the route options supports route refresh.
  *
+ * To qualify for the route refresh feature, the [RouteOptions] need to include:
+ * - [DirectionsCriteria.PROFILE_DRIVING_TRAFFIC]
+ * - [DirectionsCriteria.OVERVIEW_FULL]
+ * and one of:
+ * - [DirectionsCriteria.ANNOTATION_CONGESTION]
+ * - [DirectionsCriteria.ANNOTATION_MAXSPEED]
+ * - [DirectionsCriteria.ANNOTATION_SPEED]
+ * - [DirectionsCriteria.ANNOTATION_DURATION]
+ * - [DirectionsCriteria.ANNOTATION_DISTANCE]
+ *
  * @receiver RouteOptions
  * @return Boolean
  */
-fun RouteOptions?.supportsRefresh(): Boolean {
+fun RouteOptions?.supportsRouteRefresh(): Boolean {
     if (this == null) {
         return false
     }
