119628dfd2d4a3964605996795398f379269502c
Łukasz Paczos <lukasz.paczos@mapbox.com>
2021-02-15 16:42:41 +0100

    wip

---
+++ libnavui-base/src/main/java/com/mapbox/navigation/ui/base/Expected.kt
@@ -0,0 +1,5 @@
+package com.mapbox.navigation.ui.base
+
+sealed class Expected<out V, out E>
+data class Success<out V>(val value: V) : Expected<V, Nothing>()
+data class Failure<out E>(val error: E) : Expected<Nothing, E>()
