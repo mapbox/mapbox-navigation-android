29c318a6d716a5ddd25c515d82829f67c0e84426
Seth Bourget <seth@cafesilencio.net>
2021-03-03 09:24:19 -0800

    QA testing module and app for manual sanity testing of specific use cases.

---
+++ qa-test-app/src/main/java/com/mapbox/navigation/qa_test_app/domain/Constants.kt
@@ -0,0 +1,3 @@
+package com.mapbox.navigation.qa_test_app.domain
+
+const val CATEGORY_BUNDLE_KEY = "testActivitiesCategory"
---
+++ qa-test-app/src/main/java/com/mapbox/navigation/qa_test_app/view/AlternativeRouteActivity.kt
@@ -0,0 +1,275 @@
+package com.mapbox.navigation.qa_test_app.view
+
+import android.annotation.SuppressLint
+import android.location.Location
+import android.os.Build
+import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.location.LocationEngineCallback
+import com.mapbox.android.core.location.LocationEngineResult
+import com.mapbox.android.gestures.Utils
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.core.constants.Constants
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.EdgeInsets
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.animation.CameraAnimationsPlugin
+import com.mapbox.maps.plugin.animation.MapAnimationOptions
+import com.mapbox.maps.plugin.animation.getCameraAnimationsPlugin
+import com.mapbox.maps.plugin.gestures.OnMapClickListener
+import com.mapbox.maps.plugin.gestures.OnMapLongClickListener
+import com.mapbox.maps.plugin.gestures.getGesturesPlugin
+import com.mapbox.maps.plugin.locationcomponent.getLocationComponentPlugin
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.directions.session.RoutesObserver
+import com.mapbox.navigation.core.replay.MapboxReplayer
+import com.mapbox.navigation.core.replay.ReplayLocationEngine
+import com.mapbox.navigation.core.replay.route.ReplayProgressObserver
+import com.mapbox.navigation.core.replay.route.ReplayRouteMapper
+import com.mapbox.navigation.core.trip.session.LocationObserver
+import com.mapbox.navigation.qa_test_app.databinding.AlternativeRouteActivityLayoutBinding
+import com.mapbox.navigation.ui.maps.internal.route.line.MapboxRouteLineApiExtensions.findClosestRoute
+import com.mapbox.navigation.ui.maps.internal.route.line.MapboxRouteLineApiExtensions.setRoutes
+import com.mapbox.navigation.ui.maps.internal.route.line.MapboxRouteLineApiExtensions.updateToPrimaryRoute
+import com.mapbox.navigation.qa_test_app.utils.Utils.getMapboxAccessToken
+import com.mapbox.navigation.ui.base.model.Expected
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
+import com.mapbox.navigation.ui.maps.route.line.api.MapboxRouteLineApi
+import com.mapbox.navigation.ui.maps.route.line.api.MapboxRouteLineView
+import com.mapbox.navigation.ui.maps.route.line.model.MapboxRouteLineOptions
+import com.mapbox.navigation.ui.maps.route.line.model.RouteLine
+import com.mapbox.navigation.ui.maps.route.line.model.RouteLineResources
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+class AlternativeRouteActivity : AppCompatActivity(), OnMapLongClickListener {
+
+    private val routeClickPadding = Utils.dpToPx(30f)
+    private val navigationLocationProvider = NavigationLocationProvider()
+    private val replayRouteMapper = ReplayRouteMapper()
+    private val mapboxReplayer = MapboxReplayer()
+    private val binding: AlternativeRouteActivityLayoutBinding by lazy {
+        AlternativeRouteActivityLayoutBinding.inflate(layoutInflater)
+    }
+
+    private val mapCamera: CameraAnimationsPlugin by lazy {
+        binding.mapView.getCameraAnimationsPlugin()
+    }
+
+    private val mapboxNavigation: MapboxNavigation by lazy {
+        MapboxNavigation(
+            NavigationOptions.Builder(this)
+                .accessToken(getMapboxAccessToken(this))
+                .locationEngine(ReplayLocationEngine(mapboxReplayer))
+                .build()
+        )
+    }
+
+    private val routeLineResources: RouteLineResources by lazy {
+        RouteLineResources.Builder().build()
+    }
+
+    private val options: MapboxRouteLineOptions by lazy {
+        MapboxRouteLineOptions.Builder(this)
+            .withRouteLineResources(routeLineResources)
+            .withRouteLineBelowLayerId("road-label")
+            .build()
+    }
+
+    private val routeLineView by lazy {
+        MapboxRouteLineView(options)
+    }
+
+    private val routeLineApi: MapboxRouteLineApi by lazy {
+        MapboxRouteLineApi(options)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initNavigation()
+        initStyle()
+        initListeners()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapboxNavigation.unregisterRouteProgressObserver(replayProgressObserver)
+        mapboxNavigation.unregisterLocationObserver(locationObserver)
+        mapboxNavigation.unregisterRoutesObserver(routesObserver)
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    private fun initNavigation() {
+        binding.mapView.getLocationComponentPlugin().apply {
+            setLocationProvider(navigationLocationProvider)
+            enabled = true
+        }
+        mapboxNavigation.registerLocationObserver(locationObserver)
+        mapboxNavigation.registerRouteProgressObserver(replayProgressObserver)
+        mapboxNavigation.registerRoutesObserver(routesObserver)
+        mapboxReplayer.pushRealLocation(this, 0.0)
+        mapboxReplayer.playbackSpeed(1.5)
+        mapboxReplayer.play()
+    }
+
+    private val locationObserver: LocationObserver = object : LocationObserver {
+        override fun onRawLocationChanged(rawLocation: Location) {
+            Timber.d("raw location %s", rawLocation.toString())
+        }
+
+        override fun onEnhancedLocationChanged(
+            enhancedLocation: Location,
+            keyPoints: List<Location>
+        ) {
+            navigationLocationProvider.changePosition(enhancedLocation, keyPoints, null, null)
+            updateCamera(enhancedLocation)
+        }
+    }
+
+    private fun updateCamera(location: Location) {
+        val mapAnimationOptionsBuilder = MapAnimationOptions.Builder()
+        mapAnimationOptionsBuilder.duration(1500L)
+        mapCamera.easeTo(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(location.longitude, location.latitude))
+                .bearing(location.bearing.toDouble())
+                .zoom(15.0)
+                .padding(EdgeInsets(1000.0, 0.0, 0.0, 0.0))
+                .build(),
+            mapAnimationOptionsBuilder.build()
+        )
+    }
+
+    private val replayProgressObserver = ReplayProgressObserver(mapboxReplayer)
+
+    @SuppressLint("MissingPermission")
+    private fun initStyle() {
+        binding.mapView.getMapboxMap().loadStyleUri(
+            Style.MAPBOX_STREETS
+        ) {
+            mapboxNavigation.navigationOptions.locationEngine.getLastLocation(object :
+                LocationEngineCallback<LocationEngineResult> {
+                override fun onSuccess(result: LocationEngineResult) {
+                    result.lastLocation?.let {
+                        locationObserver.onEnhancedLocationChanged(it, listOf())
+                    }
+                }
+
+                override fun onFailure(exception: Exception) {}
+            })
+            binding.mapView.getGesturesPlugin().addOnMapLongClickListener(this)
+        }
+    }
+
+    override fun onMapLongClick(point: Point): Boolean {
+        vibrate()
+
+        val currentLocation = navigationLocationProvider.lastLocation
+        if (currentLocation != null) {
+            val originPoint = Point.fromLngLat(
+                currentLocation.longitude,
+                currentLocation.latitude
+            )
+            findRoute(originPoint, point)
+        }
+        return false
+    }
+
+    private fun findRoute(origin: Point?, destination: Point?) {
+        val routeOptions = RouteOptions.builder()
+            .baseUrl(Constants.BASE_API_URL)
+            .user(Constants.MAPBOX_USER)
+            .profile(DirectionsCriteria.PROFILE_DRIVING_TRAFFIC)
+            .geometries(DirectionsCriteria.GEOMETRY_POLYLINE6)
+            .requestUuid("")
+            .accessToken(getMapboxAccessToken(this))
+            .coordinates(listOf(origin, destination))
+            .alternatives(true)
+            .build()
+        mapboxNavigation.requestRoutes(routeOptions)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun vibrate() {
+        val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(100L, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(100L)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initListeners() {
+        binding.startNavigation.setOnClickListener {
+            mapboxNavigation.startTripSession()
+            binding.startNavigation.visibility = View.GONE
+            startSimulation(mapboxNavigation.getRoutes()[0])
+        }
+
+        binding.mapView.getGesturesPlugin().addOnMapClickListener(mapClickListener)
+    }
+
+    private fun startSimulation(route: DirectionsRoute) {
+        mapboxReplayer.stop()
+        mapboxReplayer.clearEvents()
+        val replayData = replayRouteMapper.mapDirectionsRouteGeometry(route)
+        mapboxReplayer.pushEvents(replayData)
+        mapboxReplayer.seekTo(replayData[0])
+        mapboxReplayer.play()
+    }
+
+    private val routesObserver = object : RoutesObserver {
+        override fun onRoutesChanged(routes: List<DirectionsRoute>) {
+            val routelines = routes.map { RouteLine(it, null) }
+            CoroutineScope(Dispatchers.Main).launch {
+                routeLineApi.setRoutes(routelines).apply {
+                    routeLineView.renderRouteDrawData(binding.mapView.getMapboxMap().getStyle()!!, this)
+                }
+            }
+        }
+    }
+
+    private val mapClickListener = OnMapClickListener {
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = routeLineApi.findClosestRoute(
+                it,
+                binding.mapView.getMapboxMap(),
+                routeClickPadding
+            )
+
+            val routeFound = (result as Expected.Success).value.route
+            if (routeFound != routeLineApi.getPrimaryRoute()) {
+                routeLineApi.updateToPrimaryRoute(routeFound)
+                mapboxNavigation.setRoutes(routeLineApi.getRoutes())
+            }
+        }
+        false
+    }
+}
