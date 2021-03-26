c28a6476957c67c22fb364d05b95631c6139aedb
Yury Kanetski <yury.kanetski@mapbox.com>
2021-03-26 18:38:30 +0300

    fix banner instruction and route progress mismatch when in multi-leg scenarios

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/trip/session/BannerInstructionEvent.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/trip/session/BannerInstructionEvent.kt
@@ -2,18 +2,27 @@ package com.mapbox.navigation.core.trip.session
 
 import com.mapbox.api.directions.v5.models.BannerInstructions
 import com.mapbox.navigation.base.trip.model.RouteProgress
-import com.mapbox.navigation.utils.internal.ifNonNull
 
 internal class BannerInstructionEvent {
 
     var bannerInstructions: BannerInstructions? = null
         private set
 
+    var latestBannerInstructions: BannerInstructions? = null
+        private set
+
     fun isOccurring(routeProgress: RouteProgress): Boolean = updateCurrentBanner(routeProgress)
 
-    private fun updateCurrentBanner(routeProgress: RouteProgress): Boolean =
-        ifNonNull(routeProgress.bannerInstructions) {
-            bannerInstructions = it
-            true
-        } ?: false
+    fun invalidateLatestBannerInstructions() {
+        latestBannerInstructions = null
+    }
+
+    private fun updateCurrentBanner(routeProgress: RouteProgress): Boolean {
+        bannerInstructions = routeProgress.bannerInstructions
+        if (bannerInstructions != null && bannerInstructions!! != latestBannerInstructions) {
+            latestBannerInstructions = bannerInstructions
+            return true
+        }
+        return false
+    }
 }
