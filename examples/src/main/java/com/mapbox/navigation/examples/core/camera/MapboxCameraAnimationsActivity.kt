27ac0923cc2a5fd458890da8e0452e62e5ffb6f4
Łukasz Paczos <lukasz.paczos@mapbox.com>
2021-04-28 14:46:58 +0200

    removes the hardcoded layer reference from the MapboxNavigationViewportDataSourceDebugger
    This changes the default behavior to position the layer on top of the stack, unless a reference ID is provided.

--- examples/src/main/java/com/mapbox/navigation/examples/core/camera/MapboxCameraAnimationsActivity.kt
+++ examples/src/main/java/com/mapbox/navigation/examples/core/camera/MapboxCameraAnimationsActivity.kt
@@ -258,8 +258,9 @@ class MapboxCameraAnimationsActivity :
         initNavigation()
 
         val debugger = MapboxNavigationViewportDataSourceDebugger(
-            this,
-            binding.mapView
+            context = this,
+            mapView = binding.mapView,
+            layerAbove = "road-label"
         ).apply {
             enabled = true
         }
