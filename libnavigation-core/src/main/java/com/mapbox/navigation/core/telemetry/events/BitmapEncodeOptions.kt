c2beefe8612f6cf48d0eaedfcfff304bc76fa3b0
Vitaly Zayankovsky <vitaliy.zayankovskiy@mapbox.com>
2021-10-05 13:57:23 +0300

    add utilities to capture and encode screenshots

---
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/telemetry/events/BitmapEncodeOptions.kt
@@ -0,0 +1,91 @@
+package com.mapbox.navigation.core.telemetry.events
+
+import androidx.annotation.IntRange
+
+private const val DEFAULT_BITMAP_ENCODE_WIDTH = 250
+private const val DEFAULT_BITMAP_ENCODE_COMPRESS_QUALITY = 20
+
+/**
+ * Contains options for encoding screenshots.
+ *
+ * @param width maximum width of an encoded screenshot, default is 250.
+ * @param compressQuality quality of an encoded screenshot, ranging from 0 to 100, default is 20.
+ */
+class BitmapEncodeOptions private constructor(val width: Int, val compressQuality: Int) {
+
+    /**
+     * @return builder matching the one used to create this instance
+     */
+    fun toBuilder(): Builder {
+        return Builder()
+            .compressQuality(compressQuality)
+            .width(width)
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BitmapEncodeOptions
+
+        if (width != other.width) return false
+        if (compressQuality != other.compressQuality) return false
+
+        return true
+    }
+
+    /**
+     * Returns a hash code value for the object.
+     */
+    override fun hashCode(): Int {
+        var result = width
+        result = 31 * result + compressQuality
+        return result
+    }
+
+    /**
+     * Returns a string representation of the object.
+     */
+    override fun toString(): String {
+        return "BitmapEncodeOptions(width=$width, compressQuality=$compressQuality)"
+    }
+
+    /**
+     * Build your [BitmapEncodeOptions].
+     */
+    class Builder {
+
+        private var width = DEFAULT_BITMAP_ENCODE_WIDTH
+        private var compressQuality = DEFAULT_BITMAP_ENCODE_COMPRESS_QUALITY
+
+        /**
+         * Updates maximum width of an encoded screenshot, default is 250.
+         *
+         * @throws IllegalStateException if [width] is less than 1.
+         */
+        fun width(@IntRange(from = 1) width: Int) = apply {
+            require(width >= 1) { "width must be >= 1" }
+            this.width = width
+        }
+
+        /**
+         * Updates quality of an encoded screenshot, default is 20.
+         *
+         * @throws IllegalStateException if [width] is outside [0, 100].
+         */
+        fun compressQuality(@IntRange(from = 0, to = 100) compressQuality: Int) = apply {
+            require(compressQuality in 0..100) { "compressQuality must be in 0..100" }
+            this.compressQuality = compressQuality
+        }
+
+        /**
+         * Build the object.
+         */
+        fun build(): BitmapEncodeOptions {
+            return BitmapEncodeOptions(width, compressQuality)
+        }
+    }
+}
---
+++ libnavui-maps/src/main/java/com/mapbox/navigation/ui/maps/util/ViewUtils.kt
@@ -0,0 +1,83 @@
+package com.mapbox.navigation.ui.maps.util
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewGroup
+import com.mapbox.maps.MapView
+
+/**
+ * Provides utilities to capture screenshots of view trees, that may contain one or more [MapView]s.
+ */
+object ViewUtils {
+
+    /**
+     * Captures a view tree that contains a given [View] and returns the obtained [Bitmap].
+     *
+     * @param callback invoked when a screenshot of the view tree is ready
+     */
+    @JvmStatic
+    fun View.capture(callback: OnViewScreenshotReady) {
+        val root = rootView
+        val maps = collectMaps(root)
+        collectSnapshots(maps) { snapshots ->
+            val foregrounds = ArrayList<Drawable?>(snapshots.size)
+            for ((map, snapshot) in snapshots) {
+                foregrounds.add(map.foreground)
+                map.foreground = BitmapDrawable(map.resources, snapshot)
+            }
+            root.isDrawingCacheEnabled = true
+            val bitmap = Bitmap.createBitmap(root.drawingCache)
+            root.isDrawingCacheEnabled = false
+            snapshots.forEachIndexed { index, (map, _) ->
+                map.foreground = foregrounds[index]
+            }
+            callback.onViewCaptureReady(bitmap)
+        }
+    }
+
+    // finds all map views in a view tree using BFS approach
+    private fun collectMaps(root: View): List<MapView> {
+        val queue = ArrayDeque<View>()
+        val maps = arrayListOf<MapView>()
+        queue.add(root)
+        while (queue.isNotEmpty()) {
+            when (val head = queue.removeFirst()) {
+                is MapView -> {
+                    maps.add(head)
+                }
+                is ViewGroup -> {
+                    repeat(head.childCount) { index ->
+                        queue.add(head.getChildAt(index))
+                    }
+                }
+            }
+        }
+        return maps
+    }
+
+    // requests snapshots of all map views and waits for every callback
+    private inline fun collectSnapshots(
+        maps: List<MapView>,
+        crossinline onDone: (List<Pair<MapView, Bitmap>>) -> Unit,
+    ) {
+        var snapshotsLeft = maps.size
+        val snapshots = arrayListOf<Pair<MapView, Bitmap>>()
+        val mainHandler = Handler(Looper.getMainLooper())
+        for (map in maps) {
+            map.snapshot { snapshot ->
+                mainHandler.post {
+                    if (snapshot != null) {
+                        snapshots.add(map to snapshot)
+                    }
+                    if (--snapshotsLeft == 0) {
+                        onDone(snapshots)
+                    }
+                }
+            }
+        }
+    }
+}
