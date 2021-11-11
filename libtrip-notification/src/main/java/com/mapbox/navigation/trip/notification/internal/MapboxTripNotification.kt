0252b1d8aee38cecfb0c10411b77260e429df6eb
Kyle Madsen <kyle.mdsn@gmail.com>
2021-11-11 07:49:03 -0800

    Support target 31

--- libtrip-notification/src/main/java/com/mapbox/navigation/trip/notification/internal/MapboxTripNotification.kt
+++ libtrip-notification/src/main/java/com/mapbox/navigation/trip/notification/internal/MapboxTripNotification.kt
@@ -1,6 +1,5 @@
 package com.mapbox.navigation.trip.notification.internal
 
-import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -212,12 +211,11 @@ class MapboxTripNotification constructor(
      * @param applicationContext the application's [Context]
      * @return [PendingIntent] to opening application
      */
-    @SuppressLint("UnspecifiedImmutableFlag")
     private fun createPendingOpenIntent(applicationContext: Context): PendingIntent? {
         val pm = applicationContext.packageManager
         val intent = pm.getLaunchIntentForPackage(applicationContext.packageName) ?: return null
         intent.setPackage(null)
-        return PendingIntent.getActivity(applicationContext, 0, intent, 0)
+        return PendingIntent.getActivity(applicationContext, 0, intent, immutableFlag)
     }
 
     /**
@@ -227,10 +225,17 @@ class MapboxTripNotification constructor(
      * @param applicationContext the application's [Context]
      * @return [PendingIntent] for stopping trip session
      */
-    @SuppressLint("UnspecifiedImmutableFlag")
     private fun createPendingCloseIntent(applicationContext: Context): PendingIntent? {
         val endNavigationBtn = Intent(END_NAVIGATION_ACTION)
-        return PendingIntent.getBroadcast(applicationContext, 0, endNavigationBtn, 0)
+        return PendingIntent.getBroadcast(applicationContext, 0, endNavigationBtn, immutableFlag)
+    }
+
+    private val immutableFlag = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_MUTABLE
+        }
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M -> PendingIntent.FLAG_IMMUTABLE
+        else -> 0
     }
 
     private fun createNotificationChannel() {
