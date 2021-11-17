446dd624cb1be25929a4b4201130e1f27a4d9e4b
Kyle Madsen <kyle.mdsn@gmail.com>
2021-11-17 09:20:35 -0800

    Fix route alternative thread issue

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/routealternatives/RouteAlternativesController.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/routealternatives/RouteAlternativesController.kt
@@ -1,22 +1,29 @@
 package com.mapbox.navigation.core.routealternatives
 
-import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.base.common.logger.model.Message
 import com.mapbox.base.common.logger.model.Tag
 import com.mapbox.navigation.base.internal.utils.parseDirectionsResponse
 import com.mapbox.navigation.base.route.RouteAlternativesOptions
 import com.mapbox.navigation.base.route.RouterOrigin
+import com.mapbox.navigation.base.trip.model.RouteProgress
 import com.mapbox.navigation.core.trip.session.TripSession
 import com.mapbox.navigation.navigator.internal.MapboxNativeNavigator
+import com.mapbox.navigation.utils.internal.ThreadController
 import com.mapbox.navigation.utils.internal.logI
+import com.mapbox.navigator.RouteAlternative
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.CopyOnWriteArraySet
 
 internal class RouteAlternativesController constructor(
     private val options: RouteAlternativesOptions,
     private val navigator: MapboxNativeNavigator,
-    private val tripSession: TripSession
+    private val tripSession: TripSession,
+    private val threadController: ThreadController
 ) {
+
+    private val mainJobControl by lazy { threadController.getMainScopeAndRootJob() }
 
     private val nativeRouteAlternativesController = navigator.createRouteAlternativesController()
         .apply {
@@ -44,6 +51,7 @@ internal class RouteAlternativesController constructor(
         observers.remove(routeAlternativesObserver)
         if (observers.isEmpty()) {
             nativeRouteAlternativesController.removeObserver(nativeObserver)
+            mainJobControl.job.cancelChildren()
         }
     }
 
@@ -58,16 +66,28 @@ internal class RouteAlternativesController constructor(
 
     private val nativeObserver = object : com.mapbox.navigator.RouteAlternativesObserver {
         override fun onRouteAlternativesChanged(
-            routeAlternatives: List<com.mapbox.navigator.RouteAlternative>
+            routeAlternatives: List<RouteAlternative>
         ): List<Int> {
             val routeProgress = tripSession.getRouteProgress()
                 ?: return emptyList()
 
-            // Map the alternatives from nav-native, add the existing RouteOptions.
+            onRouteAlternativesChanged(routeProgress, routeAlternatives)
 
-            // TODO make async
-            val alternatives: List<DirectionsRoute> = runBlocking {
-                routeAlternatives.map { routeAlternative ->
+            // This is supposed to be able to filter alternatives. If we want to provide
+            // a mechanism to let downstream developers edit the routes - we should remove
+            // the call to directionsSession.setRoutes
+            return emptyList()
+        }
+    }
+
+    private fun onRouteAlternativesChanged(
+        routeProgress: RouteProgress,
+        routeAlternatives: List<RouteAlternative>
+    ) {
+        // Map the alternatives from nav-native, add the existing RouteOptions.
+        val alternatives = runBlocking {
+            routeAlternatives
+                .map { routeAlternative ->
                     parseDirectionsResponse(
                         routeAlternative.route,
                         routeProgress.route.routeOptions()
@@ -75,19 +95,15 @@ internal class RouteAlternativesController constructor(
                         logI(TAG, Message("Response metadata: $it"))
                     }.first()
                 }
-            }
+        }
 
+        mainJobControl.scope.launch {
             // Notify the listeners.
             // TODO https://github.com/mapbox/mapbox-navigation-native/issues/4409
             // There is no way to determine if the route was Onboard or Offboard
             observers.forEach {
                 it.onRouteAlternatives(routeProgress, alternatives, RouterOrigin.Onboard)
             }
-
-            // This is supposed to be able to filter alternatives. If we want to provide
-            // a mechanism to let downstream developers edit the routes - we should remove
-            // the call to directionsSession.setRoutes
-            return emptyList()
         }
     }
 
