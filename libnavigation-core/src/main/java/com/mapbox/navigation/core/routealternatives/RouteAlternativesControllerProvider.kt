ac8f6a945d55e7f18f83a5787bfa000c24a78265
Kyle Madsen <kyle.mdsn@gmail.com>
2021-11-17 09:20:35 -0800

    Fix route alternative thread issue

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/routealternatives/RouteAlternativesControllerProvider.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/routealternatives/RouteAlternativesControllerProvider.kt
@@ -3,16 +3,19 @@ package com.mapbox.navigation.core.routealternatives
 import com.mapbox.navigation.base.route.RouteAlternativesOptions
 import com.mapbox.navigation.core.trip.session.TripSession
 import com.mapbox.navigation.navigator.internal.MapboxNativeNavigator
+import com.mapbox.navigation.utils.internal.ThreadController
 
 internal object RouteAlternativesControllerProvider {
 
     fun create(
         options: RouteAlternativesOptions,
         navigator: MapboxNativeNavigator,
-        tripSession: TripSession
+        tripSession: TripSession,
+        threadController: ThreadController
     ) = RouteAlternativesController(
         options,
         navigator,
-        tripSession
+        tripSession,
+        threadController
     )
 }
