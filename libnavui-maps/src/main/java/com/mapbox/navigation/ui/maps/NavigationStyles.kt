d280b14b7e43addaa6f4f82e5e833a3be4332090
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2021-11-22 10:20:32 -0800

    add valid dokka docs

--- libnavui-maps/src/main/java/com/mapbox/navigation/ui/maps/NavigationStyles.kt
+++ libnavui-maps/src/main/java/com/mapbox/navigation/ui/maps/NavigationStyles.kt
@@ -5,6 +5,13 @@ package com.mapbox.navigation.ui.maps
  * We recommend using these map styles for an optimized map appearance for navigation use-cases.
  */
 object NavigationStyles {
+    /**
+     * Default navigation style for day mode
+     */
     const val NAVIGATION_DAY_STYLE = "mapbox://styles/mapbox/navigation-day-v1"
+
+    /**
+     * Default navigation sty;e for night mode
+     */
     const val NAVIGATION_NIGHT_STYLE = "mapbox://styles/mapbox/navigation-night-v1"
 }
