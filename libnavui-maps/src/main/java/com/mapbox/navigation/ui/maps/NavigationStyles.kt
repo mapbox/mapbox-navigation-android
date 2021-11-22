90c23562f5791788abcbee04ee59b0552e2fae30
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2021-11-22 09:18:16 -0800

    updated public docs

--- libnavui-maps/src/main/java/com/mapbox/navigation/ui/maps/NavigationStyles.kt
+++ libnavui-maps/src/main/java/com/mapbox/navigation/ui/maps/NavigationStyles.kt
@@ -1,7 +1,8 @@
 package com.mapbox.navigation.ui.maps
 
 /**
- * An object that exposes default navigation styles for day and night mode
+ * An object that links to default navigation styles.
+ * We recommend using these map styles for an optimized map appearance for navigation use-cases.
  */
 object NavigationStyles {
     const val NAVIGATION_DAY_STYLE = "mapbox://styles/mapbox/navigation-day-v1"
