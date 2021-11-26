62ef8949fc7d7970e2b33c80349af74a428a3d36
vadzim_v <vadzim.vysotski@mapbox.com>
2021-11-26 15:32:54 +0300

    removed a workaround in connectivity handler. we don't need it any more.

--- libnavigation-util/src/main/java/com/mapbox/navigation/utils/internal/ConnectivityHandler.kt
+++ libnavigation-util/src/main/java/com/mapbox/navigation/utils/internal/ConnectivityHandler.kt
@@ -1,11 +1,8 @@
 package com.mapbox.navigation.utils.internal
 
-import android.content.Context
-import android.net.ConnectivityManager
 import com.mapbox.base.common.logger.Logger
 import com.mapbox.base.common.logger.model.Message
 import com.mapbox.base.common.logger.model.Tag
-import com.mapbox.common.MapboxSDKCommon.getContext
 import com.mapbox.common.NetworkStatus
 import com.mapbox.common.ReachabilityChanged
 import kotlinx.coroutines.channels.Channel
@@ -15,8 +12,6 @@ class ConnectivityHandler(
     private val logger: Logger,
     private val networkStatusChannel: Channel<Boolean>
 ) : ReachabilityChanged {
-    private val cm =
-        getContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
     fun getNetworkStatusChannel(): ReceiveChannel<Boolean> = networkStatusChannel
 
@@ -29,15 +24,8 @@ class ConnectivityHandler(
             NetworkStatus.REACHABLE_VIA_WI_FI,
             NetworkStatus.REACHABLE_VIA_ETHERNET,
             NetworkStatus.REACHABLE_VIA_WWAN -> {
-                // TODO: Remove workaround when fix from Common is available
-                val activeNetwork = cm.activeNetworkInfo
-                if (activeNetwork == null) {
-                    logger.d(Tag(TAG), Message("NetworkStatus=${NetworkStatus.NOT_REACHABLE}"))
-                    networkStatusChannel.trySend(false).isSuccess
-                } else {
-                    logger.d(Tag(TAG), Message("NetworkStatus=$status"))
-                    networkStatusChannel.trySend(true).isSuccess
-                }
+                logger.d(Tag(TAG), Message("NetworkStatus=$status"))
+                networkStatusChannel.trySend(true).isSuccess
             }
         }
     }
