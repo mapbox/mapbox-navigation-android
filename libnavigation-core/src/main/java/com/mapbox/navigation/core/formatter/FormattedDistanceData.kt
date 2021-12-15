f5d8602282b2114ffde4ee402f1f61667313cda3
Seth Bourget <seth.bourget@mapbox.com>
2021-12-14 16:01:07 -0800

    Separating the functionality of the previous distance formatter to make it more usable by components that need a less opinionated formatter. (#5182)
    Co-authored-by: Seth Bourget <seth@cafesilencio.net>

---
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/formatter/FormattedDistanceData.kt
@@ -0,0 +1,59 @@
+package com.mapbox.navigation.core.formatter
+
+import com.mapbox.navigation.base.formatter.UnitType
+
+/**
+ * Represents a distance that has been formatted and potentially rounded for display purposes.
+ *
+ * @param distance the calculated distance value
+ * @param distanceAsString a rounded string representation of the distance. For example a distance value of 19.3121 might result in a string value of '19'
+ * @param distanceSuffix a suffix that goes with the string value of the distance like 'km' for kilometers, 'ft' for foot/feet, 'mi' for miles etc. depending on the Locale and the UnitType
+ * @param unitType indicates if the values represented are metric or imperial
+ */
+class FormattedDistanceData internal constructor(
+    val distance: Double,
+    val distanceAsString: String,
+    val distanceSuffix: String,
+    val unitType: UnitType
+) {
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FormattedDistanceData
+
+        if (distance != other.distance) return false
+        if (distanceAsString != other.distanceAsString) return false
+        if (distanceSuffix != other.distanceSuffix) return false
+        if (unitType != other.unitType) return false
+
+        return true
+    }
+
+    /**
+     * Returns a hash code value for the object.
+     */
+    override fun hashCode(): Int {
+        var result = distance.hashCode()
+        result = 31 * result + distanceAsString.hashCode()
+        result = 31 * result + distanceSuffix.hashCode()
+        result = 31 * result + unitType.hashCode()
+        return result
+    }
+
+    /**
+     * The toString implementation
+     */
+    override fun toString(): String {
+        return "FormattedDistanceData(" +
+            "distance=$distance, " +
+            "distanceAsString=$distanceAsString, " +
+            "distanceSuffix=$distanceSuffix, " +
+            "unitType=$unitType" +
+            ")"
+    }
+}
--- libnavigation-core/src/main/java/com/mapbox/navigation/core/formatter/MapboxDistanceFormatter.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/formatter/MapboxDistanceFormatter.kt
@@ -1,8 +1,5 @@
 package com.mapbox.navigation.core.formatter
 
-import android.content.Context
-import android.content.res.Configuration
-import android.content.res.Resources
 import android.graphics.Typeface
 import android.text.SpannableString
 import android.text.Spanned
@@ -10,13 +7,6 @@ import android.text.style.RelativeSizeSpan
 import android.text.style.StyleSpan
 import com.mapbox.navigation.base.formatter.DistanceFormatter
 import com.mapbox.navigation.base.formatter.DistanceFormatterOptions
-import com.mapbox.navigation.base.formatter.UnitType
-import com.mapbox.navigation.core.R
-import com.mapbox.turf.TurfConstants
-import com.mapbox.turf.TurfConversion
-import java.text.NumberFormat
-import java.util.Locale
-import kotlin.math.roundToInt
 
 /**
  * Implementation of DistanceFormatter, which can format distances in meters
@@ -28,21 +18,6 @@ class MapboxDistanceFormatter(
     val options: DistanceFormatterOptions
 ) : DistanceFormatter {
 
-    private val smallUnit = when (options.unitType) {
-        UnitType.IMPERIAL -> TurfConstants.UNIT_FEET
-        UnitType.METRIC -> TurfConstants.UNIT_METERS
-    }
-
-    private val largeUnit = when (options.unitType) {
-        UnitType.IMPERIAL -> TurfConstants.UNIT_MILES
-        UnitType.METRIC -> TurfConstants.UNIT_KILOMETERS
-    }
-
-    private companion object {
-        private const val smallDistanceUpperThresholdInMeters = 400.0
-        private const val mediumDistanceUpperThresholdInMeters = 10000.0
-    }
-
     /**
      * Returns a formatted SpannableString with bold and size formatting. I.e., "10 mi", "350 m"
      *
@@ -51,63 +26,15 @@ class MapboxDistanceFormatter(
      * relative size of .65 times the size of the number
      */
     override fun formatDistance(distance: Double): SpannableString {
-        val distanceAndSuffix = when (distance) {
-            !in 0.0..Double.MAX_VALUE -> {
-                formatDistanceAndSuffixForSmallUnit(0.0)
-            }
-            in 0.0..smallDistanceUpperThresholdInMeters -> {
-                formatDistanceAndSuffixForSmallUnit(distance)
-            }
-            in smallDistanceUpperThresholdInMeters..mediumDistanceUpperThresholdInMeters -> {
-                formatDistanceAndSuffixForLargeUnit(distance, 1)
-            }
-            else -> {
-                formatDistanceAndSuffixForLargeUnit(distance, 0)
-            }
-        }
-        return getSpannableDistanceString(distanceAndSuffix)
-    }
-
-    private fun formatDistanceAndSuffixForSmallUnit(distance: Double): Pair<String, String> {
-        val resources = options.applicationContext.resourcesWithLocale(options.locale)
-        val unitStringSuffix = getUnitString(resources, smallUnit)
-
-        if (distance < 0) {
-            return Pair("0", unitStringSuffix)
-        }
-
-        val distanceUnit = TurfConversion.convertLength(
+        return MapboxDistanceUtil.formatDistance(
             distance,
-            TurfConstants.UNIT_METERS,
-            smallUnit
-        )
-
-        val roundedValue = if (options.roundingIncrement > 0) {
-            val roundedDistance = distanceUnit.roundToInt()
-            if (roundedDistance < options.roundingIncrement) {
-                options.roundingIncrement
-            } else {
-                roundedDistance / options.roundingIncrement * options.roundingIncrement
-            }
-        } else {
-            distanceUnit.roundToInt()
-        }.toString()
-
-        return Pair(roundedValue, unitStringSuffix)
-    }
-
-    private fun formatDistanceAndSuffixForLargeUnit(
-        distance: Double,
-        maxFractionDigits: Int
-    ): Pair<String, String> {
-        val resources = options.applicationContext.resourcesWithLocale(options.locale)
-        val unitStringSuffix = getUnitString(resources, largeUnit)
-        val distanceUnit =
-            TurfConversion.convertLength(distance, TurfConstants.UNIT_METERS, largeUnit)
-        val roundedValue = NumberFormat.getNumberInstance(options.locale).also {
-            it.maximumFractionDigits = maxFractionDigits
-        }.format(distanceUnit)
-        return Pair(roundedValue, unitStringSuffix)
+            options.roundingIncrement,
+            options.unitType,
+            options.applicationContext,
+            options.locale
+        ).run {
+            getSpannableDistanceString(Pair(this.distanceAsString, this.distanceSuffix))
+        }
     }
 
     /**
@@ -138,21 +65,5 @@ class MapboxDistanceFormatter(
         )
 
         return spannableString
-    }
-
-    private fun getUnitString(resources: Resources, @TurfConstants.TurfUnitCriteria unit: String) =
-        when (unit) {
-            TurfConstants.UNIT_KILOMETERS -> resources.getString(R.string.mapbox_unit_kilometers)
-            TurfConstants.UNIT_METERS -> resources.getString(R.string.mapbox_unit_meters)
-            TurfConstants.UNIT_MILES -> resources.getString(R.string.mapbox_unit_miles)
-            TurfConstants.UNIT_FEET -> resources.getString(R.string.mapbox_unit_feet)
-            else -> ""
-        }
-
-    private fun Context.resourcesWithLocale(locale: Locale?): Resources {
-        val config = Configuration(this.resources.configuration).also {
-            it.setLocale(locale)
-        }
-        return this.createConfigurationContext(config).resources
     }
 }
