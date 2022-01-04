b1437aa25f9b96ebcd37363346e622898aec71a0
Yury Kanetski <yury.kanetski@mapbox.com>
2022-01-04 23:17:45 +0100

    RouteAlternativesCacheManager: limit cache to 3 portions

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/routealternatives/RouteAlternativesCacheManager.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/routealternatives/RouteAlternativesCacheManager.kt
@@ -1,15 +1,16 @@
 package com.mapbox.navigation.core.routealternatives
 
+import androidx.annotation.VisibleForTesting
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.navigation.core.trip.session.NavigationSession
 import com.mapbox.navigation.core.trip.session.NavigationSessionState
 import com.mapbox.navigation.core.trip.session.NavigationSessionStateObserver
-import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.ConcurrentLinkedQueue
 
 internal class RouteAlternativesCacheManager(
     navigationSession: NavigationSession,
 ) {
-    private val cachedAlternatives = CopyOnWriteArrayList<DirectionsRoute>()
+    private val cachedAlternatives = ConcurrentLinkedQueue<List<DirectionsRoute>>()
 
     private val sessionStateObserver = NavigationSessionStateObserver { state ->
         when (state) {
@@ -21,14 +22,24 @@ internal class RouteAlternativesCacheManager(
         }
     }
 
+    @VisibleForTesting
+    internal companion object {
+        internal const val CACHE_SIZE = 3
+    }
+
     init {
         navigationSession.registerNavigationSessionStateObserver(sessionStateObserver)
     }
 
     fun push(alternatives: List<DirectionsRoute>) {
-        cachedAlternatives.addAll(alternatives)
+        if (cachedAlternatives.size == CACHE_SIZE) {
+            cachedAlternatives.remove()
+        }
+        cachedAlternatives.add(alternatives)
     }
 
     fun areAlternatives(routes: List<DirectionsRoute>): Boolean =
-        routes.map { it in cachedAlternatives }.any { it }
+        cachedAlternatives.any { cachedAlternative ->
+            routes.any { cachedAlternative.contains(it) }
+        }
 }
