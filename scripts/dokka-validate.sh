6bae61cee593b5e68b8f110cb54afd272ae7a779
Yury Kanetski <yury.kanetski@mapbox.com>
2022-01-06 21:15:24 +0100

    Remove unused `undocumented_classes.txt` and refactored `dokka-validate.sh`

--- scripts/dokka-validate.sh
+++ scripts/dokka-validate.sh
@@ -4,11 +4,6 @@ EXIT_TYPE="exit 1"
 MODULE=""
 GRADLE_TASK="dokkaHtmlMultiModule"
 
-fileItemString=$(cat  scripts/undocumented_classes.txt |tr "\n" " ")
-UNDOCUMENTED_CLASSES=($fileItemString)
-
-echo "Array = ${UNDOCUMENTED_CLASSES[*]}"
-
 SEARCH_REGEX='Undocumented: com.mapbox.navigation.'
 SEARCH_REGEX_EXCEPT='\.Companion/ ' # skip `Companion object` headers
 
@@ -20,15 +15,8 @@ do
   esac
 done
 
-
-FINAL_SEARCH_REGEX_EXCEPT="${SEARCH_REGEX_EXCEPT}"
-for i in "${UNDOCUMENTED_CLASSES[@]}"; do
-    FINAL_SEARCH_REGEX_EXCEPT="${FINAL_SEARCH_REGEX_EXCEPT}|${i}"
-done
-
-echo "Dokka validation is starting; clean project"
-./gradlew clean
-echo "Dokka is validating ${MODULE}"
-./gradlew $GRADLE_TASK | grep -i "$SEARCH_REGEX" | egrep -v "$FINAL_SEARCH_REGEX_EXCEPT" && { echo 'kdoc validation failed'; ${EXIT_TYPE}; }
+echo "Dokka validation is starting"
+echo "Validating kdoc ${MODULE} ..."
+./gradlew $GRADLE_TASK | grep -i "$SEARCH_REGEX" | egrep -v "$SEARCH_REGEX_EXCEPT" && { echo 'kdoc validation failed'; ${EXIT_TYPE}; }
 echo "Dokka validation has finished"
 exit 0
