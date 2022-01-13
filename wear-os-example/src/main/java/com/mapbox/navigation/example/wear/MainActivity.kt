347ec14614e01af3d43e6e5f9a09be43cc362ec6
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-01-13 14:31:01 +0100

    use wear specific views

--- wear-os-example/src/main/java/com/mapbox/navigation/example/wear/MainActivity.kt
+++ wear-os-example/src/main/java/com/mapbox/navigation/example/wear/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.mapbox.navigation.example.wear
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.mapbox.api.directions.v5.DirectionsCriteria
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import com.mapbox.api.directions.v5.models.RouteOptions
 import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.animation.camera
+import com.mapbox.maps.plugin.locationcomponent.location
 import com.mapbox.navigation.base.extensions.applyDefaultNavigationOptions
 import com.mapbox.navigation.base.options.NavigationOptions
 import com.mapbox.navigation.base.route.RouterCallback
 import com.mapbox.navigation.base.route.RouterFailure
 import com.mapbox.navigation.base.route.RouterOrigin
 import com.mapbox.navigation.core.MapboxNavigationProvider
+import com.mapbox.navigation.core.trip.session.LocationMatcherResult
+import com.mapbox.navigation.core.trip.session.LocationObserver
+import com.mapbox.navigation.ui.maps.camera.NavigationCamera
+import com.mapbox.navigation.ui.maps.camera.data.MapboxNavigationViewportDataSource
+import com.mapbox.navigation.ui.maps.camera.lifecycle.NavigationBasicGesturesHandler
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,11 +33,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val infoText = findViewById<TextView>(R.id.infoText)
 
+        val mapView = findViewById<MapView>(R.id.mapView)
+        val navigationLocationProvider = NavigationLocationProvider()
+        mapView.location.apply {
+            setLocationProvider(navigationLocationProvider)
+            enabled = true
+        }
+
         val navigation = MapboxNavigationProvider.create(
             NavigationOptions.Builder(this)
                 .accessToken(getString(R.string.mapbox_access_token))
                 .build()
         )
+
+        val viewportDataSource = MapboxNavigationViewportDataSource(
+            mapView.getMapboxMap()
+        )
+        val navigationCamera = NavigationCamera(
+            mapView.getMapboxMap(),
+            mapView.camera,
+            viewportDataSource
+        )
+        mapView.camera.addCameraAnimationsLifecycleListener(
+            NavigationBasicGesturesHandler(navigationCamera)
+        )
+        navigationCamera.requestNavigationCameraToFollowing()
+
+        navigation.registerLocationObserver(object : LocationObserver {
+            override fun onNewRawLocation(rawLocation: Location) { }
+            override fun onNewLocationMatcherResult(locationMatcherResult: LocationMatcherResult) {
+                navigationLocationProvider.changePosition(
+                    location = locationMatcherResult.enhancedLocation,
+                    keyPoints = locationMatcherResult.keyPoints,
+                )
+
+                viewportDataSource.onLocationChanged(locationMatcherResult.enhancedLocation)
+                viewportDataSource.evaluate()
+            }
+        })
 
         fun setRouteAndStartNavigation(route: DirectionsRoute) {
             navigation.startTripSession(true)
