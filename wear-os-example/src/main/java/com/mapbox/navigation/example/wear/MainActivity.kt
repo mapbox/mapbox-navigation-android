2c8824460618b77ad19655b2c70b2c1aa82ff1f6
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-01-13 11:25:58 +0100

    trying to build a route

--- wear-os-example/src/main/java/com/mapbox/navigation/example/wear/MainActivity.kt
+++ wear-os-example/src/main/java/com/mapbox/navigation/example/wear/MainActivity.kt
@@ -2,10 +2,61 @@ package com.mapbox.navigation.example.wear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.mapbox.api.directions.v5.DirectionsCriteria
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.geojson.Point
+import com.mapbox.navigation.base.extensions.applyDefaultNavigationOptions
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.base.route.RouterCallback
+import com.mapbox.navigation.base.route.RouterFailure
+import com.mapbox.navigation.base.route.RouterOrigin
+import com.mapbox.navigation.core.MapboxNavigationProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navigation = MapboxNavigationProvider.create(
+            NavigationOptions.Builder(this)
+                .accessToken(getString(R.string.mapbox_access_token))
+                .build()
+        )
+
+        fun setRouteAndStartNavigation(route: DirectionsRoute) {
+            navigation.startTripSession(true)
+            navigation.setRoutes(listOf(route))
+        }
+        navigation.requestRoutes(
+            RouteOptions.builder()
+                .applyDefaultNavigationOptions(DirectionsCriteria.PROFILE_WALKING)
+                .coordinatesList(listOf(
+                    Point.fromLngLat(54.410983, 18.613039),
+                    Point.fromLngLat(54.420143, 18.608832),
+                    Point.fromLngLat(54.415626, 18.602196)
+                ))
+                .build(),
+            object : RouterCallback {
+                override fun onRoutesReady(
+                    routes: List<DirectionsRoute>,
+                    routerOrigin: RouterOrigin
+                ) {
+                    setRouteAndStartNavigation(routes.first())
+                }
+
+                override fun onFailure(
+                    reasons: List<RouterFailure>,
+                    routeOptions: RouteOptions
+                ) {
+                    // no impl
+                }
+
+                override fun onCanceled(routeOptions: RouteOptions, routerOrigin: RouterOrigin) {
+                    // no impl
+                }
+            }
+        )
     }
 }
