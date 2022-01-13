e4efb1c622fa64ac475da1e1c9b1fa0c5c073184
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-01-13 11:00:58 +0100

    added wear os project

---
+++ wear-os-example/src/main/java/com/mapbox/navigation/example/wear/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mapbox.navigation.example.wear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
