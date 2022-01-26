1237487119bfb85c58bcdb2a57977af12a493d05
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-01-26 15:12:47 +0100

    added tests for detectling route options creation

--- libnavigation-core-lint/src/test/java/com/mapbox/navigation/core/lint/RouteOptionsDetectorTest.kt
+++ libnavigation-core-lint/src/test/java/com/mapbox/navigation/core/lint/RouteOptionsDetectorTest.kt
@@ -1,25 +1,60 @@
 package com.mapbox.navigation.core.lint
 
+import com.android.tools.lint.checks.infrastructure.TestFile
+import com.android.tools.lint.checks.infrastructure.TestFiles
 import com.android.tools.lint.checks.infrastructure.TestFiles.kotlin
 import org.junit.Test
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import java.io.File
 
 class RouteOptionsDetectorTest {
     @Test
-    fun testBasic() {
+    fun `no errors when default navigation options is applied`() {
         lint().files(
             kotlin(
                 """
                 package test.pkg;
+                import com.mapbox.api.directions.v5.models.RouteOptions
                 fun buildRouteOptions() {
-                    val a = "lint"
+                    RouteOptions.builder()
+                        .applyDefaultNavigationOptions()
+                        .build()
                 }
                 """
-            ).indented()
+            ).indented(),
+            mapboxDirectionsModelsFile()
+        )
+            .issues(RouteOptionsDetector.ISSUE)
+            .allowMissingSdk()
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun `error when default navigation is not applied`() {
+        lint().files(
+            kotlin(
+                """
+                package test.pkg;
+                import com.mapbox.api.directions.v5.models.RouteOptions
+                fun buildRouteOptions() {
+                    RouteOptions.builder()
+                        .build()
+                }
+                """
+            ).indented(),
+            mapboxDirectionsModelsFile()
         )
             .issues(RouteOptionsDetector.ISSUE)
             .allowMissingSdk()
             .run()
             .expectErrorCount(1)
     }
+}
+
+private fun mapboxDirectionsModelsFile(): TestFile {
+    val path = System.getProperty("java.class.path")
+        .split(':')
+        .first { it.contains("mapbox-sdk-directions-models") }
+    return TestFiles.LibraryReferenceTestFile(File(path))
 }
