bf43b127de797b3bc2b59937b0d73834b7e0992c
Kyle Madsen <kyle.mdsn@gmail.com>
2022-03-09 18:55:15 -0800

    Fix initial location puck missing

--- libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/binder/map/FreeDriveMapBinder.kt
+++ libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/binder/map/FreeDriveMapBinder.kt
@@ -7,7 +7,7 @@ import com.mapbox.navigation.dropin.DropInNavigationViewContext
 import com.mapbox.navigation.dropin.binder.Binder
 import com.mapbox.navigation.dropin.binder.navigationListOf
 import com.mapbox.navigation.dropin.component.camera.CameraComponent
-import com.mapbox.navigation.dropin.component.location.LocationPuck
+import com.mapbox.navigation.dropin.component.location.LocationComponent
 import com.mapbox.navigation.dropin.component.marker.LongPressMapComponent
 import com.mapbox.navigation.dropin.component.marker.MapMarkersComponent
 
@@ -18,7 +18,10 @@ internal class FreeDriveMapBinder(
 
     override fun bind(value: MapView): MapboxNavigationObserver {
         return navigationListOf(
-            LocationPuck(value),
+            LocationComponent(
+                value,
+                navigationViewContext.viewModel.locationViewModel,
+            ),
             CameraComponent(
                 value,
                 navigationViewContext.viewModel.cameraViewModel,
--- libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/component/location/LocationViewModel.kt
+++ libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/component/location/LocationViewModel.kt
@@ -7,11 +7,13 @@ import com.mapbox.android.core.location.LocationEngineResult
 import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
 import com.mapbox.navigation.core.MapboxNavigation
-import com.mapbox.navigation.core.trip.session.LocationMatcherResult
-import com.mapbox.navigation.core.trip.session.LocationObserver
+import com.mapbox.navigation.dropin.extensions.flowLocationMatcherResult
 import com.mapbox.navigation.dropin.lifecycle.UIViewModel
 import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
 import com.mapbox.navigation.utils.internal.logE
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 sealed class LocationAction {
     data class Update(val location: Location) : LocationAction()
@@ -22,24 +24,24 @@ sealed class LocationAction {
 class LocationViewModel : UIViewModel<Location?, LocationAction>(null) {
     val navigationLocationProvider = NavigationLocationProvider()
 
+    /**
+     * Accessor function to get the last [Point]
+     */
     val lastPoint: Point?
         get() = navigationLocationProvider.lastLocation?.run {
             Point.fromLngLat(longitude, latitude)
         }
 
-    private val locationObserver: LocationObserver = object : LocationObserver {
-        override fun onNewRawLocation(rawLocation: Location) {
-            // We are not sending LocationAction.UpdateRaw here, because we are expecting to
-            // receive locationMatcherResult
+    /**
+     * Suspend until a non-null location is available.
+     */
+    suspend fun firstLocation(): Location {
+        var nonNullLocation: Location? = navigationLocationProvider.lastLocation
+        while (nonNullLocation == null) {
+            delay(DELAY_FIRST_LOCATION_MS)
+            nonNullLocation = navigationLocationProvider.lastLocation
         }
-
-        override fun onNewLocationMatcherResult(locationMatcherResult: LocationMatcherResult) {
-            navigationLocationProvider.changePosition(
-                location = locationMatcherResult.enhancedLocation,
-                keyPoints = locationMatcherResult.keyPoints,
-            )
-            invoke(LocationAction.Update(locationMatcherResult.enhancedLocation))
-        }
+        return nonNullLocation
     }
 
     override fun process(
@@ -70,11 +72,18 @@ class LocationViewModel : UIViewModel<Location?, LocationAction>(null) {
             }
         })
 
-        mapboxNavigation.registerLocationObserver(locationObserver)
+        mainJobControl.scope.launch {
+            mapboxNavigation.flowLocationMatcherResult().collect { locationMatcherResult ->
+                navigationLocationProvider.changePosition(
+                    location = locationMatcherResult.enhancedLocation,
+                    keyPoints = locationMatcherResult.keyPoints,
+                )
+                invoke(LocationAction.Update(locationMatcherResult.enhancedLocation))
+            }
+        }
     }
 
-    override fun onDetached(mapboxNavigation: MapboxNavigation) {
-        mapboxNavigation.unregisterLocationObserver(locationObserver)
-        super.onDetached(mapboxNavigation)
+    private companion object {
+        const val DELAY_FIRST_LOCATION_MS = 100L
     }
 }
