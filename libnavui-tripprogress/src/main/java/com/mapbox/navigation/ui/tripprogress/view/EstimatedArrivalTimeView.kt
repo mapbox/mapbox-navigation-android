990df00a2623c0ea673e333e878baf94df01ddff
Abhishek Kejriwal <abhishek.kejriwal@mapbox.com>
2022-04-26 18:25:42 -0700

    update trip progress to add options

--- libnavui-tripprogress/src/main/java/com/mapbox/navigation/ui/tripprogress/view/EstimatedArrivalTimeView.kt
+++ libnavui-tripprogress/src/main/java/com/mapbox/navigation/ui/tripprogress/view/EstimatedArrivalTimeView.kt
@@ -8,42 +8,18 @@ import androidx.appcompat.widget.AppCompatTextView
 /**
  * A view that could be used to render estimated arrival time to reach the destination
  */
-class EstimatedArrivalTimeView : AppCompatTextView {
+internal class EstimatedArrivalTimeView : AppCompatTextView {
 
-    /**
-     *
-     * @param context Context
-     * @constructor
-     */
     constructor(context: Context) : super(context)
 
-    /**
-     *
-     * @param context Context
-     * @param attrs AttributeSet?
-     * @constructor
-     */
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    /**
-     *
-     * @param context Context
-     * @param attrs AttributeSet?
-     * @param defStyleAttr Int
-     * @constructor
-     */
     constructor(
         context: Context,
         attrs: AttributeSet?,
         defStyleAttr: Int
     ) : super(context, attrs, defStyleAttr)
 
-    /**
-     * Invoke the function to render estimated arrival to reach the destination.
-     *
-     * @param estimatedArrivalTime value represented in [SpannableString]
-     * @param bufferType
-     */
     fun renderEstimatedArrivalTime(
         estimatedArrivalTime: SpannableString,
         bufferType: BufferType
--- libnavui-tripprogress/src/main/java/com/mapbox/navigation/ui/tripprogress/view/TimeRemainingView.kt
+++ libnavui-tripprogress/src/main/java/com/mapbox/navigation/ui/tripprogress/view/TimeRemainingView.kt
@@ -8,42 +8,18 @@ import androidx.appcompat.widget.AppCompatTextView
 /**
  * A view that could be used to render time remaining to reach the destination
  */
-class TimeRemainingView : AppCompatTextView {
+internal class TimeRemainingView : AppCompatTextView {
 
-    /**
-     *
-     * @param context Context
-     * @constructor
-     */
     constructor(context: Context) : super(context)
 
-    /**
-     *
-     * @param context Context
-     * @param attrs AttributeSet?
-     * @constructor
-     */
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
-    /**
-     *
-     * @param context Context
-     * @param attrs AttributeSet?
-     * @param defStyleAttr Int
-     * @constructor
-     */
     constructor(
         context: Context,
         attrs: AttributeSet?,
         defStyleAttr: Int
     ) : super(context, attrs, defStyleAttr)
 
-    /**
-     * Invoke the function to render time remaining to reach the destination.
-     *
-     * @param timeRemaining value represented in [SpannableString]
-     * @param bufferType
-     */
     fun renderTimeRemaining(
         timeRemaining: SpannableString,
         bufferType: BufferType
