a085f4117519db840d35de5d126e9f11a806a4c1
mapbox-ci <no-reply@mapbox.com>
2022-04-28 21:38:30 +0000

    Docs v2.4.1

---
+++ 2.4.1/libnavui-voice/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":libnavui-voice:dokkaHtml/androidTestRelease": [], ":libnavui-voice:dokkaHtml/debug": [], ":libnavui-voice:dokkaHtml/main": [], ":libnavui-voice:dokkaHtml/release": []}'
