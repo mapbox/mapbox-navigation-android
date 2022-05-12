e29446e84c478cf91e75098e817534d2cb172dc9
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-05-12 10:28:25 +0200

    improved docs

--- libnavigation-core/src/main/java/com/mapbox/navigation/core/trip/session/VoiceInstructionsObserver.kt
+++ libnavigation-core/src/main/java/com/mapbox/navigation/core/trip/session/VoiceInstructionsObserver.kt
@@ -9,7 +9,8 @@ import com.mapbox.navigation.core.MapboxNavigation
 fun interface VoiceInstructionsObserver {
     /**
      * Called every time on a new [VoiceInstructions] is applicable. Only new voice instructions
-     * are available via this observer.
+     * are available via this observer because the timing of delivering and playing out
+     * an instruction is critical for correct navigation experience.
      * See {@link com.mapbox.navigation.base.trip.model.RouteProgress.voiceInstructions} to get
      * a current voice instruction.
      */
