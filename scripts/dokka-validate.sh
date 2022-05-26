e693407bc34e525d5132fa1a7c2be3a85972bd85
Yury Kanetski <yury.kanetski@mapbox.com>
2022-05-26 11:19:49 +0200

    dokka multimodule: refactor tasks

--- scripts/dokka-validate.sh
+++ scripts/dokka-validate.sh
@@ -1,22 +1,19 @@
 #!/bin/bash -e
 
 EXIT_TYPE="exit 1"
-MODULE=""
-GRADLE_TASK="dokkaHtmlMultiModule"
 
 SEARCH_REGEX='Undocumented: com.mapbox.navigation.'
 SEARCH_REGEX_EXCEPT='\.Companion/ ' # skip `Companion object` headers
 
-while getopts 'sm:' c
+while getopts 's:' c
 do
   case $c in
     s) EXIT_TYPE="" ;; # soft failing (optional)
-    m) MODULE="${OPTARG}" && GRADLE_TASK="${MODULE}:dokkaHtml";; # module to validate dokka (optional)
   esac
 done
 
 echo "Dokka validation is starting"
 echo "Validating kdoc ${MODULE} ..."
-./gradlew $GRADLE_TASK | grep -i "$SEARCH_REGEX" | egrep -v "$SEARCH_REGEX_EXCEPT" && { echo 'kdoc validation failed'; ${EXIT_TYPE}; }
+./gradlew dokkaHtmlMultiModule | grep -i "$SEARCH_REGEX" | egrep -v "$SEARCH_REGEX_EXCEPT" && { echo 'kdoc validation failed'; ${EXIT_TYPE}; }
 echo "Dokka validation has finished"
 exit 0
