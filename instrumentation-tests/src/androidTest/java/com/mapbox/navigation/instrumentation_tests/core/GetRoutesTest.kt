fdfa1cc5ac8600825209d44c4a2168a817f98f1b
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-06-22 09:55:37 +0200

    fixed formatting

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/GetRoutesTest.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/GetRoutesTest.kt
@@ -21,77 +21,81 @@ import java.net.URL
 import kotlin.coroutines.resume
 
 class GetRoutesTest {
-@Test
-fun requestManyRoutes() = sdkTest {
-    val context = getApplicationContext<Context>()
-    val navigationOptions = NavigationOptions.Builder(context)
-        .accessToken(getMapboxAccessTokenFromResources(context))
-        .build()
-    val mapboxNavigation = MapboxNavigationProvider.create(navigationOptions)
-    try {
+    @Test
+    fun requestManyRoutes() = sdkTest {
+        val context = getApplicationContext<Context>()
+        val navigationOptions = NavigationOptions.Builder(context)
+            .accessToken(getMapboxAccessTokenFromResources(context))
+            .build()
+        val mapboxNavigation = MapboxNavigationProvider.create(navigationOptions)
+        try {
 
-        val url = URL(
-            "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/11.5679995,48.1341107;13.37691,52.51604?access_token=****jgZw&geometries=polyline6&alternatives=true&overview=full&steps=true&bearings=332.4625244%2C45%3B&layers=0%3B&continue_straight=false&annotations=congestion%2Cclosure%2Cdistance%2Cspeed%2Cstate_of_charge&language=en&roundabout_exits=true&voice_instructions=true&banner_instructions=true&voice_units=imperial&enable_refresh=true&snapping_include_closures=true%3Btrue&max_width=2.2&metadata=true"
-        )
+            val url = URL(
+                "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/11.5679995,48.1341107;13.37691,52.51604?access_token=****jgZw&geometries=polyline6&alternatives=true&overview=full&steps=true&bearings=332.4625244%2C45%3B&layers=0%3B&continue_straight=false&annotations=congestion%2Cclosure%2Cdistance%2Cspeed%2Cstate_of_charge&language=en&roundabout_exits=true&voice_instructions=true&banner_instructions=true&voice_units=imperial&enable_refresh=true&snapping_include_closures=true%3Btrue&max_width=2.2&metadata=true"
+            )
 
-        coroutineScope {
-            repeat(20) {
-                val options = RouteOptions.fromUrl(url)
-                val route = mapboxNavigation.requestRoutes(options).getSuccessfulResultOrThrowException()
+            coroutineScope {
+                repeat(20) {
+                    val options = RouteOptions.fromUrl(url)
+                    val route = mapboxNavigation.requestRoutes(options)
+                        .getSuccessfulResultOrThrowException()
+                }
             }
-        }
-    } finally {
-        mapboxNavigation.onDestroy()
-    }
-}
-
-// HELPER FUNCTIONS WE USE IN OUR TESTS
-fun sdkTest(
-    timeout: Long = 60_000,
-    block: suspend () -> Unit
-) {
-    runBlocking(Dispatchers.Main) {
-        withTimeout(timeout) {
-            block()
-        }
-    }
-}
-
-suspend fun MapboxNavigation.requestRoutes(options: RouteOptions) =
-    suspendCancellableCoroutine<RouteRequestResult> { continuation ->
-        val callback = object : NavigationRouterCallback {
-            override fun onRoutesReady(routes: List<NavigationRoute>, routerOrigin: RouterOrigin) {
-                continuation.resume(RouteRequestResult.Success(routes, routerOrigin))
-            }
-
-            override fun onFailure(reasons: List<RouterFailure>, routeOptions: RouteOptions) {
-                continuation.resume(RouteRequestResult.Failure(reasons))
-            }
-
-            override fun onCanceled(routeOptions: RouteOptions, routerOrigin: RouterOrigin) {
-            }
-        }
-        val id = requestRoutes(options, callback)
-        continuation.invokeOnCancellation {
-            cancelRouteRequest(id)
+        } finally {
+            mapboxNavigation.onDestroy()
         }
     }
 
-sealed class RouteRequestResult {
-    data class Success(
-        val routes: List<NavigationRoute>,
-        val routerOrigin: RouterOrigin
-    ) : RouteRequestResult()
-
-    data class Failure(
-        val reasons: List<RouterFailure>
-    ) : RouteRequestResult()
-}
-
-fun RouteRequestResult.getSuccessfulResultOrThrowException(): RouteRequestResult.Success {
-    return when (this) {
-        is RouteRequestResult.Success -> this
-        is RouteRequestResult.Failure -> error("result is failure: ${this.reasons}")
+    // HELPER FUNCTIONS WE USE IN OUR TESTS
+    fun sdkTest(
+        timeout: Long = 60_000,
+        block: suspend () -> Unit
+    ) {
+        runBlocking(Dispatchers.Main) {
+            withTimeout(timeout) {
+                block()
+            }
+        }
     }
-}
+
+    suspend fun MapboxNavigation.requestRoutes(options: RouteOptions) =
+        suspendCancellableCoroutine<RouteRequestResult> { continuation ->
+            val callback = object : NavigationRouterCallback {
+                override fun onRoutesReady(
+                    routes: List<NavigationRoute>,
+                    routerOrigin: RouterOrigin
+                ) {
+                    continuation.resume(RouteRequestResult.Success(routes, routerOrigin))
+                }
+
+                override fun onFailure(reasons: List<RouterFailure>, routeOptions: RouteOptions) {
+                    continuation.resume(RouteRequestResult.Failure(reasons))
+                }
+
+                override fun onCanceled(routeOptions: RouteOptions, routerOrigin: RouterOrigin) {
+                }
+            }
+            val id = requestRoutes(options, callback)
+            continuation.invokeOnCancellation {
+                cancelRouteRequest(id)
+            }
+        }
+
+    sealed class RouteRequestResult {
+        data class Success(
+            val routes: List<NavigationRoute>,
+            val routerOrigin: RouterOrigin
+        ) : RouteRequestResult()
+
+        data class Failure(
+            val reasons: List<RouterFailure>
+        ) : RouteRequestResult()
+    }
+
+    fun RouteRequestResult.getSuccessfulResultOrThrowException(): RouteRequestResult.Success {
+        return when (this) {
+            is RouteRequestResult.Success -> this
+            is RouteRequestResult.Failure -> error("result is failure: ${this.reasons}")
+        }
+    }
 }
