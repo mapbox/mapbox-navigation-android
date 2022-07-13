57640accf1ab244e8f89a71de28d35cbc650e6f4
Dzina Dybouskaya <dzina.dybouskaya@mapbox.com>
2022-07-13 17:48:05 +0300

    use another approach for clearing routes

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/coroutines/TestUtils.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/coroutines/TestUtils.kt
@@ -3,6 +3,8 @@ package com.mapbox.navigation.instrumentation_tests.utils.coroutines
 import com.mapbox.navigation.base.route.NavigationRoute
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.directions.session.RoutesExtra
+import com.mapbox.navigation.core.directions.session.RoutesObserver
+import com.mapbox.navigation.core.directions.session.RoutesUpdatedResult
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.filter
@@ -10,6 +12,8 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 private const val MAX_TIME_TO_UPDATE_ROUTE = 5_000L
 private const val DEFAULT_TIMEOUT_FOR_SDK_TEST = 30_000L
@@ -25,15 +29,34 @@ fun sdkTest(
     }
 }
 
+fun MapboxNavigation.clearNavigationRoutesAndWaitForUpdate() {
+    val latch = CountDownLatch(1)
+    val observer = object : RoutesObserver {
+        override fun onRoutesChanged(result: RoutesUpdatedResult) {
+            if (result.reason == RoutesExtra.ROUTES_UPDATE_REASON_CLEAN_UP) {
+                unregisterRoutesObserver(this)
+                latch.countDown()
+            }
+        }
+    }
+    registerRoutesObserver(observer)
+    setNavigationRoutes(emptyList())
+    if (getNavigationRoutes().isEmpty()) {
+        latch.countDown()
+    }
+    latch.await(MAX_TIME_TO_UPDATE_ROUTE, TimeUnit.MILLISECONDS)
+}
+
 suspend fun MapboxNavigation.setNavigationRoutesAndWaitForUpdate(routes: List<NavigationRoute>) {
+    if (routes.isEmpty()) {
+        throw IllegalArgumentException(
+            "For empty routes use `clearNavigationRoutesAndWaitForUpdate` instead"
+        )
+    }
     withTimeout(MAX_TIME_TO_UPDATE_ROUTE) {
         coroutineScope {
             launch {
-                if (routes.isEmpty()) {
-                    waitForRoutesCleanup()
-                } else {
-                    waitForNewRoute()
-                }
+                waitForNewRoute()
             }
             setNavigationRoutes(routes)
         }
@@ -42,10 +65,6 @@ suspend fun MapboxNavigation.setNavigationRoutesAndWaitForUpdate(routes: List<Na
 
 suspend fun MapboxNavigation.waitForNewRoute() {
     waitForRoutesUpdate(RoutesExtra.ROUTES_UPDATE_REASON_NEW)
-}
-
-suspend fun MapboxNavigation.waitForRoutesCleanup() {
-    waitForRoutesUpdate(RoutesExtra.ROUTES_UPDATE_REASON_CLEAN_UP)
 }
 
 private suspend fun MapboxNavigation.waitForRoutesUpdate(
