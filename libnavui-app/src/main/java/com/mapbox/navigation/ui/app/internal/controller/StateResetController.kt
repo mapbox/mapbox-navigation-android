95e1b7d6e2b2f1ee723f36a8e95783485ff5bd69
Tomasz Rybakiewicz <tomaszrybakiewicz@users.noreply.github.com>
2022-08-10 12:57:08 -0400

    [Drop-In UI] Fixed: Drop-In UI goes back to Free drive state when the device is rotated in active guidance with replay enabled. (#6140)
    * Fixed Android Drop-In UI : Drop-In UI goes back to Free drive state when the device is rotated in active guidance with replay enabled.
    
    Introduced SharedApp.tripSessionTransaction { } block that temporarily disables StateResetController to avoid cleanup logic execution when TripSession state changes.
    Updated StateResetController to register/unregister TripSessionStateObserver instead of collect Flowable. This ensures observer logic gets immediately called when TripSession changes.
    
    * CHANGELOG entry

--- libnavui-app/src/main/java/com/mapbox/navigation/ui/app/internal/controller/StateResetController.kt
+++ libnavui-app/src/main/java/com/mapbox/navigation/ui/app/internal/controller/StateResetController.kt
@@ -2,36 +2,42 @@ package com.mapbox.navigation.ui.app.internal.controller
 
 import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
 import com.mapbox.navigation.core.MapboxNavigation
-import com.mapbox.navigation.core.internal.extensions.flowTripSessionState
 import com.mapbox.navigation.core.trip.session.TripSessionState
+import com.mapbox.navigation.core.trip.session.TripSessionStateObserver
 import com.mapbox.navigation.ui.app.internal.Store
 import com.mapbox.navigation.ui.app.internal.endNavigation
 import com.mapbox.navigation.ui.app.internal.extension.dispatch
 import com.mapbox.navigation.ui.base.lifecycle.UIComponent
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.debounce
+import java.util.concurrent.atomic.AtomicBoolean
 
-@OptIn(ExperimentalPreviewMapboxNavigationAPI::class, FlowPreview::class)
-internal class StateResetController(private val store: Store) : UIComponent() {
+@OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
+internal class StateResetController(
+    private val store: Store,
+    private val ignoreTripSessionUpdates: AtomicBoolean
+) : UIComponent() {
+
+    private var prevState: TripSessionState? = null
+
+    private val tripSessionStateObserver = TripSessionStateObserver { newState ->
+        // we only reset Store state when TripSessionState switches from STARTED to STOPPED.
+        if (!ignoreTripSessionUpdates.get() &&
+            prevState == TripSessionState.STARTED &&
+            newState == TripSessionState.STOPPED
+        ) {
+            store.dispatch(endNavigation())
+        }
+        prevState = newState
+    }
 
     override fun onAttached(mapboxNavigation: MapboxNavigation) {
         super.onAttached(mapboxNavigation)
-
-        // TripSessionState is triggered rapidly when enabling and disabling replay by
-        // calling `startTripSession, `stopTripSession`, `startReplayTripSession`
-        // Debouncing it by 100 ms ensures we skip transient values.
-        var prevState = mapboxNavigation.getTripSessionState()
-        mapboxNavigation.flowTripSessionState().debounce(100).observe { newState ->
-            if (prevState == TripSessionState.STARTED && newState == TripSessionState.STOPPED) {
-                // we only reset Store state when TripSessionState switches from STARTED to STOPPED.
-                store.dispatch(endNavigation())
-            }
-            prevState = newState
-        }
+        prevState = mapboxNavigation.getTripSessionState()
+        mapboxNavigation.registerTripSessionStateObserver(tripSessionStateObserver)
     }
 
     override fun onDetached(mapboxNavigation: MapboxNavigation) {
         super.onDetached(mapboxNavigation)
+        mapboxNavigation.unregisterTripSessionStateObserver(tripSessionStateObserver)
 
         // we reset Store state every time MapboxNavigation gets destroyed
         store.reset()
