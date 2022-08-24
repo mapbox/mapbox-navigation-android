4e30f50476dcc2ad50d48fa74d0083e561bda46b
Kyle Madsen <kyle.mdsn@gmail.com>
2022-08-24 15:37:47 -0700

    Remove mapbox navigation from route preview

--- libnavui-androidauto/src/main/java/com/mapbox/androidauto/car/preview/CarRoutePreviewScreen.kt
+++ libnavui-androidauto/src/main/java/com/mapbox/androidauto/car/preview/CarRoutePreviewScreen.kt
@@ -34,6 +34,7 @@ import com.mapbox.maps.extension.androidauto.MapboxCarMapObserver
 import com.mapbox.maps.extension.androidauto.MapboxCarMapSurface
 import com.mapbox.maps.plugin.delegates.listeners.OnStyleLoadedListener
 import com.mapbox.navigation.base.route.NavigationRoute
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
 
 /**
  * After a destination has been selected. This view previews the route and lets
@@ -62,7 +63,7 @@ class CarRoutePreviewScreen(
 
         override fun handleOnBackPressed() {
             logAndroidAuto("CarRoutePreviewScreen OnBackPressedCallback")
-            routePreviewCarContext.mapboxNavigation.setNavigationRoutes(emptyList())
+            MapboxNavigationApp.current()?.setNavigationRoutes(emptyList())
             screenManager.pop()
         }
     }
@@ -181,10 +182,12 @@ class CarRoutePreviewScreen(
                 Action.Builder()
                     .setTitle(carContext.getString(R.string.car_action_preview_navigate_button))
                     .setOnClickListener {
-                        routePreviewCarContext.mapboxNavigation.setNavigationRoutes(
-                            carRoutesProvider.navigationRoutes.value
-                        )
-                        MapboxCarApp.updateCarAppState(ActiveGuidanceState)
+                        MapboxNavigationApp.current()?.let { mapboxNavigation ->
+                            mapboxNavigation.setNavigationRoutes(
+                                carRoutesProvider.navigationRoutes.value
+                            )
+                            MapboxCarApp.updateCarAppState(ActiveGuidanceState)
+                        }
                     }
                     .build(),
             )
