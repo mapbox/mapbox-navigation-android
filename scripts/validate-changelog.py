63e4e60a763f829b770888d9901d4a9e560bb607
Dzina Dybouskaya <dzina.dybouskaya@mapbox.com>
2022-08-24 14:59:35 +0300

    fix changelog validation

--- scripts/validate-changelog.py
+++ scripts/validate-changelog.py
@@ -23,15 +23,15 @@ with requests.get(api_url, headers=headers) as pr_response:
         pr_diff_url = response_json["diff_url"]
         with requests.get(pr_diff_url, headers) as diff_response:
             diff = diff_response.text
-            print("[ddlog] diff: " + diff)
             validate_changelog_utils.check_has_changelog_diff(diff)
-            added_lines = validate_changelog_utils.extract_added_lines(diff)
-            validate_changelog_utils.check_contains_pr_link(added_lines)
+            added_lines_by_file = validate_changelog_utils.extract_added_lines(diff)
+            validate_changelog_utils.check_contains_pr_link(added_lines_by_file)
             with requests.get(files_url, headers) as files_response:
-                contents_url = validate_changelog_utils.parse_contents_url(files_response.json())
-                with requests.get(contents_url, headers) as contents_response:
-                    content = base64.b64decode(contents_response.json()["content"]).decode("utf-8")
-                    validate_changelog_utils.check_version_section(content, added_lines)
+                contents_urls_by_file = validate_changelog_utils.parse_contents_url(files_response.json())
+                for (filename, contents_url) in contents_urls_by_file.items():
+                    with requests.get(contents_url, headers) as contents_response:
+                        content = base64.b64decode(contents_response.json()["content"]).decode("utf-8")
+                        validate_changelog_utils.check_version_section(content, added_lines_by_file[filename])
             print("Changelog entry validation successful.")
     else:
         print("`skip changelog` label present, exiting.")
