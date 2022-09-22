3f2441dea81d14f8b54615f8b6cd028cd7e74fcf
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-09-22 18:44:11 +0200

    handle different ids

--- libnavigation-core/src/test/java/com/mapbox/navigation/core/internal/utils/SimilarRoutesTest.kt
+++ libnavigation-core/src/test/java/com/mapbox/navigation/core/internal/utils/SimilarRoutesTest.kt
@@ -2,6 +2,7 @@ package com.mapbox.navigation.core.internal.utils
 
 import com.mapbox.api.directions.v5.models.DirectionsResponse
 import com.mapbox.api.directions.v5.models.RouteOptions
+import com.mapbox.navigation.base.internal.route.updateDirectionsRouteOnly
 import com.mapbox.navigation.testing.factories.createNavigationRoutes
 import org.apache.commons.io.IOUtils
 import org.junit.Assert.assertEquals
@@ -17,6 +18,17 @@ class SimilarRoutesTest {
         assertEquals(1.0, similarity, 0.00001)
     }
 
+
+    @Test
+    fun `the same routes but different ids`() {
+        val original = loadNavigationRoute("a")
+        val newId = original.updateDirectionsRouteOnly {
+            toBuilder().requestUuid("different-id").build()
+        }
+        val similarity = calculateSimilarity(original, newId)
+        assertEquals(1.0, similarity, 0.00001)
+    }
+
     @Test
     fun `different routes`() {
         val a = loadNavigationRoute("a")
@@ -26,11 +38,19 @@ class SimilarRoutesTest {
     }
 
     @Test
-    fun `half the same routes`() {
+    fun `compare route with its half`() {
         val a = loadNavigationRoute("a")
         val halfA = loadNavigationRoute("half_a")
         val similarity = calculateSimilarity(a, halfA)
         assertEquals(0.47, similarity, 0.6)
+    }
+
+    @Test
+    fun `half of a route matches the route`() {
+        val a = loadNavigationRoute("a")
+        val halfA = loadNavigationRoute("half_a")
+        val similarity = calculateSimilarity(halfA, a)
+        assertEquals(1.0, similarity, 0.001)
     }
 
     private fun loadNavigationRoute(name: String) = createNavigationRoutes(
