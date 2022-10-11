d01ab95df306d73d224a59694a23a1718ac84f8e
Vitaly Zayankovsky <vitaliy.zayankovskiy@mapbox.com>
2022-10-11 19:02:05 +0300

    do not reset navigation camera state after view recreation

--- libnavui-app/src/main/java/com/mapbox/navigation/ui/app/internal/controller/CameraStateController.kt
+++ libnavui-app/src/main/java/com/mapbox/navigation/ui/app/internal/controller/CameraStateController.kt
@@ -1,19 +1,39 @@
 package com.mapbox.navigation.ui.app.internal.controller
 
 import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
+import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.ui.app.internal.Action
 import com.mapbox.navigation.ui.app.internal.State
 import com.mapbox.navigation.ui.app.internal.Store
 import com.mapbox.navigation.ui.app.internal.camera.CameraAction
 import com.mapbox.navigation.ui.app.internal.camera.CameraState
 import com.mapbox.navigation.ui.app.internal.camera.TargetCameraMode
+import com.mapbox.navigation.ui.app.internal.navigation.NavigationState
 
 @OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
 class CameraStateController(
-    store: Store
+    private val store: Store,
 ) : StateController() {
     init {
         store.register(this)
+    }
+
+    override fun onAttached(mapboxNavigation: MapboxNavigation) {
+        super.onAttached(mapboxNavigation)
+
+        store.select { it.navigation }.observe { navigationState ->
+            when (navigationState) {
+                NavigationState.FreeDrive, NavigationState.RoutePreview -> {
+                    store.dispatch(CameraAction.SetCameraMode(TargetCameraMode.Overview))
+                }
+                NavigationState.DestinationPreview -> {
+                    store.dispatch(CameraAction.SetCameraMode(TargetCameraMode.Idle))
+                }
+                NavigationState.ActiveNavigation, NavigationState.Arrival -> {
+                    store.dispatch(CameraAction.SetCameraMode(TargetCameraMode.Following))
+                }
+            }
+        }
     }
 
     override fun process(state: State, action: Action): State {
