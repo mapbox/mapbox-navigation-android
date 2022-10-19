2a26f6e1227299b55f7a5b97253931c622f8b57c
Kyle Madsen <kyle.mdsn@gmail.com>
2022-10-19 12:04:33 -0700

    Simplify action provider (#6494)

--- libnavui-androidauto/src/main/java/com/mapbox/androidauto/car/action/MapboxActionProvider.kt
+++ libnavui-androidauto/src/main/java/com/mapbox/androidauto/car/action/MapboxActionProvider.kt
@@ -3,11 +3,6 @@ package com.mapbox.androidauto.car.action
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
 
-sealed interface MapboxActionProvider {
-    interface ActionProvider : MapboxActionProvider {
-        fun getAction(): Action
-    }
-    interface ScreenActionProvider : MapboxActionProvider {
-        fun getAction(screen: Screen): Action
-    }
+fun interface MapboxActionProvider {
+    fun getAction(screen: Screen): Action
 }
