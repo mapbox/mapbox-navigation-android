5d7f73bcd40aa262630b2d991e46d6fa59533583
Tomasz Rybakiewicz <tomasz.rybakiewicz@mapbox.com>
2022-10-21 13:23:56 -0400

    Updated ViewOptionsCustomization and added support for showing/hiding: - SpeedLimitView - TripProgressView - ManeuverView - RoadNameView - ActionButtonView - Camera Mode Button - Toggle Audio Button - Recenter Camera Button - Start navigation button - End navigation button - Route preview button
    Added examples for toggling the visibility of NavigationView views
    Added CHANGELOG entry

--- libnavui-dropin/src/test/java/com/mapbox/navigation/dropin/roadname/RoadNameCoordinatorTest.kt
+++ libnavui-dropin/src/test/java/com/mapbox/navigation/dropin/roadname/RoadNameCoordinatorTest.kt
@@ -1,7 +1,6 @@
 package com.mapbox.navigation.dropin.roadname
 
 import android.content.res.Configuration
-import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.test.core.app.ApplicationProvider
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -11,7 +10,6 @@ import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
 import com.mapbox.navigation.dropin.R
 import com.mapbox.navigation.dropin.navigationview.NavigationViewContext
 import com.mapbox.navigation.testing.MainCoroutineRule
-import com.mapbox.navigation.ui.base.lifecycle.Binder
 import com.mapbox.navigation.ui.base.lifecycle.UIBinder
 import io.mockk.clearAllMocks
 import io.mockk.every
@@ -25,11 +23,9 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -45,11 +41,12 @@ class RoadNameCoordinatorTest {
     val coroutineRule = MainCoroutineRule()
     private val bottomSheetFlow = MutableStateFlow<Int?>(null)
     private val roadNameFlow = MutableStateFlow<UIBinder?>(null)
+    private val showRoadNameFlow = MutableStateFlow(true)
     private val mapboxNavigation = mockk<MapboxNavigation>()
     private val configuration = Configuration()
     private val layoutParams = ConstraintLayout.LayoutParams(50, 50)
 
-//    private val roadNameLayout = mockk<ViewGroup> {
+    //    private val roadNameLayout = mockk<ViewGroup> {
     private val roadNameLayout = mockk<ConstraintLayout>(relaxed = true) {
         every { resources } returns mockk {
             every { configuration } returns this@RoadNameCoordinatorTest.configuration
@@ -61,6 +58,9 @@ class RoadNameCoordinatorTest {
         }
         every { uiBinders } returns mockk {
             every { roadName } returns roadNameFlow
+        }
+        every { options } returns mockk {
+            every { showRoadName } returns showRoadNameFlow
         }
     }
     private val observer = mockk<MapboxNavigationObserver>(relaxed = true)
@@ -100,43 +100,6 @@ class RoadNameCoordinatorTest {
             roadNameFlow.value = customBinder
             val binders = mapboxNavigation.flowViewBinders().take(1).toList()
             assertTrue(binders.first() === customBinder)
-        }
-    }
-
-    @Test
-    fun `should reload binder when speedLimit changes`() = runBlockingTest {
-        val customBinder1 = mockk<UIBinder>()
-        val customBinder2 = mockk<UIBinder>()
-        val collectedBinders = mutableListOf<Binder<ViewGroup>>()
-        coordinator.apply {
-            val job = launch {
-                mapboxNavigation.flowViewBinders().take(4).toList(collectedBinders)
-            }
-            roadNameFlow.value = customBinder1
-            roadNameFlow.value = null
-            roadNameFlow.value = customBinder2
-            job.join()
-            assertEquals(4, collectedBinders.size)
-            assertTrue(collectedBinders[0] is RoadNameViewBinder)
-            assertTrue(collectedBinders[1] === customBinder1)
-            assertTrue(collectedBinders[2] is RoadNameViewBinder)
-            assertTrue(collectedBinders[3] === customBinder2)
-        }
-    }
-
-    @Test
-    fun `should use different default binder instances`() = runBlockingTest {
-        val collectedBinders = mutableListOf<Binder<ViewGroup>>()
-        coordinator.apply {
-            val job = launch {
-                mapboxNavigation.flowViewBinders().take(3).toList(collectedBinders)
-            }
-            roadNameFlow.value = mockk()
-            roadNameFlow.value = null
-            job.join()
-            assertTrue(collectedBinders[0] is RoadNameViewBinder)
-            assertTrue(collectedBinders[2] is RoadNameViewBinder)
-            assertFalse(collectedBinders[0] === collectedBinders[2])
         }
     }
 
--- qa-test-app/src/main/java/com/mapbox/navigation/qa_test_app/view/customnavview/CustomizedViewModel.kt
+++ qa-test-app/src/main/java/com/mapbox/navigation/qa_test_app/view/customnavview/CustomizedViewModel.kt
@@ -4,9 +4,43 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CustomizedViewModel : ViewModel() {
-    val useCustomViews = MutableLiveData(false)
+    // Debug
+    val showCameraDebugInfo = MutableLiveData(false)
+
+    // Activity
+    val fullScreen = MutableLiveData(false)
+
+    // General
+    val showManeuver = MutableLiveData(true)
+    val showSpeedLimit = MutableLiveData(true)
+    val showRoadName = MutableLiveData(true)
+    val showActionButtons = MutableLiveData(true)
     val useCustomStyles = MutableLiveData(false)
+    val useCustomViews = MutableLiveData(false)
+    val distanceFormatterMetric = MutableLiveData(false)
+    val routingProfile = MutableLiveData("--")
+
+    // Map
     val showCustomMapView = MutableLiveData(false)
+    val enableScalebar = MutableLiveData(false)
+
+    // Action Buttons
+    val actionsShowCompassButton = MutableLiveData(false)
+    val actionsShowCameraButton = MutableLiveData(true)
+    val actionsShowAudioButton = MutableLiveData(true)
+    val actionsShowRecenterButton = MutableLiveData(true)
+    val actionsAdditionalButtons = MutableLiveData(false)
+    val actionsCustomCompassButton = MutableLiveData(false)
+    val actionsCustomCameraButton = MutableLiveData(false)
+    val actionsCustomAudioButton = MutableLiveData(false)
+    val actionsCustomRecenterButton = MutableLiveData(false)
+    val actionsCustomLayout = MutableLiveData(false)
+
+    // Info Panel
+    val infoPanelShowTripProgress = MutableLiveData(true)
+    val infoPanelShowRoutePreviewButton = MutableLiveData(true)
+    val infoPanelShowStartNavigationButton = MutableLiveData(true)
+    val infoPanelShowEndNavigationButton = MutableLiveData(true)
     val customInfoPanelLayout = MutableLiveData("--")
     val useCustomInfoPanelStyles = MutableLiveData(false)
     val customInfoPanelContent = MutableLiveData("--")
@@ -16,17 +50,4 @@ class CustomizedViewModel : ViewModel() {
     val enableDestinationPreview = MutableLiveData(true)
     val isInfoPanelHideable = MutableLiveData(false)
     val infoPanelStateOverride = MutableLiveData("--")
-    val fullScreen = MutableLiveData(false)
-    val distanceFormatterMetric = MutableLiveData(false)
-    val showCameraDebugInfo = MutableLiveData(false)
-    val enableScalebar = MutableLiveData(false)
-    val routingProfile = MutableLiveData("--")
-
-    val actionsShowCompassButton = MutableLiveData(false)
-    val actionsAdditionalButtons = MutableLiveData(false)
-    val actionsCustomCompassButton = MutableLiveData(false)
-    val actionsCustomCameraButton = MutableLiveData(false)
-    val actionsCustomAudioButton = MutableLiveData(false)
-    val actionsCustomRecenterButton = MutableLiveData(false)
-    val actionsCustomLayout = MutableLiveData(false)
 }
