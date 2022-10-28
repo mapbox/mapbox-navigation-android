d925ca3276a3cd2a0d81680dd0b1d1354b804b71
VysotskiVadim <vadzim.vysotski@mapbox.com>
2022-10-28 12:15:11 +0200

    ignore errors

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/TestRouteRequest.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/TestRouteRequest.kt
@@ -54,8 +54,10 @@ class TestRouteRequest : BaseTest<EmptyTestActivity>(EmptyTestActivity::class.ja
     }
 
     @Test
-    fun set_navigation_routes_successfully() = sdkTest {
-        listOf(
+    fun set_navigation_routes_successfully() = sdkTest(timeout = 5 * 60 * 1000) {
+        val testRunId = Date().time.toString()
+        val testCases = listOf(
+            // short
             "7.544358,51.50893;7.262347,51.450093",
             "-0.469474,39.620483;-0.287629,39.564798",
             "121.338932,24.999322;121.212432,25.014613",
@@ -65,9 +67,40 @@ class TestRouteRequest : BaseTest<EmptyTestActivity>(EmptyTestActivity::class.ja
             "3.594571,50.966804;3.744523,51.038471",
             "18.035172,53.120348;17.980242,53.141947",
             "151.063636,-33.761515;150.924209,-33.676139",
-            "8.282866,48.836644;8.342386,49.031469"
-        ).forEach { coordinates ->
-            var response: RouteRequestResult.Success
+            "8.282866,48.836644;8.342386,49.031469",
+            // medium
+            "11.068773,48.395769;11.78753,48.351929",
+            "6.841077,50.641112;7.119309,50.879783",
+            "10.053927,48.973696;9.94959,49.39326",
+            "4.785689,51.166292;4.335537,51.172037",
+            "151.930139,-28.644001;152.014111,-29.03781",
+            "10.238754,49.72532;10.916919,49.892882",
+            "-117.8544,34.0684;-118.242378,34.047664",
+            "8.449441,49.661692;8.540599,49.340809",
+            // large
+            "7.497153,51.370885;9.923661,53.563794",
+            "9.850301,52.323563;12.51728,49.195625",
+            "15.854543,50.193145;13.235352,49.137469",
+            "9.26678,48.68429;14.432992,50.079143",
+            "15.640537,56.204498;13.00278,55.581761",
+            "8.538636,49.396247;10.741578,47.559664",
+            "6.315747,51.500173;10.157902,48.679648",
+            "7.241153,43.690799;5.832344,43.174577",
+            "9.462456,54.81521;8.70746,53.270071",
+            // huge
+            "11.5104917,48.1921736;-0.12719,51.50643",
+            "7.790255,48.3072049;5.00308,43.58463",
+            "13.569852,52.443647;8.423119,48.101636",
+            "6.785334,52.728778;13.600185,45.177684",
+            "-0.045608,46.441391;-6.942835,37.25444",
+            "6.769568,52.286483;16.322807,48.177671",
+            "7.228465,52.46652;13.600185,45.177684",
+            "2.475978,48.94695;0.042546,38.653993",
+            "0.87803,40.970268;6.44268,51.1955",
+            "0.967296,41.047474;6.44268,51.1955",
+        )
+        for (coordinates in testCases) {
+            var response: RouteRequestResult
             var requestTime: Long? = null
             var responseTime: Long? = null
             var responseSize: Int? = null
@@ -87,7 +120,7 @@ class TestRouteRequest : BaseTest<EmptyTestActivity>(EmptyTestActivity::class.ja
                     override fun onResponse(response: HttpResponse): HttpResponse {
                         if (response.request.url.contains(coordinates)) {
                             responseTime = Date().time
-                            responseSize = response.result.value!!.data.size
+                            responseSize = response.result.value?.data?.size
                         }
                         return response
                     }
@@ -99,12 +132,18 @@ class TestRouteRequest : BaseTest<EmptyTestActivity>(EmptyTestActivity::class.ja
                     .coordinates(coordinates)
                     .alternatives(true)
                     .build()
-                val result = mapboxNavigation.requestRoutes(routeOptions)
-                response = result as RouteRequestResult.Success
+                response = mapboxNavigation.requestRoutes(routeOptions)
             }
+            if (response is RouteRequestResult.Failure) {
+                val failure = response as RouteRequestResult.Failure
+                Log.e("time-test","error getting route for $coordinates: ${failure.reasons}")
+                continue
+            }
+            val successfulResponse = response as RouteRequestResult.Success
             writeResults(
+                testRunId = testRunId,
                 coordinates = coordinates,
-                distance = response.routes.first().directionsRoute.distance(),
+                distance = successfulResponse.routes.first().directionsRoute.distance(),
                 sdkResponseTime = time.inWholeMilliseconds,
                 networkResponseTime = responseTime!! - requestTime!!,
                 responseSize = responseSize!!
@@ -113,6 +152,7 @@ class TestRouteRequest : BaseTest<EmptyTestActivity>(EmptyTestActivity::class.ja
     }
 
     private fun writeResults(
+        testRunId: String,
         coordinates: String,
         distance: Double,
         sdkResponseTime: Long,
@@ -121,12 +161,13 @@ class TestRouteRequest : BaseTest<EmptyTestActivity>(EmptyTestActivity::class.ja
     ) {
         val file = File(
             Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
-            "route-request-time-test-results.csv"
+            "route-request-time-test-results-${testRunId}.csv"
         )
         val header = if (file.createNewFile()) {
             "coordinates,distance,time from request to parsed response in milliseconds,network request time,response size bytes,device\n"
         } else ""
-        val result = "$coordinates,$distance,$sdkResponseTime,$networkResponseTime,${responseSize},${Build.MODEL}\n"
+        val result =
+            "$coordinates,$distance,$sdkResponseTime,$networkResponseTime,${responseSize},${Build.MODEL}\n"
         file.appendText(header + result)
         Log.d("time-test", result + "(written to ${file.absolutePath})")
     }
