53f8c176aac0ed238594bae6f26c4c1dce091405
Kyle Madsen <kyle.mdsn@gmail.com>
2022-11-03 12:09:40 -0700

    Add MapboxCarMapLoader (#6530)

--- qa-test-app/src/main/java/com/mapbox/navigation/qa_test_app/testing/JavaInterfaceChecker.java
+++ qa-test-app/src/main/java/com/mapbox/navigation/qa_test_app/testing/JavaInterfaceChecker.java
@@ -1,8 +1,16 @@
 package com.mapbox.navigation.qa_test_app.testing;
 
+import static com.mapbox.maps.extension.style.StyleExtensionImplKt.style;
+
+import com.mapbox.androidauto.map.MapboxCarMapLoader;
+import com.mapbox.maps.extension.style.StyleContract;
+import com.mapbox.maps.extension.style.StyleExtensionImpl;
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp;
+import com.mapbox.navigation.ui.maps.NavigationStyles;
 import com.mapbox.navigation.ui.voice.api.MapboxAudioGuidance;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 import kotlinx.coroutines.CoroutineScope;
 
 class JavaInterfaceChecker {
@@ -19,5 +27,15 @@ class JavaInterfaceChecker {
     JavaFlow.collect(sut.stateFlow(), coroutineScope, (enabled) -> {
       // observe state
     });
+  }
+
+  void MapboxCarMapLoader() {
+    MapboxCarMapLoader sut = new MapboxCarMapLoader();
+    sut.setDarkStyleOverride(styleExtension(NavigationStyles.NAVIGATION_NIGHT_STYLE));
+    sut.setLightStyleOverride(styleExtension(NavigationStyles.NAVIGATION_DAY_STYLE));
+  }
+
+  StyleContract.StyleExtension styleExtension(String styleUri) {
+    return style(styleUri, builder -> Unit.INSTANCE);
   }
 }
