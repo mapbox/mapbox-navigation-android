84c0ab43dc1b81fafb09c37d70a62c1ae378aba0
Pablo Guardiola <pablo.guardiola@mapbox.com>
2022-11-11 15:09:16 -0500

    follow up to improve copilot kdocs (#6591)

--- libnavigation-copilot/src/main/java/com/mapbox/navigation/copilot/MapboxCopilot.kt
+++ libnavigation-copilot/src/main/java/com/mapbox/navigation/copilot/MapboxCopilot.kt
@@ -11,29 +11,31 @@ import java.util.concurrent.CopyOnWriteArraySet
 /**
  * Mapbox Copilot is a Navigation SDK component that collects detailed trace files of navigation sessions together with search analytics data.
  *
- * Mapbox Copilot collects information that helps customers in two different ways:
- * - It makes the feedback provided by drivers more actionable.
+ * Copilot collects information that helps you in two different ways:
+ * - It makes driver feedback more actionable.
  * - It provides Mapbox teams with the necessary information to investigate and resolve driver-reported issues and, consequently, helps expedite SDK updates that improve the driver experience for the entire developer community.
  *
- * It's a [MapboxNavigationObserver] so it's tied to [MapboxNavigation] lifecycle automatically.
- * It's highly recommended to keep track of the [DeveloperMetadata.copilotSessionId] (see [DeveloperMetadataObserver]) so that Mapbox teams can easily act on specific end-users feedback as Mapbox teams can easily find the respective traces and troubleshoot issues faster.
+ * Copilot is a [MapboxNavigationObserver], so it's tied to the [MapboxNavigation] lifecycle automatically.
+ * We recommended tracking the [DeveloperMetadata.copilotSessionId] (see [DeveloperMetadataObserver]) so that Mapbox teams can better act on specific end-user feedback. This ID helps Mapbox teams find the respective traces and troubleshoot issues faster.
  *
- * Mapbox Copilot is an opt-in by the customer (see [start] and [stop]) i.e. Navigation SDK customers have the choice to enable it for their drivers.
- * Depending on the use case, some customers can enable Copilot to all drivers (e.g. during a pilot) or just a subset of them.
- * **The application developer is responsible for communicating to their drivers when and what data is being collected from their drives.**
+ * Copilot is an opt-in feature (see [start] and [stop]), which means you have the choice to enable it for your users (drivers).
+ * Depending on the use case, you can enable Copilot for either all drivers (for example, during a pilot) or a subset of them.
+ * **As the application developer, you are responsible for communicating to drivers about the data that is being collected from their drives, including what kind of data is being collected and when it is collected.**
  *
  * Nav SDK exposes configuration settings (see [NavigationOptions.copilotOptions]) to use Copilot in two ways:
  * 1) Automatic data collection:
- * - Copilot will be enabled for all trips performed by a specific driver (default option).
+ * - Enable Copilot for all trips performed by a specific driver (default option).
  * 2) Manual data collection:
- * - Copilot data will only be sent when an end user submits negative feedback about a specific route to help take action on the issue.
- * Noting that this is tightly coupled to the Navigation SDK Feedback i.e. this is only effective if the feedback events are pushed through [MapboxNavigation] Feedback APIs (see [MapboxNavigation.postUserFeedback] and [MapboxNavigation.provideFeedbackMetadataWrapper]).
+ * - Copilot data is only sent when an end user submits negative feedback about a specific route to help take action on the issue.
+ * Data collection for Copilot is tightly coupled to the Navigation SDK Feedback, which means this is only effective if the feedback events are pushed through [MapboxNavigation] Feedback APIs (see [MapboxNavigation.postUserFeedback] and [MapboxNavigation.provideFeedbackMetadataWrapper]).
  *
  * If you would like to provide Search analytics into Copilot, you can send the Search events over to Copilot (see [push]).
  * This information would include whether a routable point for navigation was available.
  * Copilot helps understand the impact of search results to a navigation session (arrival experience, routable points).
  *
- * WARNING: Mapbox Copilot is currently in public-preview and hence Copilot related entities and APIs are currently marked as [ExperimentalPreviewMapboxNavigationAPI] and subject to change. These will be removed when going GA.
+ * WARNING: Mapbox Copilot is currently in public-preview. Copilot-related entities and APIs are currently marked as [ExperimentalPreviewMapboxNavigationAPI] and subject to change. These markings will be removed when the feature is generally available.
+ *
+ * Copilot is a library included in the Navigation SDK that Processes full-trip-trace longitude and latitude data ("**Copilot**"). Copilot is turned off by default and optionally enabled by Customer at the application developer level to improve feedback resolution. If Customer enables Copilot, Customer shall obtain and maintain all necessary consents and permissions, including providing notice to and obtaining End Users' affirmative express consent before any access or use of Copilot.
  *
  * @see [NavigationOptions.copilotOptions]
  * @see [MapboxNavigation.navigationOptions] [EventsAppMetadata]
