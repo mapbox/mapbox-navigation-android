9448080b015a1eaa6ed85f6a611659cdc420101c
Tomasz Rybakiewicz <tomasz.rybakiewicz@mapbox.com>
2022-11-15 16:59:23 -0500

    Disabled layout transitions in all InfoPanelHeader***Binders. Manually installing header content using LayoutInflater instead of Scene API.

--- libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/infopanel/InfoPanelHeaderActiveGuidanceBinder.kt
+++ libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/infopanel/InfoPanelHeaderActiveGuidanceBinder.kt
@@ -1,11 +1,9 @@
 package com.mapbox.navigation.dropin.infopanel
 
-import android.transition.Scene
-import android.transition.TransitionManager
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.mapbox.navigation.core.internal.extensions.navigationListOf
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
-import com.mapbox.navigation.dropin.R
 import com.mapbox.navigation.dropin.databinding.MapboxInfoPanelHeaderActiveGuidanceLayoutBinding
 import com.mapbox.navigation.dropin.internal.extensions.endNavigationButtonComponent
 import com.mapbox.navigation.dropin.internal.extensions.tripProgressComponent
@@ -17,13 +15,11 @@ internal class InfoPanelHeaderActiveGuidanceBinder(
 ) : UIBinder {
 
     override fun bind(viewGroup: ViewGroup): MapboxNavigationObserver {
-        val scene = Scene.getSceneForLayout(
-            viewGroup,
-            R.layout.mapbox_info_panel_header_active_guidance_layout,
-            viewGroup.context
+        viewGroup.removeAllViews()
+        val binding = MapboxInfoPanelHeaderActiveGuidanceLayoutBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup
         )
-        TransitionManager.go(scene)
-        val binding = MapboxInfoPanelHeaderActiveGuidanceLayoutBinding.bind(viewGroup)
 
         return context.run {
             navigationListOf(
--- libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/infopanel/InfoPanelHeaderRoutesPreviewBinder.kt
+++ libnavui-dropin/src/main/java/com/mapbox/navigation/dropin/infopanel/InfoPanelHeaderRoutesPreviewBinder.kt
@@ -1,11 +1,9 @@
 package com.mapbox.navigation.dropin.infopanel
 
-import android.transition.Scene
-import android.transition.TransitionManager
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.mapbox.navigation.core.internal.extensions.navigationListOf
 import com.mapbox.navigation.core.lifecycle.MapboxNavigationObserver
-import com.mapbox.navigation.dropin.R
 import com.mapbox.navigation.dropin.databinding.MapboxInfoPanelHeaderRoutePreviewLayoutBinding
 import com.mapbox.navigation.dropin.internal.extensions.startNavigationButtonComponent
 import com.mapbox.navigation.dropin.internal.extensions.tripProgressComponent
@@ -17,13 +15,11 @@ internal class InfoPanelHeaderRoutesPreviewBinder(
 ) : UIBinder {
 
     override fun bind(viewGroup: ViewGroup): MapboxNavigationObserver {
-        val scene = Scene.getSceneForLayout(
-            viewGroup,
-            R.layout.mapbox_info_panel_header_route_preview_layout,
-            viewGroup.context
+        viewGroup.removeAllViews()
+        val binding = MapboxInfoPanelHeaderRoutePreviewLayoutBinding.inflate(
+            LayoutInflater.from(viewGroup.context),
+            viewGroup
         )
-        TransitionManager.go(scene)
-        val binding = MapboxInfoPanelHeaderRoutePreviewLayoutBinding.bind(viewGroup)
 
         return context.run {
             navigationListOf(
