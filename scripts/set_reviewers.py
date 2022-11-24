51bc88b5e0ee1ceb631c6f31a67110d6c36b141a
sevastian.zhukov <sevastian.zhukov@mapbox.com>
2022-11-24 18:19:24 +0700

    test gh token

--- scripts/set_reviewers.py
+++ scripts/set_reviewers.py
@@ -1,5 +1,11 @@
-import os
+import requests, os
 
+pr_number = os.environ['PR_NUMBER']
 token = os.environ['GITHUB_WRITER_TOKEN']
 
-print(token)
+pr_url = "https://api.github.com/repos/mapbox/mapbox-navigation-android/pulls/" + pr_number
+
+headers = {"Authorization": "Bearer " + token}
+pr = requests.get(pr_url, headers=headers).json()
+
+print(pr)
