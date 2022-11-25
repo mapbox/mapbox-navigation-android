f75381d0995065442891ae275be392263c99a590
sevastian.zhukov <sevastian.zhukov@mapbox.com>
2022-11-25 16:28:51 +0700

    fix creds

--- scripts/set_reviewers.py
+++ scripts/set_reviewers.py
@@ -1,11 +1,30 @@
 import requests, os
 
 pr_number = os.environ['PR_NUMBER']
-token = os.environ['GITHUB_WRITER_TOKEN']
+token = os.environ['GITHUB_TOKEN']
 
 pr_url = "https://api.github.com/repos/mapbox/mapbox-navigation-android/pulls/" + pr_number
 
 headers = {"Authorization": "Bearer " + token}
 pr = requests.get(pr_url, headers=headers).json()
 
-print(pr)
+if pr['draft']:
+    print("It is draft pr")
+    exit()
+
+author = pr['user']['login']
+current_reviewers = list(map(lambda reviewer: reviewer['login'], pr['requested_reviewers']))
+
+# check existing approvals on pr
+
+reviews_url = "https://api.github.com/repos/mapbox/mapbox-navigation-android/pulls/" + pr_number + "/reviews"
+reviews = requests.get(reviews_url, headers=headers).json()
+for review in reviews:
+    if review['state'] == 'APPROVED':
+        current_reviewers.append(review['user']['login'])
+
+if len(current_reviewers) >= 2:
+    print("2 or more reviewers already assigned")
+    exit()
+
+users = []
