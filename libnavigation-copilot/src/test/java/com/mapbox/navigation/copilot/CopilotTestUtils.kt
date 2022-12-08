71bf2bbf0696d78eba65d26c05deea42619af7d6
Pablo Guardiola <pablo.guardiola@mapbox.com>
2022-12-08 00:21:42 -0500

    use either mapbox navigation app lifecycle owner or process lifecycle owner based on if mapbox navigation app is set up or not respectively to support both use cases in which mapbox navigation app is or is not integrated from the client side and add is copilot initialized check to prevent issues if apis are misused (#6690)

--- libnavigation-copilot/src/test/java/com/mapbox/navigation/copilot/CopilotTestUtils.kt
+++ libnavigation-copilot/src/test/java/com/mapbox/navigation/copilot/CopilotTestUtils.kt
@@ -1,13 +1,29 @@
 package com.mapbox.navigation.copilot
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.mapbox.navigation.core.lifecycle.MapboxNavigationApp
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
 
-object CopilotTestUtils {
+internal object CopilotTestUtils {
 
-    internal fun retrieveAttachments(metadata: String): List<AttachmentMetadata> {
+    fun retrieveAttachments(metadata: String): List<AttachmentMetadata> {
         val gson = Gson()
         val itemType = object : TypeToken<List<AttachmentMetadata>>() {}.type
         return gson.fromJson(metadata, itemType)
+    }
+
+    fun prepareLifecycleOwnerMockk(): LifecycleOwner {
+        mockkStatic(MapboxNavigationApp::class)
+        every { MapboxNavigationApp.isSetup() } returns true
+        val mockedLifecycleOwner = mockk<LifecycleOwner>(relaxed = true)
+        val mockedLifecycle = mockk<Lifecycle>(relaxed = true)
+        every { mockedLifecycleOwner.lifecycle } returns mockedLifecycle
+        every { MapboxNavigationApp.lifecycleOwner } returns mockedLifecycleOwner
+        return mockedLifecycleOwner
     }
 }
