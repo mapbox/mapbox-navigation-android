46fb4df757e77aeb9348c95049d27c32b0347cde
Tomasz Rybakiewicz <tomaszrybakiewicz@users.noreply.github.com>
2022-12-16 15:29:08 -0500

    Updated the default, NavigationView navigation puck. (#6678)
    Added QA-Test-App “use legacy puck” toggle.
    
    Updated navigation puck vector drawable.
    
    CHANGELOG entry
    
    Updated navigation puck asset.
    Added NavPuck spinner to QA-Test-App.
    
    More changes to puck asset
    
    Using RenderScript to apply blurred shadow to new navigation puck.

--- libnavui-util/src/main/java/com/mapbox/navigation/ui/utils/internal/extensions/DrawableEx.kt
+++ libnavui-util/src/main/java/com/mapbox/navigation/ui/utils/internal/extensions/DrawableEx.kt
@@ -1,9 +1,15 @@
 package com.mapbox.navigation.ui.utils.internal.extensions
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
+import kotlin.math.roundToInt
 
 /**
  * Converts a [Drawable] to [Bitmap]
@@ -25,3 +31,35 @@ fun Drawable.getBitmap(): Bitmap {
         bitmap
     }
 }
+
+/**
+ * Return Drawable with blur effect.
+ */
+fun Drawable.withBlurEffect(context: Context, radius: Float): Drawable {
+    return BitmapDrawable(
+        context.resources,
+        blurBitmap(context, getBitmap(), radius)
+    )
+}
+
+private fun blurBitmap(
+    context: Context,
+    image: Bitmap,
+    radius: Float,
+    scale: Float = 1.0f
+): Bitmap = runCatching {
+    val width = (image.width * scale).roundToInt()
+    val height = (image.height * scale).roundToInt()
+    val inputBitmap = Bitmap.createScaledBitmap(image, width, height, false)
+    val outputBitmap = Bitmap.createBitmap(inputBitmap)
+    val rs = RenderScript.create(context)
+    val intrinsic = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+    val tmpIn = Allocation.createFromBitmap(rs, inputBitmap)
+    val tmpOut = Allocation.createFromBitmap(rs, outputBitmap)
+    intrinsic.setRadius(radius)
+    intrinsic.setInput(tmpIn)
+    intrinsic.forEach(tmpOut)
+    tmpOut.copyTo(outputBitmap)
+    rs.destroy()
+    outputBitmap
+}.getOrDefault(image) // return original image on failure
