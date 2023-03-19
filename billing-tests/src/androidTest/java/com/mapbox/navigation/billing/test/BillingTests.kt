872f1443508d332bda2e087b5be87b7f2239add7
Dzmitry Fomchyn <dimafomchin@gmail.com>
2023-03-19 17:27:28 +0300

    Base module

---
+++ billing-tests/src/androidTest/java/com/mapbox/navigation/billing/test/BillingTests.kt
@@ -0,0 +1,11 @@
+package com.mapbox.navigation.billing_tests.billing
+
+import android.location.Location
+import com.mapbox.navigation.testing.ui.BaseCoreNoCleanUpTest
+
+class BillingTests : BaseCoreNoCleanUpTest() {
+
+    override fun setupMockLocation(): Location {
+        TODO("Not yet implemented")
+    }
+}
