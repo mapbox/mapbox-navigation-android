043df1fc1a3eae1e6db1ab86fae06031caeddc7a
sevastian.zhukov <sevastian.zhukov@mapbox.com>
2023-03-27 19:42:50 +0700

    checkout branch tests

--- scripts/checkout-branch.py
+++ scripts/checkout-branch.py
@@ -2,22 +2,12 @@ import subprocess
 
 import requests
 
+import checkout_branch_utils
+
 tags = requests.get('https://api.github.com/repos/mapbox/mapbox-navigation-android/git/refs/tags').json()
+latest_tag = checkout_branch_utils.get_latest_tag(tags)
+print('Latest no-patch release is ' + latest_tag)
 
-for tag in reversed(tags):
-    tag_name = tag['ref'].replace('refs/tags/', '')
-    if tag_name.startswith('v') and tag_name.partition('-')[0].endswith('.0'):
-        latest_no_patch_tag = tag_name
-        break
-print('Latest no-patch release is ' + latest_no_patch_tag)
-
-branch_name = 'release-' + latest_no_patch_tag.partition('.0')[0]
-print('Checking if ' + branch_name + ' branch exists...')
-
-branch_response = requests.get('https://api.github.com/repos/mapbox/mapbox-navigation-android/branches/' + branch_name)
-
-if branch_response.ok:
-    print(branch_name + ' exists. Checking out...')
-    subprocess.run("git checkout $release_branch", shell=True, check=True)
-else:
-    print(branch_name + ' does not exist.')
+snapshot_branch = checkout_branch_utils.get_snapshot_branch(latest_tag)
+print('Snapshot branch is ' + snapshot_branch)
+subprocess.run("git checkout " + snapshot_branch, shell=True, check=True)
