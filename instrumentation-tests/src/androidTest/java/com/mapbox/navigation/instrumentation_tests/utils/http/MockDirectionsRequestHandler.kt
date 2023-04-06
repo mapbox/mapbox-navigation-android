981527a0571a9e438fac0f54a96491d00ad337ee
vadzim_v <vadzim.vysotski@mapbox.com>
2023-04-06 16:45:35 +0200

    test for NN-654 (#7075)

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/http/MockDirectionsRequestHandler.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/http/MockDirectionsRequestHandler.kt
@@ -13,12 +13,19 @@ import okhttp3.mockwebserver.RecordedRequest
  * @param expectedCoordinates optionally the expected coordinates
  * that the handler should match when providing the response
  */
-data class MockDirectionsRequestHandler(
+data class MockDirectionsRequestHandler constructor(
     val profile: String,
-    val jsonResponse: String,
+    val lazyJsonResponse: () -> String,
     val expectedCoordinates: List<Point>?,
     val relaxedExpectedCoordinates: Boolean = false,
 ) : BaseMockRequestHandler() {
+
+    constructor(
+        profile: String,
+        jsonResponse: String,
+        expectedCoordinates: List<Point>?,
+        relaxedExpectedCoordinates: Boolean = false,
+    ) : this(profile, { jsonResponse }, expectedCoordinates, relaxedExpectedCoordinates)
 
     var jsonResponseModifier: ((String) -> String) = { it }
 
@@ -30,7 +37,7 @@ data class MockDirectionsRequestHandler(
         }
 
         return if (request.path!!.startsWith(prefix)) {
-            MockResponse().setBody(jsonResponseModifier(jsonResponse))
+            MockResponse().setBody(jsonResponseModifier(lazyJsonResponse()))
         } else {
             null
         }
@@ -49,7 +56,7 @@ data class MockDirectionsRequestHandler(
             "profile='$profile', " +
             "expectedCoordinates=$expectedCoordinates, " +
             "relaxedExpectedCoordinates=$relaxedExpectedCoordinates, " +
-            "jsonResponse='$jsonResponse'" +
+            "lazyJsonResponse='$lazyJsonResponse'" +
             ")"
     }
 }
