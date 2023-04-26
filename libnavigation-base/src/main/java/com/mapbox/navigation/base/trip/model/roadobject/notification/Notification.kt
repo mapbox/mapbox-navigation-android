bc3d0fa1c43e0a78595f95a874b573482e862fba
VysotskiVadim <vadzim.vysotski@mapbox.com>
2023-04-26 16:36:00 +0200

    more fixes for equals

--- libnavigation-base/src/main/java/com/mapbox/navigation/base/trip/model/roadobject/notification/Notification.kt
+++ libnavigation-base/src/main/java/com/mapbox/navigation/base/trip/model/roadobject/notification/Notification.kt
@@ -31,6 +31,7 @@ internal class Notification internal constructor(
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
 
         return true
     }
