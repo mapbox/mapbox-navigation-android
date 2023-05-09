8376618a4d0142e745f279b5840fce87e427e3ea
VysotskiVadim <vadzim.vysotski@mapbox.com>
2023-05-09 10:58:49 +0200

    extracted tilestore creator

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/MapboxNavigationCreator.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/MapboxNavigationCreator.kt
@@ -1,7 +1,10 @@
 package com.mapbox.navigation.instrumentation_tests.utils
 
 import androidx.test.platform.app.InstrumentationRegistry
+import com.mapbox.bindgen.Value
+import com.mapbox.common.TileDataDomain
 import com.mapbox.common.TileStore
+import com.mapbox.common.TileStoreOptions
 import com.mapbox.navigation.base.options.NavigationOptions
 import com.mapbox.navigation.base.options.RoutingTilesOptions
 import com.mapbox.navigation.core.MapboxNavigation
@@ -38,4 +41,16 @@ inline fun BaseCoreNoCleanUpTest.withMapboxNavigation(
     } finally {
         navigation.onDestroy()
     }
+}
+
+
+fun createTileStore(): TileStore {
+    val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+    val tileStore = TileStore.create()
+    tileStore.setOption(
+        TileStoreOptions.MAPBOX_ACCESS_TOKEN,
+        TileDataDomain.NAVIGATION,
+        Value.valueOf(getMapboxAccessTokenFromResources(targetContext))
+    )
+    return tileStore
 }
