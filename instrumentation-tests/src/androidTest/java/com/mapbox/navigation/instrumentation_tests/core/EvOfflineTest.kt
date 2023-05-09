840e2b515496162b856291c1fb064ac3ce6d81fa
VysotskiVadim <vadzim.vysotski@mapbox.com>
2023-05-09 17:13:20 +0200

    use mock web server

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/EvOfflineTest.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/EvOfflineTest.kt
@@ -1,15 +1,20 @@
 package com.mapbox.navigation.instrumentation_tests.core
 
 import android.location.Location
+import com.mapbox.api.directions.v5.DirectionsCriteria
 import com.mapbox.api.directions.v5.models.RouteOptions
 import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.extensions.applyDefaultNavigationOptions
 import com.mapbox.navigation.base.route.RouterOrigin
 import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.directions.session.RoutesExtra
+import com.mapbox.navigation.instrumentation_tests.R
 import com.mapbox.navigation.instrumentation_tests.utils.OfflineRegion
 import com.mapbox.navigation.instrumentation_tests.utils.createTileStore
+import com.mapbox.navigation.instrumentation_tests.utils.http.MockDirectionsRequestHandler
 import com.mapbox.navigation.instrumentation_tests.utils.loadRegion
+import com.mapbox.navigation.instrumentation_tests.utils.readRawFileText
 import com.mapbox.navigation.instrumentation_tests.utils.withMapboxNavigation
 import com.mapbox.navigation.instrumentation_tests.utils.withoutInternet
 import com.mapbox.navigation.testing.ui.BaseCoreNoCleanUpTest
@@ -31,6 +36,7 @@ import kotlinx.coroutines.launch
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 class EvOfflineTest : BaseCoreNoCleanUpTest() {
@@ -40,6 +46,16 @@ class EvOfflineTest : BaseCoreNoCleanUpTest() {
             longitude = 13.361378213031003
             latitude = 52.49813341962201
         }
+    }
+
+    @Before
+    fun setupMockRoutes() {
+        val evRouteRequestHandler = MockDirectionsRequestHandler(
+            profile = DirectionsCriteria.PROFILE_DRIVING_TRAFFIC,
+            jsonResponse = readRawFileText(context, R.raw.ev_routes_berlin),
+            expectedCoordinates = evRouteInBerlin().coordinatesList(),
+        )
+        mockWebServerRule.requestHandlers.add(evRouteRequestHandler)
     }
 
     @Test
@@ -68,7 +84,6 @@ class EvOfflineTest : BaseCoreNoCleanUpTest() {
                 longitude = 13.361378213031003,
                 latitude = 52.49813341962201
             )
-            // TODO: use mock server
             val onlineAlternative = navigation.alternativesUpdates()
                 .filterIsInstance<NavigationRouteAlternativesResult.OnRouteAlternatives>()
                 .filter { it.routerOrigin == RouterOrigin.Offboard }
@@ -80,6 +95,17 @@ class EvOfflineTest : BaseCoreNoCleanUpTest() {
 
     @Test
     fun deviateFromOnlinePrimaryRouteWithoutInternet() = sdkTest(timeout = 60_000) {
+
+        val newRouteOnlineRouteRequestHandler = MockDirectionsRequestHandler(
+            profile = DirectionsCriteria.PROFILE_DRIVING_TRAFFIC,
+            jsonResponse = readRawFileText(context, R.raw.ev_routes_berlin_reroute),
+            expectedCoordinates = listOf(
+                Point.fromLngLat(13.36742058325467, 52.49745756017697),
+                evRouteInBerlin().coordinatesList().last()
+            )
+        )
+        mockWebServerRule.requestHandlers.add(newRouteOnlineRouteRequestHandler)
+
         withMapboxNavigationAndOfflineTilesForRegion(
             BERLIN_OFFLINE_REGION
         ) { navigation ->
@@ -100,7 +126,7 @@ class EvOfflineTest : BaseCoreNoCleanUpTest() {
                 assertEquals(RouterOrigin.Onboard, newRoutes.navigationRoutes.first().origin)
                 updates.cancel()
             }
-            val updates = stayOnPositionAsync( //origin position
+            val updates = stayOnPositionAsync( // origin position after reroute
                 longitude = 13.36742058325467,
                 latitude = 52.49745756017697
             )
@@ -130,6 +156,23 @@ class EvOfflineTest : BaseCoreNoCleanUpTest() {
         }
     }
 
+    private fun evRouteInBerlin() = RouteOptions.builder()
+        .applyDefaultNavigationOptions()
+        .baseUrl(mockWebServerRule.baseUrl) // comment to use real server
+        .coordinates("13.361378213031003,52.49813341962201;13.393450988895268,52.50913924804004")
+        .annotations("state_of_charge")
+        .alternatives(true)
+        .waypointsPerRoute(true)
+        .unrecognizedProperties(mapOf(
+            "engine" to "electric",
+            "ev_initial_charge" to "6000",
+            "ev_max_charge" to "50000",
+            "ev_connector_types" to "ccs_combo_type1,ccs_combo_type2",
+            "energy_consumption_curve" to "0,300;20,160;80,140;120,180",
+            "ev_charging_curve" to "0,100000;40000,70000;60000,30000;80000,10000",
+            "ev_min_charge_at_charging_station" to "1"
+        ))
+        .build()
 }
 
 private suspend inline fun BaseCoreNoCleanUpTest.withMapboxNavigationAndOfflineTilesForRegion(
@@ -150,22 +193,7 @@ private suspend fun downloadBerlinRoutingTiles(navigation: MapboxNavigation, reg
     loadRegion(navigation, region)
 }
 
-private fun evRouteInBerlin() = RouteOptions.builder()
-    .applyDefaultNavigationOptions()
-    .coordinates("13.361378213031003,52.49813341962201;13.393450988895268,52.50913924804004")
-    .annotations("state_of_charge")
-    .alternatives(true)
-    .waypointsPerRoute(true)
-    .unrecognizedProperties(mapOf(
-        "engine" to "electric",
-        "ev_initial_charge" to "6000",
-        "ev_max_charge" to "50000",
-        "ev_connector_types" to "ccs_combo_type1,ccs_combo_type2",
-        "energy_consumption_curve" to "0,300;20,160;80,140;120,180",
-        "ev_charging_curve" to "0,100000;40000,70000;60000,30000;80000,10000",
-        "ev_min_charge_at_charging_station" to "1"
-    ))
-    .build()
+
 
 private val BERLIN_GEOMETRY = FeatureCollection.fromJson(
     "{\n" +
