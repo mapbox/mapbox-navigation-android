f775a6eba6322fa3c668f7a5c793fabc645ab51b
vadzim_v <vadzim.vysotski@mapbox.com>
2023-05-17 11:27:34 +0200

    NAVAND-1250 auto tests for offline EV routing fallback + NavigationRoute#waypoints workaround  (#7165)

---
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/utils/location/LocationUpdate.kt
@@ -0,0 +1,32 @@
+package com.mapbox.navigation.instrumentation_tests.utils.location
+
+import com.mapbox.navigation.testing.ui.BaseCoreNoCleanUpTest
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+suspend fun BaseCoreNoCleanUpTest.stayOnPosition(
+    latitude: Double,
+    longitude: Double,
+    bearing: Float,
+    frequencyHz: Int = 10,
+    block: suspend () -> Unit
+) {
+    coroutineScope {
+        val updateLocations = launch {
+            while (true) {
+                mockLocationUpdatesRule.pushLocationUpdate {
+                    this.latitude = latitude
+                    this.longitude = longitude
+                    this.bearing = bearing
+                }
+                delay(1000L / frequencyHz)
+            }
+        }
+        try {
+            block()
+        } finally {
+            updateLocations.cancel()
+        }
+    }
+}
