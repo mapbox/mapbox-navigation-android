2d935c3e1b2413ddd3e7a32096b01575462d9fe6
Dzina <dzina.dybouskaya@mapbox.com>
2023-12-08 17:00:47 +0300

    NAVAND-1636: do not set routes to NN when session is already started

---
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/ui/VoiceInstructionsTest.kt
@@ -0,0 +1,97 @@
+package com.mapbox.navigation.instrumentation_tests.ui
+
+import android.location.Location
+import androidx.test.espresso.Espresso
+import com.mapbox.api.directions.v5.models.VoiceInstructions
+import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.base.options.RoutingTilesOptions
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.MapboxNavigationProvider
+import com.mapbox.navigation.core.replay.route.ReplayRouteSession
+import com.mapbox.navigation.core.trip.session.VoiceInstructionsObserver
+import com.mapbox.navigation.instrumentation_tests.utils.assertions.waitUntilHasSize
+import com.mapbox.navigation.instrumentation_tests.utils.history.MapboxHistoryTestRule
+import com.mapbox.navigation.instrumentation_tests.utils.location.MockLocationReplayerRule
+import com.mapbox.navigation.instrumentation_tests.utils.location.stayOnPosition
+import com.mapbox.navigation.instrumentation_tests.utils.routes.RoutesProvider
+import com.mapbox.navigation.instrumentation_tests.utils.routes.RoutesProvider.toNavigationRoutes
+import com.mapbox.navigation.testing.ui.BaseCoreNoCleanUpTest
+import com.mapbox.navigation.testing.ui.utils.MapboxNavigationRule
+import com.mapbox.navigation.testing.ui.utils.coroutines.sdkTest
+import com.mapbox.navigation.testing.ui.utils.coroutines.setNavigationRoutesAsync
+import com.mapbox.navigation.testing.ui.utils.getMapboxAccessTokenFromResources
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.net.URI
+
+class VoiceInstructionsTest : BaseCoreNoCleanUpTest() {
+
+    @get:Rule
+    val mapboxNavigationRule = MapboxNavigationRule()
+
+    @get:Rule
+    val mockLocationReplayerRule = MockLocationReplayerRule(mockLocationUpdatesRule)
+
+    @get:Rule
+    val mapboxHistoryTestRule = MapboxHistoryTestRule()
+
+    @Before
+    fun setup() {
+        Espresso.onIdle()
+    }
+
+    override fun setupMockLocation(): Location {
+        val mockRoute = RoutesProvider.dc_very_short(context)
+        return mockLocationUpdatesRule.generateLocationUpdate {
+            latitude = mockRoute.routeWaypoints.first().latitude()
+            longitude = mockRoute.routeWaypoints.first().longitude()
+        }
+    }
+
+    @OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
+    @Test
+    fun voiceInstructionIsDuplicatedOnceWhenReplayIsStarted() = sdkTest {
+        val mockRoute = RoutesProvider.dc_very_short(context)
+        mockWebServerRule.requestHandlers.addAll(mockRoute.mockRequestHandlers)
+        val voiceInstructions = mutableListOf<VoiceInstructions>()
+        val voiceInstructionsObserver = VoiceInstructionsObserver {
+            voiceInstructions.add(it)
+        }
+        val mapboxNavigation = createMapboxNavigation()
+        mapboxNavigation.registerVoiceInstructionsObserver(voiceInstructionsObserver)
+        stayOnPosition(
+            mockRoute.routeWaypoints.first(),
+            0f,
+        ) {
+            mapboxNavigation.startTripSession()
+            mapboxNavigation.setNavigationRoutesAsync(mockRoute.toNavigationRoutes())
+            voiceInstructions.waitUntilHasSize(1)
+        }
+        val relayRouteSession = ReplayRouteSession()
+        relayRouteSession.onAttached(mapboxNavigation)
+        voiceInstructions.waitUntilHasSize(3, timeoutMillis = 15000)
+
+        // the first instruction is duplicated once as a result of starting replay session
+        assertEquals(voiceInstructions[0], voiceInstructions[1])
+        // the first instruction id not duplicated anymore
+        assertNotEquals(voiceInstructions[1], voiceInstructions[2])
+    }
+
+    private fun createMapboxNavigation(): MapboxNavigation {
+        val navigationOptions = NavigationOptions.Builder(context)
+            .accessToken(getMapboxAccessTokenFromResources(context))
+            .routingTilesOptions(
+                RoutingTilesOptions.Builder()
+                    .tilesBaseUri(URI(mockWebServerRule.baseUrl))
+                    .build()
+            )
+            .build()
+        return MapboxNavigationProvider.create(navigationOptions).also {
+            mapboxHistoryTestRule.historyRecorder = it.historyRecorder
+        }
+    }
+}
