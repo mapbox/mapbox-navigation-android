33183efc5414b6b35219368e71a8ae6e9d6b84f8
VysotskiVadim <vadzim.vysotski@mapbox.com>
2024-01-02 11:15:37 +0100

    measure together with shapes

--- benchmark/src/androidTest/java/com/mapbox/navigation/benchmark/ExampleBenchmark.kt
+++ benchmark/src/androidTest/java/com/mapbox/navigation/benchmark/ExampleBenchmark.kt
@@ -11,7 +11,7 @@ import com.mapbox.common.LogConfiguration
 import com.mapbox.common.LoggingLevel
 import com.mapbox.geojson.LineString
 import com.mapbox.navigator.RoadObjectType
-import com.mapbox.navigator.RouteAlertLocation
+import com.mapbox.navigator.RouteInterface
 import com.mapbox.navigator.RouteParser
 import com.mapbox.navigator.RouterOrigin
 import org.junit.Rule
@@ -29,6 +29,10 @@ private const val LOG_TAG = "vadzim-test"
 @RunWith(AndroidJUnit4::class)
 class ExampleBenchmark {
 
+    init {
+        LogConfiguration.setLoggingLevel(LoggingLevel.DEBUG)
+    }
+
     val context: Context
         get() = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
 
@@ -36,29 +40,22 @@ class ExampleBenchmark {
     val benchmarkRule = BenchmarkRule()
 
     @Test
-    fun log() {
-        LogConfiguration.setLoggingLevel(LoggingLevel.DEBUG)
-        val response = readRawFileText(context, R.raw.test_route)
-        val route = RouteParser.parseDirectionsResponse(
-            response,
-            "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/14.958055640969633,51.2002127680808;5.2869509774857875,51.058961031629536?access_token=***&geometries=polyline6&overview=full&steps=true&continue_straight=true&annotations=congestion_numeric%2Cmaxspeed%2Cclosure%2Cspeed%2Cduration%2Cdistance&language=en&roundabout_exits=true&voice_instructions=true&banner_instructions=true&voice_units=imperial&enable_refresh=true",
-            RouterOrigin.ONLINE
-        ).onError {
-            Log.d(LOG_TAG, "error parsing route $it")
-            Log.d(LOG_TAG, "response was $response")
+    fun timeToRetrieveRouteAlerts() {
+        val route = routeWithLongIncident()
+        logShapeOfFirstRouteAlertLocation(route)
+        benchmarkRule.measureRepeated {
+            val alerts = route.routeInfo.alerts
+            this.runWithTimingDisabled {
+                Runtime.getRuntime().gc();
+            }
         }
-            .value!!.first()
-        val alertsCount = route.routeInfo.alerts.size
-        val shape = route.routeInfo.alerts
-            .filter { it.roadObject.type == RoadObjectType.INCIDENT }
-            .map { it.roadObject.location }
-            .first()
-            .routeAlertLocation
-            .shape
-        Log.d(LOG_TAG, "alerts count is $alertsCount")
-        Log.d(LOG_TAG, "shape is $shape")
-        val lineStringCoordinates = (shape as? LineString)?.coordinates()?.size
-        Log.d(LOG_TAG, "Line string coordinates count is $lineStringCoordinates")
+    }
+
+    @Test
+    fun timeToRetrieveRouteAlertsWithShapes() {
+        LogConfiguration.setLoggingLevel(LoggingLevel.DEBUG)
+        val route = routeWithLongIncident()
+        logShapeOfFirstRouteAlertLocation(route)
         benchmarkRule.measureRepeated {
             val alerts = route.routeInfo.alerts
             alerts.forEach {
@@ -71,6 +68,33 @@ class ExampleBenchmark {
                 Runtime.getRuntime().gc();
             }
         }
+    }
+
+    private fun logShapeOfFirstRouteAlertLocation(route: RouteInterface) {
+        val alertsCount = route.routeInfo.alerts.size
+        val shape = route.routeInfo.alerts
+            .filter { it.roadObject.type == RoadObjectType.INCIDENT }
+            .map { it.roadObject.location }
+            .first()
+            .routeAlertLocation
+            .shape
+        Log.d(LOG_TAG, "alerts count is $alertsCount")
+        Log.d(LOG_TAG, "shape is $shape")
+        val lineStringCoordinates = (shape as? LineString)?.coordinates()?.size
+        Log.d(LOG_TAG, "Line string coordinates count is $lineStringCoordinates")
+    }
+
+    private fun routeWithLongIncident(): RouteInterface {
+        val response = readRawFileText(context, R.raw.test_route)
+        val route = RouteParser.parseDirectionsResponse(
+            response,
+            "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/14.958055640969633,51.2002127680808;5.2869509774857875,51.058961031629536?access_token=***&geometries=polyline6&overview=full&steps=true&continue_straight=true&annotations=congestion_numeric%2Cmaxspeed%2Cclosure%2Cspeed%2Cduration%2Cdistance&language=en&roundabout_exits=true&voice_instructions=true&banner_instructions=true&voice_units=imperial&enable_refresh=true",
+            RouterOrigin.ONLINE
+        ).onError {
+            Log.d(LOG_TAG, "error parsing route $it")
+            Log.d(LOG_TAG, "response was $response")
+        }.value!!.first()
+        return route
     }
 }
 
