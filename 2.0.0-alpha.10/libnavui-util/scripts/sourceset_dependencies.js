2f5c986243779f379d31fb31d9679eea659e266b
Pablo Guardiola <pablo.guardiola@mapbox.com>
2024-05-29 18:24:57 -0400

    remove everything except 2.18.2

--- 2.0.0-alpha.10/libnavui-util/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":libnavui-util:dokkaHtml/debug": [], ":libnavui-util:dokkaHtml/main": [], ":libnavui-util:dokkaHtml/release": []}'
--- 2.0.0-beta.6/libnavigation-metrics/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies = '{":libnavigation-metrics:dokkaHtml/debug": [], ":libnavigation-metrics:dokkaHtml/main": [], ":libnavigation-metrics:dokkaHtml/release": []}'
