b47e91ea6fd6ec6960a4b436b8855d6575f40bff
Dzmitry Fomchyn <5161509+DzmitryFomchyn@users.noreply.github.com>
2025-08-01 14:11:49 +0200

    [cherry-pick] Allow to download ADAS tiles via predictive cache (#9908)
    * Allow to download ADAS tiles via predictive cache (#9882)
    
    * Update public api
    
    GitOrigin-RevId: f9d41efc83052b84a44918dfe286875cdcfe53fc

--- base/src/main/java/com/mapbox/navigation/base/options/PredictiveCacheNavigationOptions.kt
+++ base/src/main/java/com/mapbox/navigation/base/options/PredictiveCacheNavigationOptions.kt
@@ -1,18 +1,66 @@
 package com.mapbox.navigation.base.options
 
+import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
+
 /**
  * Predictive cache Navigation related options.
  *
+ * @param tilesDataset String built out of `<account>[.<graph>]` variables. Account can be
+ * `mapbox` for default datasets or your username for other.
+ * @param tilesVersion Tiles version.
+ * @param includeAdas Whether to include ADAS tiles.
  * @param predictiveCacheLocationOptions [PredictiveCacheLocationOptions] location configuration for guidance predictive caching
+ *
+ * Note: With the current implementation, if [includeAdas] is true,
+ * [tilesDataset] and [tilesVersion] must not be null. Additionally, if either
+ * [tilesDataset] or [tilesVersion] is specified, both must be provided; neither can be null.
  */
+@OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
 class PredictiveCacheNavigationOptions private constructor(
+    @ExperimentalPreviewMapboxNavigationAPI
+    val tilesDataset: String?,
+    @ExperimentalPreviewMapboxNavigationAPI
+    val tilesVersion: String?,
+    @ExperimentalPreviewMapboxNavigationAPI
+    val includeAdas: Boolean,
     val predictiveCacheLocationOptions: PredictiveCacheLocationOptions,
 ) {
+
+    init {
+        if (includeAdas) {
+            checkNotNull(tilesDataset) {
+                "tilesDataset can't be null if ADAS tiles included"
+            }
+
+            checkNotNull(tilesVersion) {
+                "tilesVersion can't be null if ADAS tiles included"
+            }
+        }
+
+        if (tilesDataset != null) {
+            checkNotNull(tilesVersion) {
+                "tilesVersion must be specified explicitly if tilesDataset specified"
+            }
+        }
+
+        if (tilesVersion != null) {
+            checkNotNull(tilesDataset) {
+                "tilesDataset must be specified explicitly if tilesVersion specified"
+            }
+        }
+    }
 
     /**
      * Get a builder to customize a subset of current options.
      */
     fun toBuilder(): Builder = Builder().apply {
+        if (tilesDataset != null && tilesVersion != null) {
+            tilesConfiguration(
+                tilesDataset = tilesDataset,
+                tilesVersion = tilesVersion,
+                includeAdas = includeAdas,
+            )
+        }
         predictiveCacheLocationOptions(predictiveCacheLocationOptions)
     }
 
@@ -25,6 +73,9 @@ class PredictiveCacheNavigationOptions private constructor(
 
         other as PredictiveCacheNavigationOptions
 
+        if (includeAdas != other.includeAdas) return false
+        if (tilesDataset != other.tilesDataset) return false
+        if (tilesVersion != other.tilesVersion) return false
         if (predictiveCacheLocationOptions != other.predictiveCacheLocationOptions) return false
 
         return true
@@ -34,7 +85,11 @@ class PredictiveCacheNavigationOptions private constructor(
      * Regenerate whenever a change is made
      */
     override fun hashCode(): Int {
-        return predictiveCacheLocationOptions.hashCode()
+        var result = includeAdas.hashCode()
+        result = 31 * result + (tilesDataset?.hashCode() ?: 0)
+        result = 31 * result + (tilesVersion?.hashCode() ?: 0)
+        result = 31 * result + predictiveCacheLocationOptions.hashCode()
+        return result
     }
 
     /**
@@ -42,14 +97,21 @@ class PredictiveCacheNavigationOptions private constructor(
      */
     override fun toString(): String {
         return "PredictiveCacheNavigationOptions(" +
-            "predictiveCacheLocationOptions=$predictiveCacheLocationOptions" +
+            "tilesDataset=$tilesDataset, " +
+            "tilesVersion=$tilesVersion, " +
+            "includeAdas=$includeAdas, " +
+            "predictiveCacheLocationOptions=$predictiveCacheLocationOptions," +
             ")"
     }
 
     /**
-     * Build a new [PredictiveCacheZoomLevelOptions].
+     * Build a new [PredictiveCacheNavigationOptions].
      */
     class Builder {
+
+        private var tilesDataset: String? = null
+        private var tilesVersion: String? = null
+        private var includeAdas: Boolean = false
         private var predictiveCacheLocationOptions: PredictiveCacheLocationOptions? = null
 
         /**
@@ -60,10 +122,37 @@ class PredictiveCacheNavigationOptions private constructor(
         ): Builder = apply { this.predictiveCacheLocationOptions = predictiveCacheLocationOptions }
 
         /**
+         * Tileset configuration.
+         *
+         * @param tilesDataset String built out of `<account>[.<graph>]` variables. Account can be
+         * `mapbox` for default datasets or your username for other.
+         * @param tilesVersion Tiles version.
+         * @param includeAdas Whether to include ADAS tiles.
+         *
+         * Note: With the current implementation, if [includeAdas] is true,
+         * [tilesDataset] and [tilesVersion] must not be null. Additionally, if either
+         * [tilesDataset] or [tilesVersion] is specified, both must be provided; neither can be null.
+         */
+        @ExperimentalPreviewMapboxNavigationAPI
+        fun tilesConfiguration(
+            tilesDataset: String,
+            tilesVersion: String,
+            includeAdas: Boolean,
+        ): Builder = apply {
+            this.tilesDataset = tilesDataset
+            this.tilesVersion = tilesVersion
+            this.includeAdas = includeAdas
+        }
+
+        /**
          * Build [PredictiveCacheNavigationOptions].
          */
         fun build(): PredictiveCacheNavigationOptions = PredictiveCacheNavigationOptions(
-            predictiveCacheLocationOptions ?: PredictiveCacheLocationOptions.Builder().build(),
+            tilesDataset = tilesDataset,
+            tilesVersion = tilesVersion,
+            includeAdas = includeAdas,
+            predictiveCacheLocationOptions = predictiveCacheLocationOptions
+                ?: PredictiveCacheLocationOptions.Builder().build(),
         )
     }
 }
--- base/src/test/java/com/mapbox/navigation/base/options/PredictiveCacheNavigationOptionsTest.kt
+++ base/src/test/java/com/mapbox/navigation/base/options/PredictiveCacheNavigationOptionsTest.kt
@@ -1,27 +1,58 @@
 package com.mapbox.navigation.base.options
 
-import com.mapbox.navigation.testing.BuilderTest
+import com.jparams.verifier.tostring.ToStringVerifier
+import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
+import nl.jqno.equalsverifier.EqualsVerifier
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import kotlin.reflect.KClass
 
-class PredictiveCacheNavigationOptionsTest :
-    BuilderTest<PredictiveCacheNavigationOptions, PredictiveCacheNavigationOptions.Builder>() {
-    override fun getImplementationClass(): KClass<PredictiveCacheNavigationOptions> =
-        PredictiveCacheNavigationOptions::class
-
-    override fun getFilledUpBuilder(): PredictiveCacheNavigationOptions.Builder =
-        PredictiveCacheNavigationOptions.Builder().apply {
-            predictiveCacheLocationOptions(
-                PredictiveCacheLocationOptions.Builder().apply {
-                    currentLocationRadiusInMeters(200)
-                    routeBufferRadiusInMeters(10)
-                    destinationLocationRadiusInMeters(1000)
-                }.build(),
-            )
-        }
+@OptIn(ExperimentalPreviewMapboxNavigationAPI::class)
+class PredictiveCacheNavigationOptionsTest {
 
     @Test
-    override fun trigger() {
-        // trigger, see KDoc
+    fun testGeneratedEqualsHashcodeToStringFunctions() {
+        EqualsVerifier.forClass(PredictiveCacheNavigationOptions::class.java)
+            .verify()
+
+        ToStringVerifier.forClass(PredictiveCacheNavigationOptions::class.java)
+            .verify()
+    }
+
+    @Test
+    fun testDefaultBuilderParameters() {
+        val options = PredictiveCacheNavigationOptions.Builder().build()
+        assertEquals(null, options.tilesDataset)
+        assertEquals(null, options.tilesVersion)
+        assertEquals(false, options.includeAdas)
+        assertEquals(
+            PredictiveCacheLocationOptions.Builder().build(),
+            options.predictiveCacheLocationOptions,
+        )
+    }
+
+    @Test
+    fun testBuilderParameters() {
+        val locationOptions = PredictiveCacheLocationOptions.Builder()
+            .routeBufferRadiusInMeters(100)
+            .currentLocationRadiusInMeters(500)
+            .destinationLocationRadiusInMeters(1000)
+            .build()
+
+        val options = PredictiveCacheNavigationOptions.Builder()
+            .tilesConfiguration(
+                tilesDataset = "test-dataset",
+                tilesVersion = "test-version",
+                includeAdas = true,
+            )
+            .predictiveCacheLocationOptions(locationOptions)
+            .build()
+
+        assertEquals("test-dataset", options.tilesDataset)
+        assertEquals("test-version", options.tilesVersion)
+        assertEquals(true, options.includeAdas)
+        assertEquals(
+            locationOptions,
+            options.predictiveCacheLocationOptions,
+        )
     }
 }
