a5368b51f13b4415b16173b500fe078aadae0982
Dzina <dzina.dybouskaya@mapbox.com>
2025-09-15 17:39:50 +0300

    update default tiles uri to production
    GitOrigin-RevId: 5ae475df3646c6f27063c61b3e79b916168d8008

--- base/src/test/java/com/mapbox/navigation/base/options/DomainTilesOptionsTest.kt
+++ base/src/test/java/com/mapbox/navigation/base/options/DomainTilesOptionsTest.kt
@@ -60,7 +60,7 @@ class DomainTilesOptionsTest {
         val config = DomainTilesOptions.defaultHdTilesOptions()
 
         assertEquals(
-            URI("https://api-3dln-tiles-stagyprod.tilestream.net"),
+            URI("https://api-3dln-tiles-production.mapbox.com"),
             configFromBuilder.tilesBaseUri,
         )
         assertEquals("mapbox", configFromBuilder.tilesDataset)
