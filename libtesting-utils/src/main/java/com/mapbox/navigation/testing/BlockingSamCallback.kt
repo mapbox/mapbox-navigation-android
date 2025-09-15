c42b5d75e1ac2fe74bbfdf9e9cbff7ec6ef5c108
Dzmitry Fomchyn <5161509+DzmitryFomchyn@users.noreply.github.com>
2025-09-15 15:57:55 +0200

    Address review comments (#10313) (#10322)
    GitOrigin-RevId: 9aa77bfa8d091e1a42e90b1a44e087dd04a74600

--- libtesting-utils/src/main/java/com/mapbox/navigation/testing/BlockingSamCallback.kt
+++ libtesting-utils/src/main/java/com/mapbox/navigation/testing/BlockingSamCallback.kt
@@ -2,7 +2,7 @@ package com.mapbox.navigation.testing
 
 import java.util.concurrent.CountDownLatch
 
-class BlockingSAMCallback<T : Any> : (T) -> Unit {
+class BlockingSamCallback<T : Any> : (T) -> Unit {
 
     private var countDownLatch = CountDownLatch(1)
     private lateinit var result: T
