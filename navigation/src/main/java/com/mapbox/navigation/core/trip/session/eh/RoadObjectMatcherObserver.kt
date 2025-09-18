99070f0356532fa5cb87b9c6a7c21afea3b11c31
Dzina <dzina.dybouskaya@mapbox.com>
2025-09-18 09:39:35 +0300

    add WorkerThread annotation for RoadObjectMatcherObserver
    GitOrigin-RevId: c99bc9be7a6b70a0587f94ef60546591867fa6db

--- navigation/src/main/java/com/mapbox/navigation/core/trip/session/eh/RoadObjectMatcherObserver.kt
+++ navigation/src/main/java/com/mapbox/navigation/core/trip/session/eh/RoadObjectMatcherObserver.kt
@@ -1,15 +1,18 @@
 package com.mapbox.navigation.core.trip.session.eh
 
+import androidx.annotation.WorkerThread
 import com.mapbox.bindgen.Expected
 import com.mapbox.navigation.base.trip.model.roadobject.RoadObject
 import com.mapbox.navigation.base.trip.model.roadobject.RoadObjectMatcherError
 
 /**
  * Road objects matching listener. Callbacks are fired when matching is finished.
+ * Notifications are delivered on background thread.
  */
 fun interface RoadObjectMatcherObserver {
     /**
      * Road object matching result.
      */
+    @WorkerThread
     fun onRoadObjectMatched(result: Expected<RoadObjectMatcherError, RoadObject>)
 }
