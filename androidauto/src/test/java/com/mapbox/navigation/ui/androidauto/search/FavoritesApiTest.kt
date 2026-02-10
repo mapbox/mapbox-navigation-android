04af2c440af18649f531f1a8a624b39c8d60f223
Vitaly Zayankovsky <vitaliy.zayankovskiy@mapbox.com>
2026-02-10 19:57:14 +0300

    migrate to new search result type (#12500)
    GitOrigin-RevId: 8f69b8428985c1a631303f4c9dd7d87d9a8ec4a4

--- androidauto/src/test/java/com/mapbox/navigation/ui/androidauto/search/FavoritesApiTest.kt
+++ androidauto/src/test/java/com/mapbox/navigation/ui/androidauto/search/FavoritesApiTest.kt
@@ -7,7 +7,7 @@ import com.mapbox.search.common.AsyncOperationTask
 import com.mapbox.search.common.CompletionCallback
 import com.mapbox.search.record.FavoriteRecord
 import com.mapbox.search.record.FavoritesDataProvider
-import com.mapbox.search.result.SearchResultType
+import com.mapbox.search.result.NewSearchResultType
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
@@ -28,16 +28,16 @@ class FavoritesApiTest {
     fun getPlaces() = coroutineRule.runBlockingTest {
         val expectedItemList = listOf(
             FavoriteRecord(
-                "id",
-                "name",
-                "description",
-                null,
-                null,
-                null,
-                null,
+                id = "id",
+                name = "name",
+                descriptionText = "description",
+                address = null,
+                routablePoints = null,
+                categories = null,
+                makiIcon = null,
                 coordinate = Point.fromLngLat(-33.0, -44.0),
-                SearchResultType.POI,
-                null,
+                metadata = null,
+                NewSearchResultType.POI,
             ),
         )
         val callbackSlot = slot<CompletionCallback<List<FavoriteRecord>>>()
