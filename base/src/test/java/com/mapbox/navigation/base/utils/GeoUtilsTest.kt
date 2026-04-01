4e5b5b7e6f95acb787d17d8a7c42e3f905258d5a
Dzmitry Fomchyn <5161509+DzmitryFomchyn@users.noreply.github.com>
2026-04-01 13:41:44 +0200

    GeoUtils: added getWayId overloads (#13449) (#13515)
    * GeoUtils: added getWayId overloads
    
    * Rename changelog file
    
    * Bump dependencies
    
    GitOrigin-RevId: e5be4b978134ce22f9469cf4b6d400131df07479

--- base/src/test/java/com/mapbox/navigation/base/utils/GeoUtilsTest.kt
+++ base/src/test/java/com/mapbox/navigation/base/utils/GeoUtilsTest.kt
@@ -1,5 +1,7 @@
 package com.mapbox.navigation.base.utils
 
+import com.mapbox.bindgen.ExpectedFactory
+import com.mapbox.geojson.Geometry
 import com.mapbox.geojson.LineString
 import com.mapbox.geojson.MultiPoint
 import com.mapbox.geojson.Point
@@ -20,6 +22,10 @@ class GeoUtilsTest {
     fun setUp() {
         mockkObject(CoreGeoUtilsWrapper)
         every { CoreGeoUtilsWrapper.getTopoLinkId(any(), any(), any()) } returns TEST_ID
+        every { CoreGeoUtilsWrapper.getWayId(any<Long>()) } returns TEST_WAY_ID_RESULT
+        every {
+            CoreGeoUtilsWrapper.getWayId(any<Geometry>(), any(), any())
+        } returns TEST_WAY_IDS_RESULT
     }
 
     @After
@@ -106,13 +112,91 @@ class GeoUtilsTest {
         }
     }
 
+    @Test
+    fun testGetWayIdForwardsEdgeIdToCore() {
+        val result = GeoUtils.getWayId(TEST_EDGE_ID)
+        assertEquals(TEST_WAY_ID_RESULT, result)
+        verify { CoreGeoUtilsWrapper.getWayId(eq(TEST_EDGE_ID)) }
+    }
+
+    @Test
+    fun testGetWayIdForwardsLineStringToCore() {
+        val geometry = LineString.fromLngLats(TEST_POINTS)
+        val result = GeoUtils.getWayId(geometry, 0, TEST_POINTS.size)
+        assertEquals(TEST_WAY_IDS_RESULT, result)
+        verify {
+            CoreGeoUtilsWrapper.getWayId(
+                eq(geometry),
+                eq(0),
+                eq(TEST_POINTS.size),
+            )
+        }
+    }
+
+    @Test
+    fun testGetWayIdWrapsPointsWithLineString() {
+        val result = GeoUtils.getWayId(TEST_POINTS, 1, 2)
+        assertEquals(TEST_WAY_IDS_RESULT, result)
+        verify {
+            CoreGeoUtilsWrapper.getWayId(
+                eq(LineString.fromLngLats(TEST_POINTS)),
+                eq(1),
+                eq(2),
+            )
+        }
+    }
+
+    @Test
+    fun testGetWayIdDefaultStartEndIndex() {
+        val result = GeoUtils.getWayId(TEST_POINTS)
+        assertEquals(TEST_WAY_IDS_RESULT, result)
+        verify {
+            CoreGeoUtilsWrapper.getWayId(
+                eq(LineString.fromLngLats(TEST_POINTS)),
+                eq(0),
+                eq(TEST_POINTS.size),
+            )
+        }
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testGetWayIdThrowsWhenStartIndexLessThanZero() {
+        GeoUtils.getWayId(TEST_POINTS, -1, 1)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testGetWayIdThrowsWhenStartIndexGreaterThanGeometrySize() {
+        GeoUtils.getWayId(TEST_POINTS, TEST_POINTS.size + 1, 0)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testGetWayIdThrowsWhenEndIndexLessThanZero() {
+        GeoUtils.getWayId(TEST_POINTS, 0, -1)
+    }
+
+    @Test(expected = IndexOutOfBoundsException::class)
+    fun testGetWayIdThrowsWhenEndIndexGreaterThanGeometrySize() {
+        GeoUtils.getWayId(TEST_POINTS, 0, TEST_POINTS.size + 1)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testGetWayIdThrowsWhenStartIndexGreaterThanEndIndex() {
+        GeoUtils.getWayId(TEST_POINTS, 1, 0)
+    }
+
     companion object {
         const val TEST_ID = 123L
+        const val TEST_EDGE_ID = 456L
 
         val TEST_POINTS = listOf(
             Point.fromLngLat(10.0, 20.0),
             Point.fromLngLat(20.0, 30.0),
             Point.fromLngLat(30.0, 40.0),
+        )
+
+        val TEST_WAY_ID_RESULT = ExpectedFactory.createValue<String, Long>(789L)
+        val TEST_WAY_IDS_RESULT = ExpectedFactory.createValue<String, List<Long>>(
+            listOf(1L, 2L, 3L),
         )
     }
 }
