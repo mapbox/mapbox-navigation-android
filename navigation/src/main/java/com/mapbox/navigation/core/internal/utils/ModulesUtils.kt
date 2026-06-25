2ee1abe5f144ed26a633afffe0d4105a6357f8e3
Dzina Dybouskaya <dzinad@users.noreply.github.com>
2026-06-25 16:04:25 +0300

    NAVPOR-9425: customize distance and duration formatting
    Copied from https://github.com/mapbox/navigation/pull/14975
    
    GitOrigin-RevId: df849c9e9eee948296cf96de6c9839b4efb3aeaf

--- navigation/src/main/java/com/mapbox/navigation/core/internal/utils/ModulesUtils.kt
+++ navigation/src/main/java/com/mapbox/navigation/core/internal/utils/ModulesUtils.kt
@@ -2,10 +2,8 @@ package com.mapbox.navigation.core.internal.utils
 
 import com.mapbox.common.module.provider.ModuleProviderArgument
 import com.mapbox.navigation.base.formatter.DistanceFormatter
-import com.mapbox.navigation.base.formatter.DistanceFormatterOptions
 import com.mapbox.navigation.base.internal.trip.notification.TripNotificationInterceptorOwner
 import com.mapbox.navigation.base.options.NavigationOptions
-import com.mapbox.navigation.core.formatter.MapboxDistanceFormatter
 
 internal fun paramsProvider(moduleParams: ModuleParams): Array<ModuleProviderArgument> =
     when (moduleParams) {
@@ -20,7 +18,7 @@ internal fun paramsProvider(moduleParams: ModuleParams): Array<ModuleProviderArg
             ),
             ModuleProviderArgument(
                 DistanceFormatter::class.java,
-                MapboxDistanceFormatter(moduleParams.distanceFormatterOptions),
+                moduleParams.distanceFormatter,
             ),
         )
     }
@@ -29,6 +27,6 @@ internal sealed class ModuleParams {
     class NavigationTripNotification(
         val navigationOptions: NavigationOptions,
         val tripNotificationInterceptorOwner: TripNotificationInterceptorOwner,
-        val distanceFormatterOptions: DistanceFormatterOptions,
+        val distanceFormatter: DistanceFormatter,
     ) : ModuleParams()
 }
