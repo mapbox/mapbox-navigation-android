6172f0acfd05cdd393ec4e21ca0d3ddd7cc95a1e
Dmitry Novikov <dmitry.novikov@mapbox.com>
2026-07-10 13:36:45 +0200

    NAVPOR-9639: Clamp geometry index for alternative routes on refresh
    GitOrigin-RevId: b94b9c2c1b72702b7be7c50dcb5301c773718c9c

--- instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/RouteRefreshOnDemandTest.kt
+++ instrumentation-tests/src/androidTest/java/com/mapbox/navigation/instrumentation_tests/core/RouteRefreshOnDemandTest.kt
@@ -116,8 +116,7 @@ class RouteRefreshOnDemandTest : BaseTest<EmptyTestActivity>(EmptyTestActivity::
 
         mapboxNavigation.routeRefreshController.requestImmediateRouteRefresh()
         val refreshedRoutes = mapboxNavigation.routesUpdates()
-            .filter { it.reason == RoutesExtra.ROUTES_UPDATE_REASON_REFRESH }
-            .first()
+            .first { it.reason == RoutesExtra.ROUTES_UPDATE_REASON_REFRESH }
         assertEquals(1, routeRefreshes.size)
         assertEquals(
             224.2239,
@@ -214,8 +213,7 @@ class RouteRefreshOnDemandTest : BaseTest<EmptyTestActivity>(EmptyTestActivity::
         val refreshTimeout = testRouteRefreshIntervalMs * 2
         val plannedRefreshResult = withTimeoutOrNull(refreshTimeout) {
             mapboxNavigation.routesUpdates()
-                .filter { it.reason == RoutesExtra.ROUTES_UPDATE_REASON_REFRESH }
-                .first()
+                .first { it.reason == RoutesExtra.ROUTES_UPDATE_REASON_REFRESH }
         }
         assertNotNull(
             "No route refresh happened in $refreshTimeout " +
